7fbcf93917160091e301590db402db92c42f8df2
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-01-23 22:17:37 +0000

    Small change
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@122 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC/C_objects/test_C_objects.py
+++ PyMC/C_objects/test_C_objects.py
@@ -18,24 +18,24 @@ def Parameter_A(value=4.,First = A, Second = B):
 	"""Parameter_A ~ my_dist(First, Second, )"""
 
 	def logp(value, First, Second, ):
-		#return float(First ** 2) * value
-		return 0.
+		return float(First ** 2) * value
+		#return 0.
     
 	def random(First, Second, ):
-		#return random.random() * float(First)
-		return 0.
+		return random.random() * float(First)
+		#return 0.
 
 @node
 def Node_A(Base = Parameter_A, Exponent = 3.):
 	"""Node_A = Exp(Base, Exponent, )"""
-	#return Base ** Exponent
-	return 0.
+	return Base ** Exponent
+	#return 0.
 	
 @parameter
 def Parameter_B(value=3.,First = Parameter_A, Second = Node_A):
 	"""Parameter_B ~ my_dist(First, Second, )"""
-	#return float(First) / float(Second)
-	return 0.
+	return float(First) / float(Second)
+	#return 0.
 
 """
 The following test is 5.5X faster on my laptop than with pure-Python objects,
