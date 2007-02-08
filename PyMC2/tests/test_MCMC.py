2a4998d6d3d3dc6e99655b518b73180f03b339ef
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-02-08 19:32:00 +0000

    Small bugfixes
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@179 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC2/tests/test_MCMC.py
+++ PyMC2/tests/test_MCMC.py
@@ -16,14 +16,14 @@ def test():
     M.sample(50000,0,100)
     
     # It would be nicer to write plot(M.trace(switchpoint)), since switchpoint is local to M.
-    plot(M.switchpoint.trace.gettrace())
+    plot(M.s.trace.gettrace())
     title('switchpoint')
     figure()
-    plot(M.early_mean.trace.gettrace())
+    plot(M.e.trace.gettrace())
     title('early mean')
     figure()
     title('late mean')
-    plot(M.late_mean.trace.gettrace())
+    plot(M.l.trace.gettrace())
     show()
 
 if __name__=='__main__':
