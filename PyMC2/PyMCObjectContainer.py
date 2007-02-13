737a5ca8e5a02519a57c16fefad2ea1d847c0cd4
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-02-13 14:50:50 +0000

    Added class to implement C.value[i]
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@191 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC2/PyMCObjectContainer.py
+++ PyMC2/PyMCObjectContainer.py
@@ -9,36 +9,36 @@ class PyMCObjectContainer(ContainerBase):
         Takes any iterable in its constructor. On instantiation, it
         recursively 'unpacks' the iterable and adds all PyMC objects
         it finds to its set 'pymc_objects.' It also adds sampling
-        methods to its set 'sampling_methods.
+        methods to its set 'sampling_methods'.
 
         When Model's constructor finds a PyMCObjectContainer, it should
         add all the objects in the sets to its own sets of the same names.
 
         When Parameter/ Node's constructor's 'parents' dictionary contains 
-        a, PyMCObjectContainer, it's retained as a parent.
+        a PyMCObjectContainer, it's retained as a parent.
 
         However, the parent_pointers array will contain the members of the
         set pymc_objects, and all the members of that set will have the
         parameter/ node added as a child.
 
-		Ultimately, the 'value' attribute should return a copy of the input
-		iterable, but with references to all PyMC objects replaced with
-		references to their values, for ease of programming log-probability
-		functions. That is, if A is a list of parameters,
-		
-		M=PyMCObjectContainer(A)
-		M.value[3]
-		
-		should return the _value_ of the fourth parameter, not the Parameter
-		object itself. That means that in a log-probability or eval function
-		
-		M[3]
-		
-		would serve the same function. Who knows how to actually do this...
+        Ultimately, the 'value' attribute should return a copy of the input
+        iterable, but with references to all PyMC objects replaced with
+        references to their values, for ease of programming log-probability
+        functions. That is, if A is a list of parameters,
+        
+        M=PyMCObjectContainer(A)
+        M.value[3]
+        
+        should return the _value_ of the fourth parameter, not the Parameter
+        object itself. That means that in a log-probability or eval function
+        
+        M[3]
+        
+        would serve the same function. Who knows how to actually do this...
         """
         if not hasattr(iterable,'__iter__'):
             raise ValueError, 'PyMC object containers can only be made from iterables.'
-        self.value = iterable
+        self.value = ValueContainer(iterable)
         self.pymc_objects = set()
         self.nodes = set()
         self.parameters = set()
@@ -63,3 +63,25 @@ class PyMCObjectContainer(ContainerBase):
                 self.sampling_methods.add(item)
             elif hasattr(item,'__iter__'):
                 self.unpack(item)
+
+
+"""
+Anand, here is something that works for Parameters and Nodes, 
+but not for SamplingMethods since they don't have a value attribute.
+"""
+class ValueContainer(object):
+    def __init__(self, value):
+        self._value = value
+    def __getitem__(self,index):
+        return self._value[index].value
+
+
+def test():
+    from examples import DisasterModel as DM
+    A = [DM.e, DM.s, DM.l, DM.S, DM.D]
+    C = PyMCObjectContainer(A)
+    print C.value[0]
+    print C.value[1]
+    print C.value[2]
+    print C.value[3]
+    return C
