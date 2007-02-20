b8f3167b9effcdeafbdbc1ccb0fe74b09b3d993f
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-02-20 15:31:16 +0000

    added an example for gibbs.
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@203 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- special_SamplingMethods/Gibbs/GibbsSampler.py
+++ special_SamplingMethods/Gibbs/GibbsSampler.py
@@ -1,19 +1,26 @@
 from PyMC2.SamplingMethods import SamplingMethod
 
 class Gibbs(SamplingMethod):
-	"""
-	It would be nice to organize Gibbs samplers somehow,
-	to keep conjugate and non-conjugate versions together.
+    """
+    It would be nice to organize Gibbs samplers somehow,
+    to keep conjugate and non-conjugate versions together.
 
-	This could avoid some code duplication: The conjugate and
-	non-conjugate versions do similar things, but the
-	non-conjugate version needs to test the proposal and the
-	conjugate version needs to incorporate the prior.
-	
-	Also, it would be nice if the various non-parent, non-child
-	values used in Gibbs sampling could be either nodes, parameters,
-	or just plain values without us having to write the cases in
-	every single Gibbs sampler.
-	"""
-	def __init__(self,parameter):
-		
+    This could avoid some code duplication: The conjugate and
+    non-conjugate versions do similar things, but the
+    non-conjugate version needs to test the proposal and the
+    conjugate version needs to incorporate the prior.
+    
+    Also, it would be nice if the various non-parent, non-child
+    values used in Gibbs sampling could be either nodes, parameters,
+    or just plain values without us having to write the cases in
+    every single Gibbs sampler.
+    """
+    def step():
+        for parameter in self.parameters:
+            parameter.random()
+        
+        
+        
+
+
+    
