19681439db697673bbb75e713294d07f3d955d77
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-02-21 04:33:25 +0000

    the gui can now sample
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@205 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys,os, PyMC2
 try:
 	import pygtk
 	pygtk.require("2.0")
@@ -42,6 +42,8 @@ class GladeWidget:
 					   ' as a signal handler' % (attr,))
 		self.__dict__.update(W)
 
+	def on_window1_destroy(self, widget):
+		gtk.main_quit()
 
 	def on_button1_clicked(self, widget):
 		dialog = gtk.FileChooserDialog("Open python module",
@@ -55,15 +57,38 @@ class GladeWidget:
 		filter.set_name("Python files")
 		filter.add_pattern("*.py")
 		dialog.add_filter(filter)
+		dialog.set_filename('C:\\Documents and Settings\\david\\Mes documents\\PyMC2\\PyMC2\\examples\\DisasterModel.py')
 		response = dialog.run()
 		if response == gtk.RESPONSE_OK:
 			self.filename= dialog.get_filename()
-			print self.filename, ' selected.'
+			self.modulename = os.path.splitext(os.path.basename(self.filename))[0]
+			sys.path.append(os.path.dirname(self.filename))
+			mod = __import__(self.modulename)
+			self.model = PyMC2.Model(mod)
+
 		elif response == gtk.RESPONSE_CANCEL:
 			print 'Closed, no files selected'
 		dialog.destroy()
-		self.button1.modify_text(self.filename.split('.')[-1])
+		self.button1.set_label(self.modulename)
 
+	def on_spinbuttonit_changed(self, widget):
+		pass
+
+	def on_spinbuttonburn_changed(self, widget):
+		pass
+
+	def on_spinbuttonthin_changed(self, widget):
+		pass
+
+	def on_button2_clicked(self, widget):
+		self.iter = self.spinbuttonit.get_value()
+		self.burn = self.spinbuttonburn.get_value()
+		self.thin = self.spinbuttonthin.get_value()
+		print "Before sampling"
+		self.model.sample(self.iter, self.burn, self.thin)
+		print 'Sampling finished.'
+
+    
 if __name__ == "__main__":
 	hwg = GladeWidget('gui.glade', 'window1')
 	gtk.main()
