6498e3b1124b544a3cd31ae4c88694f9356d4031
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-04-06 17:35:19 +0000

    Sorry
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@285 15d7aa0b-6f1a-0410-991a-d59f85d14984

---
+++ PyMC2/sandbox/GaussianProcess/setup.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python  
+try:
+    import setuptools
+except ImportError:
+    pass
+from numpy.distutils.core import setup, Extension
+
+fcov = Extension(name='fcov',sources=['GaussianProcess/cov_funs/fcov.f'])
+
+distrib = setup(
+name="GaussianProcess",
+version="0.0",
+description = "GaussianProcess version 0.0",
+license="Academic Free License",
+author="Anand Patil",
+packages=["GaussianProcess", "GaussianProcess.cov_funs"],
+ext_modules = [fcov]
+)
