6674b57b6db4d9f4307cd0056770a77ef3431ac3
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-04-17 19:18:02 +0000

    Removed GP
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@308 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC2/sandbox/GaussianProcess/__init__.py
+++
@@ -1,7 +0,0 @@
-from GaussianProcess import GaussianProcess
-from Mean import Mean
-from Covariance import Covariance
-from Realization import Realization
-from GPutils import observe
-
-import cov_funs
