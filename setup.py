33ff1d3b3b359a251b5426f8e6ef10164d746ed4
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-04-18 21:09:20 +0000

    Hopefully this works.
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@313 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- setup.py
+++ setup.py
@@ -22,16 +22,40 @@ try:
     config.add_extension(name='PyrexLazyFunction',sources=['PyMC2/PyrexLazyFunction.c'])
 except:
     pass
-    
-packages=["PyMC2", "PyMC2.database", "PyMC2/tests", "PyMC2.examples", "PyMC2.MultiModelInference"]
-for package in packages:
-    config.add_subpackage(package)
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
     setup(  version="2.0",
             description = "PyMC version 2.0",
             license="Academic Free License",
+            packages=["PyMC2", "PyMC2.database", "PyMC2.examples", "PyMC2.MultiModelInference", "PyMC2/tests"],
             url="trichech.us",
             **(config.todict()))
 
+
+# #/usr/bin/env python  
+# try:
+#     import setuptools
+# except ImportError:
+#     pass
+# from numpy.distutils.core import setup, Extension
+# 
+# # Compile flib (fortran source for statistical distributions.)
+# flib = Extension(name='flib',sources=['PyMC2/flib.f'])
+# flib_blas = Extension(name='flib_blas',sources=['PyMC2/flib_blas.f'])
+# try:
+#     PyrexLazyFunction = Extension(name='PyrexLazyFunction',sources=['PyMC2/PyrexLazyFunction.c'])
+#     ext_modules = [flib, flib_blas, PyrexLazyFunction]
+# except:
+#     ext_modules = [flib, flib_blas]
+# 
+# distrib = setup(
+# name="PyMC2",
+# version="2.0",
+# description = "PyMC version 2.0",
+# license="Academic Free License",
+# url="trichech.us",
+# packages=["PyMC2", "PyMC2.database", "PyMC2/tests", "PyMC2.examples", 
+# "PyMC2.MultiModelInference"],
+# ext_modules = ext_modules
+# )
