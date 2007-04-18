f381725a7c204b38c7f2b434232d0d50e6bd541e
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-04-18 21:09:48 +0000

    Hopefully this works.
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@314 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC2/MultiModelInference/__init__.py
+++ PyMC2/MultiModelInference/__init__.py
@@ -2,7 +2,8 @@
 PyMC multi-model inference support
 """
 
-__modules__ = ["RJMCMC", "ModelPosterior"]
+# __modules__ = ["RJMCMC", "ModelPosterior"]
+__modules__ = ["ModelPosterior"]
 
 for mod in __modules__:
     exec "from %s import *" % mod
