9436e117d594a1c34235f0211836cb2d8e31cf6e
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2007-08-10 04:48:40 +0000

    test_LazyFunction exposes bug.
    git-svn-id: https://pymc.googlecode.com/svn/branches/trial@404 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- PyMC2/tests/test_LazyFunction.py
+++ PyMC2/tests/test_LazyFunction.py
@@ -1,59 +1,82 @@
 from numpy.testing import *
 import numpy as np
-from numpy.random import random
+from numpy.random import random, normal
 from PyMC2.PyrexLazyFunction import LazyFunction
-from PyMC2 import parameter, data, node, Normal, potential
+from PyMC2 import parameter, data, node, Normal, potential, ZeroProbability
+
+verbose = False
+
+@parameter
+def A(value=1.):
+   return -10.*value
+
+# If normcoef is positive, there will be an uncaught ZeroProbability
+normcoef = 1.
+
+# B's value is a random function of A.
+@node(verbose=verbose)
+def B(A=A):
+   return A + normcoef * normal()
+
+# Guarantee that initial state is OK
+while B.value<0.:
+   @node(verbose=verbose)
+   def B(A=A):
+       return A + normcoef * normal()
 
 @data
-def mu(value=5.):
-    return 0.
+@parameter(verbose=verbose)
+def C(value = 0., B=B):
+   if B<0.:
+       return -np.Inf
+   else:
+       return 0.
+       
+L = C._logp
+C.logp
+acc = True
+
+
+for i in range(1000):
+
+    # Record last values
+    last_B_value = B.value
+    last_C_logp = C.logp
     
-@data
-def tau(value=1.):
-    return 0.
+    # Propose a value
+    A.value = 1. + normal()
+    
+    # Check the argument values
+    L.refresh_argument_values()    
+    assert_equal(L.argument_values['B'], B.value)
+    assert_equal(L.ultimate_arg_values[0], B.value)
+    
+    # Accept or reject value
+    acc = True
+    try:
+        C.logp
+        print 'Accepting'
+    except ZeroProbability:
+        print 'Rejecting'
+        acc = False
+        A.value = A.last_value
+        print [A.value, A.last_value], B._value.cached_args
+        print [B.value, last_B_value], B._value.cached_values 
+        assert_equal(B.value, last_B_value)
+        
+    # print L.get()
+    
+    # If jump was accepted:
+    if acc:
+        # Check the head of the cache
+        assert_equal(L.cached_args[0], B.value)
+        assert_equal(L.cached_values[0], C.logp)
+        
+    # If jump was rejected:        
+    else:
+        
+        # Should currently be using the second entry in the cache
+        assert_equal(L.cached_args[2], B.value)
+        assert_equal(L.cached_values[1], C.logp)
+        
 
-p = Normal('p', value=5., mu=mu, tau=tau)
-
-@node
-def DumbNode(p=p):
-    return random()*p
-
-@potential
-def MyPotential(p=p,d=DumbNode):
-    return -(p-d)**2
-
-def f(p, d):
-    return p**2 / d
-
-class test_cache(NumpyTestCase):
-
-    def check_simple(self):
-        L = LazyFunction(f, {'p':p,'d':DumbNode}, 2)
-        for i in range(1000):    
-            last_dumbnode_value = DumbNode.value        
-            p.random()
-            
-            # Make sure returned value is correct.
-            assert_equal(L.get(), p.value**2 / DumbNode.value)
-            
-            # Make sure correct values got into cache.
-            assert_equal(L.cached_values[0], p.value**2 / DumbNode.value)
-            assert_equal(L.cached_args[0], p.value)
-            assert_equal(L.argument_values['p'], p.value)
-            assert_equal(L.ultimate_arg_values[0], p.value)
-            
-            assert_equal(L.cached_args[1], DumbNode.value)
-            assert_equal(L.argument_values['d'], DumbNode.value)
-            assert_equal(L.ultimate_arg_values[1], DumbNode.value)
-            
-            L.get()
-            
-            if i>1:
-                
-                # Make sure last values got pushed down cache.
-                assert_equal(L.cached_values[1], p.last_value**2 / last_dumbnode_value)
-                assert_equal(L.cached_args[2], p.last_value)
-                assert_equal(L.cached_args[3], last_dumbnode_value)
-                        
-if __name__ == '__main__':
-    NumpyTest().run()
