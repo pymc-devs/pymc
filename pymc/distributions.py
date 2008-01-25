arnormal should be arlognormal. Changed back parameterization to sigma. Fixed hypergeometric loglikelihood (missed conditions on x). Fixed multinomial, which requires 2d array for p and n.
git-svn-id: https://pymc.googlecode.com/svn/trunk@605 15d7aa0b-6f1a-0410-991a-d59f85d14984

pymc/distributions.py
@@ -376,9 +376,9 @@ def GOFpoints(x,y,expval,loss):
 # random generator, expval, log-likelihood
 #--------------------------------------------------------
 
-# Autoregressive normal 
-def rarnormal(a, tau, rho, size=1):
-    """rarnormal(a, tau, rho)
+# Autoregressive lognormal 
+def rarlognormal(a, sigma, rho, size=1):
+    """rarnormal(a, sigma, rho)
     
     Autoregressive normal random variates.
     
@@ -388,27 +388,27 @@ def rarnormal(a, tau, rho, size=1):
     """
     f = pymc.utils.ar1
     if np.isscalar(a):
-        r = f(rho, 0, tau, size)
+        r = f(rho, 0, sigma, size)
     else:
         n = len(a)
-        r = [f(rho, 0, tau, n) for i in range(size)]
+        r = [f(rho, 0, sigma, n) for i in range(size)]
         if size == 1:
             r = r[0]
     return a*np.exp(r)
             
 
-def arnormal_like(x, a, tau, rho):
+def arlognormal_like(x, a, sigma, rho):
     """arnormal(x, a, tau, rho, beta=1)
     
-    Autoregressive normal log-likelihood.
+    Autoregressive lognormal log-likelihood.
     
     .. math::
         x_i & = a_i \\exp(e_i) \\\\
         e_i & = \\rho e_{i-1} + \\epsilon_i
     
-    where :math:'\\epsilon_i \\sim N(0,\\tau)'.
+    where :math:'\\epsilon_i \\sim N(0,\\sigma)'.
     """
-    return flib.arlognormal(x, np.log(a), 1./tau, rho, beta=1)
+    return flib.arlognormal(x, np.log(a), sigma, rho, beta=1)
     
 
 # Bernoulli----------------------------------------------
@@ -1007,7 +1007,7 @@ def hypergeometric_like(x, n, m, N):
         Number of successes in a sample drawn from a population.
         :math:'\\max(0, draws-failures) \\leq x \\leq \\min(draws, success)'
       n : int
-        Size of sample.
+        Size of sample drawn from the population.
       m : int
         Number of successes in the population.
       N : int
@@ -1146,7 +1146,7 @@ def multinomial_like(x, n, p):
         :math:'\\sum_{i=1}^k x_i=n', :math:'x_i \\ge 0'.
       n : int
         Number of trials.
-      p : (k,1) float
+      p : (k,) float
         Probability of each one of the different outcomes,
         :math:'\\sum_{i=1}^k p_i = 1)', :math:'p_i \\ge 0'.
 
@@ -1157,8 +1157,8 @@ def multinomial_like(x, n, p):
 
     """
 
-    # x = np.atleast_2d(x)
-    # p = np.atleast_2d(p)
+    x = np.atleast_2d(x) #flib expects 2d arguments. Do we still want to support multiple p values along realizations ?
+    p = np.atleast_2d(p)
     
     return flib.multinomial(x, n, p)
 

pymc/tests/test_distributions.py
@@ -79,9 +79,14 @@ def hypergeometric(x, n, m, N):
     x : number of successes drawn
     n : number of draws
     m : number of successes in total
-    N : nuccesses + failures in total.
+    N : successes + failures in total.
     """
-    return comb(N-m, x) * comb(m, n-x) / comb(N,n)
+    if x < max(0, n-(N-m)):
+        return 0.
+    elif x > min(n, m):
+        return 0.
+    else:
+        return comb(N-m, x) * comb(m, n-x) / comb(N,n)
 
 def mv_hypergeometric(x,m):
     """
@@ -242,45 +247,45 @@ def normalization(like, stochs, domain, N=100):
             y.append(f(i))
         return np.trapz(y,x)
 
-class test_arnormal(NumpyTestCase):
+class test_arlognormal(NumpyTestCase):
     def like(self, stochs, r):
         a = stochs[2:]
-        tau = stochs[1]
+        sigma = stochs[1]
         rho = stochs[0]
-        like = np.array([arnormal_like(x, a, tau, rho) for x in r])
+        like = np.array([arlognormal_like(x, a, sigma, rho) for x in r])
         return -like.sum()
             
     def check_random(self):
         a = (1,2)
-        tau = 10
+        sigma = .1
         rho = 0
-        r = rarnormal(a, tau, rho, size=1000) 
+        r = rarlognormal(a, sigma, rho, size=1000) 
         assert_array_almost_equal(np.median(r), [1,2],1)
         
         rho =.8
         sigma = .1
-        r = rarnormal(1, tau, rho, size=1000)
+        r = rarlognormal(1, sigma, rho, size=1000)
         corr = utils.autocorr(np.log(r))
         assert_almost_equal(corr, rho, 1)
-        assert_almost_equal(r.std(), tau/sqrt(1-rho**2),1)
+        assert_almost_equal(r.std(), sigma/sqrt(1-rho**2),1)
     
     def check_consistency(self):
     
         # 1D case
         a = 1
         rho =.8
-        tau = 10
-        r = rarnormal(a, tau, rho, size=1000)
+        sigma = .1
+        r = rarlognormal(a, sigma, rho, size=1000)
         opt = fmin(self.like, (.8, .4, .9), args=([r],), disp=0)
-        assert_array_almost_equal(opt, [rho, tau, a], 1)
+        assert_array_almost_equal(opt, [rho, sigma, a], 1)
 
         # 2D case
         a = (1,2)
-        tau = 10
+        sigma = .1
         rho = .7
-        r = rarnormal(a, tau, rho, size=2000) 
+        r = rarlognormal(a, sigma, rho, size=1000) 
         opt = fmin(self.like, (.75, .15, 1.1, 2.1), xtol=.05, args=(r,), disp=0)
-        assert_array_almost_equal(opt, (rho, tau)+a, 1)
+        assert_array_almost_equal(opt, (rho, sigma)+a, 1)
     
 
 class test_bernoulli(NumpyTestCase):
@@ -306,7 +311,7 @@ class test_beta(NumpyTestCase):
     def check_consistency(self):
         stochs ={'alpha':3, 'beta':5}
         hist, like, figdata = consistency(rbeta, flib.beta_like, stochs, nrandom=5000, range=[0,1])
-        assert_array_almost_equal((hist-like)/sqrt(like),0,1)
+        assert_array_almost_equal(hist, like,1)
         if PLOT:
             compare_hist(figname='beta', **figdata)
 