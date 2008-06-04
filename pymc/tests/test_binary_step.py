e9388ce04719d0a0742c8d7e046fce25892431ed
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2008-06-04 03:27:07 +0000

    started to write a test for the binary step method.
    git-svn-id: https://pymc.googlecode.com/svn/trunk@757 15d7aa0b-6f1a-0410-991a-d59f85d14984

---
+++ pymc/tests/test_binary_step.py
@@ -0,0 +1,40 @@
+"""Test the binary step method.
+Assume we have a series of heads (0) or tail (1) experiments : 
+[0,0,1,1,0,0,0,1,1,0]
+Assume that the coin is either fair (p=.5), or unfair (p!=.5). 
+We have two parameters: 
+ * fair : which can be True or False,
+ * coin : the probability of tail on flipping the coin 
+"""
+
+from numpy.testing import NumpyTestCase, NumpyTest
+import pymc
+from pymc import stoch
+import numpy as np
+
+
+class BinaryTestModel:
+    series = np.array([0,0,1,1,0,0,0,1,1,1])
+    
+    fair = pymc.Bernoulli('fair', p=.5, value=1)
+    
+    @stoch
+    def coin(value=.5, fair=fair):
+        """Return the probability of a tail on flipping a coin."""
+        if fair is True:
+            return pymc.truncnorm_like(value, .5, .001, 0, 1)
+        else:
+            return pymc.uniform_like(value, .3, .7)
+
+    tail = pymc.Bernoulli('tail', p=coin, value=series, isdata=True)
+    
+
+class TestBinary(NumpyTestCase):
+    S = pymc.MCMC(input = BinaryTestModel)
+    S.sample(1000,500)
+    f = S.fair.trace()
+    assert(1.0*f.sum()/len(f) > .5)
+    
+if __name__ == '__main__':
+    NumpyTest().run()
+
