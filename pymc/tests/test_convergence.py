91b209462078c2146cc81d6cad068c8b492799a6
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2008-06-04 20:53:26 +0000

    added smoke test for geweke convergence diagnostics.
    git-svn-id: https://pymc.googlecode.com/svn/trunk@761 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/tests/test_convergence.py
+++ pymc/tests/test_convergence.py
@@ -5,23 +5,28 @@
 ###
 
 
-from numpy.testing import *
+from numpy.testing import assert_equal, assert_array_equal, NumpyTestCase, NumpyTest
+import numpy as np
 import pymc
-import pymc.examples.maximum_rainfall as mr
+import pymc.examples.weibull_fit as model
 
-S = pymc.MCMC(mr.Gumbelfit, 'ram')
-S.sample(1000)
-sigma = S.sigma.trace()
+S = pymc.MCMC(model, 'ram')
+S.sample(2000, 1000)
+a = S.a.trace()
+b = S.b.trace()
 
 class test_geweke(NumpyTestCase):
     def check_simple(self):
+        scores = pymc.geweke(a, intervals=5)
+        assert_equal(len(scores), 5)
         
-        scores = pymc.geweke(sigma)
-        print scores
+        # If the model has converged, the scores should lie
+        # between -1 and 1.
+        assert_array_equal(np.abs(np.array(scores)[:,1]) < 1, True)
         
 class test_raftery_lewis(NumpyTestCase):
     def check_simple(self):
-        print pymc.raftery_lewis(sigma, 0.5, .05)
+        print pymc.raftery_lewis(a, 0.5, .05)
 
 
 if __name__ == "__main__":
