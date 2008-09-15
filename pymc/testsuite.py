79a7a50cc731533f9d0f425aa7b56d34526b67ed
david.huard <david.huard@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2008-09-15 16:14:26 +0000

    testsuite now uses nose framework.
    git-svn-id: https://pymc.googlecode.com/svn/trunk@860 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/testsuite.py
+++ pymc/testsuite.py
@@ -6,29 +6,25 @@ import os
 # Changeset
 # 19/03/2007 -DH- The modules in tests/ are now imported when test is called. 
 # June 17, 2008 -DH- Switched to unittests since NumpyTest made way to nose framework.
-
+# Sept 15, 2008 Switching to nose to run the test suite. 
 warnings.simplefilter('ignore', FutureWarning)
 
 __all__=['test']
 def test():
+    import nose
+    
     try:
         os.mkdir('test_results')
     except:
         pass
     os.chdir('test_results')
     
-    # Import all tests modules declared in pymc/tests/__init__.py
-    __import__('pymc.tests', fromlist=tests.__modules__)
+    nose.main()
     
-    # Create a test suite from all the tests
-    L = unittest.TestLoader()
-    S = L.loadTestsFromNames(tests.__modules__, tests)
-    
-    # Run the test suite.
-    unittest.TextTestRunner(verbosity=1).run(S)
     os.chdir('..')
 
 if __name__=='__main__':
-    test()
+    import nose
+    nose.main()
     
     
