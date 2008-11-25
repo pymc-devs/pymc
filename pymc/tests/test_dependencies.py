4e4c1ad1a3f5e6e0a351458cff33cfe42ab040d6
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2008-11-25 08:54:44 +0000

    Changed test_dependencies
    git-svn-id: https://pymc.googlecode.com/svn/trunk@1032 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/tests/test_dependencies.py
+++ pymc/tests/test_dependencies.py
@@ -1,31 +1,33 @@
 from numpy.testing import *
+import pymc
 import os
 
-mod_strs = ['IPython', 'pylab', 'matplotlib', 'scipy','Pdb']
-class test_dependencies(TestCase):
-    def test(self):
-        dep_files = {}
-        for mod_str in mod_strs:
-            dep_files[mod_str] = []
+# This is a function, not a test case, because it has to be run from inside
+# the source tree to work well.
 
-        for dirname, dirs, files in os.walk('..'):
-            for fname in files:
-                if fname[-3:]=='.py' or fname[-4:]=='.pyx':
-                    if dirname.find('sandbox')==-1 and fname != 'test_dependencies.py'\
-                        and dirname.find('examples')==-1:
-                        for mod_str in mod_strs:
-                            if file(dirname+'/'+fname).read().find(mod_str)>=0:
-                                dep_files[mod_str].append(dirname+'/'+fname)
-                            
-        print 'Instances of optional dependencies found are:'
-        for mod_str in mod_strs:
-            print '\t'+mod_str+':'
-            for fname in dep_files[mod_str]:
-                print '\t\t'+fname
-        if len(dep_files['Pdb'])>0:
-            raise ValueError, 'Looks like Pdb was not commented out in '+', '.join(dep_files[mod_str])
+mod_strs = ['IPython', 'pylab', 'matplotlib', 'scipy','Pdb']
+def test_dependencies():
+    dep_files = {}
+    for mod_str in mod_strs:
+        dep_files[mod_str] = []
+    
+    for dirname, dirs, files in os.walk('..'):
+        for fname in files:
+            if fname[-3:]=='.py' or fname[-4:]=='.pyx':
+                if dirname.find('sandbox')==-1 and fname != 'test_dependencies.py'\
+                    and dirname.find('examples')==-1:
+                    for mod_str in mod_strs:
+                        if file(dirname+'/'+fname).read().find(mod_str)>=0:
+                            dep_files[mod_str].append(dirname+'/'+fname)
+                        
+    print 'Instances of optional dependencies found are:'
+    for mod_str in mod_strs:
+        print '\t'+mod_str+':'
+        for fname in dep_files[mod_str]:
+            print '\t\t'+fname
+    if len(dep_files['Pdb'])>0:
+        raise ValueError, 'Looks like Pdb was not commented out in '+', '.join(dep_files[mod_str])
                         
             
 if __name__ == "__main__":
-    import unittest
-    unittest.main()
+    test_dependencies()
