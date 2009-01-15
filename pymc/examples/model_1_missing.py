63d03ad3ab5c4ebea9c1a363aea19d55e8553576
fonnesbeck <fonnesbeck@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2009-01-15 00:32:21 +0000

    Update of model_1_missing with more compact 2.1 imputation syntax.
    git-svn-id: https://pymc.googlecode.com/svn/trunk@1154 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/examples/model_1_missing.py
+++ pymc/examples/model_1_missing.py
@@ -9,10 +9,11 @@ disasters[t] ~ Po(early_mean if t <= switchpoint, late_mean otherwise)
 """
 __all__ = ['swichpoint','early_mean','late_mean','disasters']
 
-from pymc import DiscreteUniform, Exponential, deterministic, Poisson, Uniform, Lambda, MCMC
-from pymc.distributions import ImputeMissing
+from pymc import DiscreteUniform, Exponential, deterministic, Poisson, Uniform, Lambda, MCMC, observed, poisson_like
+from pymc.distributions import Impute
 import numpy as np
 
+# Missing values indicated by -999 placeholders
 disasters_array =   np.array([ 4, 5, 4, 0, 1, 4, 3, 4, 0, 6, 3, 3, 4, 0, 2, 6,
                    3, 3, 5, 4, 5, 3, 1, 4, 4, 1, 5, 5, 3, 4, 2, 5,
                    2, 2, 3, 4, 2, 1, 3, -999, 2, 1, 1, 1, 1, 3, 0, 0,
@@ -20,9 +21,7 @@ disasters_array =   np.array([ 4, 5, 4, 0, 1, 4, 3, 4, 0, 6, 3, 3, 4, 0, 2, 6,
                    0, 1, 0, 1, 0, 0, 0, 2, 1, 0, 0, 0, 1, 1, 0, 2,
                    3, 3, 1, -999, 2, 1, 1, 1, 1, 2, 4, 2, 0, 0, 1, 4,
                    0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1])
-                   
-# Mask for missing values
-disasters_mask = disasters_array == -999
+               
 
 # Switchpoint
 s = DiscreteUniform('s', lower=0, upper=110)   
@@ -41,6 +40,5 @@ def r(s=s, e=e, l=l):
     out[s:] = l
     return out
 
-# Where the mask is true, the value is taken as missing. 
-masked_data = np.ma.masked_array(disasters_array, disasters_mask)
-D = ImputeMissing('D', Poisson, masked_data, mu=r)
+# Where the value is -999, the datum is assumed to be missing. 
+D = Impute('D', Poisson, disasters_array, missing=-999, mu=r)
