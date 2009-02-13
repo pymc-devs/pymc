Minor fixes to model validation procedures; accounted for autocorrelation in MC error calculation by using averaged batches.
git-svn-id: https://pymc.googlecode.com/svn/trunk@1179 15d7aa0b-6f1a-0410-991a-d59f85d14984

pymc/Node.py
@@ -10,6 +10,30 @@ import os, pdb
 import numpy as np
 import types
 
+def batchsd(trace, batches=5):
+    """
+    Calculates the simulation standard error, accounting for non-independent
+    samples. The trace is divided into batches, and the standard deviation of
+    the batch means is calculated.
+    """
+    
+    if len(shape(trace)) > 1:
+        
+        return np.array([batchsd(t, batches) for t in trace])
+        
+    else:
+        if batches == 1: return np.std(trace)/np.sqrt(len(trace))
+    
+        try:
+            batched_traces = np.resize(trace, (batches, len(trace)/batches))
+        except ValueError:
+            # If batches do not divide evenly, trim excess samples
+            resid = len(trace) % batches
+            batched_traces = np.resize(trace[:-resid], (batches, len(trace)/batches))
+        
+        means = np.mean(batched_traces, 1)
+    
+        return np.std(means)/np.sqrt(batches)
 
 class ZeroProbability(ValueError):
     "Log-probability is undefined or negative infinity"
@@ -166,7 +190,7 @@ class Variable(Node):
 
     plot = property(_get_plot, _set_plot, doc='A flag indicating whether self should be plotted.')
 
-    def stats(self, alpha=0.05, start=0):
+    def stats(self, alpha=0.05, start=0, batches=100):
         """
         Generate posterior statistics for node.
         """
@@ -187,7 +211,7 @@ class Variable(Node):
                 'standard deviation': trace.std(0),
                 'mean': trace.mean(0),
                 '%s%s HPD interval' % (int(100*(1-alpha)),'%'): hpd(trace, alpha),
-                'mc error': trace.std(0) / sqrt(n),
+                'mc error': batchsd(trace, batches),
                 'quantiles': quantiles(trace)
             }
         except:

pymc/utils.py
@@ -85,9 +85,9 @@ def round_array(array_in):
     Rounds an array and recasts it to int. Also works on scalars.
     """
     if isinstance(array_in, ndarray):
-        return asarray(array_in, dtype=int)
+        return np.round(array_in).astype(int)
     else:
-        return int(array_in)
+        return int(np.round(array_in))
 
 try:
     from flib import dchdc_wrap
@@ -831,3 +831,6 @@ def discrepancy(observed, simulated, expected):
     D_sim = sum([(sqrt(s)-sqrt(e))**2 for s,e in zip(simulated, expected)])
 
     return D_obs, D_sim
+    
+        
+    