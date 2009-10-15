8bed14b625e48a4ece33df5ca7f3bca9f34c566c
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2009-10-15 12:18:20 +0000

    Resolved conflict
    git-svn-id: https://pymc.googlecode.com/svn/trunk@1344 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/distributions.py
+++ pymc/distributions.py
@@ -198,16 +198,15 @@ def new_dist_class(*new_class_args):
                 init_val = arg_dict_out['value']
                 init_val_shape = None if init_val is None else np.shape(init_val)
 
-
                 if len(parents) > 0:
                     pv = [np.shape(value(v)) for v in parents.values()]
                     biggest_parent = np.argmax([np.prod(v) for v in pv])
                     parents_shape = pv[biggest_parent]
-
+                    
                     # Scalar parents can support any shape.
                     if np.prod(parents_shape) <= 1:
                         parents_shape = None
-
+                    
                 else:
                     parents_shape = None
 
