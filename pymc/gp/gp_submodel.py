bdc68254e7174a7115eff68b9f8174c69b6de1c6
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2010-01-06 14:07:34 +0000

    GPEvaluationAM works
    git-svn-id: https://pymc.googlecode.com/svn/trunk@1414 15d7aa0b-6f1a-0410-991a-d59f85d14984

---
+++ pymc/gp/gp_submodel.py
@@ -0,0 +1,82 @@
+# Copyright (c) Anand Patil, 2007
+
+__docformat__ = 'reStructuredText'
+
+# __all__ = ['GP_array_random', 'GP', 'GPMetropolis', 'GPNormal', 'GPParentMetropolis']
+
+# FIXME: New pickle protocol for Realization. Just pickle the observation mesh and values, and maybe nugget or whatever. Be sure to delay the linear algebra until the last possible minute!
+
+import pymc as pm
+import copy
+import numpy as np
+from Realization import Realization
+
+
+__all__ = ['GaussianProcess', 'GPEvaluation', 'GPSubmodel']
+
+def gp_logp(x, M, C, mesh, f_eval):
+    raise TypeError, 'GP objects have no logp function'
+
+def gp_rand(M, C, mesh, f_eval, size=None):
+    return pm.gp.Realization(M, C, mesh, f_eval)
+
+class GaussianProcess(pm.Stochastic):
+    
+    def __init__(self,name,submodel,doc=None,trace=True,value=None,rseed=False,
+                    observed=False,cache_depth=2,plot=None,verbose=None,isdata=None):
+                
+        self.submodel = submodel
+        
+        pm.Stochastic.__init__(self,gp_logp,doc,name,
+                                {'M':submodel.M, 'C':submodel.C,'mesh':submodel.mesh, 'f_eval':submodel.f_eval},
+                                gp_rand,trace,value,np.dtype('object'),rseed,observed,cache_depth,plot,verbose,isdata,False)
+        
+        self.rand()
+        
+    def gen_lazy_function(self):
+        pass
+        
+    def get_logp(self):
+        raise TypeError, 'Gaussian process %s has no logp.'%self.__name__
+        
+    def set_logp(self, new_logp):
+        raise TypeError, 'Gaussian process %s has no logp.'%self.__name__
+        
+    logp = property(fget = get_logp, fset = set_logp)
+
+
+class GPEvaluation(pm.MvNormalCov):
+    pass
+
+    
+class GPSubmodel(pm.ObjectContainer):
+    """
+    f = GPSubmodel('f', M, C, mesh, [obs_values, obs_nugget, tally_all])
+
+    A stochastic variable valued as a Realization object.
+
+    :Arguments:
+        - M: A Mean instance or pm.deterministic variable valued as a Mean instance.
+        - C: A Covariance instance or pm.deterministic variable valued as a Covariance instance.
+        - mesh: The mesh on which self's log-probability will be computed. See documentation.
+        - tally_all: By default, only f_eval and f are tallied. Turn this on to tally all.
+
+    :SeeAlso: Realization, GPMetropolis, GPNormal
+    """
+    
+    def __init__(self, name, M, C, mesh, init_vals=None, tally_all=False):
+        # FIXME: Use S_eval for both logp and observation, meaning get it into f somehow.
+        self.M = M
+        self.C = C
+        self.mesh = mesh
+        self.name = name
+        M_eval = pm.Lambda('%s_M_eval'%name, lambda M=M, mesh=mesh: M(mesh), trace=tally_all)
+        C_eval = pm.Lambda('%s_C_eval'%name, lambda C=C, mesh=mesh: C(mesh,mesh), trace=tally_all)
+        f_eval = GPEvaluation('%s_f_eval'%name, mu=M_eval, C=C_eval, value=init_vals, trace=True)
+        self.f_eval = f_eval
+        f = GaussianProcess('%s_f'%name, self, trace=tally_all)
+        pm.ObjectContainer.__init__(self, locals())
+        
+    def getobjects(self):
+        names = ['M_eval','C_eval','f_eval','f']
+        return dict(zip(['%s_%s'%(self.name, name) for name in names], [getattr(self, name) for name in names]))
