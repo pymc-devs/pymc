7341d094bf4ffc3880a092235fc319dae76e6891
anand.prabhakar.patil <anand.prabhakar.patil@15d7aa0b-6f1a-0410-991a-d59f85d14984>
2010-01-06 14:08:57 +0000

    MCMC working for both f-handling step methods, and both Covariance and FullRankCovariance
    git-svn-id: https://pymc.googlecode.com/svn/trunk@1429 15d7aa0b-6f1a-0410-991a-d59f85d14984

--- pymc/gp/gp_submodel.py
+++ pymc/gp/gp_submodel.py
@@ -60,15 +60,13 @@ class GPSubmodel(pm.ObjectContainer):
     FIXME: docstring
     """
     
-    def __init__(self, name, M, C, mesh, init_vals=None, tally_all=False):
+    def __init__(self, name, M, C, mesh, init_vals=None, obs_on_mesh=False, tally_all=False):
         
-        self.M = M
-        self.C = C
-        self.mesh = pm.Lambda('%s_mesh'%name, lambda mesh=mesh: pm.gp.regularize_array(mesh))
-        self.name = name
-
+        mesh = pm.Lambda('%s_mesh'%name, lambda mesh=mesh: pm.gp.regularize_array(mesh))
+        name = name
+        
         @pm.deterministic(trace=tally_all, name='%s_covariance_bits'%name)
-        def covariance_bits(C=C,mesh=self.mesh):
+        def covariance_bits(C=C,mesh=mesh):
             """
             Both the realization 'f' and the on-mesh evaluation 'f_eval' need the 
             Cholesky factor of the covariance evaluation. The Gibbs step method 
@@ -85,11 +83,11 @@ class GPSubmodel(pm.ObjectContainer):
             except np.linalg.LinAlgError:
                 return None
 
-        self.covariance_bits=covariance_bits
-        for i,name in enumerate(['S_eval','C_eval','C_obs']):
-            setattr(self, name, pm.Lambda(name, lambda cb=covariance_bits, i=i: cb[i]))
+        S_eval = covariance_bits[0]
+        C_eval = covariance_bits[1]
+        C_obs = covariance_bits[2]
         
-        self.M_eval = pm.Lambda('%s_M_eval'%name, lambda M=M, mesh=mesh: M(mesh), trace=tally_all)
+        M_eval = pm.Lambda('%s_M_eval'%name, lambda M=M, mesh=mesh: M(mesh), trace=tally_all)
                 
         @pm.potential(name = '%s_fr_check'%name)
         def fr_check(cb=covariance_bits):
@@ -100,22 +98,32 @@ class GPSubmodel(pm.ObjectContainer):
                 return -np.inf
             else:
                 return 0
-        self.fr_check=fr_check
-        f_eval = GPEvaluation('%s_f_eval'%name, mu=self.M_eval, sig=self.S_eval, value=init_vals, trace=True)
+        fr_check=fr_check
+        f_eval = GPEvaluation('%s_f_eval'%name, mu=M_eval, sig=S_eval, value=init_vals, trace=True, observed=obs_on_mesh)
         
         @pm.deterministic(trace=tally_all, name='%s_covariance_bits'%name)
-        def M_obs(M=M, f_eval=f_eval, C_obs=self.C_obs, mesh=self.mesh):
+        def M_obs(M=M, f_eval=f_eval, C_obs=C_obs, mesh=mesh):
             """
             Creates an observed mean object to match C_obs.
             """
             M_obs = copy.copy(M)
             M_obs.observe(C_obs,mesh,f_eval)
             return M_obs
+        M_obs = M_obs
+        f_eval = f_eval
+        self.mesh = mesh
+        
+        self.M = M
+        self.C = C
         self.M_obs = M_obs
+        self.C_obs = C_obs
         self.f_eval = f_eval
-        self.f = GaussianProcess('%s_f'%name, self, trace=tally_all)
-        self.f.rand()
-        pm.ObjectContainer.__init__(self, locals())
+        f = GaussianProcess('%s_f'%name, self, trace=tally_all)
+        f.rand()
+        l = locals()
+        lk = filter(lambda k:isinstance(l[k],pm.Node), l.keys())
+        l = dict([(k,l[k]) for k in lk])
+        pm.ObjectContainer.__init__(self, l)
         
     def getobjects(self):
         names = ['M_eval','C_eval','S_eval','f_eval','f','fr_check']
