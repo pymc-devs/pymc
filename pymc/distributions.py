97208406323c0c4614cb768c4f0e395c297e469f
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2011-04-13 11:01:41 -0500

    Syntax error fixed in distributions.py

--- pymc/distributions.py
+++ pymc/distributions.py
@@ -1185,7 +1185,7 @@ def gamma_expval(alpha, beta):
 
     Expected value of gamma distribution.
     """
-    return 1. * np.asrray(alpha) / beta
+    return 1. * np.asarray(alpha) / beta
 
 def gamma_like(x, alpha, beta):
     R"""
@@ -1362,7 +1362,7 @@ def half_normal_expval(tau):
     Expected value of half normal distribution.
     """
 
-    return np.sqrt(2. * pi / np.asrray(tau))
+    return np.sqrt(2. * pi / np.asarray(tau))
 
 def half_normal_like(x, tau):
     R"""
@@ -1449,7 +1449,7 @@ def inverse_gamma_expval(alpha, beta):
 
     Expected value of inverse gamma distribution.
     """
-    return 1. * np.asrray(beta) / (alpha-1.)
+    return 1. * np.asarray(beta) / (alpha-1.)
 
 def inverse_gamma_like(x, alpha, beta):
     R"""
@@ -1752,7 +1752,7 @@ def multinomial_expval(n,p):
 
     Expected value of multinomial distribution.
     """
-    return np.asrray([pr * n for pr in p])
+    return np.asarray([pr * n for pr in p])
 
 def multinomial_like(x, n, p):
     R"""
