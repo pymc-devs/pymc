5b2d9245f99ef405b305d005518214f2dfa7b690
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2012-01-10 20:22:55 -0600

    Updated modelbuilding.rst

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if not lapack_info or dist in ['bdist', 'sdist']:
     config.add_extension(name='flib',sources=f_sources)
 
 
-
+# TODO Convert Pyrex to Cython
 # ============================
 # = Compile Pyrex extensions =
 # ============================
