5ac7c51bd9784ab6d8ae3d2ffbd00a6c8e61ef4b
jsalvatier <jsalvatier@gmail.com>
2012-04-18 10:38:44 -0700

    got hierachial model to work right

--- examples/hierarchial.py
+++ examples/hierarchial.py
@@ -1,7 +1,7 @@
 from mcex import *
 import theano.tensor as T 
 from numpy import random, sum as nsum, ones, concatenate, newaxis, dot, arange
-
+from __builtin__ import map
 import numpy as np 
 #import pydevd 
 #pydevd.set_pm_excepthook()
@@ -9,7 +9,7 @@ import numpy as np
 
 random.seed(1)
 
-n_groups = 5
+n_groups = 29
 no_pergroup = 30 
 n_observed = no_pergroup * n_groups
 n_group_predictors = 2
@@ -35,40 +35,54 @@ AddVar(model, group_effects, Normal(0, .1))
 
 
 # sg ~ 
-#sg = FreeVariable("sg", 1, 'float64')
-#AddVar(model, sg, Uniform(.05, 10))
-sg = 1
+sg = FreeVariable("sg", 1, 'float64')
+AddVar(model, sg, Uniform(.05, 10))
+#sg = 1
 
 #m ~ N(mg * pg, sg)
 effects = FreeVariable("effects", (n_groups, n_predictors), 'float64')
 AddVar(model, effects, Normal( sum(group_predictors[:, :, newaxis] * group_effects ,1)  ,sg))
 
 #s ~ 
-#s = FreeVariable("s", n_groups, 'float64')
-s = 1#AddVar(model, s, Uniform(.01, 10))
+s = FreeVariable("s", n_groups, 'float64')
+AddVar(model, s, Uniform(.01, 10))
 
 g = T.constant(group)
 
 #y ~ Normal(m[g] * p, s)
-AddVar(model, T.constant(y), Normal( sum(effects[g] * predictors, 1),1))#s[g]))
+AddVar(model, T.constant(y), Normal( sum(effects[g] * predictors, 1),s[g]))
 
 
 
 view = ModelView(model, 'all')
                  
-chain = {#'sg' : np.array([2.]), 
-         #'s'  : np.ones(n_groups) * 2.,
+chain = {'sg' : np.array([2.]), 
+         's'  : np.ones(n_groups) * 2.,
          'group_effects' : np.zeros((1,) + group_effects_a.shape),
          'effects' : np.zeros(effects_a.shape ) }
 
 chain2 = find_MAP(view, chain)
 
-hmc_cov = approx_cov( view, chain) #find a good orientation using the hessian at the MAP
+hmc_cov = approx_cov( view, chain2) #find a good orientation using the hessian at the MAP
 
-step_method = CompoundStep([hmc.HMCStep(view, hmc_cov)])
+step_method = CompoundStep([hmc.HMCStep(view, hmc_cov, step_size_scaling = .05)])
 
 
 ndraw = 3e3
 
 history = NpHistory(view, ndraw) # an object that keeps track
-print "took :", sample(ndraw, step_method, chain, history)
+print "took :", sample(ndraw, step_method, chain2, history)
+
+def project(chain, name, slc, value):
+    c = chain.copy() 
+    c[name] = c[name].copy()
+    c[name][slc] = value
+    return c
+
+def get_like(chain, name, slc):
+    def like(x):
+        return view.evaluate(project(chain2, name, slc, x))[0]
+    return like
+    
+#x = arange(.001, 5, .001)
+#y = np.array(map(get_like(chain2, 's', 0), x))
