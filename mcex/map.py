56c4b39de33782a559309e4854203a09543f63a4
John Salvatier <jsalvatier@gmail.com>
2012-04-22 01:19:55 -0700

    added minimization function to parameters

--- mcex/map.py
+++ mcex/map.py
@@ -6,7 +6,7 @@ Created on Mar 12, 2011
 from scipy.optimize import fmin_bfgs, fmin_ncg
 import numpy as np 
 
-def find_MAP( model, chain_state, disp = False, retall = False):
+def find_MAP( model, chain_state, minalg = fmin_bfgs, disp = False, retall = False):
     """
     moves the chain to the local maximum a posteriori point given a model
     """
@@ -16,7 +16,7 @@ def find_MAP( model, chain_state, disp = False, retall = False):
     def grad_logp(x):
         return np.nan_to_num(-(model.evaluate_as_vector(model.project(chain_state, x)))[1])
 
-    x = fmin_bfgs(logp, model.subspace(chain_state), grad_logp, disp = disp, retall = retall, full_output = True)
+    x = minalg(logp, model.subspace(chain_state), grad_logp, disp = disp, retall = retall, full_output = True)
     
     chain_state = model.project(chain_state, x[0])
     
@@ -34,7 +34,7 @@ def nan_to_high(x):
         return x
     else: 
         if np.logical_not(np.isfinite(x)): 
-            return 1.79769313e+308
+            return 1.79769313e+200
         else:
             return x
     
