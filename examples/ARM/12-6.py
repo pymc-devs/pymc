3b618067e5e37738254358be4edc3099c7265d40
jsalvatier <jsalvatier@gmail.com>
2012-07-09 07:33:41 -0700

    example changes

--- examples/ARM/12-6.py
+++ examples/ARM/12-6.py
@@ -4,6 +4,8 @@ from mcex import *
 
 
 data = readtabledict('srrs2.dat', delimiter = ',', quotechar='"', skipinitialspace = True)
+
+cty_data= readtabledict('cty.dat', delimiter = ',', quotechar='"', skipinitialspace = True)
 def dictmap(f, d):
     return dict((k, f(v)) for k, v in d.iteritems())
 
@@ -16,6 +18,10 @@ radon = data['activity']
 floor = data['floor']
 lradon = np.log(np. where(radon ==0, .1, radon))
 fips = data['stfips']*1000 + data['cntyfips']
+
+cty_data['fips'] = cty_data['stfips']*1000 + cty_data['ctfips']
+
+ufull = cty_data['Uppm'][searchsorted(cty_data['fips'], fips)]
 
 
 stfips,ctfips,st,cty,lon,lat,Uppm = readtable('cty.dat', delimiter = ',' )
@@ -30,7 +36,10 @@ model = Model()
 chain = {'groupmean' : np.mean(obs_means )[None],
          'groupsd' : np.std(obs_means)[None], 
          'sd' : np.std(lradon)[None], 
-         'means' : obs_means }
+         'means' : obs_means,
+         #'u_m' : np.array([.72]),
+         'floor_m' : np.array([0.]),
+         }
 
 groupmean = AddVar(model, 'groupmean', Normal(0, (10)**-2), test = chain['groupmean'])
 
@@ -39,34 +48,29 @@ groupsd = AddVar(model, 'groupsd', Uniform(0,10), test = chain['groupsd'])
 
 sd = AddVar(model, 'sd', Uniform(0, 10), test = chain['sd'])
 
+floor_m = AddVar(model, 'floor_m', Normal(0, 5.** -2), test = chain['floor_m'])
+#u_m = AddVar(model, 'u_m', Normal(0, 5.** -2), test = chain['u_m'])
+
 means = AddVar(model, 'means', Normal(groupmean, groupsd ** -2), n, test = chain['means'])
 
-AddData(model, lradon, Normal(means[group], sd**-2))
 
+#the gradient of indexing into an array is generally slow unless you have the experimental branch of theano
+AddData(model, lradon, Normal( floor*floor_m + means[group], sd**-2))
 
-def fn(var, idx, C, logp):
-    def lp(x):
-        c = C.copy()
-        v = c[var].copy()
-        v[idx] = x 
-        c[var] = v
-        return logp(c)
-    return lp
 
 #first take some trial samples
-ndraw = 3e2
-C = approx_cov(model, chain)
-step_method = hmc_step(model, model.vars, C,.12, .75)
+ndraw = 1e4
 
-history = NpHistory(model.vars, ndraw)
-state0, t = sample(ndraw, step_method, chain, history)
-print "prelim accept ratio ", np.eman(state0.acceptr())
 
-# now we can use the trial samples to get a better covariance matrix to get better movement
-c = covar(map(history.__getitem__, model.vars))
-step_method = hmc_step(model, model.vars, c, .12, .75)
+c = approx_hess(model, chain)
+
+step_method = hmc_step(model, model.vars, c,.12, .75, is_cov = False)
 
 history = NpHistory(model.vars, ndraw)
 state, t = sample(ndraw, step_method, chain, history)
-print "took :", t
+    
+print  " took: ", t
 print "accept ratio ", np.mean(state.acceptr())
+c = hist_covar(history, model.vars)
+
+# now we can use the trial samples to get a better covariance matrix to get better acceptance 
--- examples/ARM/5-4.py
+++ examples/ARM/5-4.py
@@ -6,7 +6,7 @@ Created on May 18, 2012
 import numpy as np 
 from mcex import * 
 import theano.tensor as t 
-from  ..utils import * 
+from  utils import * 
 
 data  = readtable('wells.dat', delimiter = ' ', quotechar='"')
 
@@ -18,13 +18,13 @@ switched = switched.astype(bool)
 dist100 = dist/100.
 educ4 = educ/4.
 
-predictors = array([ones_like(id), dist, arsenic, dist * arsenic, assoc, educ]).T
+predictors = array([np.ones_like(id), dist, arsenic, dist * arsenic, assoc, educ]).T
 npr = predictors.shape[1]
 
 model = Model()
 
 effects = AddVar(model, 'effects', Normal(mu = 0, tau = 10.**-2), (1,  npr))
-p = tinvlogit(sum(effects * predictors, 1))
+p = invlogit(sum(effects * predictors, 1))
 
 AddData(model, switched, Bernoulli(p))
 
@@ -34,11 +34,11 @@ chain = {'effects' : np.zeros((1,npr))}
 
 #move the chain to the MAP which should be a good starting point
 map = find_MAP(model, chain)
-hmc_cov = approx_cov(model, chain) #find a good orientation using the hessian at the MAP
+hmc_hess = approx_hess(model, chain) #find a good orientation using the hessian at the MAP
 
-step_method = hmc_step(model, model.vars, hmc_cov)#,step_size_scaling = .1, trajectory_length =1. ) 
+step_method = hmc_step(model, model.vars, hmc_hess, is_cov = False)
 
-ndraw = 3e3
+ndraw = 3e2
 
 history = NpHistory(model.vars, ndraw)
 state, t = sample(ndraw, step_method, map, history)
