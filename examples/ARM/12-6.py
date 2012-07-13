85374aa7668dcb59cb91bf49db5e33e1cdad8609
jsalvatier <jsalvatier@gmail.com>
2012-07-13 16:39:49 -0700

    some modifications for tyring to get this to work with the uranium measurements

--- examples/ARM/12-6.py
+++ examples/ARM/12-6.py
@@ -33,46 +33,41 @@ obs_means = np.array([np.mean(lradon[fips == fip]) for fip in np.unique(fips)])
 
 model = Model()
 
-chain = {'groupmean' : np.mean(obs_means )[None],
+start = {'groupmean' : np.mean(obs_means )[None],
          'groupsd' : np.std(obs_means)[None], 
          'sd' : np.std(lradon)[None], 
          'means' : obs_means,
-         #'u_m' : np.array([.72]),
+         'u_m' : np.array([.72]),
          'floor_m' : np.array([0.]),
          }
 
-groupmean = AddVar(model, 'groupmean', Normal(0, (10)**-2), test = chain['groupmean'])
+groupmean = AddVar(model, 'groupmean', Normal(0, (10)**-2), test = start['groupmean'])
 
 #as recommended by "Prior distributions for variance parameters in hierarchical models"
-groupsd = AddVar(model, 'groupsd', Uniform(0,10), test = chain['groupsd'])
+groupsd = AddVar(model, 'groupsd', Uniform(0,10), test = start['groupsd'])
 
-sd = AddVar(model, 'sd', Uniform(0, 10), test = chain['sd'])
+sd = AddVar(model, 'sd', Uniform(0, 10), test = start['sd'])
 
-floor_m = AddVar(model, 'floor_m', Normal(0, 5.** -2), test = chain['floor_m'])
-#u_m = AddVar(model, 'u_m', Normal(0, 5.** -2), test = chain['u_m'])
+floor_m = AddVar(model, 'floor_m', Normal(0, 5.** -2), test = start['floor_m'])
+u_m = AddVar(model, 'u_m', Normal(0, 5.** -2), test = start['u_m'])
 
-means = AddVar(model, 'means', Normal(groupmean, groupsd ** -2), n, test = chain['means'])
+means = AddVar(model, 'means', Normal(groupmean, groupsd ** -2), n, test = start['means'])
 
 
 #the gradient of indexing into an array is generally slow unless you have the experimental branch of theano
-AddData(model, lradon, Normal( floor*floor_m + means[group], sd**-2))
+AddData(model, lradon, Normal( floor*floor_m + means[group] + u_m*ufull, sd**-2))
 
 
-#first take some trial samples
-ndraw = 3e3
 
-#step_method = velocity_hmc_step(model, model.vars, 20,.20, 1.5, )
+nd = np.sum([v.dsize for v in model.vars])
 
-hmc_hess = approx_hess(model, chain) 
-
-step_method = hmc_step(model, model.vars, hmc_hess, .5, 1.5, is_cov = False)
-
-history = NpHistory(model.vars, ndraw)
-
-state, t = sample(ndraw, step_method, chain, history)
+C = np.eye(nd)*1e-3
+for n in [200, 400, 800, 2000]:
+    step_method = hmc_step(model, model.vars, C, .2, 1.0, is_cov = True)
+    history = NpHistory(model.vars, n)
+    state, t = sample(n, step_method, start, history)
+    C = hist_covar(history, model.vars)
+    
+    print "accept ratio ", np.mean(state.acceptr())
     
 print  " took: ", t
-print "accept ratio ", np.mean(state.acceptr())
-c = hist_covar(history, model.vars)
-
-# now we can use the trial samples to get a better covariance matrix to get better acceptance 
