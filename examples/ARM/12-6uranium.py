3a0e48461d82f766796809fe971fd09f6b846806
John Salvatier <jsalvatier@gmail.com>
2012-08-01 14:28:41 -0700

    added the full uranium example

---
+++ examples/ARM/12-6uranium.py
@@ -0,0 +1,78 @@
+from  utils import * 
+import numpy as np 
+from mcex import *
+
+
+data = readtabledict('srrs2.dat', delimiter = ',', quotechar='"', skipinitialspace = True)
+
+cty_data= readtabledict('cty.dat', delimiter = ',', quotechar='"', skipinitialspace = True)
+def dictmap(f, d):
+    return dict((k, f(v)) for k, v in d.iteritems())
+
+mn = data['state'] == 'MN'
+data = dictmap(lambda v: v[mn], data)
+
+
+
+radon = data['activity']
+floor = data['floor']
+lradon = np.log(np. where(radon ==0, .1, radon))
+fips = data['stfips']*1000 + data['cntyfips']
+
+cty_data['fips'] = cty_data['stfips']*1000 + cty_data['ctfips']
+
+ufull = cty_data['Uppm'][searchsorted(cty_data['fips'], fips)]
+
+
+stfips,ctfips,st,cty,lon,lat,Uppm = readtable('cty.dat', delimiter = ',' )
+
+ufips = np.unique(fips)
+n = ufips.shape[0]
+group = np.searchsorted(ufips, fips)
+obs_means = np.array([np.mean(lradon[fips == fip]) for fip in np.unique(fips)])
+
+model = Model()
+
+start = {'groupmean' : np.mean(obs_means )[None],
+         'groupsd' : np.std(obs_means)[None], 
+         'sd' : np.std(lradon)[None], 
+         'means' : obs_means,
+         'u_m' : np.array([.72]),
+         'floor_m' : np.array([0.]),
+         }
+
+groupmean = AddVar(model, 'groupmean', Normal(0, (10)**-2), test = start['groupmean'])
+
+#as recommended by "Prior distributions for variance parameters in hierarchical models"
+groupsd = AddVar(model, 'groupsd', Uniform(0,10), test = start['groupsd'])
+
+sd = AddVar(model, 'sd', Uniform(0, 10), test = start['sd'])
+
+floor_m = AddVar(model, 'floor_m', Normal(0, 5.** -2), test = start['floor_m'])
+u_m = AddVar(model, 'u_m', Normal(0, 5.** -2), test = start['u_m'])
+
+means = AddVar(model, 'means', Normal(groupmean, groupsd ** -2), n, test = start['means'])
+
+
+#the gradient of indexing into an array is generally slow unless you have the experimental branch of theano
+AddData(model, lradon, Normal( floor*floor_m + means[group] + u_m*ufull, sd**-2))
+
+
+hess = approx_hess(model, start)
+hess2 = diag(diag(hess))
+    
+
+n= 1000
+step_method = hmc_step(model, model.vars, hess2, .2, 1.0, is_cov = False)
+history0 = NpHistory(model.vars, n)
+state, t = sample(n, step_method, start, history0)
+
+
+n= 3000
+cov = hist_covar(history0, model.vars)
+step_method = hmc_step(model, model.vars, cov, .5, 1.5, is_cov = True, elow = .4, ehigh = 1.5)
+history = NpHistory(model.vars, n)
+state, t = sample(n, step_method, start, history)
+    
+    
+print  " took: ", t
