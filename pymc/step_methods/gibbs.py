3cb4b46a60c24d76dc70d43999c2ad4b7603f16b
John Salvatier <jsalvatier@gmail.com>
2013-02-08 20:46:53 -0800

    missing references

--- pymc/step_methods/gibbs.py
+++ pymc/step_methods/gibbs.py
@@ -20,10 +20,11 @@ class elemwise_cat_gibbs_step():
         self.elogp = elemwise_logp(model, var)
         self.sh = ones(var.dshape, var.dtype)
         self.values = values
+        self.var = var
     
     def step(self, state, chain):
         p = array([self.elogp(project(chain, self.var, v * self.sh)) for v in self.values])
-        return state, project(categorical(p, var.dshape))
+        return state, project(chain, self.var, categorical(p, self.var.dshape))
 
 def project(chain,var, x):
     c = chain.copy()
