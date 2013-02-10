a769982e06a8ea9a16ddb2533fd1aa29881ade16
John Salvatier <jsalvatier@gmail.com>
2013-02-09 18:31:15 -0800

    trace tests

---
+++ tests/test_trace.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pymc as pm
+from models import simple_init
+
+def check_trace(h, n, step, start):
+
+    #try using a history object a few times
+    for _ in range(2):
+        h, _, _ = psample(n, step, start, h)
+
+        for (var, val) in start.iteritems(): 
+
+            assert np.shape(h[v]) == (n*2,) + np.shape(val)
+
+
+def test_traces():
+    step, start = simple_init()
+
+    for     h in [pm.NpHistory()]:
+        for n in [0, 20, 1000]: 
+
+            yield check_trace(h, n, step, start)
+
+
