2292793246f6587be3be6b0bd2006b7e0fcadc02
John Salvatier <jsalvatier@gmail.com>
2013-02-10 21:24:35 -0800

    metropolis now uses quad potential so it gets better error checking; maybe a bad idea?

--- pymc/step_methods/metropolis.py
+++ pymc/step_methods/metropolis.py
@@ -6,6 +6,7 @@ Created on Mar 7, 2011
 from numpy.linalg import cholesky
 
 from ..core import *
+from quadpotential import quad_potential
 
 from utils import *
 
@@ -14,12 +15,13 @@ class metropolis_step(array_step):
     def __init__(self, model, vars, C, scaling=.25):
         logp_d = model_logp(model)
         
-        self.cholC = cholesky(scaling * C)
+
+        self.potential = quad_potential(C*scaling, False) #
         super(metropolis_step,self).__init__(vars, [logp_d])
         
     def astep(self, state, q0, logp):
 
-        delta = cholesky_normal(q0.shape, self.cholC)
+        delta = self.potential.random()
         
         q = q0 + delta  
         
