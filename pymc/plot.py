53643b6f9098df41046e65dc1ae39f23d486b1b8
John Salvatier <jsalvatier@gmail.com>
2013-02-12 23:35:19 -0800

    added summary plot for trace

---
+++ pymc/plot.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as p
+import numpy as np
+from scipy.stats import kde 
+
+def traceplot(trace, vars=None): 
+    if vars is None:
+        vars = trace.samples.keys()
+
+    n = len(vars)
+    f, ax = p.subplots(2, n, squeeze = False)
+
+    for i,v in enumerate(vars):
+        d = np.squeeze(trace[v])
+
+        kdeplot(ax[0,i], d)
+        ax[1,i].plot(d)
+
+    return f 
+
+def kdeplot(ax, data):
+    for i in range(data.shape[1]):
+        d = data[:,i]
+        density = kde.gaussian_kde(d) 
+        l = np.min(d)
+        u = np.max(d)
+        x = np.linspace(0,1,100)*(u-l)+l
+
+        ax.plot(x,density(x))
+    
+
+
+
+
