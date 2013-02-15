e0fdc987831ea10fdce1c5a4ff7f5b169129930e
John Salvatier <jsalvatier@gmail.com>
2013-02-14 23:04:28 -0500

    history -> trace everywhere

--- pymc/__init__.py
+++ pymc/__init__.py
@@ -2,7 +2,7 @@ from core import *
 from distributions import *
 from math import * 
 
-from history import *
+from trace import *
 from sample import * 
 from step_methods import *
 from tuning import *
