917f413b5e7e3869d545515110a7d3e5fb3ca5a8
John Salvatier <jsalvatier@gmail.com>
2013-02-14 22:57:02 -0500

    examples now found recursively

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,28 +1,37 @@
 import matplotlib
 matplotlib.use('Agg')
 
-import traceback, sys
+import os
 from os import path
 from glob import glob
-
+import fnmatch
 
 def test_examples(): 
-    print example_files()
-    for path in example_files():
+    for path in all_matching_files(example_dir(), '*.py'):
         yield check_example, path
 
 
-def example_files():
+def all_matching_files(d, pattern):
+
+    def addfiles(fls, dir, nfiles):
+        nfiles = fnmatch.filter(nfiles, pattern)
+        nfiles = [path.join(dir,f) for f in nfiles]
+        fls.extend(nfiles)
+        
+    files = []
+    path.walk(d, addfiles, files)
+    return files
+
+def example_dir():
     import pymc
     d = path.dirname(pymc.__file__)
 
-    return glob(path.join(d, "../examples/*.py"))
+    return path.join(d, '../examples/')
 
 
-
-
-
-def check_example(path):
-    with open(path) as f :
-        c = compile(f.read(), path, 'exec')
-        exec(c)
+def check_example(p):
+    with open(p) as f :
+        #c = compile(f.read(), p, 'exec')
+        #exec(c)
+        os.chdir(path.dirname(p))
+        execfile(path.basename(p))
