3d07f1cacb1b12ed0f10b7a105d15f7a11207e04
John Salvatier <jsalvatier@gmail.com>
2013-02-26 14:17:36 -0500

    added int division to math

--- pymc/math.py
+++ pymc/math.py
@@ -1,4 +1,4 @@
-
+from __future__ import division
 from theano.tensor import constant, flatten, zeros_like, ones_like, stack, concatenate, sum, prod, lt,gt, le, ge,eq, neq, switch, clip, where, and_, or_, abs_ 
 
 from theano.tensor import exp,log,cos, sin, tan, cosh, sinh, tanh,sqr, sqrt,erf, erfinv, dot
