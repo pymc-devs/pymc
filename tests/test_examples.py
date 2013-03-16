cc22e22c47b14904dab8da1c5f0a607d54750ce3
John Salvatier <jsalvatier@gmail.com>
2013-03-16 12:36:17 -0700

    fixed up example tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,9 +7,8 @@ import fnmatch
 import imp
 
 def test_examples(): 
-    os.chdir(example_dir())
 
-    for path in all_matching_files('.', '*.py'):
+    for path in all_matching_files(example_dir(), '*.py'):
         yield check_example, path
 
 
@@ -32,7 +31,5 @@ def example_dir():
 
 
 def check_example(p):
-    imp.load_source('__main__', p)
-
-        os.chdir(path.dirname(p))
-        execfile(path.basename(p))
+    os.chdir(path.dirname(p))
+    imp.load_source('__main__', path.basename(p))
