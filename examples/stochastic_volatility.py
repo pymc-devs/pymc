f76a6b151dcda7a77193125c224030cdccef7032
John Salvatier <jsalvatier@gmail.com>
2013-03-20 11:53:39 -0700

    made a notebook for svol example

--- examples/stochastic_volatility.py
+++ examples/stochastic_volatility.py
@@ -1,49 +1,58 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
 import numpy as np
 from pymc import  *
 from pymc.distributions.timeseries import *
 
 from scipy.sparse import csc_matrix
 from  scipy import optimize
+import pylab as pl
 
-"""
-1. Data
--------
-"""
-returns = np.genfromtxt("data/SP500.csv")
+# <markdowncell>
 
-n = 400
-returns = returns[-n:]
+# 1. Build Model
+# --------------
+# Stochastic volatility model described in Hoffman (2011) on p21.
 
-"""
-2. Build Model
---------------
-Stochastic volatility model described in Hoffman (2011) on p21.
-"""
+# <codecell>
+
 model = Model()
 Var = model.Var
 Data = model.Data 
 
-#its easier to sample the scale of the volatility process innovations on a log scale 
+# <markdowncell>
+
+# Its easier to sample the scale of the volatility process innovations on a log scale, so we use `TransformedVar`.
+
+# <codecell>
+
 sd, log_sd = model.TransformedVar('sd', Exponential(1./.02),
                  logtransform, testval = -2.5)
 
 nu = Var('nu', Exponential(1./10))
 
+n = 400
 lvol = Var('lvol', GaussianRandomWalk(sd**-2), shape = n)
 
-lreturns = Data(returns, T(nu, lam = exp(-2*lvol)))
+returns = np.genfromtxt("data/SP500.csv")[-n:]
 
-"""
-3. Fit Model
-------------
-"""
+Data(returns, T(nu, lam = exp(-2*lvol)))
+
+# <markdowncell>
+
+# 2. Fit Model
+# ------------
+# To get a decent scale for the hamiltonaian sampler, we find the hessian at a point. However, the 2nd derivatives for the degrees of freedom are negative and thus not very informative, so we make an educated guess. The interactions between lsd/nu and lvol are also not very useful, so we set them to zero. 
+# 
+# The hessian matrix is also very sparse, so we make it a sparse matrix for faster sampling.
+
+# <codecell>
+
 H = model.d2logpc()
 
-"""
-To get a decent scale for the hamiltonaian sampler, we find the hessian at a point. However, the 2nd derivatives for the degrees of freedom are negative and thus not very informative, so we make an educated guess. The interactions between lsd/nu and lvol are also not very useful, so we set them to zero. 
-
-The hessian matrix is also very sparse, so we make it a sparse matrix for faster sampling.
-"""
 def hessian(point, nusd): 
     h = H(point)
     h[1,1] = nusd**-2
@@ -51,23 +60,35 @@ def hessian(point, nusd):
 
     return csc_matrix(h)
 
-"""
-the full MAP is a degenerate case wrt to sd and nu, so we find the MAP wrt the volatility process, keeping log_sd and nu constant at their default values. we use l_bfgs_b because it is more efficient for high dimensional functions (lvol has n elements)
-"""
+# <markdowncell>
+
+# The full MAP is a degenerate case wrt to sd and nu, so we find the MAP wrt the volatility process, keeping log_sd and nu constant at their default values. We use l_bfgs_b because it is more efficient for high dimensional functions (lvol has n elements)
+
+# <codecell>
 
 s = find_MAP(model, vars = [lvol], fmin = optimize.fmin_l_bfgs_b)
 
-#we do a short initial run to get near the right area
+# <markdowncell>
+
+# We do a short initial run to get near the right area, then start again using a new hessian at the new starting point.
+
+# <codecell>
+
 step = hmc_step(model, model.vars, hessian(s, 6))
 trace, _,t = sample(200, step, s) 
 
-#then start again using a new hessian at the new start
 s2 = trace.point(-1)
 step = hmc_step(model, model.vars, hessian(s2, 6), trajectory_length = 4.)
-trace, _,t = sample(4000, step, trace = trace) 
+trace, _,t = sample(8000, step, trace = trace) 
 
-"""
-4. References
--------------
-    1. Hoffman & Gelman. (2011). The No-U-Turn Sampler: Adaptively Setting Path Lengths in Hamiltonian Monte Carlo. http://arxiv.org/abs/1111.4246 
-"""
+# <codecell>
+
+pl.plot(trace[lvol][::10].T,'b', alpha = .01);
+traceplot(trace, model.vars[:-1])
+
+# <markdowncell>
+
+# 3. References
+# -------------
+#     1. Hoffman & Gelman. (2011). The No-U-Turn Sampler: Adaptively Setting Path Lengths in Hamiltonian Monte Carlo. http://arxiv.org/abs/1111.4246 
+
