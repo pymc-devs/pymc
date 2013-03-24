172f3bed8bb4a9cfaa24073277dc4d8698811322
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-03-24 16:27:47 -0700

    TST: Do not run parallel test if multiprocessing is not available.

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,13 +1,21 @@
 from checks import *
 from models import simple_init
 
+# Test if multiprocessing is available
+import multiprocessing
+try:
+    multiprocessing.Pool(2)
+    test_parallel = True
+except:
+    test_parallel = False
+
 def check_trace(trace, n, step, start):
 
     #try using a trace object a few times
     for i in range(2):
         trace, _, _ = sample(n, step, start, trace)
 
-        for (var, val) in start.iteritems(): 
+        for (var, val) in start.iteritems():
 
             assert np.shape(trace[var]) == (n*(i+1),) + np.shape(val)
 
@@ -16,15 +24,17 @@ def test_trace():
     start, step,_  = simple_init()
 
     for     h in [pm.NpTrace]:
-        for n in [20, 1000]: 
+        for n in [20, 1000]:
             trace = h()
 
             yield check_trace, trace, n, step, start
 
 def test_multitrace():
+    if not test_parallel:
+        return
     start, step,_  = simple_init()
     trace = None
-    for n in [20, 1000]: 
+    for n in [20, 1000]:
 
         yield check_multi_trace, trace, n, step, start
 
@@ -36,12 +46,12 @@ def check_multi_trace(trace, n, step, start):
     for i in range(2):
         trace, _, _ = psample(n, step, start, trace)
 
-        for (var, val) in start.iteritems(): 
+        for (var, val) in start.iteritems():
             for t in trace[var]:
                 assert np.shape(t) == (n*(i+1),) + np.shape(val)
-        
+
         ctrace = trace.combined()
-        for (var, val) in start.iteritems(): 
+        for (var, val) in start.iteritems():
 
             assert np.shape(ctrace[var]) == (len(trace.traces)*n*(i+1),) + np.shape(val)
 
