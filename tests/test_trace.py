02c5132e96900fdd9bd60a3a6df7cc937baa0311
John Salvatier <jsalvatier@gmail.com>
2013-04-14 04:08:37 -0700

    oops

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -25,8 +25,9 @@ def test_trace():
 
     for     h in [pm.NpTrace]:
         for n in [20, 1000]:
-            for [model.vars, model.vars + [model.vars[0]**2]]:
-                trace = h(model.vars)
+            for vars in [model.vars, model.vars + [model.vars[0]**2]]:
+                trace = h(vars)
+                
 
                 yield check_trace, model, trace, n, step, start
 
