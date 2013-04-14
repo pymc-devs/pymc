8302257c00c459ea390ee9b88879da162d348d78
John Salvatier <jsalvatier@gmail.com>
2013-04-14 00:12:44 -0700

    added progress bar

---
+++ pymc/progressbar.py
@@ -0,0 +1,60 @@
+"""
+A simple progress bar to monitor MCMC sampling progress.
+Modified from original code by Corey Goldberg (2010)
+"""
+
+from __future__ import print_function
+
+import sys, time
+try:
+    from IPython.core.display import clear_output
+    have_ipython = True
+except ImportError:
+    have_ipython = False
+
+class ProgressBar:
+    def __init__(self, iterations):
+        self.iterations = iterations
+        self.prog_bar = '[]'
+        self.fill_char = '*'
+        self.width = 40
+        self.__update_amount(0)
+        if have_ipython:
+            self.animate = self.animate_ipython
+        else:
+            self.animate = self.animate_noipython
+
+    def animate_noipython(self, iter):
+        if sys.platform.lower().startswith('win'):
+            print(self, '\r', end='')
+        else:
+            print(self)
+        self.update_iteration(iter)
+        # time.sleep(0.5)
+
+    def animate_ipython(self, iter):
+        try:
+            clear_output()
+        except Exception:
+            # terminal IPython has no clear_output
+            pass
+        print('\r', self, end='')
+        sys.stdout.flush()
+        self.update_iteration(iter)
+
+    def update_iteration(self, elapsed_iter):
+        self.__update_amount((elapsed_iter / float(self.iterations)) * 100.0)
+        self.prog_bar += '  %d of %s complete' % (elapsed_iter, self.iterations)
+
+    def __update_amount(self, new_amount):
+        percent_done = int(round((new_amount / 100.0) * 100.0))
+        all_full = self.width - 2
+        num_hashes = int(round((percent_done / 100.0) * all_full))
+        self.prog_bar = '[' + self.fill_char * num_hashes + ' ' * (all_full - num_hashes) + ']'
+        pct_place = (len(self.prog_bar) / 2) - len(str(percent_done))
+        pct_string = '%d%%' % percent_done
+        self.prog_bar = self.prog_bar[0:pct_place] + \
+            (pct_string + self.prog_bar[pct_place + len(pct_string):])
+
+    def __str__(self):
+        return str(self.prog_bar)
