122d0f430f8e124d233e00ae6116110f924d1c7a
John Salvatier <jsalvatier@gmail.com>
2013-04-26 00:55:34 -0700

    aribtrary stochastic example

---
+++ examples/arbitrary_stochastic.py
@@ -0,0 +1,18 @@
+from pymc import * 
+import numpy as np 
+with Model() as model: 
+    lam = Exponential('lam', 1)
+
+
+    failure = np.array([0, 1])
+    value = np.array([1,0])
+    
+    def logp(failure, value):
+        return sum(failure*log(lam) - lam*value)
+
+    x =  DensityDist('x', logp, observed = (failure, value))
+    
+    start = model.test_point
+    h = find_hessian(start)
+    step = Metropolis(model.vars, h)
+    trace = sample(3000, step, start)
