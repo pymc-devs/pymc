1a2503f667d511c50ab06aaf113323329747a3f2
John Salvatier <jsalvatier@gmail.com>
2013-04-27 03:51:39 -0700

    add factor potential example

--- examples/factor_potential.py
+++ examples/factor_potential.py
@@ -4,7 +4,7 @@ with Model() as model:
     x = Normal('x', 1,1)
     model.AddPotential(-x**2)
 
-    
-    h = find_hessian()
-    step = Metropolis(h)
-    trace = sample(3000, step)
+    start = model.test_point
+    h = find_hessian(start)
+    step = Metropolis(model.vars, h)
+    trace = sample(3000, step, start)
