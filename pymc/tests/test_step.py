3004bd163a28a7f51e1cdaafc7d6591588997e8c
Christopher Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2013-05-03 23:15:20 -0500

    Added slice sampler to test_step

--- pymc/tests/test_step.py
+++ pymc/tests/test_step.py
@@ -1,5 +1,5 @@
-from checks import *
-from models import simple_model, mv_simple
+from .checks import *
+from .models import simple_model, mv_simple
 from theano.tensor import constant
 from scipy.stats.mstats import moment
 
@@ -11,8 +11,8 @@ def check_stat(name, trace, var, stat, value, bound):
 def test_step_continuous():
     start, model, (mu, C) = mv_simple()
 
-    hmc = pm.HamiltonianMC(model.vars,  C, is_cov = True, model = model)
-    mh = pm.Metropolis(model.vars , C, is_cov = True, scaling = 2, model = model)
+    hmc = pm.HamiltonianMC(model.vars,  C, is_cov=True, model=model)
+    mh = pm.Metropolis(model.vars, model=model)
     slicer = pm.Slice(model.vars, model=model)
     compound = pm.CompoundStep([hmc, mh])
 
@@ -25,9 +25,9 @@ def test_step_continuous():
 
     for st in steps:
         np.random.seed(1)
-        h = sample(8000, st, start, track_progress=False, model = model)
+        h = sample(8000, st, start, model=model)
         for (var, stat, val, bound) in check:
             np.random.seed(1)
-            h = sample(8000, st, start, track_progess=False, model = model)
+            h = sample(8000, st, start, model=model)
 
             yield check_stat,repr(st), h, var, stat, val, bound
