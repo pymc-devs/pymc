f99873046a1fb55cf315e5ae7cae78917d459e5b
John Salvatier <jsalvatier@gmail.com>
2013-05-15 00:36:28 -0700

    added knownfail

---
+++ pymc/tests/knownfailure.py
@@ -0,0 +1,15 @@
+import functools
+import nose
+
+def knownfailure(msg):
+    def decorator(test):
+        @functools.wraps(test)
+        def inner(*args, **kwargs):
+            try:
+                test(*args, **kwargs)
+            except Exception:
+                raise nose.SkipTest
+            else:
+                raise AssertionError('Failure expected: ' + msg)
+        return inner
+    return decorator
