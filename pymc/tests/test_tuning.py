3861707df0fac80d5256887ca6ce6c33166bc94d
John Salvatier <jsalvatier@gmail.com>
2013-06-18 08:35:21 -0700

    update test

--- pymc/tests/test_tuning.py
+++ pymc/tests/test_tuning.py
@@ -13,7 +13,6 @@ def test_adjust_precision():
 def test_guess_scaling():
 
     start, model,_ = models.non_normal(n = 5)
-    with model:
-        a1 = scaling.guess_scaling(start)
+    a1 = scaling.guess_scaling(start)
 
     assert all((a1 > 0) & (a1 < 1e200))
