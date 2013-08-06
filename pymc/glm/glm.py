6336012733f17c6ea3ca9f780922fba105f1ed54
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-08-06 09:58:30 -0400

    glm.plot_posterior_predictive more flexible + doc string.

--- pymc/glm/glm.py
+++ pymc/glm/glm.py
@@ -162,9 +162,42 @@ def glm(*args, **kwargs):
     return [y_est] + coeffs + list(non_init_vars)
 
 
-def plot_posterior_predictive(trace):
-    for rand_loc in np.random.randint(0, len(trace), 30):
+def plot_posterior_predictive(trace, eval=None, lm=None, samples=30, **kwargs):
+    """Plot posterior predictive of a linear model.
+
+    :Arguments:
+        trace : <array>
+            Array of posterior samples with columns
+        eval : <array>
+            Array over which to evaluate lm
+        lm : function <default: linear function>
+            Function mapping parameters at different points
+            to their respective outputs.
+            input: point, sample
+            output: estimated value
+        samples : int <default=30>
+            How many posterior samples to draw.
+
+    Additional keyword arguments are passed to pylab.plot().
+
+    """
+
+    if lm is None:
+        lm = lambda x, sample: sample['Intercept'] + sample['x'] * x
+
+    if eval is None:
+        eval = np.linspace(0, 1, 100)
+
+    # Set default plotting arguments
+    if 'lw' not in kwargs and 'linewidth' not in kwargs:
+        kwargs['lw'] = .2
+    if 'c' not in kwargs and 'color' not in kwargs:
+        kwargs['c'] = 'k'
+
+    for rand_loc in np.random.randint(0, len(trace), samples):
         rand_sample = trace[rand_loc]
-        plt.plot([0, 1], [rand_sample['Intercept'],
-                          rand_sample['Intercept'] + rand_sample['x']], lw=.2, c='k')
+        plt.plot(eval, lm(eval, rand_sample), **kwargs)
+        # Make sure to not plot label multiple times
+        kwargs.pop('label', None)
+
     plt.title('Posterior predictive')
