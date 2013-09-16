2e79f7d46ed7ad20027eda09e28806405bd07930
Christopher Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2013-09-15 23:58:10 -0500

    Updated test suite call

--- pymc/tests/__init__.py
+++ pymc/tests/__init__.py
@@ -1,31 +1,11 @@
-from __future__ import with_statement
 import warnings
 
 try:
     from numpy.testing import Tester
-    import numpy
-    old_settings = numpy.seterr(divide='raise', invalid='raise')
 
-    # TODO: Restore this implementation in 2.2, when minimum requirements are changed to Python 2.6
-    # with warnings.catch_warnings():
-    #         warnings.simplefilter('ignore')
-    #         test = Tester().test
-
-    # Taken from
-    # http://stackoverflow.com/questions/2059675/catching-warnings-pre-python-2-6
-    original_filters = warnings.filters[:]
-
-    # Ignore warnings.
-    warnings.simplefilter("ignore")
-
-    try:
-        # Execute the code that presumably causes the warnings.
-        test = Tester().test
-
-    finally:
-        # Restore the list of warning filters.
-        warnings.filters = original_filters
-        numpy.seterr(**old_settings)
+    with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            test = Tester().test
 
 except ImportError:
     warnings.warn(
