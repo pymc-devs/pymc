22e661e7dc9a893e880d5be0b592c0f85e2bcef8
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-11-10 19:50:13 -0500

    Only import glm submodule if statsmodels is present.

--- pymc/tests/test_glm.py
+++ pymc/tests/test_glm.py
@@ -1,14 +1,14 @@
 import unittest
 
 from pymc import *
+import sys
+try:
+    import statsmodels.api as sm
+except ImportError:
+    sys.exit(0)
 
 from pymc.examples import glm_linear, glm_robust
 
-try:
-    import statsmodels.api as sm
-    sm_import = True
-except ImportError:
-    sm_import = False
 
 np.random.seed(1)
 # Generate data
@@ -33,17 +33,12 @@ data_logistic = dict(x=x_logistic, y=bern_trials)
 class TestGLM(unittest.TestCase):
     def test_linear_component(self):
         with Model() as model:
-            y_est, coeffs = glm.linear_component('y ~ x', data_linear, init=sm_import)
-            if sm_import:
-                for coeff, true_val in zip(coeffs, [true_intercept, true_slope]):
-                    self.assertAlmostEqual(coeff.tag.test_value, true_val, 1)
+            y_est, coeffs = glm.linear_component('y ~ x', data_linear)
+            for coeff, true_val in zip(coeffs, [true_intercept, true_slope]):
+                self.assertAlmostEqual(coeff.tag.test_value, true_val, 1)
             sigma = Uniform('sigma', 0, 20)
             y_obs = Normal('y_obs', mu=y_est, sd=sigma, observed=y_linear)
-            if sm_import:
-                start = find_MAP(vars=[sigma])
-            else:
-                start = find_MAP()
-
+            start = find_MAP(vars=[sigma])
             step = Slice(model.vars)
             trace = sample(2000, step, start, progressbar=False)
 
@@ -52,8 +47,6 @@ class TestGLM(unittest.TestCase):
             self.assertAlmostEqual(np.mean(trace.samples['sigma'].value), true_sd, 1)
 
     def test_glm(self):
-        if not sm_import:
-            return
         with Model() as model:
             vars = glm.glm('y ~ x', data_linear)
             for coeff, true_val in zip(vars[1:], [true_intercept, true_slope, true_sd]):
@@ -66,9 +59,6 @@ class TestGLM(unittest.TestCase):
             self.assertAlmostEqual(np.mean(trace.samples['sigma'].value), true_sd, 1)
 
     def test_glm_link_func(self):
-        if not sm_import:
-            return
-
         with Model() as model:
             vars = glm.glm('y ~ x', data_logistic,
                            family=glm.families.Binomial(link=glm.links.Logit))
