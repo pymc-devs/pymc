8d889e105abb63d04ac06f732b499419218f6f7c
John Salvatier <jsalvatier@gmail.com>
2013-12-04 09:27:51 -0800

    finish removing quickclass

--- pymc/distributions/transforms.py
+++ pymc/distributions/transforms.py
@@ -3,40 +3,44 @@ from .dist_math import *
 __all__ = ['transform', 'logtransform', 'simplextransform']
 
 
-@quickclass(object)
-def transform(name, forward, backward, jacobian_det, getshape=None):
-    def apply(dist):
 
-        @quickclass(TensorDist)
-        def TransformedDistribtuion():
-            try:
-                testval = forward(dist.testval)
-            except TypeError:
-                testval = dist.testval
+class Transform(object):
+    def __init__(self, name, forward, backward, jacobian_det, getshape=None):
+        self.__dict__.update(locals())
 
-            def logp(x):
-                return dist.logp(backward(x)) + jacobian_det(x)
+    def apply(self, dist):
+        return TransformedDistribtuion(dist, self)
 
-            if hasattr(dist, "mode"):
-                mode = forward(dist.mode)
-            if hasattr(dist, "median"):
-                mode = forward(dist.median)
-
-            _v = forward(dist.makevar('_v'))
-            type = _v.type
-            shape = _v.shape.tag.test_value
-            dtype = _v.dtype
-
-            default_testvals = dist.default_testvals
-
-            return locals()
-
-        return TransformedDistribtuion.dist()
-
-    def __str__():
+    def __str__(self):
         return name + " transform"
 
-    return locals()
+class TransformedDistribtuion(TensorDist):
+    def __init__(self, dist, transform, *args, **kwargs):
+        TensorDist.__init__(self, *args, **kwargs)
+        try:
+            testval = forward(dist.testval)
+        except TypeError:
+            testval = dist.testval
+        
+        if hasattr(dist, "mode"):
+            mode = forward(dist.mode)
+        if hasattr(dist, "median"):
+            mode = forward(dist.median)
+
+        _v = forward(dist.makevar('_v'))
+        type = _v.type
+        shape = _v.shape.tag.test_value
+        dtype = _v.dtype
+
+        default_testvals = dist.default_testvals
+        self.__dict__.update(locals())
+
+
+    def logp(self, x):
+        return self.dist.logp(self.backward(x)) + self.jacobian_det(x)
+
+
+transform = Transform
 
 logtransform = transform("log", log, exp, idfn)
 
