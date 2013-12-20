8208c1cd53ba706d593e534203ae26b38a22508c
John Salvatier <jsalvatier@gmail.com>
2013-12-20 05:28:18 -0800

    simplify get data file

--- pymc/data.py
+++ pymc/data.py
@@ -1,8 +1,8 @@
 import pkgutil
-from io import StringIO
+import io
 
 __all__ = ['get_data_file']
 
 def get_data_file(pkg, path):
-        return StringIO(unicode(pkgutil.get_data(pkg, path)))
+        return io.BytesIO(pkgutil.get_data(pkg, path))
 
