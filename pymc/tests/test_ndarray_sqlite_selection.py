156fc425849e27785e21433389e41b5cffca11de
Kyle Meyer <kyle@kyleam.com>
2014-01-06 15:17:11 -0500

    Test equality of NDArray and SQLite selections
    This test compares of selection methods for NDArray to SQLite traces.

---
+++ pymc/tests/test_ndarray_sqlite_selection.py
@@ -0,0 +1,114 @@
+import os
+import numpy as np
+import numpy.testing as npt
+import unittest
+import multiprocessing as mp
+
+import pymc as pm
+
+
+def remove_db(db):
+    try:
+        os.remove(db)
+    except FileNotFoundError:
+        pass
+
+
+class TestCompareNDArraySQLite(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        ## Use two threads if available
+        try:
+            mp.Pool(2)
+            threads = 2
+        except:
+            threads = 1
+
+        data = np.random.normal(size=(3, 20))
+        n = 1
+
+        model = pm.Model()
+        with model:
+            x = pm.Normal('x', 0, 1., shape=n)
+
+            # start sampling at the MAP
+            start = {'x': 0.}
+            step = pm.Metropolis()
+            cls.db = 'test.db'
+
+            try:
+                cls.draws = 10
+                cls.ntrace = pm.sample(cls.draws, step=step,
+                                       threads=threads, random_seed=4)
+                cls.strace = pm.sample(cls.draws, step=step,
+                                       threads=threads, random_seed=4,
+                                       db=pm.backends.SQLite(cls.db))
+            except:
+                remove_db(cls.db)
+                raise
+
+    @classmethod
+    def tearDownClass(cls):
+        remove_db(cls.db)
+
+    def test_chain_length(self):
+        assert self.ntrace.nchains == self.strace.nchains
+        assert len(self.ntrace) == len(self.strace)
+
+    def test_get_item(self):
+        npt.assert_equal(self.ntrace['x'], self.strace['x'])
+
+    def test_get_values(self):
+        for cf in [False, True]:
+            npt.assert_equal(self.ntrace.get_values('x', combine=cf),
+                             self.strace.get_values('x', combine=cf))
+
+    def test_get_values_no_squeeze(self):
+        npt.assert_equal(self.ntrace.get_values('x', combine=False,
+                                                squeeze=False),
+                         self.strace.get_values('x', combine=False,
+                                                squeeze=False))
+
+    def test_get_values_combine_and_no_squeeze(self):
+        npt.assert_equal(self.ntrace.get_values('x', combine=True,
+                                                squeeze=False),
+                         self.strace.get_values('x', combine=True,
+                                                squeeze=False))
+
+    def test_get_values_with_burn(self):
+        for cf in [False, True]:
+            npt.assert_equal(self.ntrace.get_values('x', combine=cf, burn=3),
+                             self.strace.get_values('x', combine=cf, burn=3))
+
+            ## burn so just one value left
+            npt.assert_equal(self.ntrace.get_values('x', combine=cf,
+                                                    burn=self.draws - 1),
+                             self.strace.get_values('x', combine=cf,
+                                                    burn=self.draws - 1))
+
+    def test_get_values_with_thin(self):
+        for cf in [False, True]:
+            npt.assert_equal(self.ntrace.get_values('x', combine=cf, thin=2),
+                             self.strace.get_values('x', combine=cf, thin=2))
+
+    def test_get_values_with_burn_and_thin(self):
+        for cf in [False, True]:
+            npt.assert_equal(self.ntrace.get_values('x', combine=cf,
+                                                    burn=2, thin=2),
+                             self.strace.get_values('x', combine=cf,
+                                                    burn=2, thin=2))
+
+    def test_get_values_with_chains_arg(self):
+        for cf in [False, True]:
+            npt.assert_equal(self.ntrace.get_values('x', chains=[0]),
+                             self.strace.get_values('x', chains=[0]))
+
+    def test_point(self):
+        npoint, spoint = self.ntrace[4], self.strace[4]
+        npt.assert_equal(npoint['x'], spoint['x'])
+
+    def test_point_with_chain_arg(self):
+        npoint = self.ntrace.point(4, chain=0)
+        spoint = self.strace.point(4, chain=0)
+        npt.assert_equal(npoint['x'], spoint['x'])
