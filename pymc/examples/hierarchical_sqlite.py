408bf25432fd7c9b355bc549f18f552d89a02a43
Kyle Meyer <kyle@kyleam.com>
2014-03-20 00:10:59 -0400

    Add SQLite backend example for hierarchical.py

---
+++ pymc/examples/hierarchical_sqlite.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':  # Avoid loading during tests.
+
+    import pymc as pm
+    import pymc.examples.hierarchical as hier
+
+    with hier.model:
+        trace = pm.sample(3000, hier.step, hier.start, trace='sqlite')
