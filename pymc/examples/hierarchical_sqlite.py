e6a32c66714990e30987381a880e7915733d81c1
Kyle Meyer <kyle@kyleam.com>
2014-03-20 17:10:07 -0400

    Hide hierarchical SQLite example under main
    I'm moving this under main so that it doesn't run with "test_examples".
    This could be set up like the other examples, with a run definition that
    allows for a short version, but I'd prefer not to for a couple of
    reasons.
    
    1. Everything aside from the SQLite backend is the same as
       "hierarchical.py", so it isn't testing much more for the time added
       to the run.
    2. This results in an SQLite file, so a cleanup should be added
       somewhere if it is run with "test_examples".

--- pymc/examples/hierarchical_sqlite.py
+++ pymc/examples/hierarchical_sqlite.py
@@ -1,5 +1,8 @@
-import pymc as pm
-import pymc.examples.hierarchical as hier
 
-with hier.model:
-    trace = pm.sample(3000, hier.step, hier.start, trace='sqlite')
+if __name__ == '__main__':
+    ## Avoid loading during tests.
+    import pymc as pm
+    import pymc.examples.hierarchical as hier
+
+    with hier.model:
+        trace = pm.sample(3000, hier.step, hier.start, trace='sqlite')
