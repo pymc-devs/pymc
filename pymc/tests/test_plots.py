f0cf7c5d551465aaeae97e43344d6973f925367c
John Salvatier <jsalvatier@gmail.com>
2014-03-27 00:37:35 -0700

    add support for variables > 1d in traceplot
    traceplot would generate an error if a trace of a more than 1-d
    variable was.
    
    Now we ravel the dimensions after the first.
    
    We should support for this in forestplot and autocorrplot.

--- pymc/tests/test_plots.py
+++ pymc/tests/test_plots.py
@@ -1,6 +1,10 @@
 import matplotlib
 matplotlib.use('Agg', warn=False)
 
+import numpy as np 
+from checks import close_to
+
+import pymc.plots
 from pymc.plots import *
 from pymc import psample, Slice, Metropolis, find_hessian, sample
 
@@ -17,9 +21,26 @@ def test_plots():
         step = Metropolis(model.vars, h)
         trace = sample(3000, step, start)
 
+        traceplot(trace)
         forestplot(trace)
 
         autocorrplot(trace)
+
+def test_plots_multidimensional():
+
+    # Test single trace
+    from models import multidimensional_model
+
+
+    start, model, _ = multidimensional_model()
+    with model as model:
+        h = np.diag(find_hessian(start))
+        step = Metropolis(model.vars, h)
+        trace = sample(3000, step, start)
+
+        traceplot(trace)
+        #forestplot(trace)
+        #autocorrplot(trace)
 
 
 def test_multichain_plots():
@@ -36,3 +57,16 @@ def test_multichain_plots():
     forestplot(ptrace, vars=['early_mean', 'late_mean'])
 
     autocorrplot(ptrace, vars=['switchpoint'])
+
+def test_make_2d(): 
+
+    a = np.arange(4)
+    close_to(pymc.plots.make_2d(a), a[:,None], 0)
+
+    n = 7
+    a = np.arange(n*4*5).reshape((n,4,5))
+    res = pymc.plots.make_2d(a)
+
+    assert res.shape == (n,20)
+    close_to(a[:,0,0], res[:,0], 0)
+    close_to(a[:,3,2], res[:,2*4+3], 0)
