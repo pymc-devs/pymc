a3f0a100e0c6a9f2e4aaa7af311f6ba582907187
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2014-05-30 06:34:51 -0500

    Added Slice sampler to 2.3 code base

--- pymc/StepMethods.py
+++ pymc/StepMethods.py
@@ -8,6 +8,8 @@ from numpy.linalg import pinv, cholesky
 from numpy.random import randint, random
 from numpy.random import normal as rnormal
 from numpy.random import poisson as rpoisson
+from numpy.random import uniform as runiform
+from numpy.random import exponential as rexponential
 from .PyMCObjects import Stochastic, Potential, Deterministic
 from .Container import Container
 from .Node import ZeroProbability, Node, Variable, StochasticBase
@@ -52,7 +54,8 @@ __all__ = [
     'Gibbs',
     'conjugate_Gibbs_competence',
     'nonconjugate_Gibbs_competence',
-    'DrawFromPrior']
+    'DrawFromPrior',
+    'Slicer']
 
 
 StepMethodRegistry = []
@@ -945,7 +948,6 @@ class BinaryMetropolis(Metropolis):
                     (self._id, self.stochastic))
 
     def propose(self):
-        # Propose new values
 
         if self.proposal_distribution == 'Prior':
             self.stochastic.random()
@@ -1875,9 +1877,101 @@ class TWalk(StepMethod):
             self.values[0] = self.stochastic.value
 
 
-class IIDSStepper(StepMethod):
+class Slicer(StepMethod):
+    """ 
+    Slice Sampler Step Method
+    
+    (c) 2013 Dominik Wabersich <dominik.wabersich [aet] gmail.com>
+    """
 
-    """
-    See written documentation.
-    """
-    pass
+    def __init__(self, stochastic, w=1, m=20, n_tune=0, verbose=-1, tally=False):
+        """ 
+        Slice sampler class initialization
+        """
+        # Initialize superclass
+        StepMethod.__init__(self, [stochastic], tally=tally)
+
+        # id string
+        self._id = "Slicer"
+
+        # Set public attributes
+        self.stochastic = stochastic
+        if verbose > -1:
+            self.verbose = verbose
+        else:
+            self.verbose = stochastic.verbose
+
+        self.w_tune = []
+        self.n_tune = n_tune
+        self._tuning_info = ["w_tune", "n_tune"]
+        self.w = w
+        self.m = m
+
+    @staticmethod
+    def competence(s):
+        """ 
+        The competence function for Slice
+        """
+        if s.dtype in float_dtypes:
+            return 1
+        else:
+            return 0
+
+    def step(self):
+        """ 
+        Slice step method
+        """
+        y = self.loglike - rexponential(1)
+
+        # Stepping out procedure
+        L = self.stochastic.value - self.w*random()
+        R = L + self.w
+        J = np.floor(self.m*random())
+        K = (self.m-1)-J
+        while(J>0 and y<self.fll(L)):
+            L = L - self.w
+            J = J - 1
+        while(K>0 and y<self.fll(R)):
+            R = R + self.w
+            K = K - 1
+        #self.stochastic.last_value = self.stochastic.value
+        self.stochastic.value = runiform(L,R)
+        try:
+            y_new = self.loglike
+        except ZeroProbability:
+            y_new = -np.infty
+        while(y_new<y):
+            if (self.stochastic.value < self.stochastic.last_value):
+                L = float(self.stochastic.value)
+            else:
+                R = float(self.stochastic.value)
+            self.stochastic.revert()
+            self.stochastic.value = runiform(L,R)
+            try:
+                y_new = self.loglike
+            except ZeroProbability:
+                y_new = -np.infty
+  
+
+    def fll(self, value):
+        """
+        fll(value) returns loglike of value
+        """
+        self.stochastic.value = value
+        try:
+            ll = self.loglike
+        except ZeroProbability:
+            ll = -np.infty
+        self.stochastic.revert()
+        return ll
+
+    def tune(self, verbose=-1):
+        """
+        Slice tune method
+        """
+        if (len(self.w_tune) >= self.n_tune):
+            return False
+        else:
+            self.w_tune.append(abs(self.stochastic.last_value - self.stochastic.value))
+            self.w = 2 * (sum(self.w_tune)/len(self.w_tune))
+            return True
