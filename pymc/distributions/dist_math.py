872370ddc583c7cf06eba8958337603258490101
Kai Londenberg <kai.londenberg@gfk.com>
2014-06-05 14:21:23 +0000

    Changed bound() function so that it efficiently deals with scalars and vector inputs without calculating unneccessary and possibly invalid expressions

--- pymc/distributions/dist_math.py
+++ pymc/distributions/dist_math.py
@@ -13,15 +13,62 @@ from theano.tensor import (
     zeros_like, ones, ones_like,
     concatenate, constant, argmax)
 
+import theano
 
-from numpy import pi, inf, nan
+
+from numpy import pi, inf, nan, float64, array
 from .special import gammaln, multigammaln
-
+from theano.ifelse import ifelse
 from theano.printing import Print
 from .distribution import *
 
+def guess_is_scalar(c):
+    bcast = None
+    if (isinstance(c, t.TensorType)):
+        return c.broadcastable == ()
+    
+    if (isinstance(c, t.Variable) or isinstance(c, t.Constant)):
+        return c.type.broadcastable == ()
+    
+    tt = t.as_tensor_variable(c)
+    return tt.type.broadcastable == ()
 
 def bound(logp, *conditions):
+    if (guess_is_scalar(logp)):
+        return bound_scalar(logp, *conditions)
+    seqs = [logp]
+    non_seqs = []
+    for i, c in enumerate(conditions):
+        if (guess_is_scalar(c)):
+            non_seqs.append(c)
+        else:
+            seqs.append(c)
+    logp_vec, updates = theano.scan(fn=bound_scalar,
+                                      outputs_info=None,
+                                      sequences=seqs, non_sequences=non_seqs)
+    return logp_vec
+
+impossible = constant(-inf, dtype=float64) # We re-use that constant
+
+def bound_scalar(logp, *conditions):
+    """
+    Bounds a log probability density with several conditions
+
+    Parameters
+    ----------
+    logp : float
+    *conditionss : booleans
+
+    Returns
+    -------
+    logp if all conditions are true
+    -inf if some are false
+    """        
+    cond = alltrue(conditions)
+    return ifelse(cond, logp, impossible)
+
+  
+def bound_switched(logp, *conditions):
     """
     Bounds a log probability density with several conditions
 
@@ -35,9 +82,7 @@ def bound(logp, *conditions):
     logp if all conditions are true
     -inf if some are false
     """
-
-    return switch(alltrue(conditions), logp, -inf)
-
+    return switch(alltrue(conditions), logp, impossible)
 
 def alltrue(vals):
     ret = 1
