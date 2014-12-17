aec56976cc1f001dc65830e980ea444e899e708c
Thomas Wiecki <thomas.wiecki@gmail.com>
2014-12-17 08:30:37 +0100

    Change NUTS average acceptance ratio to 0.8
    Stan changed their target acceptance ratio of the
    NUTS sampler recently to 0.8:
    https://twitter.com/betanalpha/status/537574485302714368
    because of strong theoretical considerations:
    http://arxiv.org/abs/1411.6669

--- pymc/step_methods/nuts.py
+++ pymc/step_methods/nuts.py
@@ -20,7 +20,7 @@ class NUTS(ArrayStep):
     default_blocked = True
     def __init__(self, vars=None, scaling=None, step_scale=0.25, is_cov=False, state=None,
                  Emax=1000,
-                 target_accept=0.65,
+                 target_accept=0.8,
                  gamma=0.05,
                  k=0.75,
                  t0=10,
