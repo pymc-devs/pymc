db0cc31e0f46cd65029abdaf60300c17cc82fca8
Kyle Meyer <kyle@kyleam.com>
2015-09-07 00:23:00 -0400

    Use tracetab functions in SQLite backend

--- pymc3/tests/test_sqlite_backend.py
+++ pymc3/tests/test_sqlite_backend.py
@@ -56,33 +56,3 @@ class TestNDArraySqliteEquality(bf.BackendEqualityTestCase):
     backend1 = sqlite.SQLite
     name1 = DBNAME
     shape = (2, 3)
-
-
-def test_create_column_0d():
-    shape = ()
-    result = sqlite._create_colnames(shape)
-    expected = ['v1']
-    assert result == expected
-    assert sqlite._create_shape(result) == shape
-
-
-def test_create_column_1d():
-    shape = 2,
-    result = sqlite._create_colnames(shape)
-    expected = ['v1', 'v2']
-    assert result == expected
-    assert sqlite._create_shape(result) == shape
-
-
-def test_create_column_2d():
-    shape = 2, 3
-    result = sqlite._create_colnames(shape)
-    expected = ['v1_1', 'v1_2', 'v1_3',
-                'v2_1', 'v2_2', 'v2_3']
-    assert result == expected
-    assert sqlite._create_shape(result) == shape
-
-
-def test_create_column_3d():
-    shape = 2, 3, 4
-    assert sqlite._create_shape(sqlite._create_colnames(shape)) == shape
