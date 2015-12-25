7ba3ea351aff34e26302b942a715a626e7823f22
Sergei Lebedev <superbobry@gmail.com>
2015-12-25 14:56:03 +0300

    Applying the reverted #903
    This reverts commit 4d77268c95b8a041a3bac848860765afdad1af46.

--- pymc3/distributions/discrete.py
+++ pymc3/distributions/discrete.py
@@ -1,14 +1,16 @@
-from .dist_math import *
-from theano.tensor import as_tensor_variable
-from .distribution import draw_values, generate_samples
-import theano
-import scipy.stats as st
-import numpy as np
-import numpy.random as nr
+from functools import partial
 
-__all__ = ['Binomial',  'BetaBin',  'Bernoulli',  'Poisson', 'NegativeBinomial',
-'ConstantDist', 'ZeroInflatedPoisson', 'DiscreteUniform', 'Geometric',
-'Categorical']
+import numpy as np
+import theano
+import theano.tensor as T
+from scipy import stats
+
+from .dist_math import bound, factln, binomln, betaln, logpow
+from .distribution import Discrete, draw_values, generate_samples
+
+__all__ = ['Binomial',  'BetaBin',  'Bernoulli',  'Poisson',
+           'NegativeBinomial', 'ConstantDist', 'ZeroInflatedPoisson',
+           'DiscreteUniform', 'Geometric', 'Categorical']
 
 
 class Binomial(Discrete):
@@ -18,7 +20,8 @@ class Binomial(Discrete):
     experiments, each of which yields success with probability p.
 
     .. math::
-        f(x \mid n, p) = \frac{n!}{x!(n-x)!} p^x (1-p)^{n-x}
+
+       f(x \mid n, p) = \frac{n!}{x!(n-x)!} p^x (1-p)^{n-x}
 
     Parameters
     ----------
@@ -30,17 +33,16 @@ class Binomial(Discrete):
     .. note::
     - :math:`E(X)=np`
     - :math:`Var(X)=np(1-p)`
-
     """
     def __init__(self, n, p, *args, **kwargs):
         super(Binomial, self).__init__(*args, **kwargs)
         self.n = n
         self.p = p
-        self.mode = cast(round(n * p), self.dtype)
+        self.mode = T.cast(T.round(n * p), self.dtype)
 
     def random(self, point=None, size=None, repeat=None):
         n, p = draw_values([self.n, self.p], point=point)
-        return generate_samples(st.binom.rvs, n=n, p=p,
+        return generate_samples(stats.binom.rvs, n=n, p=p,
                                 dist_shape=self.shape,
                                 size=size)
 
@@ -49,11 +51,7 @@ class Binomial(Discrete):
         p = self.p
 
         return bound(
-
-            logpow(p, value) + logpow(
-                1 - p, n - value) + factln(
-                    n) - factln(value) - factln(n - value),
-
+            binomln(n, value) + logpow(p, value) + logpow(1 - p, n - value),
             0 <= value, value <= n,
             0 <= p, p <= 1)
 
@@ -65,7 +63,11 @@ class BetaBin(Discrete):
     :math:`\texttt{Beta}(\alpha,\beta)` distribution.
 
     .. math::
-        f(x \mid \alpha, \beta, n) = \frac{\Gamma(\alpha + \beta)}{\Gamma(\alpha)} \frac{\Gamma(n+1)}{\Gamma(x+1)\Gamma(n-x+1)} \frac{\Gamma(\alpha + x)\Gamma(n+\beta-x)}{\Gamma(\alpha+\beta+n)}
+
+       f(x \mid \alpha, \beta, n) =
+           \frac{\Gamma(\alpha + \beta)}{\Gamma(\alpha)}
+           \frac{\Gamma(n+1)}{\Gamma(x+1)\Gamma(n-x+1)}
+           \frac{\Gamma(\alpha + x)\Gamma(n+\beta-x)}{\Gamma(\alpha+\beta+n)}
 
     Parameters
     ----------
@@ -79,25 +81,24 @@ class BetaBin(Discrete):
     .. note::
     - :math:`E(X)=n\frac{\alpha}{\alpha+\beta}`
     - :math:`Var(X)=n\frac{\alpha \beta}{(\alpha+\beta)^2(\alpha+\beta+1)}`
-
     """
     def __init__(self, alpha, beta, n, *args, **kwargs):
         super(BetaBin, self).__init__(*args, **kwargs)
         self.alpha = alpha
         self.beta = beta
         self.n = n
-        self.mode = cast(round(alpha / (alpha + beta)), 'int8')
+        self.mode = T.cast(T.round(alpha / (alpha + beta)), 'int8')
 
     def _random(self, alpha, beta, n, size=None):
         size = size or 1
-        p = np.atleast_1d(st.beta.rvs(a=alpha, b=beta, size=np.prod(size)))
+        p = np.atleast_1d(stats.beta.rvs(a=alpha, b=beta, size=np.prod(size)))
         # Sometimes scipy.beta returns nan. Ugh.
         while np.any(np.isnan(p)):
             i = np.isnan(p)
-            p[i] = st.beta.rvs(a=alpha, b=beta, size=np.sum(i))
+            p[i] = stats.beta.rvs(a=alpha, b=beta, size=np.sum(i))
         # Sigh...
         _n, _p, _size = np.atleast_1d(n).flatten(), p.flatten(), np.prod(size)
-        samples = np.reshape(st.binom.rvs(n=_n, p=_p, size=_size), size)
+        samples = np.reshape(stats.binom.rvs(n=_n, p=_p, size=_size), size)
         return samples
 
     def random(self, point=None, size=None, repeat=None):
@@ -110,15 +111,11 @@ class BetaBin(Discrete):
     def logp(self, value):
         alpha = self.alpha
         beta = self.beta
-        n = self.n
-
-        return bound(gammaln(alpha + beta) - gammaln(alpha) - gammaln(beta) +
-                     gammaln(n + 1) - gammaln(value + 1) - gammaln(n - value + 1) +
-                     gammaln(alpha + value) + gammaln(n + beta - value) - gammaln(beta + alpha + n),
-
-                     0 <= value, value <= n,
-                     alpha > 0,
-                     beta > 0)
+        return bound(binomln(self.n, value)
+                     + betaln(value + alpha, self.n - value + beta)
+                     - betaln(alpha, beta),
+                     value >= 0, value <= self.n,
+                     alpha > 0, beta > 0)
 
 
 class Bernoulli(Discrete):
@@ -137,25 +134,24 @@ class Bernoulli(Discrete):
     .. note::
     - :math:`E(x)= p`
     - :math:`Var(x)= p(1-p)`
-
     """
     def __init__(self, p, *args, **kwargs):
         super(Bernoulli, self).__init__(*args, **kwargs)
         self.p = p
-        self.mode = cast(round(p), 'int8')
+        self.mode = T.cast(T.round(p), 'int8')
 
     def random(self, point=None, size=None, repeat=None):
         p = draw_values([self.p], point=point)
-        return generate_samples(st.bernoulli.rvs, p,
+        return generate_samples(stats.bernoulli.rvs, p,
                                 dist_shape=self.shape,
                                 size=size)
 
     def logp(self, value):
         p = self.p
         return bound(
-            switch(value, log(p), log(1 - p)),
+            T.switch(value, T.log(p), T.log(1 - p)),
             value >= 0, value <= 1,
-            0 <= p, p <= 1)
+            p >= 0, p <= 1)
 
 
 class Poisson(Discrete):
@@ -174,21 +170,21 @@ class Poisson(Discrete):
     Parameters
     ----------
     mu : float
-        Expected number of occurrences during the given interval, :math:`\mu \geq 0`.
+        Expected number of occurrences during the given interval,
+        :math:`\mu \geq 0`.
 
     .. note::
        - :math:`E(x)=\mu`
        - :math:`Var(x)=\mu`
-
     """
     def __init__(self, mu, *args, **kwargs):
         super(Poisson, self).__init__(*args, **kwargs)
         self.mu = mu
-        self.mode = floor(mu).astype('int32')
+        self.mode = T.floor(mu).astype('int32')
 
     def random(self, point=None, size=None, repeat=None):
         mu = draw_values([self.mu], point=point)
-        return generate_samples(st.poisson.rvs, mu, 
+        return generate_samples(stats.poisson.rvs, mu,
                                 dist_shape=self.shape,
                                 size=size)
 
@@ -203,53 +199,52 @@ class NegativeBinomial(Discrete):
     """
     Negative binomial log-likelihood.
 
-     The negative binomial distribution  describes a Poisson random variable
-     whose rate parameter is gamma distributed. PyMC's chosen parameterization
-     is based on this mixture interpretation.
+    The negative binomial distribution  describes a Poisson random variable
+    whose rate parameter is gamma distributed. PyMC's chosen parameterization
+    is based on this mixture interpretation.
 
     .. math::
-        f(x \mid \mu, \alpha) = \frac{\Gamma(x+\alpha)}{x! \Gamma(\alpha)} (\alpha/(\mu+\alpha))^\alpha (\mu/(\mu+\alpha))^x
+
+       f(x \mid \mu, \alpha) =
+           \frac{\Gamma(x+\alpha)}{x! \Gamma(\alpha)}
+           (\alpha/(\mu+\alpha))^\alpha (\mu/(\mu+\alpha))^x
 
     Parameters
     ----------
-      mu : float
-          mu > 0
-      alpha : float
-          alpha > 0
+    mu : float
+        mu > 0
+    alpha : float
+        alpha > 0
 
     .. note::
       - :math:`E[x]=\mu`
-
     """
     def __init__(self, mu, alpha, *args, **kwargs):
         super(NegativeBinomial, self).__init__(*args, **kwargs)
         self.mu = mu
         self.alpha = alpha
-        self.mode = floor(mu).astype('int32')
+        self.mode = T.floor(mu).astype('int32')
 
     def random(self, point=None, size=None, repeat=None):
         mu, alpha = draw_values([self.mu, self.alpha], point=point)
-        g = generate_samples(st.gamma.rvs, alpha, scale=alpha / mu,
+        g = generate_samples(stats.gamma.rvs, alpha, scale=alpha / mu,
                              dist_shape=self.shape,
                              size=size)
-        g[g == 0] = np.finfo(float).eps# Just in case
-        return st.poisson.rvs(g)
+        g[g == 0] = np.finfo(float).eps  # Just in case
+        return stats.poisson.rvs(g)
 
     def logp(self, value):
         mu = self.mu
         alpha = self.alpha
+        negbinom = bound(binomln(value + alpha - 1, value)
+                         + logpow(mu / (mu + alpha), value)
+                         + logpow(alpha / (mu + alpha), alpha),
+                         value >= 0, mu > 0, alpha > 0)
 
-        # Return Poisson when alpha gets very large
-        pois = bound(logpow(mu, value) - factln(value) - mu,
-                     mu > 0,
-                     value >= 0)
-        negbinom = bound(gammaln(value + alpha) - factln(value) - gammaln(alpha) +
-                     logpow(mu / (mu + alpha), value) + logpow(alpha / (mu + alpha), alpha),
-                      mu > 0, alpha > 0, value >= 0)
-
-        return switch(1*(alpha > 1e10),
-                      pois,
-                      negbinom)
+        # Return Poisson when alpha gets very large.
+        return T.switch(1 * (alpha > 1e10),
+                        Poisson.dist(self.mu).logp(value),
+                        negbinom)
 
 
 class Geometric(Discrete):
@@ -258,7 +253,8 @@ class Geometric(Discrete):
     sequence of Bernoulli trials occurs on the x'th trial.
 
     .. math::
-        f(x \mid p) = p(1-p)^{x-1}
+
+       f(x \mid p) = p(1-p)^{x-1}
 
     Parameters
     ----------
@@ -268,7 +264,6 @@ class Geometric(Discrete):
     .. note::
       - :math:`E(X)=1/p`
       - :math:`Var(X)=\frac{1-p}{p^2}`
-
     """
     def __init__(self, p, *args, **kwargs):
         super(Geometric, self).__init__(*args, **kwargs)
@@ -277,13 +272,13 @@ class Geometric(Discrete):
 
     def random(self, point=None, size=None, repeat=None):
         p = draw_values([self.p], point=point)
-        return generate_samples(nr.geometric, p,
-                                dist_shape=self.shape, 
+        return generate_samples(np.random.geometric, p,
+                                dist_shape=self.shape,
                                 size=size)
 
     def logp(self, value):
         p = self.p
-        return bound(log(p) + logpow(1 - p, value - 1),
+        return bound(T.log(p) + logpow(1 - p, value - 1),
                      0 <= p, p <= 1, value >= 1)
 
 
@@ -292,7 +287,8 @@ class DiscreteUniform(Discrete):
     Discrete uniform distribution.
 
     .. math::
-        f(x \mid lower, upper) = \frac{1}{upper-lower}
+
+       f(x \mid lower, upper) = \frac{1}{upper-lower}
 
     Parameters
     ----------
@@ -300,16 +296,18 @@ class DiscreteUniform(Discrete):
         Lower limit.
     upper : int
         Upper limit (upper > lower).
-
     """
     def __init__(self, lower, upper, *args, **kwargs):
         super(DiscreteUniform, self).__init__(*args, **kwargs)
-        self.lower, self.upper = floor(lower).astype('int32'), floor(upper).astype('int32')
-        self.mode = floor((upper - lower) / 2.).astype('int32')
+        self.lower = T.floor(lower).astype('int32')
+        self.upper = T.floor(upper).astype('int32')
+        self.mode = T.floor((upper - lower) / 2.).astype('int32')
 
     def _random(self, lower, upper, size=None):
-        # This way seems to be the only to deal with lower and upper as array-like.
-        samples = st.uniform.rvs(lower, upper - lower - np.finfo(float).eps, size=size)
+        # This way seems to be the only to deal with lower and upper
+        # as array-like.
+        samples = stats.uniform.rvs(lower, upper - lower - np.finfo(float).eps,
+                                    size=size)
         return np.floor(samples).astype('int32')
 
     def random(self, point=None, size=None, repeat=None):
@@ -322,18 +320,15 @@ class DiscreteUniform(Discrete):
     def logp(self, value):
         upper = self.upper
         lower = self.lower
-
-        return bound(
-            -log(upper - lower + 1),
-
-            lower <= value, value <= upper)
+        return bound(-T.log(upper - lower + 1),
+                     lower <= value, value <= upper)
 
 
 class Categorical(Discrete):
     """
     Categorical log-likelihood. The most general discrete distribution.
 
-    .. math::  f(x=i \mid p) = p_i
+    .. math:: f(x=i \mid p) = p_i
 
     for :math:`i \in 0 \ldots k-1`.
 
@@ -341,17 +336,16 @@ class Categorical(Discrete):
     ----------
     p : float
         :math:`p > 0`, :math:`\sum p = 1`
-
     """
     def __init__(self, p, *args, **kwargs):
         super(Categorical, self).__init__(*args, **kwargs)
         self.k = np.shape(p)[-1]
-        self.p = as_tensor_variable(p)
-        self.mode = argmax(p)
+        self.p = T.as_tensor_variable(p)
+        self.mode = T.argmax(p)
 
     def random(self, point=None, size=None, repeat=None):
         p, k = draw_values([self.p, self.k], point=point)
-        return generate_samples(lambda p, size=None: nr.choice(np.arange(k), p=p, size=size),
+        return generate_samples(partial(np.random.choice, k),
                                 p=p,
                                 broadcast_shape=p.shape[:-1] or (1,),
                                 dist_shape=self.shape,
@@ -361,13 +355,10 @@ class Categorical(Discrete):
         p = self.p
         k = self.k
 
-        sumto1 = theano.gradient.zero_grad(
-            le(abs(sum(p) - 1), 1e-5)
-            )
-        return bound(log(p[value]),
-            value >= 0,
-            value <= (k - 1),
-            sumto1)
+        sumto1 = theano.gradient.zero_grad(T.le(abs(T.sum(p) - 1), 1e-5))
+        return bound(T.log(p[value]),
+                     value >= 0, value <= (k - 1),
+                     sumto1)
 
 
 class ConstantDist(Discrete):
@@ -379,7 +370,6 @@ class ConstantDist(Discrete):
     value : float or int
         Data value(s)
     """
-
     def __init__(self, c, *args, **kwargs):
         super(ConstantDist, self).__init__(*args, **kwargs)
         self.mean = self.median = self.mode = self.c = c
@@ -387,14 +377,16 @@ class ConstantDist(Discrete):
     def random(self, point=None, size=None, repeat=None):
         c = draw_values([self.c], point=point)
         dtype = np.array(c).dtype
-        return generate_samples(lambda c=None, dtype=dtype, size=None: np.ones(shape=size, dtype=dtype) * c, 
-                                c=c,
-                                dist_shape=self.shape,
+
+        def _random(c, dtype=dtype, size=None):
+            return np.full(size, fill_value=c, dtype=dtype)
+
+        return generate_samples(_random, c=c, dist_shape=self.shape,
                                 size=size).astype(dtype)
 
     def logp(self, value):
         c = self.c
-        return bound(0, eq(value, c))
+        return bound(0, T.eq(value, c))
 
 
 class ZeroInflatedPoisson(Discrete):
@@ -412,7 +404,6 @@ class ZeroInflatedPoisson(Discrete):
         return None
 
     def logp(self, value):
-        z = self.z
-        return switch(z,
-                      self.pois.logp(value),
-                      self.const.logp(value))
+        return T.switch(self.z,
+                        self.pois.logp(value),
+                        self.const.logp(value))
