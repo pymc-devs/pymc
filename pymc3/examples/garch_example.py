2762d1fb73a59670b564aedb478b8b7ad4cd8cb3
springcoil <peadarcoyle@googlemail.com>
2016-02-10 10:20:46 +0100

    Slight updates

--- pymc3/examples/garch_example.py
+++ pymc3/examples/garch_example.py
@@ -1,6 +1,6 @@
 from pymc3 import Normal, sample, Model, Bound, traceplot, plots, NUTS
 from pymc3.distributions.timeseries import GARCH
-import theano.tensor as tt
+import theano.tensor as T
 import numpy as np
 
 """
@@ -9,9 +9,9 @@ It is interesting to note just how much more compact this is that the original S
 
 The original implementation is in the STAN documentation by Gelman et al and is reproduced below
 
+A good reference studying GARCH models in depth is  http://www.stat-d.si/mz/mz2.1/posedel.pdf
 
-Example from STAN
-// GARCH(1,1)
+Example from STAN- slightly altered
 
 data {
   int<lower=0> T;
@@ -37,23 +37,23 @@ model {
 }
 Ported to PyMC3 by Peadar Coyle (c) 2016.
 """
-T = 8
-r = np.array([28,  8, -3,  7, -1,  1, 18, 12])
-sigma1 = np.array([15, 10, 16, 11,  9, 11, 10, 18])
-alpha0 = np.array([10, 10, 16, 8,  9, 11, 12, 18])
+J = 8
+r = np.array([28, 8, -3, 7, -1, 1, 18, 12])
+sigma1 = np.array([15, 10, 16, 11, 9, 11, 10, 18])
+alpha0 = np.array([10, 10, 16, 8, 9, 11, 12, 18])
 
 with Model() as garch:
-
-    alpha1 = Normal('alpha1', 0, 1, shape=T)
-    BoundedNormal = Bound(Normal, upper=(1-alpha1))
+    alpha1 = Normal('alpha1', 0, 1, shape=J)
+    BoundedNormal = Bound(Normal, upper=(1 - alpha1))
     beta1 = BoundedNormal('beta1', 0, sd=1e6)
     mu = Normal('mu', 0, sd=1e6)
 
-    sd = tt.sqrt(alpha0 + alpha1*tt.pow(r - mu, 2) + beta1 * tt.pow(sigma1, 2))
+    theta = T.sqrt(alpha0 + alpha1 * T.pow(r - mu, 2) + beta1 * T.pow(sigma1, 2))
 
-    obs = GARCH('garchy', observed=r)
+    obs = GARCH('garchy_garch', observed=r)
 
     step = NUTS()
+
 
 def run(n=1000):
     if n == "short":
@@ -61,10 +61,11 @@ def run(n=1000):
     with garch:
         trace = sample(n, step)
 
-    burn = n/10
+    burn = n / 10
 
     traceplot(trace[burn:])
     plots.summary(trace[burn:])
+
 
 if __name__ == '__main__':
     run()
