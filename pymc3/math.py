cb5e1528a96d9721752a23ebe6fe7418da81bb81
taku-y <taku.yoshioka.4096@gmail.com>
2016-03-31 22:09:40 +0900

    Add logsumexp.

--- pymc3/math.py
+++ pymc3/math.py
@@ -7,3 +7,9 @@ from theano.tensor import maximum, minimum, sgn, ceil, floor
 from theano.tensor.nlinalg import det, matrix_inverse, extract_diag, matrix_dot, trace
 from theano.tensor.nnet import sigmoid
 import theano
+import theano.tensor as tt
+
+def LogSumExp(x, axis=None):
+    # Adapted from https://github.com/Theano/Theano/issues/1563
+    x_max = tt.max(x, axis=axis, keepdims=True)
+    return tt.log(tt.sum(tt.exp(x - x_max), axis=axis, keepdims=True)) + x_max
