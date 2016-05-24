975fb722da46e4caadaa2c4485258f7992468d30
Tobias Knuth <mail@tobiasknuth.de>
2016-05-24 23:36:02 +0200

    added vim swap files to .gitignore, removed disp parameter duplication in starting.py

--- pymc3/tuning/starting.py
+++ pymc3/tuning/starting.py
@@ -78,9 +78,9 @@ def find_MAP(start=None, vars=None, fmin=None, return_raw=False,
     # Check to see if minimization function actually uses the gradient
     if 'fprime' in getargspec(fmin).args:
         r = fmin(logp_o, bij.map(
-            start), fprime=grad_logp_o, disp=disp, *args, **kwargs)
+            start), fprime=grad_logp_o, *args, **kwargs)
     else:
-        r = fmin(logp_o, bij.map(start), disp=disp, *args, **kwargs)
+        r = fmin(logp_o, bij.map(start), *args, **kwargs)
 
     if isinstance(r, tuple):
         mx0 = r[0]
