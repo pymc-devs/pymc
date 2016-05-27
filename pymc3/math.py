1ef1a44f007af90a9641566984a9428b342f5296
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2016-05-27 09:30:10 -0500

    Fix for import error

--- pymc3/math.py
+++ pymc3/math.py
@@ -10,10 +10,7 @@ import theano
 import theano.tensor as tt
 import sys
 
-def LogSumExp(x, axis=None):
-    # Adapted from https://github.com/Theano/Theano/issues/1563
-    x_max = tt.max(x, axis=axis, keepdims=True)
-    return tt.log(tt.sum(tt.exp(x - x_max), axis=axis, keepdims=True)) + x_max
+__all__ = ['logit', 'invlogit']
 
 def invlogit(x):
     x_max = -tt.log(sys.float_info.epsilon)
