3c16282e345c2735e80dca9b71fb8725f2652445
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2016-05-27 07:31:02 -0500

    Added logit and invlogit functions to math.py

--- pymc3/math.py
+++ pymc3/math.py
@@ -8,8 +8,18 @@ from theano.tensor.nlinalg import det, matrix_inverse, extract_diag, matrix_dot,
 from theano.tensor.nnet import sigmoid
 import theano
 import theano.tensor as tt
+import sys
 
 def LogSumExp(x, axis=None):
     # Adapted from https://github.com/Theano/Theano/issues/1563
     x_max = tt.max(x, axis=axis, keepdims=True)
     return tt.log(tt.sum(tt.exp(x - x_max), axis=axis, keepdims=True)) + x_max
+
+def invlogit(x):
+    x_max = -tt.log(sys.float_info.epsilon)
+    if (x > x_max) return 1.0
+    if (x < 1-x_max) return 0.0
+    return 1/(1 + tt.exp(-x))
+    
+def logit(p):
+    return tt.log(p/(1-p))
