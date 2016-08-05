pymc-devs/pymc

commit df627ea5bdd59c100023d1b8f0383efa53983b59

Remove import * statements (#1257)
* Removed import * statements from most files

* Additional import fixes

* Additional import fixes

* Fix for relative import of sampling

* Most tests pass

* Changed relative to absolute import for sampling within ATMCMC

* Fixed circular import issue

* Fixed import syntax error

* Fixes for additional import errors

* MAINT Add failing imports.

* MAINT Add failing imports.

* More namespace fixes for tests

pymc3/sampling.py
@@ -3,11 +3,12 @@ from .backends.base import merge_traces, BaseTrace, MultiTrace
 from .backends.ndarray import NDArray
 from joblib import Parallel, delayed
 from time import time
-from .core import *
-from .step_methods import *
+from .model import modelcontext, Point
+from .step_methods import (NUTS, HamiltonianMC, Metropolis, BinaryMetropolis,
+                         BinaryGibbsMetropolis, Slice, ElemwiseCategorical, CompoundStep)
 from .progressbar import progress_bar
 from numpy.random import randint, seed
-from numpy import shape
+from numpy import shape, append, asarray
 from collections import defaultdict
 
 import sys
@@ -48,7 +49,7 @@ def assign_step_methods(model, step=None,
     steps = []
     assigned_vars = set()
     if step is not None:
-        steps = np.append(steps, step).tolist()
+        steps = append(steps, step).tolist()
         for s in steps:
             try:
                 assigned_vars = assigned_vars | set(s.vars)
@@ -155,12 +156,12 @@ def _sample(draws, step=None, start=None, trace=None, chain=0, tune=None,
     sampling = _iter_sample(draws, step, start, trace, chain,
                             tune, model, random_seed)
     progress = progress_bar(draws)
-    try:
-        for i, strace in enumerate(sampling):
+    for i, strace in enumerate(sampling):
+        try:
             if progressbar:
                 progress.update(i)
-    except KeyboardInterrupt:
-        strace.close()
+        except KeyboardInterrupt:
+            strace.close()
     return MultiTrace([strace])
 
 
@@ -346,4 +347,4 @@ def sample_ppc(trace, samples=None, model=None, vars=None, size=None):
             ppc[var.name].append(var.distribution.random(point=param,
                                                          size=size))
 
-    return {k: np.asarray(v) for k, v in ppc.items()}
+    return {k: asarray(v) for k, v in ppc.items()}

pymc3/__init__.py
@@ -1,15 +1,15 @@
 __version__ = "3.0"
 
-from .core import *
 from .distributions import *
 from .math import *
-
+from .model import *
 from .stats import *
 from .sampling import *
 from .interactive_sampling import *
 from .step_methods import *
 from .tuning import *
 from .variational import *
+from . import sampling
 
 from .debug import *
 

pymc3/core.py
@@ -1,5 +1,0 @@
-from .vartypes import *
-from .model import *
-from .theanof import *
-from .blocking import *
-import numpy as np

pymc3/debug.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .blocking import *
+from .blocking import DictToVarBijection
 
 # TODO I could not locate this function used anywhere in the code base
 # do we need it?

pymc3/distributions/__init__.py
@@ -1,8 +1,112 @@
 from . import timeseries
 from . import transforms
 
-from .continuous import *
-from .discrete import *
-from .distribution import *
-from .multivariate import *
-from .timeseries import *
+from .continuous import Uniform
+from .continuous import Flat
+from .continuous import Normal
+from .continuous import Beta
+from .continuous import Exponential
+from .continuous import Laplace
+from .continuous import StudentT
+from .continuous import Cauchy
+from .continuous import HalfCauchy
+from .continuous import Gamma
+from .continuous import Weibull
+from .continuous import Bound
+from .continuous import StudentTpos
+from .continuous import Lognormal
+from .continuous import ChiSquared
+from .continuous import HalfNormal
+from .continuous import Wald
+from .continuous import Pareto
+from .continuous import InverseGamma
+from .continuous import ExGaussian
+from .continuous import VonMises
+
+from .discrete import Binomial
+from .discrete import BetaBinomial
+from .discrete import Bernoulli
+from .discrete import Poisson
+from .discrete import NegativeBinomial
+from .discrete import ConstantDist
+from .discrete import ZeroInflatedPoisson
+from .discrete import DiscreteUniform
+from .discrete import Geometric
+from .discrete import Categorical
+
+from .distribution import DensityDist
+from .distribution import Distribution
+from .distribution import Continuous
+from .distribution import Discrete
+from .distribution import NoDistribution
+from .distribution import TensorType
+from .distribution import draw_values
+
+from .multivariate import MvNormal
+from .multivariate import MvStudentT
+from .multivariate import Dirichlet
+from .multivariate import Multinomial
+from .multivariate import Wishart
+from .multivariate import WishartBartlett
+from .multivariate import LKJCorr
+
+from .timeseries import AR1
+from .timeseries import GaussianRandomWalk
+from .timeseries import GARCH11
+
+from .transforms import transform
+from .transforms import stick_breaking
+from .transforms import logodds
+from .transforms import log
+from .transforms import sum_to_1
+
+__all__ = ['Uniform',
+    'Flat',
+    'Normal',
+    'Beta',
+    'Exponential',
+    'Laplace',
+    'StudentT',
+    'Cauchy',
+    'HalfCauchy',
+    'Gamma',
+    'Weibull',
+    'Bound',
+    'StudentTpos',
+    'Lognormal',
+    'ChiSquared',
+    'HalfNormal',
+    'Wald',
+    'Pareto',
+    'InverseGamma',
+    'ExGaussian',
+    'VonMises',
+    'Binomial',
+    'BetaBinomial',
+    'Bernoulli',
+    'Poisson',
+    'NegativeBinomial',
+    'ConstantDist',
+    'ZeroInflatedPoisson',
+    'DiscreteUniform',
+    'Geometric',
+    'Categorical',
+    'DensityDist',
+    'Distribution',
+    'Continuous',
+    'Discrete',
+    'NoDistribution',
+    'TensorType',
+    'MvNormal',
+    'MvStudentT',
+    'Dirichlet',
+    'Multinomial',
+    'Wishart',
+    'WishartBartlett',
+    'LKJCorr',
+    'AR1',
+    'GaussianRandomWalk',
+    'GARCH11'
+]
+           
+           

pymc3/distributions/distribution.py
@@ -6,7 +6,7 @@ from ..memoize import memoize
 from ..model import Model, get_named_nodes
 
 
-__all__ = ['DensityDist', 'Distribution', 'Continuous', 'Discrete', 'NoDistribution', 'TensorType']
+__all__ = ['DensityDist', 'Distribution', 'Continuous', 'Discrete', 'NoDistribution', 'TensorType', 'draw_values']
 
 
 class Distribution(object):

pymc3/distributions/transforms.py
@@ -6,7 +6,7 @@ from .distribution import Distribution
 from ..math import logit
 import numpy as np
 
-__all__ = ['transform', 'stick_breaking', 'logodds', 'log']
+__all__ = ['transform', 'stick_breaking', 'logodds', 'log', 'sum_to_1']
 
 
 class Transform(object):

pymc3/examples/ARM12_6uranium.py
@@ -55,8 +55,8 @@ def run(n=3000):
 
         start = Point({
                  'groupmean': obs_means.mean(),
-                 'groupsd_interval': 0,
-                 'sd_interval': 0,
+                 'groupsd_interval_': 0,
+                 'sd_interval_': 0,
                  'means': np.array(obs_means),
                  'u_m': np.array([.72]),
                  'floor_m': 0.,

commit 8c6a7a0ca87d75e6d8d4c4165e3dfa1d4f202be3

Catch KeyboardInterrupt during sampling.
Fix a regression introduced in df627ea5bdd59c.

The intended behavior when a keyboard interrupt is to
stop sampling and return the result.

pymc3/sampling.py
@@ -156,12 +156,12 @@ def _sample(draws, step=None, start=None, trace=None, chain=0, tune=None,
     sampling = _iter_sample(draws, step, start, trace, chain,
                             tune, model, random_seed)
     progress = progress_bar(draws)
-    for i, strace in enumerate(sampling):
-        try:
+    try:
+        for i, strace in enumerate(sampling):
             if progressbar:
                 progress.update(i)
-        except KeyboardInterrupt:
-            strace.close()
+    except KeyboardInterrupt:
+        strace.close()
     return MultiTrace([strace])
 
 