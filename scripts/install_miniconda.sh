43e416ccaaa514fde02185a9746a084fd11873e9
colin <colin@kensho.com>
2016-08-14 19:48:22 -0400

    Add debugging messages

--- scripts/install_miniconda.sh
+++ scripts/install_miniconda.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
+
+set -e # fail on first error
+
 ${PYTHON_VERSION:=-3.5} # if no python specified, use 3
 
-if [ ${PYTHON_VERSION} == "2.7" ]; then
-  wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
-else
-  wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+echo "using miniconda for python-${PYTHON_VERSION}"
+
+if [ "$(uname)" == "Darwin" ]; then
+  URL_OS="MacOSX"
+elif [ "$(expr substr "$(uname -s)" 1 5)" == "Linux" ]; then
+  URL_OS="Linux"
+elif [ "$(expr substr "$(uname -s)" 1 10)" == "MINGW32_NT" ]; then
+  URL_OS="Windows"
 fi
 
+echo "detected operating system: $URL_OS"
+
 if [ ${PYTHON_VERSION} == "2.7" ]; then
+  echo "Installing miniconda for python 2.7"
+  wget http://repo.continuum.io/miniconda/Miniconda-latest-$URL_OS-x86_64.sh -O miniconda.sh;
   INSTALL_FOLDER="$HOME/minconda2"
 else
+  echo "Installing miniconda for python 3.5"
+  wget http://repo.continuum.io/miniconda/Miniconda3-latest-$URL_OS-x86_64.sh -O miniconda.sh;
   INSTALL_FOLDER="$HOME/minconda3"
 fi
+
 
 # install miniconda to home folder
 bash miniconda.sh -b -p $INSTALL_FOLDER
