135da450dc506aa1b93595562167bee6dfb45deb
tyarkoni <tyarkoni@gmail.com>
2016-08-25 13:08:31 -0500

    fixes string checking for Python 2/3 compatibility; closes #1301

--- pymc3/distributions/distribution.py
+++ pymc3/distributions/distribution.py
@@ -4,9 +4,16 @@ from theano import function
 
 from ..memoize import memoize
 from ..model import Model, get_named_nodes
+import sys
 
 
 __all__ = ['DensityDist', 'Distribution', 'Continuous', 'Discrete', 'NoDistribution', 'TensorType', 'draw_values']
+
+
+if sys.version_info[0] == 3:
+    string_types = str
+else:
+    string_types = basestring
 
 
 class Distribution(object):
@@ -19,7 +26,7 @@ class Distribution(object):
                             "use the Normal('x', 0,1) syntax. "
                             "Add a 'with model:' block")
 
-        if isinstance(name, str):
+        if isinstance(name, string_types):
             data = kwargs.pop('observed', None)
             dist = cls.dist(*args, **kwargs)
             return model.Var(name, dist, data)
@@ -65,7 +72,7 @@ class Distribution(object):
 
 
     def getattr_value(self, val):
-        if isinstance(val, str):
+        if isinstance(val, string_types):
             val = getattr(self, val)
 
         if isinstance(val, tt.TensorVariable):
