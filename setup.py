3f045dd5d8a869dbddc7bcd9ac1bda3f70e89e43
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2016-09-06 12:30:04 -0500

    Updated classifiers in setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,13 @@ URL = "http://github.com/pymc-devs/pymc"
 LICENSE = "Apache License, Version 2.0"
 VERSION = "3.0.rc1"
 
-classifiers = ['Development Status :: 4 - Beta',
+classifiers = ['Development Status :: 5 - Production/Stable',
                'Programming Language :: Python',
                'Programming Language :: Python :: 2',
                'Programming Language :: Python :: 3',
                'Programming Language :: Python :: 2.7',
-               'Programming Language :: Python :: 3.3',
+               'Programming Language :: Python :: 3.4',
+               'Programming Language :: Python :: 3.5',
                'License :: OSI Approved :: Apache Software License',
                'Intended Audience :: Science/Research',
                'Topic :: Scientific/Engineering',
