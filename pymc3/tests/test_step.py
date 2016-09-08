8025e771bddd18927a9d4c74befa5171c3492c1a
colin <colin@kensho.com>
2016-09-08 19:15:03 -0400

    Refactor step tests

--- pymc3/tests/test_step.py
+++ pymc3/tests/test_step.py
@@ -1,11 +1,11 @@
+import unittest
+
 from .checks import close_to
-from .models import simple_model, mv_simple, mv_simple_discrete, simple_2model
-from ..step_methods import MultivariateNormalProposal
-from theano.tensor import constant
-from scipy.stats.mstats import moment
+from .models import mv_simple, mv_simple_discrete, simple_2model
 from pymc3.sampling import assign_step_methods, sample
 from pymc3.model import Model
-from pymc3.step_methods import NUTS, BinaryMetropolis, BinaryGibbsMetropolis, Metropolis, Constant, ElemwiseCategorical, Slice, CompoundStep, MultivariateNormalProposal, HamiltonianMC
+from pymc3.step_methods import (NUTS, BinaryGibbsMetropolis, Metropolis, Constant, Slice,
+                                CompoundStep, MultivariateNormalProposal, HamiltonianMC)
 from pymc3.distributions import Binomial, Normal, Bernoulli, Categorical
 from numpy.testing import assert_almost_equal
 import numpy as np
@@ -20,7 +20,6 @@ def test_step_continuous():
     start, model, (mu, C) = mv_simple()
 
     with model:
-        mh = Metropolis()
         slicer = Slice()
         hmc = HamiltonianMC(scaling=C, is_cov=True, blocked=False)
         nuts = NUTS(scaling=C, is_cov=True, blocked=False)
@@ -72,59 +71,55 @@ def test_non_blocked():
         assert isinstance(hmc_blocked, HamiltonianMC)
         nuts_blocked = NUTS()
         assert isinstance(nuts_blocked, NUTS)
-
-        compound = CompoundStep([hmc_blocked, mh_blocked])
+        CompoundStep([hmc_blocked, mh_blocked])
 
 
 def test_step_discrete():
     start, model, (mu, C) = mv_simple_discrete()
 
     with model:
-        mh = Metropolis(S=C,
-                        proposal_dist=MultivariateNormalProposal)
-        slicer = Slice()
+        mh = Metropolis(S=C, proposal_dist=MultivariateNormalProposal)
+        Slice()
 
     steps = [mh]
-
     unc = np.diag(C) ** .5
     check = [('x', np.mean, mu, unc / 10.),
              ('x', np.std, unc, unc / 10.)]
-
     for st in steps:
         h = sample(20000, st, start, model=model, random_seed=1)
-
         for (var, stat, val, bound) in check:
             yield check_stat, repr(st), h, var, stat, val, bound
 
 
 def test_constant_step():
-
-    with Model() as model:
+    with Model():
         x = Normal('x', 0, 1)
         start = {'x': -1}
         tr = sample(10, step=Constant([x]), start=start)
         assert_almost_equal(tr['x'], start['x'], decimal=10)
 
 
-def test_assign_step_methods():
+class TestAssignStepMethods(unittest.TestCase):
+    def test_bernoulli(self):
+        """Test bernoulli distribution is assigned binary gibbs metropolis method"""
+        with Model() as model:
+            Bernoulli('x', 0.5)
+            steps = assign_step_methods(model, [])
+        self.assertIsInstance(steps, BinaryGibbsMetropolis)
 
-    with Model() as model:
-        x = Bernoulli('x', 0.5)
-        steps = assign_step_methods(model, [])
+    def test_normal(self):
+        """Test normal distribution is assigned NUTS method"""
+        with Model() as model:
+            Normal('x', 0, 1)
+            steps = assign_step_methods(model, [])
+        self.assertIsInstance(steps, NUTS)
 
-        assert isinstance(steps, BinaryGibbsMetropolis)
-
-    with Model() as model:
-        x = Normal('x', 0, 1)
-        steps = assign_step_methods(model, [])
-
-        assert isinstance(steps, NUTS)
-
-    with Model() as model:
-        x = Categorical('x', np.array([0.25, 0.75]))
-        steps = assign_step_methods(model, [])
-
-        assert isinstance(steps, BinaryGibbsMetropolis)
+    def test_categorical(self):
+        """Test categorical distribution is assigned binary gibbs metropolis method"""
+        with Model() as model:
+            Categorical('x', np.array([0.25, 0.75]))
+            steps = assign_step_methods(model, [])
+        self.assertIsInstance(steps, BinaryGibbsMetropolis)
 
     # with Model() as model:
     #     x = Categorical('x', np.array([0.25, 0.70, 0.05]))
@@ -132,8 +127,9 @@ def test_assign_step_methods():
     #
     #     assert isinstance(steps, ElemwiseCategoricalStep)
 
-    with Model() as model:
-        x = Binomial('x', 10, 0.5)
-        steps = assign_step_methods(model, [])
-
-        assert isinstance(steps, Metropolis)
+    def test_binomial(self):
+        """Test binomial distribution is assigned metropolis method."""
+        with Model() as model:
+            Binomial('x', 10, 0.5)
+            steps = assign_step_methods(model, [])
+        self.assertIsInstance(steps, Metropolis)
