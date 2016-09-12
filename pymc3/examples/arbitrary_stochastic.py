d2c7aa2ba586e34b8de6f7395ed5b1986120dd3c
colin <colin@kensho.com>
2016-09-12 09:20:53 -0400

    Refactor some examples to run as tests, delete outdated

--- pymc3/examples/arbitrary_stochastic.py
+++ pymc3/examples/arbitrary_stochastic.py
@@ -1,26 +1,27 @@
-from pymc3 import *
 import numpy as np
-with Model() as model:
-    lam = Exponential('lam', 1)
-
-    failure = np.array([0, 1])
-    value = np.array([1, 0])
-
-    def logp(failure, value):
-        return sum(failure * log(lam) - lam * value)
-
-    x = DensityDist('x', logp, observed={'failure': failure, 'value': value})
+import pymc3 as pm
+import theano.tensor as tt
 
 
-def run(n=3000):
-    if n == "short":
-        n = 50
-    with model:
+def build_model():
+    with pm.Model() as model:
+        lam = pm.Exponential('lam', 1)
+        failure = np.array([0, 1])
+        value = np.array([1, 0])
 
-        start = model.test_point
-        h = find_hessian(start)
-        step = Metropolis(model.vars, h, blocked=True)
-        trace = sample(n, step, start)
+        def logp(failure, value):
+            return tt.sum(failure * np.log(lam) - lam * value)
+        pm.DensityDist('x', logp, observed={'failure': failure, 'value': value})
+    return model
+
+
+def run(n_samples=3000):
+    model = build_model()
+    start = model.test_point
+    h = pm.find_hessian(start, model=model)
+    step = pm.Metropolis(model.vars, h, blocked=True, model=model)
+    trace = pm.sample(n_samples, step, start, model=model)
+    return trace
 
 if __name__ == "__main__":
     run()
--- pymc3/examples/hierarchical_sqlite.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':  # Avoid loading during tests.
-
-    import pymc3 as pm
-    import pymc3.examples.hierarchical as hier
-
-    with hier.model:
-        trace = pm.sample(3000, hier.step, hier.start, trace='sqlite')
