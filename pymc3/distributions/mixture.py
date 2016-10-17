c30e358144a5fa85b8c51f05375412e9d5a03311
AustinRochford <arochford@monetate.com>
2016-10-16 22:02:35 -0400

    Add support for 2D weights

--- pymc3/distributions/mixture.py
+++ pymc3/distributions/mixture.py
@@ -33,9 +33,9 @@ class Mixture(Distribution):
         self.w = w
         self.comp_dists = comp_dists
 
-        comp_modes = self._comp_modes()
-        comp_mode_logps = self.logp(comp_modes)
-        self.mode = comp_modes[tt.argmax(comp_modes, axis=-1)]
+        #comp_modes = self._comp_modes()
+        #comp_mode_logps = self.logp(comp_modes)
+        #self.mode = comp_modes[tt.argmax(comp_modes, axis=-1)]
     
     def _comp_logp(self, value):
         comp_dists = self.comp_dists
@@ -50,7 +50,7 @@ class Mixture(Distribution):
 
     def _comp_modes(self):
         try:
-            return self.comp_dists.mode()
+            return self.comp_dists.mode
         except AttributeError:
             return tt.stack([comp_dist.mode for comp_dist in comp_dists],
                             axis=1)
@@ -71,15 +71,29 @@ class Mixture(Distribution):
                      w >= 0, w <= 1, tt.allclose(w.sum(axis=-1), 1))
 
     def random(self, point=None, size=None, repeat=None):
+        def random_choice(*args, **kwargs):
+            w = kwargs.pop('w')
+            w /= w.sum(axis=-1, keepdims=True)
+            k = w.shape[-1]
+
+            if w.ndim > 1:
+                return np.row_stack([np.random.choice(k, p=w_) for w_ in w])
+            else:
+                return np.random.choice(k, p=w, *args, **kwargs)
+
         w = draw_values([self.w], point=point)
 
-        w_samples = generate_samples(np.random.multinomial, 1, w,
+        w_samples = generate_samples(random_choice,
+                                     w=w,
                                      broadcast_shape=w.shape[:-1] or (1,),
                                      dist_shape=self.shape,
-                                     size=size)
+                                     size=size).squeeze()
         comp_samples = self._comp_samples(point=point, size=size, repeat=repeat)
 
-        return comp_samples[np.squeeze(w_samples == 1)]
+        if comp_samples.ndim > 1:
+            return np.squeeze(comp_samples[np.arange(w_samples.size), w_samples])
+        else:
+            return np.squeeze(comp_samples[w_samples])
 
     
 class NormalMixture(Mixture):
