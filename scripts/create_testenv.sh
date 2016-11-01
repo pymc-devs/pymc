9b2bdde9750b0290154a0c7d1c6db9dae64fa73e
AustinRochford <arochford@monetate.com>
2016-10-31 20:08:01 -0400

    Modify options to create_testenv.sh

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -2,17 +2,29 @@
 
 set -e # fail on first error
 
+while test $# -gt 0
+do
+    case "$1" in
+        --global)
+            GLOBAL=1
+            ;;
+        --no-setup)
+            NO_SETUP=1
+            ;;
+    esac
+done
+
 PYTHON_VERSION=${PYTHON_VERSION:-3.5} # if no python specified, use 3.5
 
-if [[ "$*" != *--global* ]]
+if [ -z ${GLOBAL} ]
 then
-  conda create -n testenv --yes pip python=${PYTHON_VERSION}
-  source activate testenv
+    conda create -n testenv --yes pip python=${PYTHON_VERSION}
+    source activate testenv
 fi
 
 conda install --yes pyqt=4.11.4 jupyter pyzmq numpy scipy nose matplotlib pandas Cython patsy statsmodels joblib coverage
 if [ ${PYTHON_VERSION} == "2.7" ]; then
-  conda install --yes mock enum34;
+    conda install --yes mock enum34;
 fi
 
 pip install tqdm
@@ -20,4 +32,7 @@ pip install --no-deps numdifftools
 pip install git+https://github.com/Theano/Theano.git
 pip install git+https://github.com/mahmoudimus/nose-timer.git
 
-python setup.py build_ext --inplace
+if [ -z ${NO_SETUP} ]
+then
+    python setup.py build_ext --inplace
+fi
