d96d4ffb0b7bc917b72dfe4dcbd92464e2127e2f
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-13 01:43:59 +0300

    Added some docstrings

--- pymc3/models/base.py
+++ pymc3/models/base.py
@@ -3,6 +3,10 @@ from collections import OrderedDict
 
 
 class UserModel(object):
+    """Base class for model specification
+    It is supposed to be used by pymc3 contributors
+    for simplifying usage of common bayesian models
+    """
     def __init__(self, name):
         # name should be used as prefix for
         # all variables specified within a model
@@ -11,11 +15,25 @@ class UserModel(object):
 
     @property
     def model(self):
-        # Just shortcut
+        """Shortcut to model"""
         return modelcontext(None)
 
     def add_var(self, name, dist, data=None):
-        # Signature from pymc3.Model
+        """Create and add (un)observed random variable to the model with an
+        appropriate prior distribution. Also adds prefix to the name
+
+        Parameters
+        ----------
+        name : str
+        dist : distribution for the random variable
+        data : array_like (optional)
+           If data is provided, the variable is observed. If None,
+           the variable is unobserved.
+
+        Returns
+        -------
+        FreeRV or ObservedRV
+        """
         var = self.model.Var('{}_{}'.format(self, name, name), dist=dist, data=data)
         self.vars[name] = var
         return var
