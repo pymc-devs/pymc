refactored api, fixed formula init

pymc3/models/linear.py
@@ -19,8 +19,8 @@ class LinearComponent(UserModel):
     init : dict - test_vals for coefficients
     rvars : dict - random variables instead of creating new ones
     """
-    def __init__(self, name, x, y, intercept=True, labels=None,
-                 priors=None, init=None, rvars=None):
+    def __init__(self, x, y, intercept=True, labels=None,
+                 priors=None, init=None, rvars=None, name=''):
         super(LinearComponent, self).__init__(name)
         if priors is None:
             priors = {}
@@ -30,17 +30,20 @@ class LinearComponent(UserModel):
             rvars = {}
         if isinstance(x, pd.DataFrame):
             self._init_pandas_(x, y, intercept, labels, priors, init, rvars)
-        elif isinstance(x, np.ndarray):
-            self._init_numpy_(x, y, intercept, labels, priors, init, rvars)
         else:
-            raise NotImplementedError('Not implemented for type %s' % type(x))
+            try:
+                self._init_via_pandas_(x, y, intercept, labels, priors, init, rvars)
+            except pd.core.common.PandasError:
+                raise ValueError('Not implemented for type %s' % type(x))
 
-    def _init_pandas_(self, x, y, intercept=True, labels=None, priors=None, init=None, rvars=None):
+    def _init_pandas_(self, x, y, intercept=True, labels=None,
+                      priors=None, init=None, rvars=None):
         if labels is None:
             labels = list(x.columns)
         else:
             assert len(labels) == x.shape[1], \
-                'Cannot deal with not full list of labels, need {}'.format(x.shape[1])
+                ('Cannot deal with not full list of labels, need {}'
+                 .format(x.shape[1]))
         if intercept:
             x = pd.concat([pd.Series(np.ones(x.shape[0])), x], 1)
             _dist, _var, _init = (priors.get('Intercept'),
@@ -68,16 +71,19 @@ class LinearComponent(UserModel):
         self.coeffs = tt.stack(self.vars.values(), axis=0)
         self.y_est = tt.dot(np.asarray(x), self.coeffs).reshape((1, -1))
 
-    def _init_numpy_(self, x, y, intercept=True, labels=None, priors=None, init=None, rvars=None):
-        x = pd.DataFrame(x).rename(columns=lambda i: "x%d" % i)
-        self._init_pandas_(x, y, intercept, labels, priors, init, rvars)
+    def _init_via_pandas_(self, x, y, intercept=True, labels=None,
+                          priors=None, init=None, rvars=None):
+        new = pd.DataFrame(x)
+        new.columns = ['x%d' % i for i in range(new.shape[1])]
+        self._init_pandas_(new, y, intercept, labels, priors, init, rvars)
 
     @classmethod
-    def from_formula(cls, name, formula, data, priors=None, init=None, rvars=None):
+    def from_formula(cls, formula, data, priors=None, init=None, rvars=None, name=''):
         import patsy
         y, x = patsy.dmatrices(formula, data)
         labels = x.design_info.column_names
-        return cls(name, x, y, intercept=False, labels=labels, priors=priors, init=init, rvars=rvars)
+        return cls(np.asarray(x), np.asarray(y).T, intercept=False, labels=labels,
+                   priors=priors, init=init, rvars=rvars, name=name)
 
     def _add_intercept(self, dist, var, init):
         if var:
@@ -105,9 +111,9 @@ class Glm(LinearComponent):
         rvars : dict - random variables instead of creating new ones
         family : pymc3.glm.families object
         """
-    def __init__(self, name, x, y, intercept=True, labels=None,
-                 priors=None, init=None, rvars=None, family='normal'):
-        super(Glm, self).__init__(name, x, y, intercept, labels, priors, init, rvars)
+    def __init__(self, x, y, intercept=True, labels=None,
+                 priors=None, init=None, rvars=None, family='normal', name=''):
+        super(Glm, self).__init__(x, y, intercept, labels, priors, init, rvars, name)
 
         _families = dict(
             normal=families.Normal,
@@ -124,3 +130,11 @@ class Glm(LinearComponent):
             name = 'y'
         self.add_var('y', self.model.named_vars[name])
         self.y_est = self['y']
+
+    @classmethod
+    def from_formula(cls, formula, data, priors=None, init=None, rvars=None, family='normal', name=''):
+        import patsy
+        y, x = patsy.dmatrices(formula, data)
+        labels = x.design_info.column_names
+        return cls(np.asarray(x), np.asarray(y).T, intercept=False, labels=labels,
+                   priors=priors, init=init, rvars=rvars, family=family, name=name)

pymc3/tests/test_models_linear.py
@@ -21,19 +21,19 @@ class TestGLM(SeededTest):
         cls.sd = .05
         x_linear, cls.y_linear = generate_data(cls.intercept, cls.slope, size=1000)
         cls.y_linear += np.random.normal(size=1000, scale=cls.sd)
-        cls.data_linear = dict(x=pd.DataFrame(x_linear, columns=['x']), y=cls.y_linear)
+        cls.data_linear = dict(x=x_linear, y=cls.y_linear)
 
         x_logistic, y_logistic = generate_data(cls.intercept, cls.slope, size=3000)
         y_logistic = 1 / (1 + np.exp(-y_logistic))
         bern_trials = [np.random.binomial(1, i) for i in y_logistic]
-        cls.data_logistic = dict(x=pd.DataFrame(x_logistic, columns=['x']), y=bern_trials)
+        cls.data_logistic = dict(x=x_logistic, y=bern_trials)
 
     def test_linear_component(self):
         with Model() as model:
             lm = LinearComponent(
-                'lm',
                 self.data_linear['x'],
-                self.data_linear['y']
+                self.data_linear['y'],
+                name='lm'
             )
             sigma = Uniform('sigma', 0, 20)
             Normal('y_obs', mu=lm.y_est, sd=sigma, observed=self.y_linear)
@@ -42,12 +42,12 @@ class TestGLM(SeededTest):
             trace = sample(500, step, start, progressbar=False, random_seed=self.random_seed)
 
             self.assertAlmostEqual(np.mean(trace['lm_Intercept']), self.intercept, 1)
-            self.assertAlmostEqual(np.mean(trace['lm_x']), self.slope, 1)
+            self.assertAlmostEqual(np.mean(trace['lm_x0']), self.slope, 1)
             self.assertAlmostEqual(np.mean(trace['sigma']), self.sd, 1)
 
     def test_linear_component_from_formula(self):
         with Model() as model:
-            lm = LinearComponent.from_formula('', 'y ~ x', self.data_linear)
+            lm = LinearComponent.from_formula('y ~ x', self.data_linear)
             sigma = Uniform('sigma', 0, 20)
             Normal('y_obs', mu=lm.y_est, sd=sigma, observed=self.y_linear)
             start = find_MAP(vars=[sigma])
@@ -62,10 +62,22 @@ class TestGLM(SeededTest):
         with Model() as model:
             NAME = 'glm'
             Glm(
-                NAME,
                 self.data_linear['x'],
-                self.data_linear['y']
+                self.data_linear['y'],
+                name=NAME
             )
+            start = find_MAP()
+            step = Slice(model.vars)
+            trace = sample(500, step, start, progressbar=False, random_seed=self.random_seed)
+
+            self.assertAlmostEqual(np.mean(trace['%s_Intercept' % NAME]), self.intercept, 1)
+            self.assertAlmostEqual(np.mean(trace['%s_x0' % NAME]), self.slope, 1)
+            self.assertAlmostEqual(np.mean(trace['%s_sd' % NAME]), self.sd, 1)
+
+    def test_glm_from_formula(self):
+        with Model() as model:
+            NAME = 'glm'
+            Glm.from_formula('y ~ x', self.data_linear, name=NAME)
             start = find_MAP()
             step = Slice(model.vars)
             trace = sample(500, step, start, progressbar=False, random_seed=self.random_seed)
@@ -73,3 +85,13 @@ class TestGLM(SeededTest):
             self.assertAlmostEqual(np.mean(trace['%s_Intercept' % NAME]), self.intercept, 1)
             self.assertAlmostEqual(np.mean(trace['%s_x' % NAME]), self.slope, 1)
             self.assertAlmostEqual(np.mean(trace['%s_sd' % NAME]), self.sd, 1)
+
+    def test_strange_types(self):
+        with Model():
+            self.assertRaises(
+                ValueError,
+                Glm,
+                1,
+                self.data_linear['y'],
+                name='lm'
+            )