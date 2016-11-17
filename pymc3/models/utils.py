3bf2d1465957efc3b2acdba1e4597dedb95be99d
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-17 18:55:00 +0300

    tests for `any_to_tensor_and_labels`

--- pymc3/models/utils.py
+++ pymc3/models/utils.py
@@ -4,15 +4,26 @@ import theano.tensor as tt
 
 
 def any_to_tensor_and_labels(x, labels=None):
+    def assert_shape_labels(s, l):
+        if not len(l) == s[1]:
+            raise ValueError(
+                'Please provide full list '
+                'of labels for coefficients, '
+                'got len(labels)=%d instead of %d'
+                % (len(l), s[1])
+            )
     if isinstance(x, pd.DataFrame):
-        labels = list(x.columns)
+        if not labels:
+            labels = x.columns
         x = x.as_matrix()
     elif isinstance(x, pd.Series):
-        labels = [x.name]
+        if not labels:
+            labels = [x.name]
         x = x.as_matrix()[:, None]
     elif isinstance(x, dict):
         x = pd.DataFrame(x)
-        labels = x.columns
+        if not labels:
+            labels = x.columns
         x = x.as_matrix()
     elif not isinstance(x, tt.Variable):
         x = np.asarray(x)
@@ -26,13 +37,24 @@ def any_to_tensor_and_labels(x, labels=None):
             raise ValueError('Cannot use scalars')
         elif x.ndim == 1:
             x = x[:, None]
-    if labels is None:
+    # else: trust input
+    if labels is None and not isinstance(x, tt.Variable):
         labels = ['x%d' % i for i in range(x.shape[1])]
-        # else: labels exist, trust them
+    elif labels is None:
+        raise ValueError('Please provide labels as '
+                         'we cannot infer shape of input')
+    # else: trust labels
+    if not isinstance(x, tt.Variable):
+        assert_shape_labels(x.shape, labels)
+    # else: trust labels
     if isinstance(labels, pd.RangeIndex):
         labels = ['x%d' % i for i in labels]
     if not isinstance(labels, list):
         labels = list(labels)
     if not isinstance(x, tt.Variable):
         x = tt.as_tensor_variable(x)
+        if x.ndim == 0:
+            raise ValueError('Cannot use scalars')
+        elif x.ndim == 1:
+            x = x[:, None]
     return x, labels
