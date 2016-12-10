2fe9773fc86633aeb3a35297ee38ad9f392994e4
Chris Fonnesbeck <fonnesbeck@gmail.com>
2016-12-10 15:54:00 -0600

    First pass at dealing with bound evaluation in dist_math

--- pymc3/tests/test_dist_math.py
+++ pymc3/tests/test_dist_math.py
@@ -1,36 +1,28 @@
 import numpy as np
 import theano.tensor as tt
 
-from ..distributions.dist_math import alltrue
+from ..distributions.dist_math import bound
 
 
-def test_alltrue():
-    assert alltrue([]).eval()
-    assert alltrue([True]).eval()
-    assert alltrue([tt.ones(10)]).eval()
-    assert alltrue([tt.ones(10),
-                    5 * tt.ones(101)]).eval()
-    assert alltrue([np.ones(10),
-                    5 * tt.ones(101)]).eval()
-    assert alltrue([np.ones(10),
-                    True,
-                    5 * tt.ones(101)]).eval()
-    assert alltrue([np.array([1, 2, 3]),
-                    True,
-                    5 * tt.ones(101)]).eval()
+def test_bound():
+    logp = tt.ones((10, 10))
+    cond = tt.ones((10, 10))
+    assert np.all(bound(logp, cond).eval() == logp.eval())
 
-    assert not alltrue([False]).eval()
-    assert not alltrue([tt.zeros(10)]).eval()
-    assert not alltrue([True,
-                        False]).eval()
-    assert not alltrue([np.array([0, -1]),
-                        tt.ones(60)]).eval()
-    assert not alltrue([np.ones(10),
-                        False,
-                        5 * tt.ones(101)]).eval()
+    logp = tt.ones((10, 10))
+    cond = tt.zeros((10, 10))
+    assert np.all(bound(logp, cond).eval() == (-np.inf * logp).eval())
 
+    logp = tt.ones((10, 10))
+    cond = True
+    assert np.all(bound(logp, cond).eval() == logp.eval())
 
-def test_alltrue_shape():
-    vals = [True, tt.ones(10), tt.zeros(5)]
-
-    assert alltrue(vals).eval().shape == ()
+    logp = tt.ones(3)
+    cond = np.array([1, 0, 1])
+    assert not np.all(bound(logp, cond).eval() == 1)
+    assert np.prod(bound(logp, cond).eval()) == -np.inf
+    
+    logp = tt.ones((2, 3))
+    cond = np.array([[1, 1, 1], [1, 0, 1]])
+    assert not np.all(bound(logp, cond).eval() == 1)
+    assert np.prod(bound(logp, cond).eval()) == -np.inf
