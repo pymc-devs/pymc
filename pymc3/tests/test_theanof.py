3dc6f1bcbc59d6f75aabadf1adc6a7916346a736
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-01-11 19:37:25 +0300

    added ndim test

--- pymc3/tests/test_theanof.py
+++ pymc3/tests/test_theanof.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 import numpy as np
 import theano
@@ -16,3 +17,18 @@ class TestGenerator(unittest.TestCase):
         f = theano.function([], gop)
         self.assertEqual(f(), np.float32(0))
         self.assertEqual(f(), np.float32(1))
+
+    def test_ndim(self):
+        for ndim in range(10):
+            def integers():
+                i = 0
+                while True:
+                    yield np.ones((2,) * ndim) * i
+                    i += 1
+            res = list(itertools.islice(integers(), 0, 2))
+            generator = DataGenerator(integers())
+            gop = GeneratorOp(generator)()
+            f = theano.function([], gop)
+            self.assertEqual(ndim, res[0].ndim)
+            np.testing.assert_equal(f(), res[0])
+            np.testing.assert_equal(f(), res[1])
