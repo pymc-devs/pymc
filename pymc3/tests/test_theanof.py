23a85c7a89ad4b130b8676fa9c4a9924142ac6e2
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-01-11 19:49:26 +0300

    added test for replacing generator with shared variable

--- pymc3/tests/test_theanof.py
+++ pymc3/tests/test_theanof.py
@@ -5,13 +5,22 @@ import theano
 from ..theanof import DataGenerator, GeneratorOp
 
 
+def integers():
+    i = 0
+    while True:
+        yield np.float32(i)
+        i += 1
+
+
+def integers_ndim(ndim):
+    i = 0
+    while True:
+        yield np.ones((2,) * ndim) * i
+        i += 1
+
+
 class TestGenerator(unittest.TestCase):
     def test_basic(self):
-        def integers():
-            i = 0
-            while True:
-                yield np.float32(i)
-                i += 1
         generator = DataGenerator(integers())
         gop = GeneratorOp(generator)()
         self.assertEqual(gop.tag.test_value, np.float32(0))
@@ -24,15 +33,19 @@ class TestGenerator(unittest.TestCase):
 
     def test_ndim(self):
         for ndim in range(10):
-            def integers():
-                i = 0
-                while True:
-                    yield np.ones((2,) * ndim) * i
-                    i += 1
-            res = list(itertools.islice(integers(), 0, 2))
-            generator = DataGenerator(integers())
+            res = list(itertools.islice(integers_ndim(ndim), 0, 2))
+            generator = DataGenerator(integers_ndim(ndim))
             gop = GeneratorOp(generator)()
             f = theano.function([], gop)
             self.assertEqual(ndim, res[0].ndim)
             np.testing.assert_equal(f(), res[0])
             np.testing.assert_equal(f(), res[1])
+
+    def test_cloning_available(self):
+        generator = DataGenerator(integers())
+        gop = GeneratorOp(generator)()
+        res = gop ** 2
+        shared = theano.shared(np.float32(10))
+        res1 = theano.clone(res, {gop: shared})
+        f = theano.function([], res1)
+        self.assertEqual(f(), np.float32(100))
