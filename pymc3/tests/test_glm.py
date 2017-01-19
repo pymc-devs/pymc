0c3d0f4e432565802d2b3619b266543fb7aec39c
Chris Fonnesbeck <fonnesbeck@gmail.com>
2017-01-19 14:21:13 -0600

    Fixed test_linear_component failure

--- pymc3/tests/test_glm.py
+++ pymc3/tests/test_glm.py
@@ -3,6 +3,7 @@ import numpy as np
 from .helpers import SeededTest
 from pymc3 import Model, Uniform, Normal, find_MAP, Slice, sample
 from pymc3 import families, GLM, LinearComponent
+import pandas as pd
 
 # Generate data
 def generate_data(intercept, slope, size=700):
@@ -20,7 +21,7 @@ class TestGLM(SeededTest):
         cls.sd = .05
         x_linear, cls.y_linear = generate_data(cls.intercept, cls.slope, size=1000)
         cls.y_linear += np.random.normal(size=1000, scale=cls.sd)
-        cls.data_linear = dict(x=x_linear, y=cls.y_linear)
+        cls.data_linear = pd.DataFrame(dict(x=x_linear, y=cls.y_linear))
 
         x_logistic, y_logistic = generate_data(cls.intercept, cls.slope, size=3000)
         y_logistic = 1 / (1 + np.exp(-y_logistic))
@@ -29,9 +30,9 @@ class TestGLM(SeededTest):
 
     def test_linear_component(self):
         with Model() as model:
-            y_est, _ = LinearComponent.from_formula('y ~ x', self.data_linear)
+            lm = LinearComponent.from_formula('y ~ x', self.data_linear)
             sigma = Uniform('sigma', 0, 20)
-            Normal('y_obs', mu=y_est, sd=sigma, observed=self.y_linear)
+            Normal('y_obs', mu=lm.y_est, sd=sigma, observed=self.y_linear)
             start = find_MAP(vars=[sigma])
             step = Slice(model.vars)
             trace = sample(500, step=step, start=start, progressbar=False, random_seed=self.random_seed)
