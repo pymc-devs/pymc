057c9858ea10a073b9330bb23360bc68b4a1076f
AustinRochford <arochford@monetate.com>
2017-01-22 20:02:59 -0500

    Check for single samples

--- pymc3/distributions/distribution.py
+++ pymc3/distributions/distribution.py
@@ -281,7 +281,7 @@ def reshape_sampled(sampled, size, dist_shape):
     dist_shape = infer_shape(dist_shape)
     repeat_shape = infer_shape(size)
 
-    if repeat_shape or dist_shape:
+    if sampled.size == 1 or repeat_shape or dist_shape:
         return np.reshape(sampled, repeat_shape + dist_shape)
     else:
         return sampled
