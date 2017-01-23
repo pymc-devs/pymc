67004e7958c1cf5820daa1d9d7687b6592947aa6
AustinRochford <arochford@monetate.com>
2017-01-22 19:26:57 -0500

    Use SeededTest for shared tests

--- pymc3/tests/test_shared.py
+++ pymc3/tests/test_shared.py
@@ -1,11 +1,13 @@
 import pymc3 as pm
+from .helpers import SeededTest
 import numpy as np
 import theano
 
 
-def test_deterministic():
-    with pm.Model() as model:
-        data_values = np.array([.5, .4, 5, 2])
-        X = theano.shared(np.asarray(data_values, dtype=theano.config.floatX), borrow=True)
-        pm.Normal('y', 0, 1, observed=X)
-        model.logp(model.test_point)
+class TestShared(SeededTest):
+    def test_deterministic(self):
+        with pm.Model() as model:
+            data_values = np.array([.5, .4, 5, 2])
+            X = theano.shared(np.asarray(data_values, dtype=theano.config.floatX), borrow=True)
+            pm.Normal('y', 0, 1, observed=X)
+            model.logp(model.test_point)
