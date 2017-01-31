7ef320f8de26eb4f0ecd3253ab1ac000189ab1fc
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-01-30 23:01:17 -0600

    Added Zero and Constant mean functions

--- pymc3/gp/mean.py
+++ pymc3/gp/mean.py
@@ -25,10 +25,24 @@ class Mean(object):
         raise NotImplementedError
 
     def __add__(self, other):
-        return Add([self, other])
+        return Add(self, other)
 
     def __mul__(self, other):
-        return Prod([self, other])
+        return Prod(self, other)
+        
+class Zero(Mean):
+    def __call__(self, X):
+        return tt.zeros(tt.stack([tt.shape(X)[0], 1]), dtype='float32')
+        
+class Constant(Mean):
+
+    def __init__(self, c=0):
+        MeanFunction.__init__(self)
+        self.c = c
+
+    def __call__(self, X):
+        shape = tt.stack([tt.shape(X)[0], 1])
+        return tt.tile(tt.reshape(self.c, (1, -1)), shape)
         
 class Add(Mean):
     def __init__(self, first_mean, second_mean):
