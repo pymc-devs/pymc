c94f34aab68fbdbc7e0b5661ec14a930e73f5117
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-01-30 22:51:32 -0600

    Fixed K() alias in Covariance; skeleton mean function base class

--- pymc3/gp/cov.py
+++ pymc3/gp/cov.py
@@ -43,8 +43,7 @@ class Covariance(object):
         """
         raise NotImplementedError
         
-    def K(self, X, Z):
-        return self.__call__(X, Z)
+    K = __call__
 
 
     def _slice(self, X, Z):
