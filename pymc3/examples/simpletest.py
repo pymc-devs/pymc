61d02d7563e1e794a300fdc880618758b41b087f
Colin <ColCarroll@users.noreply.github.com>
2017-01-31 04:26:40 -0500

    Remove star imports (#1731)
    * Remove star imports
    
    * BLD Add missing gp submodule.
    
    * add NegativeBinomial to __all__
    
    * Remove star imports
    
    * Pylab --> pyplot

--- pymc3/examples/simpletest.py
+++ pymc3/examples/simpletest.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from pymc3 import *
+import pymc3 as pm
 import numpy as np
 
 # import pydevd
@@ -9,23 +9,20 @@ np.seterr(invalid='raise')
 data = np.random.normal(size=(2, 20))
 
 
-model = Model()
+model = pm.Model()
 
 with model:
-    x = Normal('x', mu=.5, tau=2. ** -2, shape=(2, 1))
-
-    z = Beta('z', alpha=10, beta=5.5)
-
-    d = Normal('data', mu=x, tau=.75 ** -2, observed=data)
-
-    step = NUTS()
+    x = pm.Normal('x', mu=.5, tau=2. ** -2, shape=(2, 1))
+    z = pm.Beta('z', alpha=10, beta=5.5)
+    d = pm.Normal('data', mu=x, tau=.75 ** -2, observed=data)
+    step = pm.NUTS()
 
 
 def run(n=1000):
     if n == "short":
         n = 50
     with model:
-        trace = sample(n, step)
+        trace = pm.sample(n, step)
 
     plt.subplot(2, 2, 1)
     plt.plot(trace[x][:, 0, 0])
