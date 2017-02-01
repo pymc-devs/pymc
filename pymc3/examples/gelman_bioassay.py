3dd586f8207bad190ab7f5d739c2b1ec04951ad4
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-02-01 17:47:54 -0600

    Added random init to init_nuts

--- pymc3/examples/gelman_bioassay.py
+++ pymc3/examples/gelman_bioassay.py
@@ -1,5 +1,5 @@
-import pymc3 as pm
-from numpy import ones, array
+from pymc3 import *
+from numpy import ones, array, random
 
 # Samples for each dose level
 n = 5 * ones(4, dtype=int)
@@ -17,9 +17,6 @@ with pm.Model() as model:
 
     # Data likelihood
     deaths = pm.Binomial('deaths', n=n, p=theta, observed=[0, 1, 3, 5])
-
-    step = pm.NUTS()
-
 
 def run(n=1000):
     if n == "short":
