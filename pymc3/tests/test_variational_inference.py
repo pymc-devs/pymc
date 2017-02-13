80dee6dbc7d70c15743368a59ec26012285237b4
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-02-14 01:58:59 +0300

    implement Inference

--- pymc3/tests/test_variational_inference.py
+++ pymc3/tests/test_variational_inference.py
@@ -1,60 +1,55 @@
 import unittest
-import tqdm
 import numpy as np
 import theano
-import theano.tensor as tt
 import pymc3 as pm
 from pymc3 import Model, Normal
-from pymc3.variational.opvi import (
-    KL, MeanField, FullRank, TestFunction
+from pymc3.variational.inference import (
+    KL, MeanField, ADVI, FullRankADVI
 )
 
 from pymc3.tests import models
 from pymc3.tests.helpers import SeededTest
 
 
+class TestELBO(SeededTest):
+    def test_elbo(self):
+        mu0 = 1.5
+        sigma = 1.0
+        y_obs = np.array([1.6, 1.4])
+
+        post_mu = np.array([1.88])
+        post_sd = np.array([1])
+        # Create a model for test
+        with Model() as model:
+            mu = Normal('mu', mu=mu0, sd=sigma)
+            Normal('y', mu=mu, sd=1, observed=y_obs)
+
+        # Create variational gradient tensor
+        mean_field = MeanField(model=model)
+        elbo = -KL(mean_field)()(mean_field.random())
+
+        mean_field.shared_params['mu'].set_value(post_mu)
+        mean_field.shared_params['rho'].set_value(np.log(np.exp(post_sd) - 1))
+
+        f = theano.function([], elbo)
+        elbo_mc = sum(f() for _ in range(10000)) / 10000
+
+        # Exact value
+        elbo_true = (-0.5 * (
+            3 + 3 * post_mu ** 2 - 2 * (y_obs[0] + y_obs[1] + mu0) * post_mu +
+            y_obs[0] ** 2 + y_obs[1] ** 2 + mu0 ** 2 + 3 * np.log(2 * np.pi)) +
+                     0.5 * (np.log(2 * np.pi) + 1))
+        np.testing.assert_allclose(elbo_mc, elbo_true, rtol=0, atol=1e-1)
+
 class TestApproximates:
     class Base(SeededTest):
-        op = KL
-        approx = MeanField
-        tf = TestFunction
+        inference = None
         NITER = 30000
-
-        def test_elbo(self):
-            if self.approx is not MeanField:
-                raise unittest.SkipTest
-            mu0 = 1.5
-            sigma = 1.0
-            y_obs = np.array([1.6, 1.4])
-
-            post_mu = np.array([1.88])
-            post_sd = np.array([1])
-            # Create a model for test
-            with Model() as model:
-                mu = Normal('mu', mu=mu0, sd=sigma)
-                Normal('y', mu=mu, sd=1, observed=y_obs)
-
-            # Create variational gradient tensor
-            mean_field = self.approx(model=model)
-            elbo = -self.op(mean_field)(self.tf())(mean_field.random())
-
-            mean_field.shared_params['mu'].set_value(post_mu)
-            mean_field.shared_params['rho'].set_value(np.log(np.exp(post_sd) - 1))
-
-            f = theano.function([], elbo)
-            elbo_mc = sum(f() for _ in range(10000))/10000
-
-            # Exact value
-            elbo_true = (-0.5 * (
-                3 + 3 * post_mu**2 - 2 * (y_obs[0] + y_obs[1] + mu0) * post_mu +
-                y_obs[0]**2 + y_obs[1]**2 + mu0**2 + 3 * np.log(2 * np.pi)) +
-                0.5 * (np.log(2 * np.pi) + 1))
-            np.testing.assert_allclose(elbo_mc, elbo_true, rtol=0, atol=1e-1)
 
         def test_vars_view(self):
             _, model, _ = models.multidimensional_model()
             with model:
-                app = self.approx()
+                app = self.inference().approx
                 posterior = app.random(10)
                 x_sampled = app.view(posterior, 'x').eval()
             self.assertEqual(x_sampled.shape, (10,) + model['x'].dshape)
@@ -65,7 +60,7 @@ class TestApproximates:
             with pm.Model():
                 p = pm.Beta('p', alpha=1, beta=1)
                 pm.Binomial('xs', n=1, p=p, observed=xs)
-                app = self.approx()
+                app = self.inference().approx
                 trace = app.sample_vp(draws=1, hide_transformed=True)
                 self.assertListEqual(trace.varnames, ['p'])
                 self.assertEqual(len(trace), 1)
@@ -89,22 +84,25 @@ class TestApproximates:
                 mu_ = Normal('mu', mu=mu0, sd=sd0, testval=0)
                 Normal('x', mu=mu_, sd=sd, observed=data)
                 pm.Deterministic('mu_sq', mu_**2)
-                approx = self.approx()
-                obj_f = self.op(approx)(self.tf())
-                step = obj_f.step_function(score=False)
-                for _ in tqdm.tqdm(range(self.NITER)):
-                    step()
+                inf = self.inference()
+                self.assertEqual(len(inf.hist), 0)
+                inf.fit(10)
+                self.assertEqual(len(inf.hist), 10)
+                self.assertFalse(np.isnan(inf.hist).any())
+                approx = inf.fit(self.NITER)
+                self.assertEqual(len(inf.hist), self.NITER + 10)
+                self.assertFalse(np.isnan(inf.hist).any())
                 trace = approx.sample_vp(10000)
             np.testing.assert_allclose(np.mean(trace['mu']), mu_post, rtol=0.1)
             np.testing.assert_allclose(np.std(trace['mu']), np.sqrt(1. / d), rtol=0.4)
 
 
 class TestMeanField(TestApproximates.Base):
-    pass
+    inference = ADVI
 
 
 class TestFullRank(TestApproximates.Base):
-    approx = FullRank
+    inference = FullRankADVI
 
 if __name__ == '__main__':
     unittest.main()
---
+++ pymc3/variational/inference.py
@@ -0,0 +1,174 @@
+import numpy as np
+import theano
+from theano import tensor as tt
+import tqdm
+
+import pymc3 as pm
+from pymc3.distributions.dist_math import log_normal, rho2sd, log_normal_mv
+from pymc3.variational.opvi import Operator, Approximation, TestFunction
+
+
+__all__ = [
+    'TestFunction',
+    'KL',
+    'MeanField',
+    'FullRank',
+    'ADVI',
+    'FullRankADVI',
+    'Inference'
+]
+# OPERATORS
+
+
+class KL(Operator):
+    def apply(self, f):
+        """
+        KL divergence between posterior and approximation for input `z`
+            :math:`z ~ Approximation`
+        """
+        z = self.input
+        return self.logq(z) - self.logp(z)
+
+# APPROXIMATIONS
+
+
+class MeanField(Approximation):
+    def create_shared_params(self):
+        return {'mu': theano.shared(
+                    self.input.tag.test_value[self.global_slc]),
+                'rho': theano.shared(
+                    np.zeros((self.global_size,), dtype=theano.config.floatX))
+                }
+
+    def log_q_W_global(self, z):
+        """
+        log_q_W samples over q for global vars
+        Gradient wrt mu, rho in density parametrization
+        is set to zero to lower variance of ELBO
+        """
+        mu = self.shared_params['mu']
+        rho = self.shared_params['rho']
+        mu = self.scale_grad(mu)
+        rho = self.scale_grad(rho)
+        logq = tt.sum(log_normal(z[self.global_slc], mu, rho=rho))
+        return logq
+
+    def random_global(self, samples=None, no_rand=False):
+        initial = self.initial(samples, no_rand, l=self.global_size)
+        sd = rho2sd(self.shared_params['rho'])
+        mu = self.shared_params['mu']
+        return sd * initial + mu
+
+
+class FullRank(Approximation):
+    def create_shared_params(self):
+        return {'mu': theano.shared(
+                    self.input.tag.test_value[self.global_slc]),
+                'L': theano.shared(
+                    np.eye(self.global_size, dtype=theano.config.floatX))
+                }
+
+    def log_q_W_global(self, z):
+        """
+        log_q_W samples over q for global vars
+        Gradient wrt mu, rho in density parametrization
+        is set to zero to lower variance of ELBO
+        """
+        mu = self.shared_params['mu']
+        L = self.shared_params['L']
+        mu = self.scale_grad(mu)
+        L = self.scale_grad(L)
+        return log_normal_mv(z, mu, chol=L)
+
+    def random_global(self, samples=None, no_rand=False):
+        initial = self.initial(samples, no_rand, l=self.global_size)
+        L = self.shared_params['L']
+        mu = self.shared_params['mu']
+        return initial.dot(L) + mu
+
+
+class Inference(object):
+    def __init__(self, op, approx, tf, local_rv=None, model=None, cost_part_grad_scale=1, **kwargs):
+        self.hist = np.asarray(())
+        self.objective = op(approx(
+            local_rv=local_rv,
+            model=model,
+            cost_part_grad_scale=cost_part_grad_scale, **kwargs)
+        )(tf)
+
+    approx = property(lambda self: self.objective.approx)
+
+    def fit(self, n=10000, score_every=1, callbacks=None, **kwargs):
+        if callbacks is None:
+            callbacks = []
+        sc_n_mc = kwargs.get('sc_n_mc')
+        kwargs['score'] = False
+        step_func = self.objective.step_function(**kwargs)
+        if score_every is not None:
+            score_func = self.objective.score_function(sc_n_mc)
+        else:
+            score_func = None
+        i = 0
+        j = 0
+        if score_every is not None:
+            scores = np.empty(n // score_every)
+            scores[:] = np.nan
+        else:
+            scores = np.asarray(())
+        if score_every is not None:
+            try:
+                scores = np.empty(n // score_every)
+                progress = tqdm.trange(n)
+                for i in progress:
+                    step_func()
+                    if i % score_every == 0:
+                        e = score_func()
+                        if np.isnan(e):
+                            raise FloatingPointError('NaN occurred in optimization.')
+                        scores[j] = e
+                        j += 1
+                        for callback in callbacks:
+                            callback(self.approx, scores[:j], i)
+                        if n < 10:
+                            progress.set_description('SCORE = {:,.5g}'.format(scores[i]))
+                        elif i % (n // 10) == 0 and i > 0:
+                            avg_elbo = scores[max(0, j - 100):j].mean()
+                            progress.set_description('Average SCORE = {:,.5g}'.format(avg_elbo))
+            except KeyboardInterrupt:
+                scores = scores[:j]
+                if n < 10:
+                    pm._log.info('Interrupted at {:,d} [{:.0f}%]: SCORE = {:,.5g}'.format(
+                        i, 100 * i // n, scores[i]))
+                else:
+                    avg_elbo = scores[min(0, j - 100):j].mean()
+                    pm._log.info('Interrupted at {:,d} [{:.0f}%]: Average SCORE = {:,.5g}'.format(
+                        i, 100 * i // n, avg_elbo))
+            else:
+                if n < 10:
+                    pm._log.info('Finished [100%]: ELBO = {:,.5g}'.format(scores[-1]))
+                else:
+                    avg_elbo = scores[max(0, j - 100):j].mean()
+                    pm._log.info('Finished [100%]: Average ELBO = {:,.5g}'.format(avg_elbo))
+        else:
+            try:
+                progress = tqdm.trange(n)
+                for _ in progress:
+                    step_func()
+            except KeyboardInterrupt:
+                pass
+        self.hist = np.concatenate([self.hist, scores])
+        return self.objective.approx
+
+
+class ADVI(Inference):
+    def __init__(self, local_rv=None, model=None, cost_part_grad_scale=1):
+        super(ADVI, self).__init__(
+            KL, MeanField, None,
+            local_rv=local_rv, model=model, cost_part_grad_scale=cost_part_grad_scale)
+
+
+class FullRankADVI(Inference):
+    def __init__(self, local_rv=None, model=None, cost_part_grad_scale=1):
+        super(FullRankADVI, self).__init__(
+            KL, FullRank, None,
+            local_rv=local_rv, model=model, cost_part_grad_scale=cost_part_grad_scale)
