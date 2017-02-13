95b2046104edbcacd07d273b049a784a2c8b7fb8
Bhargav Srinivasa <bhargavvader@gmail.com>
2017-02-13 19:29:48 +0530

    Adding logdet

--- pymc3/theanof.py
+++ pymc3/theanof.py
@@ -1,4 +1,5 @@
 import numpy as np
+import theano
 
 from .vartypes import typefilter, continuous_types
 from theano import theano, scalar, tensor as tt
@@ -58,6 +59,37 @@ def floatX(X):
 """
 Theano derivative functions
 """
+
+class logdet(Op):
+    """Computes the logarithm of absolute determinant of a square
+    matrix M, log(abs(det(M))), on CPU. Avoids det(M) overflow/
+    underflow.
+
+    Note: Once PR #3959 in the Theano repo is merged, this must be removed.
+    """
+    def make_node(self, x):
+        x = theano.tensor.as_tensor_variable(x)
+        o = theano.tensor.scalar(dtype=x.dtype)
+        return Apply(self, [x], [o])
+
+    def perform(self, node, inputs, outputs):
+        try:
+            (x,) = inputs
+            (z,) = outputs
+            s = numpy.linalg.svd(x, compute_uv=False)
+            log_abs_det = numpy.sum(numpy.log(numpy.abs(s)))
+            z[0] = numpy.asarray(log_abs_det, dtype=x.dtype)
+        except Exception:
+            print('Failed to compute logabsdet of {}.'.format(x))
+            raise
+
+    def grad(self, inputs, g_outputs):
+        [gz] = g_outputs
+        [x] = inputs
+        return [gz * matrix_inverse(x).T]
+
+    def __str__(self):
+        return "LogAbsDet"
 
 def gradient1(f, v):
     """flat gradient of f wrt v"""
