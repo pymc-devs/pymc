6d6d2dbe780c33b52f0e93e4ae00ceaccf2d62ba
Sourav Singh <souravsingh@users.noreply.github.com>
2017-02-13 21:35:34 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-THEANO_FLAGS='gcc.cxxflags="-march=core2", floatX=float32' nosetests "$@"
+THEANO_FLAGS='gcc.cxxflags="-march=core2", floatX=float32, floatX=float64' nosetests "$@"
 
 if [[ "$RUN_PYLINT" == "true" ]]; then
     . ./scripts/lint.sh
