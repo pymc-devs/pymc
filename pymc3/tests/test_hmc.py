305b003b02f48d4383ec43a022b94a346eb6730e
aseyboldt <aseyboldt@users.noreply.github.com>
2017-02-17 09:50:27 +0100

    Implement higher order hmc integration (#1758)
    * Implement higher order hmc integration
    
    * Add tests for new higher order integrators
    
    * Rename step_method->integrator for nuts
    
    * Fix a bug in sparse quadpotential tests
    
    * Fix typo in test_hmc
    
    * Use a dict to store integrators

--- pymc3/tests/test_hmc.py
+++ pymc3/tests/test_hmc.py
@@ -26,6 +26,35 @@ def test_leapfrog_reversible():
             close_to(-p, p0, 1e-8, str((n_steps, epsilon)))
 
 
+def test_leapfrog_reversible_single():
+    n = 3
+    start, model, _ = models.non_normal(n)
+
+    integrators = ['leapfrog', 'two-stage', 'three-stage']
+    steps = [BaseHMC(vars=model.vars, model=model, integrator=method, use_single_leapfrog=True)
+             for method in integrators]
+    for method, step in zip(integrators, steps):
+        bij = DictToArrayBijection(step.ordering, start)
+        q0 = bij.map(start)
+        p0 = np.ones(n) * .05
+        for epsilon in [0.01, 0.1, 1.2]:
+            for n_steps in [1, 2, 3, 4, 20]:
+                dlogp0 = step.dlogp(q0)
+
+                q, p = q0, p0
+                dlogp = dlogp0
+
+                energy = step.compute_energy(q, p)
+                for _ in range(n_steps):
+                    q, p, dlogp, _ = step.leapfrog(q, p, dlogp, np.array(epsilon))
+                p = -p
+                for _ in range(n_steps):
+                    q, p, dlogp, _ = step.leapfrog(q, p, dlogp, np.array(epsilon))
+
+                close_to(q, q0, 1e-8, str(('q', method, n_steps, epsilon)))
+                close_to(-p, p0, 1e-8, str(('p', method, n_steps, epsilon)))
+
+
 def test_nuts_tuning():
     model = pymc3.Model()
     with model:
