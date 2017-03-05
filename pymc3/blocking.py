8d468b8037637dfc6d672e4e3c3f6a04b8339bd1
Eigenblutwurst <Hannes.Bathke@gmx.net>
2017-03-05 15:55:25 +0300

    added back list bijections

--- pymc3/blocking.py
+++ pymc3/blocking.py
@@ -3,12 +3,15 @@ pymc3.blocking
 
 Classes for working with subsets of parameters.
 """
+import copy
 import numpy as np
 import collections
 
 __all__ = ['ArrayOrdering', 'DictToArrayBijection', 'DictToVarBijection']
 
 VarMap = collections.namedtuple('VarMap', 'var, slc, shp, dtyp')
+DataMap = collections.namedtuple('DataMap', 'list_ind, slc, shp, dtype')
+
 
 # TODO Classes and methods need to be fully documented.
 
@@ -38,7 +41,7 @@ class DictToArrayBijection(object):
     def __init__(self, ordering, dpoint):
         self.ordering = ordering
         self.dpt = dpoint
-        
+
         # determine smallest float dtype that will fit all data
         if all([x.dtyp == 'float16' for x in ordering.vmap]):
             self.array_dtype = 'float16'
@@ -89,6 +92,98 @@ class DictToArrayBijection(object):
         f : array -> T
         """
         return Compose(f, self.rmap)
+
+
+class ListArrayOrdering(object):
+    """
+    An ordering for a list to an array space. Takes also non theano.tensors.
+    Modified from pymc3 blocking.
+
+    Parameters
+    ----------
+    list_arrays : list
+        :class:`numpy.ndarray` or :class:`theano.tensor.Tensor`
+    intype : str
+        defining the input type 'tensor' or 'numpy'
+    """
+
+    def __init__(self, list_arrays, intype='numpy'):
+        self.vmap = []
+        dim = 0
+
+        count = 0
+        for array in list_arrays:
+            if intype == 'tensor':
+                array = array.tag.test_value
+            elif intype == 'numpy':
+                pass
+
+            slc = slice(dim, dim + array.size)
+            self.vmap.append(DataMap(
+                count, slc, array.shape, array.dtype))
+            dim += array.size
+            count += 1
+
+        self.dimensions = dim
+
+
+class ListToArrayBijection(object):
+    """
+    A mapping between a List of arrays and an array space
+
+    Parameters
+    ----------
+    ordering : :class:`ListArrayOrdering`
+    list_arrays : list
+        of :class:`numpy.ndarray`
+    """
+
+    def __init__(self, ordering, list_arrays):
+        self.ordering = ordering
+        self.list_arrays = list_arrays
+
+    def fmap(self, list_arrays):
+        """
+        Maps values from List space to array space
+
+        Parameters
+        ----------
+        list_arrays : list
+            of :class:`numpy.ndarray`
+
+        Returns
+        -------
+        array : :class:`numpy.ndarray`
+            single array comprising all the input arrays
+        """
+
+        array = np.empty(self.ordering.dimensions)
+        for list_ind, slc, _, _ in self.ordering.vmap:
+            array[slc] = list_arrays[list_ind].ravel()
+        return array
+
+    def rmap(self, array):
+        """
+        Maps value from array space to List space
+        Inverse operation of fmap.
+
+        Parameters
+        ----------
+        array : :class:`numpy.ndarray`
+
+        Returns
+        -------
+        a_list : list
+            of :class:`numpy.ndarray`
+        """
+
+        a_list = copy.copy(self.list_arrays)
+
+        for list_ind, slc, shp, dtype in self.ordering.vmap:
+            a_list[list_ind] = np.atleast_1d(
+                                        array)[slc].reshape(shp).astype(dtype)
+
+        return a_list
 
 
 class DictToVarBijection(object):
