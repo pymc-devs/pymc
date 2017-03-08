c8781a2be627feb33623f60807aab8478d1d3052
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-03-08 14:35:51 -0600

    Added probit and invprobit functions

--- pymc3/tests/test_math.py
+++ pymc3/tests/test_math.py
@@ -1,8 +1,12 @@
 import numpy as np
 import theano
 from theano.tests import unittest_tools as utt
-from pymc3.math import LogDet, logdet
+from pymc3.math import LogDet, logdet, probit, invprobit
 from .helpers import SeededTest
+
+def test_probit():
+    p = np.array([0.01, 0.25, 0.5, 0.75, 0.99])
+    assert np.all(invprobit(probit(p)).eval()==p)
 
 class TestLogDet(SeededTest):
 
