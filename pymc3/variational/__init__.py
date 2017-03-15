4a713dc4519b2da8ad6e9b25240840d82dd02572
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-03-15 12:04:13 +0300

    WIP: Implement opvi (#1694)
    * migrate useful functions from previous PR
    
    (cherry picked from commit 9f61ab4)
    
    * opvi draft
    
    (cherry picked from commit d0997ff)
    
    * made some test work
    
    (cherry picked from commit b1a87d5)
    
    * refactored approximation to support aevb (without test)
    
    * refactor opvi
    
    delete unnecessary methods from operator, change method order
    
    * change log_q_local computation
    
    * add full rank approximation
    
    * add more_params argument to ObjectiveFunction.updates (aevb case)
    
    * refactor density computation in full rank approximation
    
    * typo: cast dict values to list
    
    * typo: cast dict values to list
    
    * typo: undefined T in dist_math
    
    * refactor gradient scaling as suggested in approximateinference.org/accepted/RoederEtAl2016.pdf
    
    * implement Langevin-Stein (LS) operator
    
    * fix docstring
    
    * add blank line in docs
    
    * refactor ObjectiveFunction
    
    * add not working LS Op test
    
    * experiments with not working LS Op
    
    * change activations
    
    * refactor networks
    
    * add step_function
    
    * remove Langevin Stein, done refactoring
    
    * remove Langevin Stein, done refactoring
    
    * change optimizers
    
    * refactor init params
    
    * implement tests
    
    * implement Inference
    
    * code style
    
    * test fix
    
    * add minibatch test (fails now)
    
    * add more tests for minibatch training
    
    * add logdet to FullRank approximation
    
    * add conversion of arrays to floatX
    
    * tiny changes
    
    * change number of iterations
    
    * fix test and pylint check
    
    * memoize functions in Objective function
    
    * Optimize code a lot
    
    * a bit more efficient pickling
    
    * add docs
    
    * Add MeanField -> FullRank parameter transfer
    
    * refactor MeanField and FullRank a bit
    
    * fix FullRank bug with shapes in random
    
    * refactor Model.flatten (CC @taku-y)
    
    * add `approximate` to inference
    
    * rename approximate->fit
    
    * change abbreviations
    
    * Fix bug with scaling input variable in aevb
    
    * fix theano bottleneck in graph
    
    * more efficient scaling for local vars
    
    * fix typo in local Q
    
    * add aevb test
    
    * refactor memoize to work with my objects
    
    * add tests for numpy view usage
    
    * pickle-hash fix
    
    * pickle-hash fix again
    
    * add node sampling + make up some code
    
    * add notebook with example
    
    * sample_proba explained

--- pymc3/variational/__init__.py
+++ pymc3/variational/__init__.py
@@ -1,18 +1,25 @@
 from .advi import advi, sample_vp
 from .advi_minibatch import advi_minibatch
 
-from .updates import (sgd,
-                      apply_momentum,
-                      momentum,
-                      apply_nesterov_momentum,
-                      nesterov_momentum,
-                      adagrad,
-                      rmsprop,
-                      adadelta,
-                      adam,
-                      adamax,
-                      norm_constraint,
-                      total_norm_constraint
+from .updates import (
+    sgd,
+    apply_momentum,
+    momentum,
+    apply_nesterov_momentum,
+    nesterov_momentum,
+    adagrad,
+    rmsprop,
+    adadelta,
+    adam,
+    adamax,
+    norm_constraint,
+    total_norm_constraint
 )
 
 from .svgd import svgd
+
+from .inference import (
+    ADVI,
+    FullRankADVI,
+    fit,
+)
