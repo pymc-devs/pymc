d6bd88615875c9fc0da28acb168097bf4ed45d48
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-03-16 13:03:51 -0500

    Fixed stray version string

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ AUTHOR = 'John Salvatier and Christopher Fonnesbeck'
 AUTHOR_EMAIL = 'chris.fonnesbeck@vanderbilt.edu'
 URL = "http://github.com/pymc-devs/pymc3"
 LICENSE = "Apache License, Version 2.0"
-VERSION = "3.1.rc1"
+VERSION = "3.1.rc2"
 
 classifiers = ['Development Status :: 5 - Production/Stable',
                'Programming Language :: Python',
