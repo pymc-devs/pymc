ebeb145fb863a8e5be9cf7e0dea5c3800f6c51a0
aloctavodia <aloctavodia@gmail.com>
2017-03-18 18:14:42 -0300

    improve aesthetics

--- pymc3/plots/compareplot.py
+++ pymc3/plots/compareplot.py
@@ -24,14 +24,20 @@ def compare_plot(comp_df, ax=None):
     if ax is None:
         _, ax = plt.subplots()
 
-    yticks_pos = np.linspace(0, -1, (comp_df.shape[0] * 2) - 1)
-    yticks_labels = np.repeat(comp_df.index, 2)[1:]
+    yticks_pos, step = np.linspace(0, -1, (comp_df.shape[0] * 2) - 1, retstep=True)
+    yticks_pos[1::2] = yticks_pos[1::2] + step / 2
+
+    yticks_labels = [''] * len(yticks_pos)
+    yticks_labels[0] = comp_df.index[0]
+    yticks_labels[1::2] = comp_df.index[1:]
 
     data = comp_df.values
     min_ic = data[0, 0]
 
-    ax.errorbar(x=data[:, 0], y=yticks_pos[::2], xerr=data[:, 4], fmt='ko', mfc='None', mew=1)
-    ax.errorbar(x=data[1:, 0], y=yticks_pos[1::2], xerr=data[1:, 5], fmt='^', color='grey')
+    ax.errorbar(x=data[:, 0], y=yticks_pos[::2], xerr=data[:, 4],
+                fmt='ko', mfc='None', mew=1)
+    ax.errorbar(x=data[1:, 0], y=yticks_pos[1::2],
+                xerr=data[1:, 5], fmt='^', color='grey')
 
     ax.plot(data[:, 0] - (2 * data[:, 1]), yticks_pos[::2], 'ko')
     ax.axvline(min_ic, ls='--', color='grey')
