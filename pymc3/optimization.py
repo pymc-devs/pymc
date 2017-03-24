fc00a9095f28320b6ec524d34ff0b96d18178f52
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-03-24 18:35:51 +0300

    refactored optimizer

--- pymc3/optimization.py
+++ pymc3/optimization.py
@@ -1,24 +1,34 @@
+import tqdm
 import numpy as np
 import theano
-import tqdm
-from pymc3.backends.base import BaseTrace, MultiTrace
-from pymc3.variational import Histogram, adam
+from theano.configparser import change_flags
+import pymc3 as pm
 
 
 class Optimizer(object):
-    def __init__(self, approx, loss, wrt, more_replacements=None, model=None, optimizer=None):
+    """
+    Optimization with posterior replacements
+    Parameters
+    ----------
+    approx : Approximation
+    loss : scalar
+    wrt : shared params
+    more_replacements : other replacements in the graph
+    optimizer : callable that returns updates, pm.adam by default
+    """
+    @change_flags(compute_test_value='off')
+    def __init__(self, approx, loss, wrt, more_replacements=None, optimizer=None):
         if optimizer is None:
-            optimizer = adam
+            optimizer = pm.adam
         self.optimizer = optimizer
         self.wrt = wrt
-        if isinstance(approx, (BaseTrace, MultiTrace)):
-            approx = Histogram(approx, model=model)
         self.approx = approx
         self.loss = approx.apply_replacements(loss, more_replacements=more_replacements)
         updates = self.optimizer(self.loss, self.wrt)
         self.step_function = theano.function([], self.loss, updates=updates)
         self.hist = np.asarray(())
 
+    @change_flags(compute_test_value='off')
     def refresh(self):
         updates = self.optimizer(self.loss, self.wrt)
         self.step_function = theano.function([], self.loss, updates=updates)
@@ -28,11 +38,16 @@ class Optimizer(object):
         progress = tqdm.trange(n)
         scores = np.empty(n)
         scores[:] = np.nan
-        with progress:
+        i = 0
+        try:
             for i in progress:
                 scores[i] = self.step_function()
-                if i % (n//1000) == 0:
+                if i % ((n+1000)//1000) == 0:
                     progress.set_description(
                         'E_q[Loss] = %.4f' % scores[max(0, i - n//50):i+1].mean()
                     )
-        return self.wrt
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.hist = np.concatenate((self.hist, scores[:i]))
+            progress.close()
