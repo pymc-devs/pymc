aff29f656fb6e3771f37360a07d5fc38855a88c1
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-04-06 07:36:24 -0500

    Added shading for kde in energyplot; removed frame around plot by default.

--- pymc3/plots/energyplot.py
+++ pymc3/plots/energyplot.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from .utils import fast_kde
 
-def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, lw=0, alpha=0.5, **kwargs):
+def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, lw=0, alpha=0.5, frame=False, **kwargs):
     """Plot energy transition distribution and marginal energy distribution in order
     to diagnose poor exploration by HMC algorithms.
 
@@ -23,15 +23,18 @@ def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, lw=0, alph
         Line width
     alpha : float
         Alpha value for plot line. Defaults to 0.35.
-
+    frame : bool
+        Flag for plotting frame around figure.
+    
     Returns
     -------
 
     ax : matplotlib axes
     """
     
-    series_dict = {'Energy': trace['energy'] - trace['energy'].mean(),
-                'Energy difference': np.diff(trace['energy'])}
+    energy = trace['energy']
+    series_dict = {'Energy': energy - energy.mean(),
+                'Energy difference': np.diff(energy)}
 
     if figsize is None:
         figsize = (8, 6)
@@ -39,13 +42,14 @@ def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, lw=0, alph
     if ax is None:
         _, ax = plt.subplots(figsize=figsize)
 
-    if kind=='kde':
+    if kind == 'kde':
         for series in series_dict:
             density, l, u = fast_kde(series_dict[series])
             x = np.linspace(l, u, len(density))
-            ax.plot(x, density, alpha=alpha, label=series, **kwargs)
-
-    elif kind=='hist':
+            ax.plot(x, density, label=series, **kwargs)
+            ax.fill_between(x, density, alpha=alpha)
+            
+    elif kind == 'hist':
         for series in series_dict:
             ax.hist(series_dict[series], lw=lw, alpha=alpha, label=series, **kwargs)
             
@@ -54,6 +58,10 @@ def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, lw=0, alph
 
     ax.set_xticks([])
     ax.set_yticks([])
+    
+    if not frame:
+        for spine in ax.spines.values():
+            spine.set_visible(False)
 
     if legend:
         ax.legend()
