1c2e53a4de41803bc0a93642391937751f5faaa9
Christopher Fonnesbeck <fonnesbeck@gmail.com>
2017-04-26 15:52:44 -0500

    Handled indexing error when no match in _update_start_vals

--- pymc3/sampling.py
+++ pymc3/sampling.py
@@ -466,8 +466,9 @@ def _update_start_vals(a, b, model):
     for name in a:
         for tname in b:
             if tname.startswith(name) and tname!=name:
-                transform_func = [d.transformation for d in model.deterministics if d.name==name][0]
-                b[tname] = transform_func.forward(a[name]).eval()
+                transform_func = [d.transformation for d in model.deterministics if d.name==name]
+                if transform_func:
+                    b[tname] = transform_func[0].forward(a[name]).eval()
     
     a.update({k: v for k, v in b.items() if k not in a})
 
