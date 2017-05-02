fd8d49c8b0bdffcad5e672cae06c9f39fa49a857
Bill Engels <w.j.engels@gmail.com>
2017-05-02 14:04:29 -0700

    fixed mean function output shape bug, added tests

--- pymc3/gp/mean.py
+++ pymc3/gp/mean.py
@@ -1,12 +1,61 @@
+import numpy as np
 import theano.tensor as tt
 
-__all__ = ['Zero', 'Constant']
+__all__ = ['Zero', 'Constant', 'Linear']
 
-class Mean(object):
+
+class ParameterizedFunction(object):
+    R"""
+    Base class for all functions.  The input_dim and active_dims parameters
+    allow for combining functions which act on subspaces to be added and
+    multiplied using Python built-in operators like "+" and "*".
+
+    Parameters
+    ----------
+    input_dim : integer
+        The number of input dimensions, or columns of X (or Z) the kernel will operate on.
+    active_dims : A list of booleans whose length equals input_dim.
+	The booleans indicate whether or not the covariance function operates
+        over that dimension or column of X.
+    """
+
+    def __init__(self, input_dim, active_dims=None):
+        self.input_dim = input_dim
+        if active_dims is None:
+            self.active_dims = np.arange(input_dim)
+        else:
+            self.active_dims = np.array(active_dims)
+            if len(active_dims) != input_dim:
+                raise ValueError("Length of active_dims must match input_dim")
+
+    def __call__(self, X):
+        R"""
+        Evaluate the function.
+
+        Parameters
+        ----------
+        X : The inputs to the function.
+        """
+        raise NotImplementedError
+
+    def _slice(self, X):
+        X = X[:, self.active_dims]
+        return X
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+class Mean(ParameterizedFunction):
     """
     Base class for mean functions
     """
-    
+    def __init__(self, input_dim, active_dims=None):
+        ParameterizedFunction.__init__(self, input_dim, active_dims)
+
     def __call__(self, X):
         R"""
         Evaluate the mean function.
@@ -22,34 +71,35 @@ class Mean(object):
 
     def __mul__(self, other):
         return Prod(self, other)
-        
+
 class Zero(Mean):
     def __call__(self, X):
-        return tt.zeros(X.shape, dtype='float32')
-        
+        X = self._slice(X)
+        return tt.zeros(tt.stack([X.shape[0], 1]), dtype="float32")
+
 class Constant(Mean):
     """
     Constant mean function for Gaussian process.
-    
+
     Parameters
     ----------
     c : variable, array or integer
         Constant mean value
     """
 
-    def __init__(self, c=0):
-        Mean.__init__(self)
+    def __init__(self, input_dim, c=0, active_dims=None):
+        Mean.__init__(self, input_dim, active_dims)
         self.c = c
 
     def __call__(self, X):
-        return tt.ones(X.shape) * self.c
+        X = self._slice(X)
+        return tt.ones(tt.stack([X.shape[0], 1])) * self.c
 
 class Linear(Mean):
-    
-    def __init__(self, coeffs, intercept=0):
+    def __init__(self, input_dim, coeffs, intercept=0, active_dims=None):
         """
         Linear mean function for Gaussian process.
-        
+
         Parameters
         ----------
         coeffs : variables
@@ -57,14 +107,15 @@ class Linear(Mean):
         intercept : variable, array or integer
             Intercept for linear function (Defaults to zero)
         """
-        Mean.__init__(self)
+        Mean.__init__(self, input_dim, active_dims)
         self.b = intercept
         self.A = coeffs
-        
-    def __call__(self, X):
-        return tt.dot(X, self.A) + self.b
 
-        
+    def __call__(self, X):
+        X = self._slice(X)
+        m = tt.dot(X, self.A) + self.b
+        return tt.reshape(m, (X.shape[0], 1))
+
 class Add(Mean):
     def __init__(self, first_mean, second_mean):
         Mean.__init__(self)
@@ -83,4 +134,4 @@ class Prod(Mean):
 
     def __call__(self, X):
         return tt.mul(self.m1(X), self.m2(X))
-        
+
--- pymc3/tests/test_gp.py
+++ pymc3/tests/test_gp.py
@@ -11,7 +11,15 @@ class TestZero(object):
     def test_value(self):
         X = np.linspace(0,1,10)[:,None]
         with Model() as model:
-            zero_mean = gp.mean.Zero()
+            zero_mean = gp.mean.Zero(1)
+        M = theano.function([], zero_mean(X))()
+        assert np.all(M==0)
+        assert M.shape == (10,1)
+
+    def test_multid(self):
+        X = np.linspace(0,1,30).reshape(10,3)
+        with Model() as model:
+            zero_mean = gp.mean.Zero(input_dim=3)
         M = theano.function([], zero_mean(X))()
         assert np.all(M==0)
         assert M.shape == (10,1)
@@ -20,7 +28,15 @@ class TestConstant(object):
     def test_value(self):
         X = np.linspace(0,1,10)[:,None]
         with Model() as model:
-            const_mean = gp.mean.Constant(6)
+            const_mean = gp.mean.Constant(input_dim=1, c=6)
+        M = theano.function([], const_mean(X))()
+        assert np.all(M==6)
+        assert M.shape == (10,1)
+
+    def test_multid(self):
+        X = np.linspace(0,1,30).reshape(10,3)
+        with Model() as model:
+            const_mean = gp.mean.Constant(input_dim=3, c=6)
         M = theano.function([], const_mean(X))()
         assert np.all(M==6)
         assert M.shape == (10,1)
@@ -29,9 +45,19 @@ class TestLinearMean(object):
     def test_value(self):
         X = np.linspace(0,1,10)[:,None]
         with Model() as model:
-            linear_mean = gp.mean.Linear(2, 0.5)
+            linear_mean = gp.mean.Linear(input_dim=1, coeffs=2, intercept=0.5)
         M = theano.function([], linear_mean(X))()
         npt.assert_allclose(M[1, 0], 0.7222, atol=1e-3)
+
+    def test_multid(self):
+        X = np.linspace(0,1,30).reshape(10,3)
+        A = np.array([1,2,3])
+        b = 10
+        with Model() as model:
+            linear_mean = gp.mean.Linear(input_dim=3, coeffs=A, intercept=b)
+        M = theano.function([], linear_mean(X))()
+        assert M.shape == (10,1)
+        npt.assert_allclose(M[1, 0], 10.8965, atol=1e-3)
 
 
 class TestCovAdd(object):
@@ -349,16 +375,16 @@ class TestGP(SeededTest):
         with Model() as model:
             # make a Gaussian model
             with pytest.raises(ValueError):
-                random_test = gp.GP('random_test', cov_func=gp.mean.Zero(), observed={'X':X, 'Y':Y})
+                random_test = gp.GP('random_test', cov_func=gp.mean.Zero(1), observed={'X':X, 'Y':Y})
             with pytest.raises(ValueError):
                 random_test = gp.GP('random_test', mean_func=gp.cov.Matern32(1, 1),
                                         cov_func=gp.cov.Matern32(1, 1), observed={'X':X, 'Y':Y})
 
     def test_sample(self):
         X = np.linspace(0,1,100)[:,None]
-        Y = np.random.randn(100,1)
+        Y = np.random.randn(100,1) + 1.0
         with Model() as model:
-            M = gp.mean.Zero()
+            M = gp.mean.Constant(1, 1)
             l = Uniform('l', 0, 5)
             K = gp.cov.Matern32(1, l)
             sigma = Uniform('sigma', 0, 10)
