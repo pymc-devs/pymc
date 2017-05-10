1ac6d8605909a074255b2dcfcfc0752ad9980fe7
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-05-10 15:23:00 +0300

    refactored tests

--- pymc3/tests/test_updates.py
+++ pymc3/tests/test_updates.py
@@ -16,39 +16,22 @@ _a = theano.shared(1.)
 _b = _a*2
 
 _with_params = dict(loss_or_grads=_b, params=[_a])
+_without_first_param = dict(loss_or_grads=None, params=[_a])
+_without_second_param = dict(loss_or_grads=_b, params=None)
 _without_params = dict(loss_or_grads=None, params=None)
 
 
 @pytest.mark.parametrize(
-    ['opt', 'loss_and_params', 'kwargs'],
-    [
-        [sgd, _with_params, dict()],
-        [momentum, _with_params, dict()],
-        [nesterov_momentum, _with_params, dict()],
-        [adagrad, _with_params, dict()],
-        [rmsprop, _with_params, dict()],
-        [adadelta, _with_params, dict()],
-        [adam, _with_params, dict()],
-        [adamax, _with_params, dict()],
-
-        [sgd, _with_params, dict(learning_rate=1e-1)],
-        [momentum, _with_params, dict(learning_rate=1e-1)],
-        [nesterov_momentum, _with_params, dict(learning_rate=1e-1)],
-        [adagrad, _with_params, dict(learning_rate=1e-1)],
-        [rmsprop, _with_params, dict(learning_rate=1e-1)],
-        [adadelta, _with_params, dict(learning_rate=1e-1)],
-        [adam, _with_params, dict(learning_rate=1e-1)],
-        [adamax, _with_params, dict(learning_rate=1e-1)],
-
-        [sgd, _without_params, dict()],
-        [momentum, _without_params, dict()],
-        [nesterov_momentum, _without_params, dict()],
-        [adagrad, _without_params, dict()],
-        [rmsprop, _without_params, dict()],
-        [adadelta, _without_params, dict()],
-        [adam, _without_params, dict()],
-        [adamax, _without_params, dict()],
-    ]
+    'opt',
+    [sgd, momentum, nesterov_momentum, adagrad, rmsprop, adadelta, adam, adamax]
+)
+@pytest.mark.parametrize(
+    'loss_and_params',
+    [_with_params, _without_first_param, _without_second_param, _without_params]
+)
+@pytest.mark.parametrize(
+    'kwargs',
+    [dict(), dict(learning_rate=1e-2)]
 )
 @change_flags(compute_test_value='ignore')
 def test_updates(opt, loss_and_params, kwargs):
