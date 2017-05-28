c3afa00fa625f90570d9835a4e1eeee1d2856585
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2017-05-28 11:23:18 +0300

    Vi summary (#2230)
    * add draft
    
    * add draft
    
    * move and rename
    
    * better docstring
    
    * added tests

--- pymc3/tests/test_variational_inference.py
+++ pymc3/tests/test_variational_inference.py
@@ -395,7 +395,7 @@ def test_fit(method, kwargs, error):
     'ord',
     [1, 2, np.inf]
 )
-def test_callbacks(diff, ord):
+def test_callbacks_convergence(diff, ord):
     cb = pm.variational.callbacks.CheckParametersConvergence(every=1, diff=diff, ord=ord)
 
     class _approx:
@@ -406,3 +406,27 @@ def test_callbacks(diff, ord):
     with pytest.raises(StopIteration):
         cb(approx, None, 1)
         cb(approx, None, 10)
+
+
+def test_tracker_callback():
+    import time
+    tracker = pm.callbacks.Tracker(
+        ints=lambda *t: t[-1],
+        ints2=lambda ap, h, j: j,
+        time=time.time,
+    )
+    for i in range(10):
+        tracker(None, None, i)
+    assert 'time' in tracker.hist
+    assert 'ints' in tracker.hist
+    assert 'ints2' in tracker.hist
+    assert (len(tracker['ints'])
+            == len(tracker['ints2'])
+            == len(tracker['time'])
+            == 10)
+    assert tracker['ints'] == tracker['ints2'] == list(range(10))
+    tracker = pm.callbacks.Tracker(
+        bad=lambda t: t  # bad signature
+    )
+    with pytest.raises(TypeError):
+        tracker(None, None, 1)
