150cedc27ccaf289db986ea0cd1c6c17c115a4f6
Junpeng Lao <junpeng.lao@unifr.ch>
2017-05-31 13:51:28 +0200

    fix issue #2247
    *cast value to float
    * might not be an issue any more after #2233

--- pymc3/examples/lasso_missing.py
+++ pymc3/examples/lasso_missing.py
@@ -16,32 +16,31 @@ test_scores = pd.read_csv(pm.get_data('test_scores.csv')).fillna(-999)
 with pm.Model() as model:
 
     # Impute missing values
-    sib_mean = pm.Exponential('sib_mean', 1)
+    sib_mean = pm.Exponential('sib_mean', 1.)
     siblings_imp = pm.Poisson('siblings_imp', sib_mean,
                               observed=masked_values(siblings, value=-999))
 
-    p_disab = pm.Beta('p_disab', 1, 1)
+    p_disab = pm.Beta('p_disab', 1., 1.)
     disability_imp = pm.Bernoulli(
         'disability_imp', p_disab, observed=masked_values(disability, value=-999))
 
-    p_mother = pm.Beta('p_mother', 1, 1)
+    p_mother = pm.Beta('p_mother', 1., 1.)
     mother_imp = pm.Bernoulli('mother_imp', p_mother,
                               observed=masked_values(mother_hs, value=-999))
 
-    s = pm.HalfCauchy('s', 5, testval=5)
-    beta = pm.Laplace('beta', 0, 100, shape=7, testval=.1)
+    s = pm.HalfCauchy('s', 5., testval=5)
+    beta = pm.Laplace('beta', 0., 100., shape=7, testval=.1)
 
     expected_score = (beta[0] + beta[1] * male + beta[2] * siblings_imp + beta[3] * disability_imp +
                       beta[4] * age + beta[5] * mother_imp + beta[6] * early_ident)
 
     observed_score = pm.Normal(
-        'observed_score', expected_score, s, observed=score)
+        'observed_score', expected_score, tau=s, observed=score)
 
 
 with model:
     start = pm.find_MAP()
     step1 = pm.NUTS([beta, s, p_disab, p_mother, sib_mean], scaling=start)
-
     step2 = pm.Metropolis([mother_imp.missing_values,
                            disability_imp.missing_values,
                            siblings_imp.missing_values])
