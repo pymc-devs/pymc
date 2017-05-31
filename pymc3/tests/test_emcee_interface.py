158e4a3e315d8f57bd721695123e6f475ba4b989
sread <sr15adb@herts.ac.uk>
2017-05-31 14:44:42 +0100

    added TODO

--- pymc3/tests/test_emcee_interface.py
+++ pymc3/tests/test_emcee_interface.py
@@ -1,10 +1,6 @@
 import pymc3 as pm
 import numpy as np
-from pymc3.external.emcee import AffineInvariantEnsemble
-
 import pytest
-
-
 
 def setup_model(yshape):
     yparams = np.linspace(0, 1, yshape)
@@ -14,21 +10,21 @@ def setup_model(yshape):
         y = pm.Normal('y', yparams, yparams, shape=yshape)
     return model
 
-def correct_instatiation(ndraws, nwalkers, yshape):
+def correct_instatiation(ndraws, nparticles, step, yshape):
     with setup_model(yshape) as model:
-        step = AffineInvariantEnsemble(nwalkers=nwalkers)
-        trace = pm.sample(ndraws, step=step)
-    return model, step, trace
+        from pymc3.external.emcee_samplers import sample
+        trace = sample(ndraws, step=step, nparticles=nparticles)
+    return model, trace
 
 def test_correct_instantiation():
     ndraws = 500
-    nwalkers = 10
+    nparticles = 10
     yshape = 3
-    model, step, trace = correct_instatiation(ndraws, nwalkers, yshape)
+    model, trace = correct_instatiation(ndraws, nparticles, 'affine_invariant', yshape)
 
-    assert all(k == i for k, i in zip(trace._straces.keys(), np.arange(0, nwalkers, dtype=int)))
+    assert all(k == i for k, i in zip(trace._straces.keys(), np.arange(0, nparticles, dtype=int)))
     for tr in trace._straces.values():
-        assert tr['x'].shape == (ndraws, nwalkers)
-        assert tr['y'].shape == (ndraws, nwalkers, yshape)
+        assert tr['x'].shape == (ndraws, nparticles)
+        assert tr['y'].shape == (ndraws, nparticles, yshape)
 
 
