2b4608bfdb0cf9aefaf9ac2913459f4864fa4ff8
Alexander Rodin <rodin.alexander@gmail.com>
2017-06-01 17:45:41 +0300

    Add keras and seaborn to test dependencies (required by some examples)

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -50,6 +50,9 @@ pip install pytest pytest-cov nose-parameterized pylint
 # Install untested, non-required code (linter fails without them)
 pip install ipython ipywidgets numdifftools
 
+# Install dependencies for examples
+pip install keras seaborn
+
 if [ -z ${NO_SETUP} ]; then
     python setup.py build_ext --inplace
 fi
