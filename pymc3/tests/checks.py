5f6d1a252133097bdb8e06eab123c7e8996c0ee6
Junpeng Lao <junpeng.lao@unifr.ch>
2017-06-10 13:10:19 +0200

    Fix test error after numpy 1.13
    Fix travis error:
    ```python
    E       TypeError: numpy boolean subtract, the `-` operator, is deprecated, use the bitwise_xor, the `^` operator, or the logical_xor function instead.
    pymc3/tests/checks.py:6: TypeError
    ```

--- pymc3/tests/checks.py
+++ pymc3/tests/checks.py
@@ -2,6 +2,11 @@ import numpy as np
 
 
 def close_to(x, v, bound, name="value"):
-    assert np.all(np.logical_or(
-        np.abs(x - v) < bound,
-        x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
+    if v is True:
+        assert np.all(np.logical_or(
+                np.abs(np.bitwise_xor(x, v)) < bound,
+                x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
+    else:
+        assert np.all(np.logical_or(
+                np.abs(x - v) < bound,
+                x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
