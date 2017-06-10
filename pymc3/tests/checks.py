3a44eb8dd717c65f27a83c758b66a040b84f7760
Junpeng Lao <junpeng.lao@unifr.ch>
2017-06-10 15:32:13 +0200

    refactor
    try to speed up test

--- pymc3/tests/checks.py
+++ pymc3/tests/checks.py
@@ -2,11 +2,12 @@ import numpy as np
 
 
 def close_to(x, v, bound, name="value"):
-    if v is True:
-        assert np.all(np.logical_or(
-                np.abs(np.bitwise_xor(x, v)) < bound,
-                x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
-    else:
-        assert np.all(np.logical_or(
-                np.abs(x - v) < bound,
-                x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
+    assert np.all(np.logical_or(
+            np.abs(x - v) < bound,
+            x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
+
+
+def close_to_logical(x, v, bound, name="value"):    
+    assert np.all(np.logical_or(
+            np.abs(np.bitwise_xor(x, v)) < bound,
+            x == v)), name + " out of bounds : " + repr(x) + ", " + repr(v) + ", " + repr(bound)
