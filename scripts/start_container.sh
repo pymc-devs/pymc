61316c0226f5a318014afaaaf55758a29effda57
AustinRochford <arochford@monetate.com>
2017-06-15 09:54:26 -0400

    Change container script to use setup.py

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,7 +5,7 @@ SRC_DIR=${SRC_DIR:-`pwd`}
 NOTEBOOK_DIR=${NOTEBOOK_DIR:-$SRC_DIR/notebooks}
 TOKEN=$(openssl rand -hex 24)
 
-docker build -t pymc3 $SRC_DIR/scripts
+docker build -t pymc3 -f $SRC_DIR/scripts/Dockerfile .
 docker run -d \
     -p $PORT:8888 \
     -v $SRC_DIR:/home/jovyan/pymc3 \
