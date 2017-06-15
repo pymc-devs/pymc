bcb21386c350d1870f741e509c045ef09e541de1
Bill <w.j.engels@gmail.com>
2017-06-15 01:22:03 -0700

    starting refactor of the two gp classes, fitc done

--- pymc3/gp/gp.py
+++ pymc3/gp/gp.py
@@ -3,59 +3,29 @@ from scipy import stats
 from tqdm import tqdm
 
 from theano.tensor.nlinalg import matrix_inverse
+import theano.tensor.nlinalg
+import theano.tensor.slinalg
 import theano.tensor as tt
 
 from .mean import Zero, Mean
 from .cov import Covariance
 from ..distributions import MvNormal, Continuous, draw_values, generate_samples
 from ..model import modelcontext
-
+from ..distributions.dist_math import Cholesky
 
 __all__ = ['GP', 'sample_gp']
 
-class GP(Continuous):
-    """Gausian process
-    
-    Parameters
-    ----------
-    mean_func : Mean
-        Mean function of Gaussian process
-    cov_func : Covariance
-        Covariance function of Gaussian process
-    X : array
-        Grid of points to evaluate Gaussian process over. Only required if the 
-        GP is not an observed variable.
-    sigma : scalar or array
-        Observation standard deviation (defaults to zero)
-    """
-    def __init__(self, mean_func=None, cov_func=None, X=None, sigma=0, *args, **kwargs):
-        
-        if mean_func is None:
-            self.M = Zero()
+class GPBase(Continuous):
+
+    def random(self, point=None, size=None, X_values=None, obs_noise=False, y=None, **kwargs):
+        if y is None:
+            # draw from prior
+            mean, cov = self._prior(obs_noise)
         else:
-            if not isinstance(mean_func, Mean):
-                raise ValueError('mean_func must be a subclass of Mean')
-            self.M = mean_func
-            
-        if cov_func is None:
-            raise ValueError('A covariance function must be specified for GPP')
-        if not isinstance(cov_func, Covariance):
-            raise ValueError('cov_func must be a subclass of Covariance')
-        self.K = cov_func
-        
-        self.sigma = sigma
-        
-        if X is not None:
-            self.X = X
-            self.mean = self.mode = self.M(X)
-            kwargs.setdefault("shape", X.squeeze().shape)
-            
-        super(GP, self).__init__(*args, **kwargs)
-                
-    def random(self, point=None, size=None, **kwargs):
-        X = self.X
-        mu, cov = draw_values([self.M(X).squeeze(), self.K(X) + np.eye(X.shape[0])*self.sigma**2], point=point)
-        
+            # draw from conditional
+            mean, cov = self._conditional(X_values, y, obs_noise)
+
+        mu, cov = draw_values([mean, cov], point=point)
         def _random(mean, cov, size=None):
             return stats.multivariate_normal.rvs(
                 mean, cov, None if size == mean.shape else size)
@@ -67,6 +37,57 @@ class GP(Continuous):
                                    size=size)
         return samples
 
+class GP(Continuous):
+    """Gausian process
+
+    Parameters
+    ----------
+    X : array
+        Grid of points to evaluate Gaussian process over.
+    mean_func : Mean
+        Mean function of Gaussian process
+    cov_func : Covariance
+        Covariance function of Gaussian process
+    sigma : scalar or array
+        Observation standard deviation (defaults to zero)
+    """
+    def __init__(self, X, mean_func=None, cov_func=None, sigma2=None, *args, **kwargs):
+
+        if mean_func is None:
+            self.M = Zero()
+        else:
+            if not isinstance(mean_func, Mean):
+                raise ValueError('mean_func must be a subclass of Mean')
+            self.M = mean_func
+
+        if cov_func is None:
+            raise ValueError('A covariance function must be specified for GPP')
+        if not isinstance(cov_func, Covariance):
+            raise ValueError('cov_func must be a subclass of Covariance')
+
+        self.K = cov_func
+        self.sigma2 = sigma2
+        self.cholesky = Cholesky(nofail=True, lower=True)
+        self.solve_lower = tt.slinalg.Solve(A_structure="lower_triangular")
+        self.solve_upper = tt.slinalg.Solve(A_structure="upper_triangular")
+
+        self.X = X
+        self.nx = self.X.shape[0]
+
+        self.mean = self.mode = self.M(X)
+        kwargs.setdefault("shape", X.squeeze().shape)
+        super(GP, self).__init__(*args, **kwargs)
+
+    def _prior(self, obs_noise=False):
+        if obs_noise:
+            cov = self.K(self.X) + self.sigma2 * tt.eye(self.nx)
+        else:
+            cov = self.K(self.X) + 1e-6 * tt.eye(self.nx)
+        return mean, cov
+
+    def _conditional(self, Z, y, obs_noise=False):
+        return 10
+
     def logp(self, Y, X=None):
         if X is None:
             X = self.X
@@ -74,9 +95,93 @@ class GP(Continuous):
         Sigma = self.K(X) + tt.eye(X.shape[0])*self.sigma**2
 
         return MvNormal.dist(mu, Sigma).logp(Y)
-        
 
-def sample_gp(trace, gp, X_values, samples=None, obs_noise=True, model=None, random_seed=None, progressbar=True):
+
+class GPfitc(GPBase):
+    """Gausian process, fitc
+    """
+    def __init__(self, X, mean_func=None, cov_func=None, inducing_points=None, sigma2=None, *args, **kwargs):
+
+        if mean_func is None:
+            self.M = Zero()
+        else:
+            if not isinstance(mean_func, Mean):
+                raise ValueError('mean_func must be a subclass of Mean')
+            self.M = mean_func
+
+        if cov_func is None:
+            raise ValueError('A covariance function must be specified for GPP')
+        if not isinstance(cov_func, Covariance):
+            raise ValueError('cov_func must be a subclass of Covariance')
+
+        self.K = cov_func
+        self.sigma2 = sigma2
+        self.cholesky = Cholesky(nofail=True, lower=True)
+        self.solve_lower = tt.slinalg.Solve(A_structure="lower_triangular")
+        self.solve_upper = tt.slinalg.Solve(A_structure="upper_triangular")
+
+        self.X = X
+        self.nx = self.X.shape[0]
+
+        self.mean = self.mode = self.M(X)
+        kwargs.setdefault("shape", X.squeeze().shape)
+
+        self.Xu = inducing_points
+        self.nu = self.Xu.shape[0]
+        super(GPfitc, self).__init__(*args, **kwargs)
+
+    def _prior(self, obs_noise=False):
+        Kuu = self.K(self.Xu, self.Xu) + 1e-6 * tt.eye(self.nu)
+        Kux = self.K(self.Xu, self.X)
+        Kdiag = tt.diag(self.K(self.X, self.X))  # need Kdiag methods for cov functions
+        Luu = self.cholesky(Kuu)
+        V = self.solve_lower(Luu, Kux)
+        Qff = tt.dot(tt.transpose(V), V)
+        mean = self.M(self.X).squeeze()
+
+        if obs_noise:
+            cov = Qff - (tt.diag(Qff) - Kdiag) + self.sigma2 * tt.eye(self.nx)
+        else:
+            cov = Qff - (tt.diag(Qff) - Kdiag) + 1e-6 * tt.eye(self.nx)
+        return mean, cov
+
+    def _common(self, y):
+        Kuu = self.K(self.Xu, self.Xu) + 1e-6 * tt.eye(self.nu)
+        Kux = self.K(self.Xu, self.X)
+        Kdiag = tt.diag(self.K(self.X, self.X))  # need Kdiag methods for cov functions, here is probably why we are slower
+        Luu = self.cholesky(Kuu)
+        V = self.solve_lower(Luu, Kux)
+        g = tt.clip(Kdiag - tt.sum(V * V, 0), 0.0, np.inf) + self.sigma2
+        Lu = self.cholesky(tt.eye(self.nu) + tt.dot(V / g, tt.transpose(V)))
+
+        r = y - self.M(self.X).squeeze()
+        beta = r / g
+        alpha = tt.dot(V, beta)
+        G = self.solve_lower(Lu, alpha)
+        return Luu, Lu, g, G, r
+
+    def _conditional(self, Z, y, obs_noise=False):
+        Luu, Lu, g, G, r = self._common(y)
+
+        Kuz = self.K(self.Xu, Z)
+        W = self.solve_lower(Luu, Kuz)
+        mean = tt.dot(tt.transpose(W), self.solve_upper(tt.transpose(Lu), G))
+
+        A = self.solve_lower(Lu, W)
+        if obs_noise:
+            cov = self.K(Z, Z) - tt.dot(tt.transpose(W), W) + tt.dot(tt.transpose(A), A) + self.sigma2*tt.eye(Z.shape[0])
+        else:
+            cov = self.K(Z, Z) - tt.dot(tt.transpose(W), W) + tt.dot(tt.transpose(A), A)
+        return mean, cov
+
+    def logp(self, y):
+        Luu, Lu, g, G, r = self._common(y)
+        const = -0.5 * self.nu * self.nx * tt.log(2.0 * np.pi)
+        logdet = -0.5 * tt.sum(tt.log(g)) - tt.sum(tt.log(tt.diag(Lu)))
+        return -0.5 * tt.sum(tt.square(r) / g) + 0.5 * tt.sum(tt.square(G)) + logdet + const
+
+
+def sample_gp(trace, gp, X_values=None, samples=None, obs_noise=False, model=None, random_seed=None, progressbar=True):
     """Generate samples from a posterior Gaussian process.
 
     Parameters
@@ -86,58 +191,37 @@ def sample_gp(trace, gp, X_values, samples=None, obs_noise=True, model=None, ran
     gp : Gaussian process object
         The GP variable to sample from.
     X_values : array
-        Grid of values at which to sample GP.
+        Grid of new values at which to sample GP.  If `None`, returns
+        samples from the prior.
     samples : int
         Number of posterior predictive samples to generate. Defaults to the
         length of `trace`
     obs_noise : bool
-        Flag for including observation noise in sample. Defaults to True.
-    model : Model 
+        Flag for including observation noise in sample. Defaults to False.
+    model : Model
         Model used to generate `trace`. Optional if in `with` context manager.
     random_seed : integer > 0
         Random number seed for sampling.
     progressbar : bool
         Flag for showing progress bar.
-    
+
     Returns
     -------
     Array of samples from posterior GP evaluated at Z.
     """
     model = modelcontext(model)
-    
+
     if samples is None:
         samples = len(trace)
-    
+
     if random_seed:
         np.random.seed(random_seed)
-    
+
     if progressbar:
         indices = tqdm(np.random.randint(0, len(trace), samples), total=samples)
     else:
         indices = np.random.randint(0, len(trace), samples)
 
-    K = gp.distribution.K 
-        
-    data = [v for v in model.observed_RVs if v.name==gp.name][0].data
-
-    X = data['X']
-    Y = data['Y']
-    Z = X_values
-    
-    S_xz = K(X, Z)
-    S_zz = K(Z)
-    if obs_noise:
-        S_inv = matrix_inverse(K(X) + tt.eye(X.shape[0])*gp.distribution.sigma**2)
-    else:
-        S_inv = matrix_inverse(K(X))
-
-    # Posterior mean
-    m_post = tt.dot(tt.dot(S_xz.T, S_inv), Y)
-    # Posterior covariance
-    S_post = S_zz - tt.dot(tt.dot(S_xz.T, S_inv), S_xz)
-
-    gp_post = MvNormal.dist(m_post, S_post, shape=Z.shape[0])
-    
-    samples = [gp_post.random(point=trace[idx]) for idx in indices]
-    
+    y = [v for v in model.observed_RVs if v.name==gp.name][0]
+    samples = [gp.distribution.random(point=trace[idx], X_values=X_values, y=y, obs_noise=obs_noise) for idx in indices]
     return np.array(samples)
