pymc-devs/pymc

commit 6f58dbfdeeb21d682fecf435919906f0d3289ce2

Return identity matrix if no scaling provided (#2236)

pymc3/step_methods/hmc/base_hmc.py
@@ -4,7 +4,8 @@ from .trajectory import get_theano_hamiltonian_functions
 from pymc3.tuning import guess_scaling
 from pymc3.model import modelcontext, Point
 from .quadpotential import quad_potential
-from pymc3.theanof import inputvars, make_shared_replacements
+from pymc3.theanof import inputvars, make_shared_replacements, floatX
+import numpy as np
 
 
 class BaseHMC(ArrayStepShared):
@@ -41,7 +42,7 @@ class BaseHMC(ArrayStepShared):
         vars = inputvars(vars)
 
         if scaling is None and potential is None:
-            scaling = model.test_point
+            scaling = floatX(np.ones(model.dict_to_array(model.test_point).size))
 
         if isinstance(scaling, dict):
             scaling = guess_scaling(Point(scaling, model=model), model=model, vars=vars)

commit 8948a92f1f1f0cc0f34a76ac907b16826fad7f7c

Fixed error introduced by 6f58dbf in which the scaling variable is initialized as an array instead of a dict. This commit maintains the intentions of the 6f58dbf commit by initializing 'scaling' to a dict of arrays of identity with type floatX.

pymc3/step_methods/hmc/base_hmc.py
@@ -42,7 +42,7 @@ class BaseHMC(ArrayStepShared):
         vars = inputvars(vars)
 
         if scaling is None and potential is None:
-            scaling = floatX(np.ones(model.dict_to_array(model.test_point).size))
+            scaling = {k: floatX(np.ones(v.shape)) for k, v in model.test_point.items()}
 
         if isinstance(scaling, dict):
             scaling = guess_scaling(Point(scaling, model=model), model=model, vars=vars)
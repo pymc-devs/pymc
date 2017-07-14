Fixed error introduced by 6f58dbf in which the scaling variable is initialized as an array instead of a dict. This commit maintains the intentions of the 6f58dbf commit by initializing 'scaling' to a dict of arrays of identity with type floatX.

pymc3/step_methods/hmc/base_hmc.py
@@ -42,7 +42,7 @@ class BaseHMC(ArrayStepShared):
         vars = inputvars(vars)
 
         if scaling is None and potential is None:
-            scaling = floatX(np.ones(model.dict_to_array(model.test_point).size))
+            scaling = {k: floatX(np.ones(v.shape)) for k, v in model.test_point.items()}
 
         if isinstance(scaling, dict):
             scaling = guess_scaling(Point(scaling, model=model), model=model, vars=vars)