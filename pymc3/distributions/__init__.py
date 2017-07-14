d7a1bc1499f5bfac8b36e929034fb1b2f9085c73
Ed Herbst <ed.herbst@gmail.com>
2017-07-14 15:02:24 -0400

    add AR model

--- pymc3/distributions/__init__.py
+++ pymc3/distributions/__init__.py
@@ -65,6 +65,7 @@ from .multivariate import LKJCholeskyCov
 from .multivariate import LKJCorr
 
 from .timeseries import AR1
+from .timeseries import AR
 from .timeseries import GaussianRandomWalk
 from .timeseries import GARCH11
 from .timeseries import MvGaussianRandomWalk
@@ -126,6 +127,7 @@ __all__ = ['Uniform',
            'LKJCholeskyCov',
            'LKJCorr',
            'AR1',
+           'AR',
            'GaussianRandomWalk',
            'MvGaussianRandomWalk',
            'MvStudentTRandomWalk',
