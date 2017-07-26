009899cb4529163db9fcf29666fc4516fb956e6d
Bill Engels <w.j.engels@gmail.com>
2017-07-25 21:29:27 -0500

    small fixes to mean.py

--- pymc3/gp/mean.py
+++ pymc3/gp/mean.py
@@ -1,11 +1,13 @@
 import theano.tensor as tt
 
-__all__ = ['Zero', 'Constant']
+__all__ = ['Zero', 'Constant', 'Linear']
+
 
 class Mean(object):
     R"""
     Base class for mean functions
     """
+
     def __call__(self, X):
         R"""
         Evaluate the mean function.
@@ -22,13 +24,16 @@ class Mean(object):
     def __mul__(self, other):
         return Prod(self, other)
 
+
 class Zero(Mean):
     R"""
     Zero mean function for Gaussian process.
 
     """
+
     def __call__(self, X):
         return tt.zeros(tt.stack([X.shape[0], ]), dtype='float32')
+
 
 class Constant(Mean):
     R"""
@@ -39,6 +44,7 @@ class Constant(Mean):
     c : variable, array or integer
         Constant mean value
     """
+
     def __init__(self, c=0):
         Mean.__init__(self)
         self.c = c
@@ -58,6 +64,7 @@ class Linear(Mean):
     intercept : variable, array or integer
         Intercept for linear function (Defaults to zero)
     """
+
     def __init__(self, coeffs, intercept=0):
         Mean.__init__(self)
         self.b = intercept
