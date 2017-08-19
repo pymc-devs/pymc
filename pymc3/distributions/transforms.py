Converted eps value to floatX fixes test failures of #2515 and lowered precision requirements for test_transforms on float32 machines (#2517)

pymc3/distributions/transforms.py
@@ -250,7 +250,7 @@ class StickBreaking(Transform):
 
     name = "stickbreaking"
 
-    def __init__(self, eps=np.finfo(theano.config.floatX).eps):
+    def __init__(self, eps=floatX(np.finfo(theano.config.floatX).eps)):
         self.eps = eps
 
     def forward(self, x_):
@@ -263,7 +263,7 @@ class StickBreaking(Transform):
         k = tt.arange(Km1)[(slice(None), ) + (None, ) * (x.ndim - 1)]
         eq_share = logit(1. / (Km1 + 1 - k).astype(str(x_.dtype)))
         y = logit(z) - eq_share
-        return y.T
+        return floatX(y.T)
 
     def forward_val(self, x, point=None):
         return self.forward(x)
@@ -278,7 +278,7 @@ class StickBreaking(Transform):
         yu = tt.concatenate([tt.ones(y[:1].shape), 1 - z])
         S = tt.extra_ops.cumprod(yu, 0)
         x = S * yl
-        return x.T
+        return floatX(x.T)
 
     def jacobian_det(self, y_):
         y = y_.T