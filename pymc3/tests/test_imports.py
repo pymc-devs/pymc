33e2b642d959e272aa52c6aa8495672d6cadc832
Colin Carroll <colcarroll@gmail.com>
2017-09-04 14:15:48 -0400

    Fix test to work

--- pymc3/tests/test_imports.py
+++ pymc3/tests/test_imports.py
@@ -1,8 +1,18 @@
 # pylint: skip-file
 import sys
 
-from pymc3 import *  # noqa
-
 
 def test_matplotlib_not_imported():
-    assert 'matplotlib' not in sys.modules.keys()
+    if 'matplotlib' in sys.modules:
+        del sys.modules['matplotlib']
+
+    # make sure matplotlib isn't already imported
+    assert 'matplotlib' not in sys.modules
+
+    # make sure matplotlib does not get imported from anything in the top level
+    import pymc3 as pm  # noqa
+    assert 'matplotlib' not in sys.modules
+
+    # show that it is possible for the test to fail
+    import matplotlib  # noqa
+    assert 'matplotlib' in sys.modules
