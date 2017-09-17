a94a6036bccfa9badf54869efa9ac64fe6f37f55
tsdlovell <dlovell@twosigma.com>
2017-09-17 04:11:24 -0400

    bugfix: implement sum-preserving mode for Multinomial (#2471) (#2540)
    * bugfix: implement sum-preserving mode for Multinomial (#2471)
    
    * bugfix: handle ndim > 1 case
    
    * TST: uncomment new passing test

--- pymc3/distributions/multivariate.py
+++ pymc3/distributions/multivariate.py
@@ -13,7 +13,6 @@ from theano.tensor.nlinalg import det, matrix_inverse, trace
 import theano.tensor.slinalg
 import pymc3 as pm
 
-from pymc3.math import tround
 from pymc3.theanof import floatX
 from . import transforms
 from pymc3.util import get_variable_name
@@ -486,21 +485,29 @@ class Multinomial(Discrete):
 
         p = p / tt.sum(p, axis=-1, keepdims=True)
 
-        if len(self.shape) == 2:
+        lst = range(self.shape[-1])
+        if len(self.shape) > 1:
+            m = self.shape[-2]
             try:
-                assert n.shape == (self.shape[0],)
+                assert n.shape == (m,)
             except AttributeError:
-                # this occurs when n is a scalar Python int or float
-                n *= tt.ones(self.shape[0])
-
+                n *= tt.ones(m)
             self.n = tt.shape_padright(n)
-            self.p = p if p.ndim == 2 else tt.shape_padleft(p)
+            self.p = p if p.ndim > 1 else tt.shape_padleft(p)
+            lst = list(lst for _ in range(m))
         else:
+            # n is a scalar, p is a 1d array
             self.n = tt.as_tensor_variable(n)
             self.p = tt.as_tensor_variable(p)
 
         self.mean = self.n * self.p
-        self.mode = tt.cast(tround(self.mean), 'int32')
+        mode = tt.cast(tt.round(self.mean), 'int32')
+        diff = self.n - tt.sum(mode, axis=-1, keepdims=True)
+        inc_bool_arr = tt.as_tensor_variable(lst) < diff
+        mode = tt.inc_subtensor(mode[inc_bool_arr.nonzero()], 1)
+        dec_bool_arr = tt.as_tensor_variable(lst) < -diff
+        mode = tt.inc_subtensor(mode[dec_bool_arr.nonzero()], -1)
+        self.mode = mode
 
     def _random(self, n, p, size=None):
         original_dtype = p.dtype
@@ -514,7 +521,10 @@ class Multinomial(Discrete):
             randnum = np.random.multinomial(n, p.squeeze(), size=size)
         elif p.ndim == 2:
             p = p / p.sum(axis=1, keepdims=True)
-            randnum = np.asarray([np.random.multinomial(n, pp, size=size) for pp in p])
+            randnum = np.asarray([
+                np.random.multinomial(nn, pp, size=size)
+                for (nn, pp) in zip(n, p)
+            ])
         else:
             raise ValueError('Outcome probabilities must be 1- or 2-dimensional '
                              '(supplied `p` has {} dimensions)'.format(p.ndim))
