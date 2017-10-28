23412dab50f36b7e92b51860dee8fd5ad7fb826e
Osvaldo Martin <aloctavodia@gmail.com>
2017-10-28 10:39:02 -0300

    remove ks test for sample_ppcw (#2668)
    * improve sample_ppcw_test
    
    * remove ks test

--- pymc3/tests/test_sampling.py
+++ pymc3/tests/test_sampling.py
@@ -254,30 +254,27 @@ class TestSamplePPC(SeededTest):
 
 class TestSamplePPCW(SeededTest):
     def test_sample_ppc_w(self):
-        data = np.random.normal(0, 1, size=200)
+        data0 = np.random.normal(0, 1, size=500)
+
         with pm.Model() as model_0:
             mu = pm.Normal('mu', mu=0, sd=1)
-            y = pm.Normal('y', mu=mu, sd=1, observed=data)
+            y = pm.Normal('y', mu=mu, sd=1, observed=data0)
             trace_0 = pm.sample()
 
         with pm.Model() as model_1:
-            mu = pm.Normal('mu', mu=0, sd=1, shape=len(data))
-            y = pm.Normal('y', mu=mu, sd=1, observed=data)
+            mu = pm.Normal('mu', mu=0, sd=1, shape=len(data0))
+            y = pm.Normal('y', mu=mu, sd=1, observed=data0)
             trace_1 = pm.sample()
-
 
         traces = [trace_0, trace_0]
         models = [model_0, model_0]
         ppc = pm.sample_ppc_w(traces, 1000, models)
-
         assert ppc['y'].shape == (1000,)
-        _, pval = stats.kstest(ppc['y'], stats.norm().cdf)
-        assert pval > 0.001
 
         traces = [trace_0, trace_1]
         models = [model_0, model_1]
-        ppc = pm.sample_ppc_w(traces, 50, models)
-        assert ppc['y'].shape == (50, 200)
+        ppc = pm.sample_ppc_w(traces, 100, models)
+        assert ppc['y'].shape == (100, 500)
 
 
 @pytest.mark.parametrize('method', [
