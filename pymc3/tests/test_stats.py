e8e7363f4d93973a1df4d65beb799c8f7a419605
Junpeng Lao <junpeng.lao@unifr.ch>
2017-11-01 15:59:14 +0100

    Bugfix #2681 (#2683)
    * bug fix #2681
    
     fix `pm.summary` error on a vector-valued parameter in effective sample size and Rhat
    
    * fix old test
    
    new text to be added
    
    * Fix test, remove _Summary etc
    
    remove _Summary, _StatSummary, and _PosteriorQuantileSummary (leftovers from #2659)
    
    * Add test
    
    * fix test in test_example.py

--- pymc3/tests/test_stats.py
+++ pymc3/tests/test_stats.py
@@ -203,7 +203,7 @@ class TestStats(SeededTest):
             Normal('x', mu, tau, testval=floatX_array(.1))
             step = Metropolis(model.vars, np.diag([1.]), blocked=True)
             trace = pm.sample(100, step=step)
-        pm.summary(trace)
+        summary(trace)
 
     def test_summary_1d_variable_model(self):
         mu = -2.1
@@ -212,7 +212,7 @@ class TestStats(SeededTest):
             Normal('x', mu, tau, shape=2, testval=floatX_array([.1, .1]))
             step = Metropolis(model.vars, np.diag([1.]), blocked=True)
             trace = pm.sample(100, step=step)
-        pm.summary(trace)
+        summary(trace)
 
     def test_summary_2d_variable_model(self):
         mu = -2.1
@@ -222,26 +222,7 @@ class TestStats(SeededTest):
                    testval=floatX_array(np.tile(.1, (2, 2))))
             step = Metropolis(model.vars, np.diag([1.]), blocked=True)
             trace = pm.sample(100, step=step)
-        pm.summary(trace)
-
-    def test_summary_format_values(self):
-        roundto = 2
-        summ = pm.stats._Summary(roundto)
-        d = {'nodec': 1, 'onedec': 1.0, 'twodec': 1.00, 'threedec': 1.000}
-        summ._format_values(d)
-        for val in d.values():
-            assert val == '1.00'
-
-    def test_stat_summary_format_hpd_values(self):
-        roundto = 2
-        summ = pm.stats._StatSummary(roundto, None, 0.05)
-        d = {'nodec': 1, 'hpd': [1, 1]}
-        summ._format_values(d)
-        for key, val in d.items():
-            if key == 'hpd':
-                assert val == '[1.00, 1.00]'
-            else:
-                assert val == '1.00'
+        summary(trace)
 
     def test_calculate_stats_0d_variable(self):
         sample = np.arange(10)
@@ -261,124 +242,6 @@ class TestStats(SeededTest):
         result = list(pm.stats._calculate_posterior_quantiles(sample, qlist))
         assert result[0] == ()
         assert len(result) == 2
-
-    def test_stats_value_line(self):
-        roundto = 1
-        summ = pm.stats._StatSummary(roundto, None, 0.05)
-        values = [{'mean': 0, 'sd': 1, 'mce': 2, 'hpd': [4, 4]},
-                  {'mean': 5, 'sd': 6, 'mce': 7, 'hpd': [8, 8]}, ]
-
-        expected = ['0.0              1.0              2.0              [4.0, 4.0]',
-                    '5.0              6.0              7.0              [8.0, 8.0]']
-        result = list(summ._create_value_output(values))
-        assert result == expected
-
-    def test_post_quantile_value_line(self):
-        roundto = 1
-        summ = pm.stats._PosteriorQuantileSummary(roundto, 0.05)
-        values = [{'lo': 0, 'q25': 1, 'q50': 2, 'q75': 4, 'hi': 5},
-                  {'lo': 6, 'q25': 7, 'q50': 8, 'q75': 9, 'hi': 10}, ]
-
-        expected = ['0.0            1.0            2.0            4.0            5.0',
-                    '6.0            7.0            8.0            9.0            10.0']
-        result = list(summ._create_value_output(values))
-        assert result == expected
-
-    def test_stats_output_lines_0d_variable(self):
-        roundto = 1
-        x = np.arange(5)
-        summ = pm.stats._StatSummary(roundto, 5, 0.05)
-        expected = ['  Mean             SD               MC Error         95% HPD interval',
-                    '  -------------------------------------------------------------------',
-                    '  ',
-                    '  2.0              1.4              0.6              [0.0, 4.0]', ]
-
-        result = list(summ._get_lines(x))
-        assert result == expected
-
-    def test_stats_output_lines_1d_variable(self):
-        roundto = 1
-        x = np.arange(10).reshape(5, 2)
-        summ = pm.stats._StatSummary(roundto, 5, 0.05)
-        expected = ['  Mean             SD               MC Error         95% HPD interval',
-                    '  -------------------------------------------------------------------',
-                    '  ',
-                    '  4.0              2.8              1.3              [0.0, 8.0]',
-                    '  5.0              2.8              1.3              [1.0, 9.0]', ]
-        result = list(summ._get_lines(x))
-        assert result == expected
-
-    def test_stats_output_lines_2d_variable(self):
-        roundto = 1
-        x = np.arange(20).reshape(5, 2, 2)
-        summ = pm.stats._StatSummary(roundto, 5, 0.05)
-        expected = ['  Mean             SD               MC Error         95% HPD interval',
-                    '  -------------------------------------------------------------------',
-                    '  ..............................[0, :]...............................',
-                    '  8.0              5.7              2.5              [0.0, 16.0]',
-                    '  9.0              5.7              2.5              [1.0, 17.0]',
-                    '  ..............................[1, :]...............................',
-                    '  10.0             5.7              2.5              [2.0, 18.0]',
-                    '  11.0             5.7              2.5              [3.0, 19.0]', ]
-        result = list(summ._get_lines(x))
-        assert result == expected
-
-    def test_stats_output_HPD_interval_format(self):
-        roundto = 1
-        x = np.arange(5)
-        summ = pm.stats._StatSummary(roundto, 5, 0.05)
-        expected = '  Mean             SD               MC Error         95% HPD interval'
-        result = list(summ._get_lines(x))
-        assert result[0] == expected
-
-        summ = pm.stats._StatSummary(roundto, 5, 0.001)
-        expected = '  Mean             SD               MC Error         99.9% HPD interval'
-        result = list(summ._get_lines(x))
-        assert result[0] == expected
-
-    def test_posterior_quantiles_output_lines_0d_variable(self):
-        roundto = 1
-        x = np.arange(5)
-        summ = pm.stats._PosteriorQuantileSummary(roundto, 0.05)
-        expected = ['  Posterior quantiles:',
-                    '  2.5            25             50             75             97.5',
-                    '  |--------------|==============|==============|--------------|',
-                    '  ',
-                    '  0.0            1.0            2.0            3.0            4.0', ]
-
-        result = list(summ._get_lines(x))
-        assert result == expected
-
-    def test_posterior_quantiles_output_lines_1d_variable(self):
-        roundto = 1
-        x = np.arange(10).reshape(5, 2)
-        summ = pm.stats._PosteriorQuantileSummary(roundto, 0.05)
-        expected = ['  Posterior quantiles:',
-                    '  2.5            25             50             75             97.5',
-                    '  |--------------|==============|==============|--------------|',
-                    '  ',
-                    '  0.0            2.0            4.0            6.0            8.0',
-                    '  1.0            3.0            5.0            7.0            9.0']
-
-        result = list(summ._get_lines(x))
-        assert result == expected
-
-    def test_posterior_quantiles_output_lines_2d_variable(self):
-        roundto = 1
-        x = np.arange(20).reshape(5, 2, 2)
-        summ = pm.stats._PosteriorQuantileSummary(roundto, 0.05)
-        expected = ['  Posterior quantiles:',
-                    '  2.5            25             50             75             97.5',
-                    '  |--------------|==============|==============|--------------|',
-                    '  .............................[0, :].............................',
-                    '  0.0            4.0            8.0            12.0           16.0',
-                    '  1.0            5.0            9.0            13.0           17.0',
-                    '  .............................[1, :].............................',
-                    '  2.0            6.0            10.0           14.0           18.0',
-                    '  3.0            7.0            11.0           15.0           19.0', ]
-
-        result = list(summ._get_lines(x))
-        assert result == expected
 
     def test_groupby_leading_idxs_0d_variable(self):
         result = {k: list(v) for k, v in pm.stats._groupby_leading_idxs(())}
@@ -447,8 +310,8 @@ class TestDfSummary(bf.ModelBackendSampledTestCase):
         ds = summary(self.mtrace, batches=3,
                         stat_funcs=[customf], extend=True)
         npt.assert_equal(np.array(['mean', 'sd', 'mc_error',
-                                   'hpd_2.5', 'hpd_97.5',
-                                   'n_eff', 'Rhat', 'my_mean']),
+                                   'hpd_2.5', 'hpd_97.5', 'my_mean',
+                                   'n_eff', 'Rhat']),
                          ds.columns)
 
     def test_value_alignment(self):
@@ -464,10 +327,37 @@ class TestDfSummary(bf.ModelBackendSampledTestCase):
                 npt.assert_equal(val, ds.loc[vidx, 'mean'])
 
     def test_row_names(self):
-        with Model() as model:
+        with Model():
             pm.Uniform('x', 0, 1)
             step = Metropolis()
             trace = pm.sample(100, step=step)
         ds = summary(trace, batches=3, include_transformed=True)
         npt.assert_equal(np.array(['x_interval__', 'x']),
                          ds.index)
+
+    def test_value_n_eff_rhat(self):
+        mu = -2.1
+        tau = 1.3
+        with Model():
+            Normal('x0', mu, tau, testval=floatX_array(.1)) # 0d
+            Normal('x1', mu, tau, shape=2, testval=floatX_array([.1, .1]))# 1d
+            Normal('x2', mu, tau, shape=(2, 2),
+                   testval=floatX_array(np.tile(.1, (2, 2))))# 2d
+            Normal('x3', mu, tau, shape=(2, 2, 3),
+                   testval=floatX_array(np.tile(.1, (2, 2, 3))))# 3d
+            trace = pm.sample(100, step=pm.Metropolis())
+        for varname in trace.varnames:
+            # test effective_n value
+            n_eff = pm.effective_n(trace, varnames=[varname])[varname]
+            n_eff_df = np.asarray(
+                    pm.summary(trace, varnames=[varname])['n_eff']
+                                 ).reshape(n_eff.shape)
+            npt.assert_equal(n_eff, n_eff_df)
+            
+            # test Rhat value
+            rhat = pm.gelman_rubin(trace, varnames=[varname])[varname]
+            rhat_df = np.asarray(
+                    pm.summary(trace, varnames=[varname])['Rhat']
+                                 ).reshape(rhat.shape)
+            npt.assert_equal(rhat, rhat_df)
+
