a1976a89f3cb9035203a991f7a5302c16e582c58
Junpeng Lao <junpeng.lao@unifr.ch>
2017-11-21 16:36:51 +0100

    Add test for pm.Model (#2724)
    * Add test for pm.Model
    
    This PR add test for the following functions within pm.Model:
    * model.bijection
    * model.logp_array
    I also change the output from simple_model() of the testset, so it outputs sd (same as we define in pm.Normal)
    
    * PEP8, add release note

--- pymc3/tests/test_model_func.py
+++ pymc3/tests/test_model_func.py
@@ -1,20 +1,23 @@
 import pymc3 as pm
 import numpy as np
+import scipy.stats as sp
 from .checks import close_to
 from .models import simple_model, mv_simple
-from ..distributions import Normal
 
 
-def test_lop():
-    start, model, _ = simple_model()
+tol = 2.0**-11
+
+def test_logp():
+    start, model, (mu, sig) = simple_model()
     lp = model.fastlogp
     lp(start)
+    close_to(lp(start), sp.norm.logpdf(start['x'], mu, sig).sum(), tol)
 
 
 def test_dlogp():
     start, model, (mu, sig) = simple_model()
     dlogp = model.fastdlogp()
-    close_to(dlogp(start), -(start['x'] - mu) / sig, 1. / sig / 100.)
+    close_to(dlogp(start), -(start['x'] - mu) / sig**2, 1. / sig**2 / 100.)
 
 
 def test_dlogp2():
@@ -26,8 +29,27 @@ def test_dlogp2():
 
 def test_deterministic():
     with pm.Model() as model:
-        x = Normal('x', 0, 1)
+        x = pm.Normal('x', 0, 1)
         y = pm.Deterministic('y', x**2)
 
     assert model.y == y
     assert model['y'] == y
+
+
+def test_mapping():
+    with pm.Model() as model:
+        mu = pm.Normal('mu', 0, 1)
+        sd = pm.Gamma('sd', 1, 1)
+        y = pm.Normal('y', mu, sd, observed=np.array([.1, .5]))
+    lp = model.fastlogp
+    lparray = model.logp_array
+    point = model.test_point
+    parray = model.bijection.map(point)
+    assert lp(point) == lparray(parray)
+
+    randarray = np.random.randn(*parray.shape)
+    randpoint = model.bijection.rmap(randarray)
+    assert lp(randpoint) == lparray(randarray)
+
+
+
