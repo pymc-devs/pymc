2841442270fe83e6ad8d88ab41dae8a9b99cd5d0
Michael Osthege <zufallsprinzip@hotmail.de>
2017-11-23 15:45:28 +0100

    measuring the runtime, example with both 2D z-variable and two 1D x,y variables

--- pymc3/examples/samplers_mvnormal.py
+++ pymc3/examples/samplers_mvnormal.py
@@ -5,38 +5,55 @@ Comparing different samplers on a correlated bivariate normal distribution.
 
 
 import numpy as np
+import time
 import pandas as pd
 import pymc3 as pm
 import theano.tensor as tt
 import matplotlib.pyplot as plt
 
 
-
+USE_XY = True
 
 def run(steppers, p):
     steppers = set(steppers)
     traces = {}
     effn = {}
+    runtimes = {}
 
     with pm.Model() as model:
-        mu = np.array([0.,0.])
-        cov = np.array([[1.,p],[p,1.]])
-        z = pm.MvNormal('z', mu=mu, cov=cov, shape=(2,))
+        if USE_XY:
+            x = pm.Flat('x')
+            y = pm.Flat('y')
+            mu = np.array([0.,0.])
+            cov = np.array([[1.,p],[p,1.]])
+            z = pm.MvNormal.dist(mu=mu, cov=cov, shape=(2,)).logp(tt.stack([x,y]))
+            p = pm.Potential('logp_xy', z)
+            start = {'x': 0, 'y': 0}
+        else:
+            mu = np.array([0.,0.])
+            cov = np.array([[1.,p],[p,1.]])
+            z = pm.MvNormal('z', mu=mu, cov=cov, shape=(2,))
+            start={'z': [0, 0]}
 
         for step_cls in steppers:
             name = step_cls.__name__
-            print('Step method: {}'.format(name))
+            print('\r\nStep method: {}'.format(name))
+            t_start = time.time()
             mt = pm.sample(
                 draws=10000,
                 njobs=1,
                 chains=6,
                 step=step_cls(),
-                start={'z': [0, 0]}
+                start=start
             )
+            runtimes[name] = time.time() - t_start
             traces[name] = mt
             en = pm.diagnostics.effective_n(mt)
-            effn[name] = np.mean(en['z']) / len(mt) / mt.nchains
-    return traces, effn
+            if USE_XY:
+                effn[name] = np.mean(en['x']) / len(mt) / mt.nchains
+            else:
+                effn[name] = np.mean(en['z']) / len(mt) / mt.nchains
+    return traces, effn, runtimes
 
 
 if __name__ == '__main__':
@@ -46,14 +63,22 @@ if __name__ == '__main__':
         pm.DEMetropolis
     ]
     names = [c.__name__ for c in methods]
-    df = pd.DataFrame(columns=['p'] + names)
-    df['p'] = [.0,.9]
-    df = df.set_index('p')
-    rates = []
-    for p in df.index:
-        trace, rate = run(methods, p)
-        for name in names:
-            df.set_value(p, name, rate[name])
+    df_effectiven = pd.DataFrame(columns=['p'] + names)
+    df_effectiven['p'] = [.0,.9]
+    df_effectiven = df_effectiven.set_index('p')
 
-    print('Effective sample size [0...1]')
-    print(df.T)
+    df_runtime = pd.DataFrame(columns=['p'] + names)
+    df_runtime['p'] = [.0,.9]
+    df_runtime = df_runtime.set_index('p')
+
+    for p in df_effectiven.index:
+        trace, rate, runtime = run(methods, p)
+        for name in names:
+            df_effectiven.set_value(p, name, rate[name])
+            df_runtime.set_value(p, name, runtime[name])
+
+    print('\r\nEffective sample size [0...1]')
+    print(df_effectiven.T)
+
+    print('\r\nRuntime [s]')
+    print(df_runtime.T)
