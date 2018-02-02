181052f2dc17c62e8fcbcd9453cc582d8a03909e
aloctavodia <aloctavodia@gmail.com>
2018-02-02 15:19:55 -0300

    use floats

--- pymc3/distributions/special.py
+++ pymc3/distributions/special.py
@@ -25,12 +25,12 @@ def log_i0(x):
     """
     Calculates the logarithm of the 0 order modified Bessel function of the first kind""
     """
-    return tt.switch(tt.lt(x, 5), tt.log1p(x**2 / 4. + x**4 / 64. + x**6 / 2304.
-                                           + x**8 / 147456. + x**10 / 14745600.
-                                           + x**12 / 2123366400.),
+    return tt.switch(tt.lt(x, 5), tt.log1p(x**2. / 4. + x**4. / 64. + x**6. / 2304.
+                                           + x**8. / 147456. + x**10. / 14745600.
+                                           + x**12. / 2123366400.),
                                   x - 0.5 * tt.log(2. * np.pi * x) + tt.log1p(1. / (8. * x)
-                                  + 9. / (128. * x**2) + 225. / (3072 * x**3)
-                                  + 11025. / (98304. * x**4)))
+                                  + 9. / (128. * x**2.) + 225. / (3072. * x**3.)
+                                  + 11025. / (98304. * x**4.)))
 
 scalar_psi = Psi(scalar.upgrade_to_float, name='scalar_psi')
 psi = tt.Elemwise(scalar_psi, name='psi')
