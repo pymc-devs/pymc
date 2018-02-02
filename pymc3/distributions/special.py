a8f8ea9022b0eb8f56b8507456fa95dd590624e7
aloctavodia <aloctavodia@gmail.com>
2018-02-02 14:11:19 -0300

    use log1p

--- pymc3/distributions/special.py
+++ pymc3/distributions/special.py
@@ -25,10 +25,10 @@ def log_i0(x):
     """
     Calculates the logarithm of the 0 order modified Bessel function of the first kind""
     """
-    return tt.switch(tt.lt(x, 5), tt.log(1. + x**2 / 4. + x**4 / 64. + x**6 / 2304.
-                                         + x**8 / 147456. + x**10 / 14745600.
-                                         + x**12 / 2123366400.),
-                                  x - 0.5 * np.log(2. * np.pi * x) + np.log(1. + 1. / (8. * x)
+    return tt.switch(tt.lt(x, 5), tt.log1p(x**2 / 4. + x**4 / 64. + x**6 / 2304.
+                                           + x**8 / 147456. + x**10 / 14745600.
+                                           + x**12 / 2123366400.),
+                                  x - 0.5 * tt.log(2. * np.pi * x) + tt.log1p(1. / (8. * x)
                                   + 9. / (128. * x**2) + 225. / (3072 * x**3)
                                   + 11025. / (98304. * x**4)))
 
