5ab492392bb2ebd78260024e65ff6f4d146b9666
aloctavodia <aloctavodia@gmail.com>
2018-02-02 10:48:12 -0300

    vonMises avoid overflow for large kappa

--- pymc3/distributions/special.py
+++ pymc3/distributions/special.py
@@ -3,7 +3,7 @@ import theano.tensor as tt
 from theano.scalar.basic_scipy import GammaLn, Psi, I0, I1
 from theano import scalar
 
-__all__ = ['gammaln', 'multigammaln', 'psi', 'i0', 'i1']
+__all__ = ['gammaln', 'multigammaln', 'psi', 'i0', 'i1', 'log_i0']
 
 scalar_gammaln = GammaLn(scalar.upgrade_to_float, name='scalar_gammaln')
 gammaln = tt.Elemwise(scalar_gammaln, name='gammaln')
@@ -20,6 +20,17 @@ def multigammaln(a, p):
     i = tt.arange(1, p + 1)
     return (p * (p - 1) * tt.log(np.pi) / 4.
             + tt.sum(gammaln(a + (1. - i) / 2.), axis=0))
+
+def log_i0(x):
+    """
+    Calculates the logarithm of the 0 order modified Bessel function of the first kind""
+    """
+    return tt.switch(tt.lt(x, 5), tt.log(1. + x**2 / 4. + x**4 / 64. + x**6 / 2304.
+                                         + x**8 / 147456. + x**10 / 14745600.
+                                         + x**12 / 2123366400.),
+                                  x - 0.5 * np.log(2. * np.pi * x) + np.log(1. + 1. / (8. * x)
+                                  + 9. / (128. * x**2) + 225. / (3072 * x**3)
+                                  + 11025. / (98304. * x**4)))
 
 scalar_psi = Psi(scalar.upgrade_to_float, name='scalar_psi')
 psi = tt.Elemwise(scalar_psi, name='psi')
