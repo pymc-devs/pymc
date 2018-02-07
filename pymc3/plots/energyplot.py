b20a310936161eb28a35ee5490f88ab9a3183d85
aloctavodia <aloctavodia@gmail.com>
2018-02-07 11:08:31 -0300

    add argument for bandwidth scaling factor

--- pymc3/plots/energyplot.py
+++ pymc3/plots/energyplot.py
@@ -8,8 +8,8 @@ except ImportError:  # mpl is optional
 from .kdeplot import kdeplot
 
 
-def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True,
-               shade=0.35, frame=True, kwargs_shade=None, **kwargs):
+def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True, shade=0.35, bw=4.5,
+               frame=True, kwargs_shade=None, **kwargs):
     """Plot energy transition distribution and marginal energy distribution in
     order to diagnose poor exploration by HMC algorithms.
 
@@ -28,6 +28,10 @@ def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True,
     shade : float
         Alpha blending value for the shaded area under the curve, between 0
         (no shade) and 1 (opaque). Defaults to 0.35
+    bw : float
+        Bandwidth scaling factor for the KDE. Should be larger than 0. The higher this number the
+        smoother the KDE will be. Defaults to 4.5 which is essentially the same as the Scott's rule
+        of thumb (the default rule used by SciPy). Only works if `kind='kde'`.
     frame : bool
         Flag for plotting frame around figure.
     kwargs_shade : dicts, optional
@@ -61,8 +65,8 @@ def energyplot(trace, kind='kde', figsize=None, ax=None, legend=True,
 
     if kind == 'kde':
         for label, value in series:
-            kdeplot(value, label=label, shade=shade, ax=ax,
-                    kwargs_shade=kwargs_shade, **kwargs)
+            kdeplot(value, label=label, shade=shade, bw=bw, ax=ax, kwargs_shade=kwargs_shade,
+                    **kwargs)
 
     elif kind == 'hist':
         for label, value in series:
