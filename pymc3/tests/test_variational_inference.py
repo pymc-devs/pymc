f4965f51f7c42b08ea11e82ca823989d6cdc7c0f
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-03-10 02:30:01 +0300

    use floatX

--- pymc3/tests/test_variational_inference.py
+++ pymc3/tests/test_variational_inference.py
@@ -836,8 +836,8 @@ def test_sample_replacements(binomial_model_inference):
 
 
 def test_var_replacement():
-    X_mean = np.linspace(0, 10, 10, dtype='float32')
-    y = np.random.normal(X_mean*4, .05).astype('float32')
+    X_mean = pm.floatX(np.linspace(0, 10, 10))
+    y = pm.floatX(np.random.normal(X_mean*4, .05))
     with pm.Model():
         inp = pm.Normal('X', X_mean, shape=X_mean.shape)
         coef = pm.Normal('b', 4.)
@@ -845,7 +845,7 @@ def test_var_replacement():
         pm.Normal('y', mean, .1, observed=y)
         advi = pm.fit(100)
         assert advi.sample_node(mean).eval().shape == (10, )
-        x_new = np.linspace(0, 10, 11, dtype='float32')
+        x_new = pm.floatX(np.linspace(0, 10, 11))
         assert advi.sample_node(mean, more_replacements={inp: x_new}).eval().shape == (11, )
 
 
