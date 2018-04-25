54020e3555acd3f0cd373452df7e6d7a6519f540
Junpeng Lao <junpeng.lao@unifr.ch>
2018-04-25 17:27:29 +0200

    tuple as args 2

--- pymc3/distributions/discrete.py
+++ pymc3/distributions/discrete.py
@@ -311,7 +311,7 @@ class DiscreteWeibull(Discrete):
     ========  ======================
     """
     def __init__(self, q, beta, *args, **kwargs):
-        super(DiscreteWeibull, self).__init__(*args, defaults=['median'], **kwargs)
+        super(DiscreteWeibull, self).__init__(*args, defaults=('median',), **kwargs)
 
         self.q = q = tt.as_tensor_variable(q)
         self.beta = beta = tt.as_tensor_variable(beta)
