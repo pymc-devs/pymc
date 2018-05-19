ed1fac9f82c47de5435f260253384589bf799a98
MBrouns <matthijs.brouns@gmail.com>
2018-05-19 15:33:37 +0200

    Change test domain for sd of rice distribution to pass tests with float32

--- pymc3/tests/test_distributions.py
+++ pymc3/tests/test_distributions.py
@@ -1057,9 +1057,8 @@ class TestMatchesScipy(SeededTest):
             Beta('beta', alpha=1., beta=1., shape=(10, 20))
 
     def test_rice(self):
-        self.pymc3_matches_scipy(Rice, Rplus, {'nu': Rplus, 'sd': Rplus},
-                                 lambda value, nu, sd: sp.rice.logpdf(value, b=nu, loc=0, scale=sd),
-                                 decimal=select_by_precision(float64=6, float32=1))
+        self.pymc3_matches_scipy(Rice, Rplus, {'nu': Rplus, 'sd': Rplusbig},
+                                 lambda value, nu, sd: sp.rice.logpdf(value, b=nu, loc=0, scale=sd))
 
     @pytest.mark.xfail(condition=(theano.config.floatX == "float32"), reason="Fails on float32")
     def test_interpolated(self):
