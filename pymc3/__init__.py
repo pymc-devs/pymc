85945d7936c9c79ce047bf9d39060f609d9714d5
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2018-06-19 10:50:00 -0700

    Uncomment some stuff in pymc/init

--- pymc3/__init__.py
+++ pymc3/__init__.py
@@ -3,28 +3,28 @@ __version__ = "3.4.1"
 
 from .blocking import *
 from .distributions import *
-#from .external import *
-#from .glm import *
-#from . import gp
+from .external import *
+from .glm import *
+from . import gp
 from .math import logaddexp, logsumexp, logit, invlogit, expand_packed_triangular, probit, invprobit
 from .model import *
-#from .stats import *
+from .stats import *
 from .sampling import *
 from .step_methods import *
 from .theanof import *
-#from .tuning import *
-#from .variational import *
+from .tuning import *
+from .variational import *
 from .vartypes import *
 from .exceptions import *
 from . import sampling
 
-#from .diagnostics import *
-#from .backends.tracetab import *
+from .diagnostics import *
+from .backends.tracetab import *
 
-#from .plots import *
-#from .tests import test
+from .plots import *
+from .tests import test
 
-#from .data import *
+from .data import *
 
 ## CHANGED:
 from .backends_symbolic import *
