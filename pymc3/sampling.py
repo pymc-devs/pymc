cb9872677dda7ea093170678e2463bb6c8c505e3
Demetri Pananos <dpananos@gmail.com>
2018-06-19 23:30:01 -0400

    Rename/alias

--- pymc3/sampling.py
+++ pymc3/sampling.py
@@ -27,7 +27,16 @@ from tqdm import tqdm
 import sys
 sys.setrecursionlimit(10000)
 
-__all__ = ['sample', 'iter_sample', 'sample_ppc', 'sample_ppc_w', 'init_nuts', 'sample_prior_predictive']
+__all__ = ['sample', 
+           'sample_posterior',
+           'iter_sample', 
+           'sample_ppc',
+           'sample_posterior_predictive',
+           'sample_ppc_w',
+           'sample_posterior_predictive_w',
+           'init_nuts', 
+           'sample_prior_predictive'
+           ]
 
 STEP_METHODS = (NUTS, HamiltonianMC, Metropolis, BinaryMetropolis,
                 BinaryGibbsMetropolis, Slice, CategoricalGibbsMetropolis)
@@ -483,7 +492,7 @@ def sample_posterior(draws=500, step=None, init='auto', n_init=200000, start=Non
 
 def sample(*args,**kwargs):
     """This method is deprecated.  Please use :func:`~sampling.sample_posterior`"""
-    message = 'sample() is deprecated.  Please use sample_posterior()'
+    message = 'sample() is deprecated. Please use sample_posterior()'
     warnings.warn(message, DeprecationWarning, stacklevel=2)
     return sample_posterior(*args,**kwargs)
 
@@ -1150,12 +1159,12 @@ def sample_posterior_predictive(trace, samples=None, model=None, vars=None, size
 
 def sample_ppc(*args,**kwargs):
     """This method is deprecated.  Please use :func:`~sampling.sample_posterior_predictive`"""
-    message = 'sample_ppc() is deprecated.  Please use sample_posterior_predictive().'
+    message = 'sample_ppc() is deprecated. Please use sample_posterior_predictive().'
     warnings.warn(message, DeprecationWarning, stacklevel=2)
     return sample_posterior_predictive(*args, **kwargs)
 
 
-def sample_predictive_posterior_w(traces, samples=None, models=None, weights=None,
+def sample_posterior_predictive_w(traces, samples=None, models=None, weights=None,
                  random_seed=None, progressbar=True):
     """Generate weighted posterior predictive samples from a list of models and
     a list of traces according to a set of weights.
@@ -1284,9 +1293,9 @@ def sample_predictive_posterior_w(traces, samples=None, models=None, weights=Non
 
 def sample_ppc_w(*args,**kwargs):
     """This method is deprecated.  Please use :func:`~sampling.sample_posterior_predictive_w`"""
-    message = 'sample_ppc_w() is deprecated.  Please use sample_posterior_predictive_w()'
+    message = 'sample_ppc_w() is deprecated. Please use sample_posterior_predictive_w()'
     warnings.warn(message, DeprecationWarning, stacklevel=2)
-    return sample_predictive_posterior_w(*args,**kwargs)
+    return sample_posterior_predictive_w(*args,**kwargs)
 
 
 def sample_prior_predictive(samples=500, model=None, vars=None, random_seed=None):
