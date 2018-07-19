89558c27e4e2860e2055156a68399b95d9aa637a
George Ho <georgeho1618@gmail.com>
2018-07-19 11:49:21 -0400

    Refactor code

--- pymc3/step_methods/hmc/base_hmc.py
+++ pymc3/step_methods/hmc/base_hmc.py
@@ -169,23 +169,19 @@ class BaseHMC(arraystep.GradientSharedStep):
         warnings = self._warnings[:]
 
         # Generate a global warning for divergences
+        message = ''
         n_divs = self._num_divs_sample
         if n_divs and self._samples_after_tune == n_divs:
             message = ('The chain contains only diverging samples. The model '
                        'is probably misspecified.')
-            warning = SamplerWarning(
-                WarningType.DIVERGENCES, message, 'error', None, None, None)
-            warnings.append(warning)
         elif n_divs == 1:
             message = ('There was 1 divergence after tuning. Increase '
                        '`target_accept` or reparameterize.')
-            warning = SamplerWarning(
-                WarningType.DIVERGENCES, message, 'error', None, None, None)
-            warnings.append(warning)
         elif n_divs > 1:
             message = ('There were %s divergences after tuning. Increase '
-                       '`target_accept` or reparameterize.'
-                       % n_divs)
+                       '`target_accept` or reparameterize.' % n_divs)
+
+        if message:
             warning = SamplerWarning(
                 WarningType.DIVERGENCES, message, 'error', None, None, None)
             warnings.append(warning)
