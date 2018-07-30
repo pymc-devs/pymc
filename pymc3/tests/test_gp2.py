e5050ac65b05c5989c588278d95632eb282d58e9
Bill Engels <w.j.engels@gmail.com>
2018-07-30 18:24:57 -0500

    refiguring tests again.  test models with simulated data.  hopefully this isnt too slow

--- pymc3/tests/test_gp2.py
+++ pymc3/tests/test_gp2.py
@@ -1,6 +1,8 @@
 #  pylint:disable=unused-variable
 from functools import reduce, partial
 from ..math import cartesian, kronecker
+from ..distributions import draw_values
+from .helpers import SeededTest
 from operator import add
 import pymc3 as pm
 import theano
@@ -69,20 +71,72 @@ class GPReference(object):
         return partial(sp.stats.multivariate_normal.logpdf, mean=mu, cov=cov)
 
 
+class TestMarginal2(object):
+    def setup_method(self):
+        n = 50
+        self.Xs = np.linspace(0, 10, n)[:,None]
+        self.X = self.Xs[::2, :]
+        self.true_ls, self.true_sigma, self.true_c = (3.0, 0.05, 1.0)
+        self.cov = pm.gp.cov.ExpQuad(1, ls=self.true_ls)
+        self.mean = pm.gp.mean.Constant(self.true_c)
+        self.true_f = np.random.multivariate_normal(self.mean(self.Xs).eval(),
+                                                    self.cov(self.Xs).eval(), 1).flatten()
+        true_y = self.true_f + self.true_sigma * np.random.randn(n)
+        self.obs = true_y[::2]
+
+        with pm.Model() as model:
+            ls = pm.Gamma("ls", alpha=300, beta=100)
+            sigma = pm.Gamma("sigma", alpha=20, beta=400)
+            c = pm.Normal("c", mu=1.0, sd=0.1)
+            gp = pm.gp.Marginal(mean_func=pm.gp.mean.Constant(c),
+                                cov_func=pm.gp.cov.ExpQuad(1, ls=ls))
+            y_ = gp.marginal_likelihood("y", self.X, self.obs, noise=sigma)
+            tr = pm.sample(200, tune=200, chains=1)
+
+        self.map_point = {varname: np.mean(tr[varname]) for varname in tr.varnames}
+        self.model = model
+        self.tr = tr
+        self.gp = gp
+
+        with self.model:
+            f = self.gp.conditional("f", Xnew=self.Xs)
+            self.ppc = pm.sample_ppc(self.tr, 200, vars=[f])
+
+        self.f_mu = np.mean(self.ppc["f"], 0)
+        self.f_sd = np.std(self.ppc["f"], 0)
+
+    def test_priors(self):
+        # posteriors must be close to true values (which had strong priors)
+        npt.assert_allclose(np.mean(self.tr["ls"]), self.true_ls, atol=0.1)
+        npt.assert_allclose(np.mean(self.tr["sigma"]), self.true_sigma, atol=0.01)
+        npt.assert_allclose(np.mean(self.tr["c"]), self.true_c, atol=0.1)
+
+    def test_conditionals(self):
+        npt.assert_allclose(self.f_mu, self.true_f, atol=0.1)
+        npt.assert_array_less(self.true_f, self.f_mu + 3*self.f_sd)
+        npt.assert_array_less(self.f_mu - 3*self.f_sd, self.true_f)
+
+    @pytest.mark.parametrize("diag", [True, False])
+    @pytest.mark.parametrize("pred_noise", [True, False])
+    def test_predictions(self, diag, pred_noise):
+        if diag:
+            mu, var = self.gp.predict(self.Xs, point=self.map_point, diag=diag, pred_noise=pred_noise)
+            npt.assert_allclose(np.sqrt(var), self.f_sd, atol=0.1)
+        else:
+            mu, cov = self.gp.predict(self.Xs, point=self.map_point, diag=diag, pred_noise=pred_noise)
+            npt.assert_allclose(np.sqrt(np.diag(cov)), self.f_sd, atol=0.1)
+        npt.assert_allclose(mu, self.f_mu, atol=0.1)
+
 
 class TestMarginal(object):
     def setup_method(self):
-        n, ns, d, m = (20, 30, 3, 4)
+        n, ns, d = (40, 50, 3)
         self.X, self.Xs = (np.random.randn(n, d), np.random.randn(ns, d))
         self.ls, self.sigma = (np.random.rand(d) + 0.1, 0.1)
-        self.y_vec = np.random.randn(n)
-        self.y_mat = np.random.randn(n, m)
-        self.n, self.ns = (n, ns)
-        self.d, self.m = (d, m)
+        self.y = np.random.randn(n)
+        self.n, self.ns, self.d = (n, ns, d)
 
-    @pytest.mark.parametrize('multiple_obs', [False, pytest.param(True, marks=pytest.mark.xfail)])
-    def testMarginalLogp(self, multiple_obs):
-        y = self.y_vec if not multiple_obs else self.y_mat
+    def testMarginalLogp(self):
         # pymc3 model
         ls = np.random.rand(self.d)
         with pm.Model() as model:
@@ -90,7 +144,7 @@ class TestMarginal(object):
             mean_func = pm.gp.mean.Constant(0.0)
             gp = pm.gp.Marginal(mean_func, cov_func)
             y_ = gp.marginal_likelihood("y_var",
-                                        self.X, y, self.sigma)
+                                        self.X, self.y, self.sigma)
             f = gp.conditional("f", self.Xs)
         # reference GP
         ref_gp = GPReference(mean_func, cov_func)
@@ -102,14 +156,12 @@ class TestMarginal(object):
                             rtol=1e-3)
         # test conditional logp
         x = np.random.randn(self.ns)
-        mu, cov = ref_gp.conditional(self.X, self.Xs, self.sigma, y=y)
+        mu, cov = ref_gp.conditional(self.X, self.Xs, self.sigma, y=self.y)
         npt.assert_allclose(f.distribution.logp(tt.as_tensor_variable(x)).eval(),
                             ref_gp.logp_func(mu, cov)(x),
                             rtol=1e-3)
 
-    @pytest.mark.parametrize('multiple_obs', [False, pytest.param(True, marks=pytest.mark.xfail)])
-    def testMarginalPredict(self, multiple_obs):
-        y = self.y_vec if not multiple_obs else self.y_mat
+    def testMarginalPredict(self):
         # pymc3 model
         ls = np.random.rand(self.d)
         with pm.Model() as model:
@@ -117,27 +169,21 @@ class TestMarginal(object):
             mean_func = pm.gp.mean.Constant(0.0)
             gp = pm.gp.Marginal(mean_func, cov_func)
             y_ = gp.marginal_likelihood("y_var",
-                                        self.X, y, self.sigma)
+                                        self.X, self.y, self.sigma)
             f = gp.conditional("f", self.Xs)
-        # reference GP
-        ref_gp = GPReference(mean_func, cov_func)
-        # test marginal_likelihood logp
-        x = np.random.randn(self.ns)
 
-        # test predict
-        mu_ref, cov_ref = ref_gp.conditional(self.X, self.Xs, self.sigma, y=y)
+        ref_gp = GPReference(mean_func, cov_func)
+        mu_ref, cov_ref = ref_gp.conditional(self.X, self.Xs, self.sigma, y=self.y)
         mu_pymc3, cov_pymc3 = gp.predict(self.Xs)
-        npt.assert_allclose(mu_ref, mu_pymc3, rtol=1e-3)
-        npt.assert_allclose(cov_ref, cov_pymc3, rtol=1e-3)
+        npt.assert_allclose(mu_ref, mu_pymc3, rtol=1e-2)
+        npt.assert_allclose(cov_ref, cov_pymc3, rtol=1e-2)
 
         # test predict, diag=True
         mu_pymc3, var_pymc3 = gp.predict(self.Xs, diag=True)
-        npt.assert_allclose(mu_ref, mu_pymc3, rtol=1e-3)
-        npt.assert_allclose(np.diag(cov_ref), var_pymc3, rtol=1e-3)
+        npt.assert_allclose(mu_ref, mu_pymc3, rtol=1e-2)
+        npt.assert_allclose(np.diag(cov_ref), var_pymc3, rtol=1e-2)
 
-    @pytest.mark.parametrize('multiple_obs', [False, pytest.param(True, marks=pytest.mark.xfail)])
-    def testMarginalAdditive(self, multiple_obs):
-        y = self.y_vec if not multiple_obs else self.y_mat
+    def testMarginalAdditive(self):
         means = (pm.gp.mean.Constant(0.0),
                  pm.gp.mean.Constant(0.5),
                  pm.gp.mean.Constant(1.0))
@@ -151,41 +197,39 @@ class TestMarginal(object):
             gp3 = pm.gp.Marginal(means[2], covs[2])
 
             gp_sum = gp1 + gp2 + gp3
-            f_sum = gp_sum.marginal_likelihood("f_sum", self.X, y, noise=self.sigma)
-        model1_logp = model1.logp({"f_sum": y})
+            f_sum = gp_sum.marginal_likelihood("f_sum", self.X,
+                                               self.y, noise=self.sigma)
+        model1_logp = model1.logp({"f_sum": self.y})
 
         with pm.Model() as model2:
             gp_total = pm.gp.Marginal(reduce(add, means), reduce(add, covs))
-            f_total = gp_total.marginal_likelihood("f_total", self.X, y, noise=self.sigma)
-        model2_logp = model2.logp({"f_total": y})
-
+            f_total = gp_total.marginal_likelihood("f_total", self.X,
+                                                   self.y, noise=self.sigma)
+        model2_logp = model2.logp({"f_total": self.y})
         npt.assert_allclose(model1_logp, model2_logp, atol=0, rtol=1e-2)
 
         with model1:
-            fp1 = gp_sum.conditional("fp1", self.Xs, given={"X": self.X, "y": y,
-                                                           "noise": self.sigma, "gp": gp_sum})
+            fp1 = gp_sum.conditional("fp1", self.Xs,
+                                     given={"X": self.X, "y": self.y,
+                                            "noise": self.sigma, "gp": gp_sum})
         with model2:
             fp2 = gp_total.conditional("fp2", self.Xs)
 
         fp = np.random.randn(self.ns)
-        npt.assert_allclose(fp1.logp({"fp1": fp}), fp2.logp({"fp2": fp}), atol=0, rtol=1e-2)
+        npt.assert_allclose(fp1.logp({"fp1": fp}), fp2.logp({"fp2": fp}), rtol=1e-2)
 
 
 class TestLatent(object):
+    # to test multiobs, do two/three column, compare
+    # one column results to single column result
     def setup_method(self):
-        n, ns, d, m = (20, 30, 3, 4)
+        n, ns, d = (40, 50, 3)
         self.X, self.Xs = (np.random.randn(n, d), np.random.randn(ns, d))
         self.ls = np.random.rand(d) + 0.1
-        self.f_vec = np.random.randn(n)
-        self.f_mat = np.random.randn(n, m)
-        self.n, self.ns = (n, ns)
-        self.d, self.m = (d, m)
-        # problem with parameterizing multiple_obs, will need shapes, and 'x' and 'xs' will need
-        # to be different sizes.
+        self.f = np.random.randn(n)
+        self.n, self.ns, self.d = (n, ns, d)
 
-    @pytest.mark.parametrize('multiple_obs', [False, pytest.param(True, marks=pytest.mark.xfail)])
-    def testLatentLogp(self, multiple_obs):
-        f = self.f_vec if not multiple_obs else self.f_mat
+    def testLatentLogp(self):
         # pymc3 model
         ls = np.random.rand(self.d)
         with pm.Model() as model:
@@ -195,20 +239,19 @@ class TestLatent(object):
             fv = gp.prior("fv", self.X)
             fp = gp.conditional("fp", self.Xs)
 
-        f_rotated = rotate_vector(cov_func(self.X).eval(), f)
+        f_rotated = rotate_vector(cov_func(self.X).eval(), self.f)
         fs = np.random.randn(self.ns)
         latent_logp = model.logp({"fv_rotated_": f_rotated, "fp": fs})
 
         # reference GP
         ref_gp = GPReference(mean_func, cov_func)
         ref_priorlogp = np.sum(sp.stats.norm.logpdf(f_rotated, loc=np.zeros(self.n), scale=np.ones(self.n)))
-        mu, cov = ref_gp.conditional(self.X, self.Xs, 1e-8, f=f)
+        mu, cov = ref_gp.conditional(self.X, self.Xs, 1e-8, f=self.f)
         ref_condlogp = ref_gp.logp_func(mu, cov)(fs)
-        npt.assert_allclose(latent_logp, ref_priorlogp + ref_condlogp, rtol=1e-3)
+        npt.assert_allclose(latent_logp, ref_priorlogp + ref_condlogp,
+                            rtol=1e-3)
 
-    @pytest.mark.parametrize('multiple_obs', [False, pytest.param(True, marks=pytest.mark.xfail)])
-    def testLatentAdditive(self, multiple_obs):
-        f = self.f_vec if not multiple_obs else self.f_mat
+    def testLatentAdditive(self):
         means = (pm.gp.mean.Constant(0.0),
                  pm.gp.mean.Constant(0.5),
                  pm.gp.mean.Constant(1.0))
@@ -217,6 +260,7 @@ class TestLatent(object):
                 pm.gp.cov.ExpQuad(self.d, self.ls))
         mean_total = reduce(add, means)
         cov_total = reduce(add, covs)
+
         with pm.Model() as model1:
             gp1 = pm.gp.Latent(means[0], covs[0])
             gp2 = pm.gp.Latent(means[1], covs[1])
@@ -224,7 +268,8 @@ class TestLatent(object):
             gp_sum = gp1 + gp2 + gp3
             f_sum = gp_sum.prior("f_sum", self.X)
 
-        f_rotated = rotate_vector(cov_total(self.X).eval(), f, mean_total(self.X).eval())
+        f_rotated = rotate_vector(cov_total(self.X).eval(),
+                                            self.f, mean_total(self.X).eval())
         model1_logp = model1.logp({"f_sum_rotated_": f_rotated})
 
         with pm.Model() as model2:
@@ -234,13 +279,17 @@ class TestLatent(object):
         npt.assert_allclose(model1_logp, model2_logp, atol=0, rtol=1e-2)
 
         with model1:
-            fp1 = gp_sum.conditional("fp1", self.Xs, given={"X": self.X, "f": f, "gp": gp_sum})
+            fp1 = gp_sum.conditional("fp1", self.Xs,
+                                     given={"X": self.X, "f": self.f,
+                                            "gp": gp_sum})
         with model2:
             fp2 = gp_total.conditional("fp2", self.Xs)
 
         fp = np.random.randn(self.ns)
-        npt.assert_allclose(fp1.logp({"f_sum_rotated_": f_rotated, "fp1": fp}),
-                            fp2.logp({"f_total_rotated_": f_rotated, "fp2": fp}), atol=0, rtol=1e-2)
+        npt.assert_allclose(fp1.logp({"f_sum_rotated_": f_rotated,
+                                      "fp1": fp}),
+                            fp2.logp({"f_total_rotated_": f_rotated,
+                                      "fp2": fp}), atol=0, rtol=1e-2)
 
 
 
@@ -282,6 +331,103 @@ class TestGPAdditiveRaises(object):
             with pytest.raises(Exception) as e_info:
                 gp1 + gp2
 
+    def testAdditiveTPRaises(self):
+        with pm.Model() as model:
+            cov_func = pm.gp.cov.ExpQuad(3, [0.1, 0.2, 0.3])
+            gp1 = pm.gp.TP(cov_func=cov_func, nu=10)
+            gp2 = pm.gp.TP(cov_func=cov_func, nu=10)
+            with pytest.raises(Exception) as e_info:
+                gp1 + gp2
+
+
+class TestTP(object):
+    # to test multiobs, do two/three column, compare
+    # one column results to single column result
+    def setup_method(self):
+        n, ns, d = (10, 15, 2)
+        self.X, self.Xs = (np.random.randn(n, d), np.random.randn(ns, d))
+        self.ls = np.random.rand(d) + 0.1
+        self.f = np.random.randn(n)
+        self.n, self.ns, self.d = (n, ns, d)
+
+    def testTPPredict(self):
+        ls = np.random.rand(self.d)
+        with pm.Model() as model:
+            cov_func = pm.gp.cov.ExpQuad(self.d, ls)
+            mean_func = pm.gp.mean.Constant(1.0)
+            tp = pm.gp.TP(mean_func=mean_func, cov_func=cov_func, nu=10000)
+            fv = tp.prior("fv", self.X)
+            fp = tp.conditional("fp", self.Xs)
+
+        f_rotated = rotate_vector(cov_func(self.X).eval(),
+                                  self.f - mean_func(self.X).eval())
+        mu, cov = draw_values([fp.distribution.mu, fp.distribution.cov],
+                               point={"fv_rotated_": f_rotated,
+                                      "chi2__log__": np.log(10000)})
+
+        # reference GP
+        ref_gp = GPReference(mean_func, cov_func)
+        mu_ref, cov_ref = ref_gp.conditional(self.X, self.Xs, 0.0, f=self.f)
+        npt.assert_allclose(mu_ref, mu, rtol=1e-2)
+        npt.assert_allclose(cov_ref, cov, rtol=1e-2)
+
+
+class TestMarginalKron(object):
+    def setup_method(self):
+        self.Xs = [np.linspace(0, 1, 7)[:, None],
+                   np.linspace(0, 1, 5)[:, None],
+                   np.linspace(0, 1, 6)[:, None]]
+        self.X = cartesian(*self.Xs)
+        self.N = np.prod([len(X) for X in self.Xs])
+        self.y = np.random.randn(self.N) * 0.1
+        self.Xnews = [np.random.randn(5, 1),
+                      np.random.randn(5, 1),
+                      np.random.randn(5, 1)]
+        self.Xnew = np.concatenate(tuple(self.Xnews), axis=1)
+        self.sigma = 0.2
+        self.pnew = np.random.randn(len(self.Xnew))*0.01
+        ls = 0.2
+        with pm.Model() as model:
+            self.cov_funcs = [pm.gp.cov.ExpQuad(1, ls),
+                              pm.gp.cov.ExpQuad(1, ls),
+                              pm.gp.cov.ExpQuad(1, ls)]
+            cov_func = pm.gp.cov.Kron(self.cov_funcs)
+            self.mean = pm.gp.mean.Constant(0.5)
+            gp = pm.gp.Marginal(mean_func=self.mean, cov_func=cov_func)
+            f = gp.marginal_likelihood("f", self.X, self.y, noise=self.sigma)
+            p = gp.conditional("p", self.Xnew)
+            self.mu, self.cov = gp.predict(self.Xnew)
+        self.logp = model.logp({"p": self.pnew})
+
+    def testMarginalKronvsMarginalpredict(self):
+        with pm.Model() as kron_model:
+            kron_gp = pm.gp.MarginalKron(mean_func=self.mean,
+                                         cov_funcs=self.cov_funcs)
+            f = kron_gp.marginal_likelihood('f', self.Xs, self.y,
+                                            sigma=self.sigma, shape=self.N)
+            p = kron_gp.conditional('p', self.Xnew)
+            mu, cov = kron_gp.predict(self.Xnew)
+        npt.assert_allclose(mu, self.mu, atol=0, rtol=1e-2)
+        npt.assert_allclose(cov, self.cov, atol=0, rtol=1e-2)
+
+    def testMarginalKronvsMarginal(self):
+        with pm.Model() as kron_model:
+            kron_gp = pm.gp.MarginalKron(mean_func=self.mean,
+                                         cov_funcs=self.cov_funcs)
+            f = kron_gp.marginal_likelihood('f', self.Xs, self.y,
+                                            sigma=self.sigma, shape=self.N)
+            p = kron_gp.conditional('p', self.Xnew)
+        kron_logp = kron_model.logp({'p': self.pnew})
+        npt.assert_allclose(kron_logp, self.logp, atol=0, rtol=1e-2)
+
+    def testMarginalKronRaises(self):
+        with pm.Model() as kron_model:
+            gp1 = pm.gp.MarginalKron(mean_func=self.mean,
+                                     cov_funcs=self.cov_funcs)
+            gp2 = pm.gp.MarginalKron(mean_func=self.mean,
+                                     cov_funcs=self.cov_funcs)
+        with pytest.raises(TypeError):
+            gp1 + gp2
 
 
 
