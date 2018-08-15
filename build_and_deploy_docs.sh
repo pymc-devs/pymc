d4f850a8e9265513ec1269b1607833d8771fbe7f
Thomas Wiecki <thomas.wiecki@gmail.com>
2018-08-15 16:52:41 +0200

    DOC Check out latest release tag before building docs.

--- build_and_deploy_docs.sh
+++ build_and_deploy_docs.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+latesttag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo checking out ${latesttag}
+git checkout ${latesttag}
 pushd docs/source
 make html
 ghp-import -c docs.pymc.io -n -p _build/html/
