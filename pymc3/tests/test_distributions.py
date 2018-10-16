d36eb421ba815587108fae214587f5c956ffdbb7
Robert P. Goldman <rpgoldman@sift.net>
2018-10-16 16:48:26 -0500

    Fixed new test per @ColCarroll.
    Marked the test of the alternative parameterization as an expected
    failure.
    Also put in Colin's rewrite to make the test more elegant.

--- pymc3/tests/test_distributions.py
+++ pymc3/tests/test_distributions.py
@@ -701,10 +701,15 @@ class TestMatchesScipy(SeededTest):
         self.pymc3_matches_scipy(
             InverseGamma, Rplus, {'alpha': Rplus, 'beta': Rplus},
             lambda value, alpha, beta: sp.invgamma.logpdf(value, alpha, scale=beta))
+
+    @pytest.mark.xfail(condition=(theano.config.floatX == "float32"),
+                           reason="Fails on float32 due to scaling issues")
+    def test_inverse_gamma_alt_params(self):
+        def test_fun(value, mu, sd):
+            alpha, beta = InverseGamma._get_alpha_beta(None, None, mu, sd)
+            return sp.invgamma.logpdf(value, alpha, scale=beta)
         self.pymc3_matches_scipy(
-            InverseGamma, Rplus, {'mu': Rplus, 'sd': Rplus},
-            lambda value, mu, sd: sp.invgamma.logpdf(value, InverseGamma._get_alpha_beta(None, None, mu, sd)[0],
-                                                     scale=InverseGamma._get_alpha_beta(None, None, mu, sd)[1]))
+            InverseGamma, Rplus, {'mu': Rplus, 'sd': Rplus}, test_fun)
 
     def test_pareto(self):
         self.pymc3_matches_scipy(Pareto, Rplus, {'alpha': Rplusbig, 'm': Rplusbig},
