ce5504684c3df55741d8b692305dc48f25b138b5
Osvaldo Martin <aloctavodia@gmail.com>
2018-11-19 08:55:07 -0300

    SMC: clean code, fix docstring and add tuning arguments (#3259)
    * SMC: clean code, fix docstring and add tuning arguments
    
    * fix pylint issues
    
    * fix error
    
    * ensure float division python 2.7
    
    * ensure float division python 2.7

--- pymc3/sampling.py
+++ pymc3/sampling.py
@@ -6,7 +6,6 @@ import warnings
 
 from six import integer_types
 from joblib import Parallel, delayed
-from tempfile import mkdtemp
 import numpy as np
 import theano.gradient as tg
 
@@ -245,7 +244,8 @@ def sample(draws=500, step=None, init='auto', n_init=200000, start=None, trace=N
     chains : int
         The number of chains to sample. Running independent chains is important for some
         convergence statistics and can also reveal multiple modes in the posterior. If `None`,
-        then set to either `cores` or 2, whichever is larger. For SMC the default value is 100.
+        then set to either `cores` or 2, whichever is larger. For SMC the number of chains is the
+        number of draws.
     cores : int
         The number of chains to run in parallel. If `None`, set to the number of CPUs in the
         system, but at most 4 (for 'SMC' defaults to 1). Keep in mind that some chains might
@@ -323,7 +323,6 @@ def sample(draws=500, step=None, init='auto', n_init=200000, start=None, trace=N
     if isinstance(step, pm.step_methods.smc.SMC):
         if step_kwargs is None:
             step_kwargs = {}
-        test_folder = mkdtemp(prefix='SMC_TEST')
         trace = smc.sample_smc(draws=draws,
                                step=step,
                                progressbar=progressbar,
