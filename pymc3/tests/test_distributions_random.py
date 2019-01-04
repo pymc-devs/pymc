1e0c6aab6f2f4756dfce730933f93a08467d8afd
Tom Gilliss <gilliss@live.unc.edu>
2019-01-04 16:16:35 -0700

    minor

--- pymc3/tests/test_distributions_random.py
+++ pymc3/tests/test_distributions_random.py
@@ -480,13 +480,13 @@ class TestScalarParameterSamples(SeededTest):
 
     def test_truncated_normal_lower(self):
         def ref_rand(size, mu, sigma, lower, upper):
-            return st.truncnorm.rvs((lower-mu)/sd, np.inf, size=size, loc=mu, scale=sd)
+            return st.truncnorm.rvs((lower-mu)/sigma, np.inf, size=size, loc=mu, scale=sigma)
         pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'lower':-Rplusbig, 'upper':Rplusbig},
                      ref_rand=ref_rand)
 
     def test_truncated_normal_upper(self):
         def ref_rand(size, mu, sigma, lower, upper):
-            return st.truncnorm.rvs(-np.inf, (upper-mu)/sd, size=size, loc=mu, scale=sd)
+            return st.truncnorm.rvs(-np.inf, (upper-mu)/sigma, size=size, loc=mu, scale=sigma)
         pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'lower':-Rplusbig, 'upper':Rplusbig},
                      ref_rand=ref_rand)
 
