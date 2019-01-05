b18623bb3a7f230fd54e3321f8e85be30435c4b0
Tom Gilliss <gilliss@live.unc.edu>
2019-01-04 22:20:38 -0700

    removed corresponding paramdomains from upper and lower tests

--- pymc3/tests/test_distributions_random.py
+++ pymc3/tests/test_distributions_random.py
@@ -481,13 +481,13 @@ class TestScalarParameterSamples(SeededTest):
     def test_truncated_normal_lower(self):
         def ref_rand(size, mu, sigma, lower, upper):
             return st.truncnorm.rvs((lower-mu)/sigma, np.inf, size=size, loc=mu, scale=sigma)
-        pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'lower':-Rplusbig, 'upper':Rplusbig},
+        pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'lower':-Rplusbig},
                      ref_rand=ref_rand)
 
     def test_truncated_normal_upper(self):
         def ref_rand(size, mu, sigma, lower, upper):
             return st.truncnorm.rvs(-np.inf, (upper-mu)/sigma, size=size, loc=mu, scale=sigma)
-        pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'lower':-Rplusbig, 'upper':Rplusbig},
+        pymc3_random(pm.TruncatedNormal, {'mu': R, 'sigma': Rplusbig, 'upper':Rplusbig},
                      ref_rand=ref_rand)
 
     def test_skew_normal(self):
