3ca5f10fee869edc02c28a88e63969369a1434f8
Colin Carroll <colcarroll@gmail.com>
2019-02-14 18:10:04 -0500

    Keep old function names too

--- pymc3/plots/__init__.py
+++ pymc3/plots/__init__.py
@@ -4,11 +4,16 @@ Plots are delegated to the ArviZ library, a general purpose library for
 "exploratory analysis of Bayesian models." See https://arviz-devs.github.io/arviz/
 for details on plots.
 """
+import functools
+import sys
+import warnings
 try:
     import arviz as az
 except ImportError:  # arviz is optional, throw exception when used
 
     class _ImportWarner:
+        __all__ = []
+
         def __init__(self, attr):
             self.attr = attr
 
@@ -21,17 +26,47 @@ except ImportError:  # arviz is optional, throw exception when used
         def __getattr__(self, attr):
             return _ImportWarner(attr)
 
+
     az = _ArviZ()
 
+def map_args(func):
+    swaps = [
+        ('varnames', 'var_names')
+    ]
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        for (old, new) in swaps:
+            if old in kwargs and new not in kwargs:
+                warnings.warn('Keyword argument `{old}` renamed to `{new}`, and will be removed in pymc3 3.8'.format(old=old, new=new))
+                kwargs[new] = kwargs.pop(old)
+            return func(*args, **kwargs)
+    return wrapped
 
-autocorrplot = az.plot_autocorr
-compareplot = az.plot_compare
-forestplot = az.plot_forest
-kdeplot = az.plot_kde
-plot_posterior = az.plot_posterior
-traceplot = az.plot_trace
-energyplot = az.plot_energy
-densityplot = az.plot_density
-pairplot = az.plot_pair
+autocorrplot = map_args(az.plot_autocorr)
+compareplot = map_args(az.plot_compare)
+forestplot = map_args(az.plot_forest)
+kdeplot = map_args(az.plot_kde)
+plot_posterior = map_args(az.plot_posterior)
+traceplot = map_args(az.plot_trace)
+energyplot = map_args(az.plot_energy)
+densityplot = map_args(az.plot_density)
+pairplot = map_args(az.plot_pair)
 
 from .posteriorplot import plot_posterior_predictive_glm
+
+
+for plot in az.plots.__all__:
+    setattr(sys.modules[__name__], plot, map_args(getattr(az.plots, plot)))
+
+__all__ = tuple(az.plots.__all__) + (
+    'autocorrplot',
+    'compareplot',
+    'forestplot',
+    'kdeplot',
+    'plot_posterior',
+    'traceplot',
+    'energyplot',
+    'densityplot',
+    'pairplot',
+    'plot_posterior_predictive_glm',
+)
