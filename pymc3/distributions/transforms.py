0716814fda64ac498bfc0ea090478b1dd0682ab3
Hari <harivallabha.rangarajan@gmail.com>
2019-06-23 22:18:45 +0530

    Update doc strings of sumto1 and stick_breaking transforms in transforms.py

--- pymc3/distributions/transforms.py
+++ pymc3/distributions/transforms.py
@@ -400,7 +400,7 @@ ordered = Ordered()
 
 class SumTo1(Transform):
     """
-    Transforms K - 1 dimensional simplex space (k values in [0,1] and sum to 1) to K - 1 vector of values in [0,1]
+    Transforms K - 1 dimensional simplex space (k values in [0,1] and that sum to 1) to K - 1 vector of values in [0,1]
     This Transformation operates on the last dimension of the input tensor.
     """
 
@@ -428,9 +428,10 @@ class SumTo1(Transform):
 sum_to_1 = SumTo1()
 
 
-class StickBreaking(Transform):
+class 
+Breaking(Transform):
     """
-    Transforms K - 1 dimensional simplex space (k values in [0,1] and sum to 1) to K - 1 vector of real values.
+    Transforms K - 1 dimensional simplex space (k values in [0,1] and that sum to 1) to K - 1 vector of real values.
     Primarily borrowed from the STAN implementation.
 
     Parameters
