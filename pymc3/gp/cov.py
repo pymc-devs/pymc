3873f3ce6daea604750b896d45bbf59f7cee72be
Chris Fonnesbeck <fonnesbeck@gmail.com>
2019-07-03 09:35:52 -0500

    Added Matern12 kernel for GPs (#3538)
    * Added Matern12 kernel for GPs
    
    * Added line to RELEASE-NOTES
    
    * Ran Black formatting on cov.py

--- pymc3/gp/cov.py
+++ pymc3/gp/cov.py
@@ -3,26 +3,28 @@ import theano.tensor as tt
 from functools import reduce
 from operator import mul, add
 
-__all__ = ['Constant',
-           'WhiteNoise',
-           'ExpQuad',
-           'RatQuad',
-           'Exponential',
-           'Matern52',
-           'Matern32',
-           'Linear',
-           'Polynomial',
-           'Cosine',
-           'Periodic',
-           'WarpedInput',
-           'Gibbs',
-           'Coregion',
-           'ScaledCov',
-           'Kron']
+__all__ = [
+    "Constant",
+    "WhiteNoise",
+    "ExpQuad",
+    "RatQuad",
+    "Exponential",
+    "Matern52",
+    "Matern32",
+    "Linear",
+    "Polynomial",
+    "Cosine",
+    "Periodic",
+    "WarpedInput",
+    "Gibbs",
+    "Coregion",
+    "ScaledCov",
+    "Kron",
+]
 
 
 class Covariance:
-    R"""
+    r"""
     Base class for all kernels/covariance functions.
 
     Parameters
@@ -43,7 +45,7 @@ class Covariance:
             self.active_dims = np.asarray(active_dims, np.int)
 
     def __call__(self, X, Xs=None, diag=False):
-        R"""
+        r"""
         Evaluate the kernel/covariance function.
 
         Parameters
@@ -103,8 +105,13 @@ class Covariance:
 
 class Combination(Covariance):
     def __init__(self, factor_list):
-        input_dim = max([factor.input_dim for factor in factor_list
-                             if isinstance(factor, Covariance)])
+        input_dim = max(
+            [
+                factor.input_dim
+                for factor in factor_list
+                if isinstance(factor, Covariance)
+            ]
+        )
         super().__init__(input_dim=input_dim)
         self.factor_list = []
         for factor in factor_list:
@@ -124,9 +131,14 @@ class Combination(Covariance):
                     factor_list.append(np.diag(factor))
                 else:
                     factor_list.append(factor)
-            elif isinstance(factor, (tt.TensorConstant,
-                                     tt.TensorVariable,
-                                     tt.sharedvar.TensorSharedVariable)):
+            elif isinstance(
+                factor,
+                (
+                    tt.TensorConstant,
+                    tt.TensorVariable,
+                    tt.sharedvar.TensorSharedVariable,
+                ),
+            ):
                 if factor.ndim == 2 and diag:
                     factor_list.append(tt.diag(factor))
                 else:
@@ -147,7 +159,7 @@ class Prod(Combination):
 
 
 class Kron(Covariance):
-    R"""Form a covariance object that is the kronecker product of other covariances.
+    r"""Form a covariance object that is the kronecker product of other covariances.
 
     In contrast to standard multiplication, where each covariance is given the
     same inputs X and Xs, kronecker product covariances first split the inputs
@@ -179,13 +191,14 @@ class Kron(Covariance):
 
     def __call__(self, X, Xs=None, diag=False):
         X_split, Xs_split = self._split(X, Xs)
-        covs = [cov(x, xs, diag) for cov, x, xs
-                in zip(self.factor_list, X_split, Xs_split)]
+        covs = [
+            cov(x, xs, diag) for cov, x, xs in zip(self.factor_list, X_split, Xs_split)
+        ]
         return reduce(mul, covs)
 
 
 class Constant(Covariance):
-    R"""
+    r"""
     Constant valued covariance function.
 
     .. math::
@@ -208,7 +221,7 @@ class Constant(Covariance):
 
 
 class WhiteNoise(Covariance):
-    R"""
+    r"""
     White noise covariance function.
 
     .. math::
@@ -231,7 +244,7 @@ class WhiteNoise(Covariance):
 
 
 class Stationary(Covariance):
-    R"""
+    r"""
     Base class for stationary kernels/covariance functions.
 
     Parameters
@@ -256,13 +269,15 @@ class Stationary(Covariance):
         X = tt.mul(X, 1.0 / self.ls)
         X2 = tt.sum(tt.square(X), 1)
         if Xs is None:
-            sqd = (-2.0 * tt.dot(X, tt.transpose(X))
-                   + (tt.reshape(X2, (-1, 1)) + tt.reshape(X2, (1, -1))))
+            sqd = -2.0 * tt.dot(X, tt.transpose(X)) + (
+                tt.reshape(X2, (-1, 1)) + tt.reshape(X2, (1, -1))
+            )
         else:
             Xs = tt.mul(Xs, 1.0 / self.ls)
             Xs2 = tt.sum(tt.square(Xs), 1)
-            sqd = (-2.0 * tt.dot(X, tt.transpose(Xs))
-                   + (tt.reshape(X2, (-1, 1)) + tt.reshape(Xs2, (1, -1))))
+            sqd = -2.0 * tt.dot(X, tt.transpose(Xs)) + (
+                tt.reshape(X2, (-1, 1)) + tt.reshape(Xs2, (1, -1))
+            )
         return tt.clip(sqd, 0.0, np.inf)
 
     def euclidean_dist(self, X, Xs):
@@ -277,7 +292,7 @@ class Stationary(Covariance):
 
 
 class Periodic(Stationary):
-    R"""
+    r"""
     The Periodic kernel.
 
     .. math::
@@ -287,19 +302,20 @@ class Periodic(Stationary):
     def __init__(self, input_dim, period, ls=None, ls_inv=None, active_dims=None):
         super().__init__(input_dim, ls, ls_inv, active_dims)
         self.period = period
+
     def full(self, X, Xs=None):
         X, Xs = self._slice(X, Xs)
         if Xs is None:
             Xs = X
-        f1 = X.dimshuffle(0, 'x', 1)
-        f2 = Xs.dimshuffle('x', 0, 1)
+        f1 = X.dimshuffle(0, "x", 1)
+        f2 = Xs.dimshuffle("x", 0, 1)
         r = np.pi * (f1 - f2) / self.period
         r = tt.sum(tt.square(tt.sin(r) / self.ls), 2)
         return tt.exp(-0.5 * r)
 
 
 class ExpQuad(Stationary):
-    R"""
+    r"""
     The Exponentiated Quadratic kernel.  Also refered to as the Squared
     Exponential, or Radial Basis Function kernel.
 
@@ -314,7 +330,7 @@ class ExpQuad(Stationary):
 
 
 class RatQuad(Stationary):
-    R"""
+    r"""
     The Rational Quadratic kernel.
 
     .. math::
@@ -328,12 +344,14 @@ class RatQuad(Stationary):
 
     def full(self, X, Xs=None):
         X, Xs = self._slice(X, Xs)
-        return (tt.power((1.0 + 0.5 * self.square_dist(X, Xs)
-                         * (1.0 / self.alpha)), -1.0 * self.alpha))
+        return tt.power(
+            (1.0 + 0.5 * self.square_dist(X, Xs) * (1.0 / self.alpha)),
+            -1.0 * self.alpha,
+        )
 
 
 class Matern52(Stationary):
-    R"""
+    r"""
     The Matern kernel with nu = 5/2.
 
     .. math::
@@ -346,12 +364,13 @@ class Matern52(Stationary):
     def full(self, X, Xs=None):
         X, Xs = self._slice(X, Xs)
         r = self.euclidean_dist(X, Xs)
-        return ((1.0 + np.sqrt(5.0) * r + 5.0 / 3.0 * tt.square(r))
-                * tt.exp(-1.0 * np.sqrt(5.0) * r))
+        return (1.0 + np.sqrt(5.0) * r + 5.0 / 3.0 * tt.square(r)) * tt.exp(
+            -1.0 * np.sqrt(5.0) * r
+        )
 
 
 class Matern32(Stationary):
-    R"""
+    r"""
     The Matern kernel with nu = 3/2.
 
     .. math::
@@ -366,8 +385,21 @@ class Matern32(Stationary):
         return (1.0 + np.sqrt(3.0) * r) * tt.exp(-np.sqrt(3.0) * r)
 
 
+class Matern12(Stationary):
+    r"""
+    The Matern kernel with nu = 1/2
+
+    k(x, x') = \mathrm{exp}\left[ -\frac{(x - x')^2}{\ell} \right]
+    """
+
+    def full(self, X, Xs=None):
+        X, Xs = self._slice(X, Xs)
+        r = self.euclidean_dist(X, Xs)
+        return tt.exp(-r)
+
+
 class Exponential(Stationary):
-    R"""
+    r"""
     The Exponential kernel.
 
     .. math::
@@ -381,7 +413,7 @@ class Exponential(Stationary):
 
 
 class Cosine(Stationary):
-    R"""
+    r"""
     The Cosine kernel.
 
     .. math::
@@ -394,7 +426,7 @@ class Cosine(Stationary):
 
 
 class Linear(Covariance):
-    R"""
+    r"""
     The Linear kernel.
 
     .. math::
@@ -424,7 +456,7 @@ class Linear(Covariance):
 
 
 class Polynomial(Linear):
-    R"""
+    r"""
     The Polynomial kernel.
 
     .. math::
@@ -446,7 +478,7 @@ class Polynomial(Linear):
 
 
 class WarpedInput(Covariance):
-    R"""
+    r"""
     Warp the inputs of any kernel using an arbitrary function
     defined using Theano.
 
@@ -462,8 +494,7 @@ class WarpedInput(Covariance):
         Additional inputs (besides X or Xs) to warp_func.
     """
 
-    def __init__(self, input_dim, cov_func, warp_func, args=None,
-                 active_dims=None):
+    def __init__(self, input_dim, cov_func, warp_func, args=None, active_dims=None):
         super().__init__(input_dim, active_dims)
         if not callable(warp_func):
             raise TypeError("warp_func must be callable")
@@ -486,7 +517,7 @@ class WarpedInput(Covariance):
 
 
 class Gibbs(Covariance):
-    R"""
+    r"""
     The Gibbs kernel.  Use an arbitrary lengthscale function defined
     using Theano.  Only tested in one dimension.
 
@@ -503,17 +534,18 @@ class Gibbs(Covariance):
         Additional inputs (besides X or Xs) to lengthscale_func.
     """
 
-    def __init__(self, input_dim, lengthscale_func, args=None,
-                 active_dims=None):
+    def __init__(self, input_dim, lengthscale_func, args=None, active_dims=None):
         super().__init__(input_dim, active_dims)
         if active_dims is not None:
             if len(active_dims) > 1:
-                raise NotImplementedError(("Higher dimensional inputs ",
-                                           "are untested"))
+                raise NotImplementedError(
+                    ("Higher dimensional inputs ", "are untested")
+                )
         else:
             if input_dim != 1:
-                raise NotImplementedError(("Higher dimensional inputs ",
-                                           "are untested"))
+                raise NotImplementedError(
+                    ("Higher dimensional inputs ", "are untested")
+                )
         if not callable(lengthscale_func):
             raise TypeError("lengthscale_func must be callable")
         self.lfunc = handle_args(lengthscale_func, args)
@@ -522,12 +554,14 @@ class Gibbs(Covariance):
     def square_dist(self, X, Xs=None):
         X2 = tt.sum(tt.square(X), 1)
         if Xs is None:
-            sqd = (-2.0 * tt.dot(X, tt.transpose(X))
-                   + (tt.reshape(X2, (-1, 1)) + tt.reshape(X2, (1, -1))))
+            sqd = -2.0 * tt.dot(X, tt.transpose(X)) + (
+                tt.reshape(X2, (-1, 1)) + tt.reshape(X2, (1, -1))
+            )
         else:
             Xs2 = tt.sum(tt.square(Xs), 1)
-            sqd = (-2.0 * tt.dot(X, tt.transpose(Xs))
-                   + (tt.reshape(X2, (-1, 1)) + tt.reshape(Xs2, (1, -1))))
+            sqd = -2.0 * tt.dot(X, tt.transpose(Xs)) + (
+                tt.reshape(X2, (-1, 1)) + tt.reshape(Xs2, (1, -1))
+            )
         return tt.clip(sqd, 0.0, np.inf)
 
     def full(self, X, Xs=None):
@@ -541,15 +575,16 @@ class Gibbs(Covariance):
             r2 = self.square_dist(X, Xs)
         rx2 = tt.reshape(tt.square(rx), (-1, 1))
         rz2 = tt.reshape(tt.square(rz), (1, -1))
-        return (tt.sqrt((2.0 * tt.outer(rx, rz)) / (rx2 + rz2))
-                * tt.exp(-1.0 * r2 / (rx2 + rz2)))
+        return tt.sqrt((2.0 * tt.outer(rx, rz)) / (rx2 + rz2)) * tt.exp(
+            -1.0 * r2 / (rx2 + rz2)
+        )
 
     def diag(self, X):
         return tt.alloc(1.0, X.shape[0])
 
 
 class ScaledCov(Covariance):
-    R"""
+    r"""
     Construct a kernel by multiplying a base kernel with a scaling
     function defined using Theano.  The scaling function is
     non-negative, and can be parameterized.
@@ -566,6 +601,7 @@ class ScaledCov(Covariance):
     args : optional, tuple or list of scalars or PyMC3 variables
         Additional inputs (besides X or Xs) to lengthscale_func.
     """
+
     def __init__(self, input_dim, cov_func, scaling_func, args=None, active_dims=None):
         super().__init__(input_dim, active_dims)
         if not callable(scaling_func):
@@ -593,7 +629,7 @@ class ScaledCov(Covariance):
 
 
 class Coregion(Covariance):
-    R"""Covariance function for intrinsic/linear coregionalization models.
+    r"""Covariance function for intrinsic/linear coregionalization models.
     Adapted from GPy http://gpy.readthedocs.io/en/deploy/GPy.kern.src.html#GPy.kern.src.coregionalize.Coregionalize.
 
     This covariance has the form:
@@ -628,10 +664,12 @@ class Coregion(Covariance):
     def __init__(self, input_dim, W=None, kappa=None, B=None, active_dims=None):
         super().__init__(input_dim, active_dims)
         if len(self.active_dims) != 1:
-            raise ValueError('Coregion requires exactly one dimension to be active')
+            raise ValueError("Coregion requires exactly one dimension to be active")
         make_B = W is not None or kappa is not None
         if make_B and B is not None:
-            raise ValueError('Exactly one of (W, kappa) and B must be provided to Coregion')
+            raise ValueError(
+                "Exactly one of (W, kappa) and B must be provided to Coregion"
+            )
         if make_B:
             self.W = tt.as_tensor_variable(W)
             self.kappa = tt.as_tensor_variable(kappa)
@@ -639,20 +677,22 @@ class Coregion(Covariance):
         elif B is not None:
             self.B = tt.as_tensor_variable(B)
         else:
-            raise ValueError('Exactly one of (W, kappa) and B must be provided to Coregion')
+            raise ValueError(
+                "Exactly one of (W, kappa) and B must be provided to Coregion"
+            )
 
     def full(self, X, Xs=None):
         X, Xs = self._slice(X, Xs)
-        index = tt.cast(X, 'int32')
+        index = tt.cast(X, "int32")
         if Xs is None:
             index2 = index.T
         else:
-            index2 = tt.cast(Xs, 'int32').T
+            index2 = tt.cast(Xs, "int32").T
         return self.B[index, index2]
 
     def diag(self, X):
         X, _ = self._slice(X, None)
-        index = tt.cast(X, 'int32')
+        index = tt.cast(X, "int32")
         return tt.diag(self.B)[index.ravel()]
 
 
@@ -664,6 +704,5 @@ def handle_args(func, args):
             if not isinstance(args, tuple):
                 args = (args,)
             return func(x, *args)
+
     return f
-
-
