Fixed float32 precision errors

pymc3/tests/test_minibatches.py
@@ -232,7 +232,14 @@ class TestScaling:
         with pm.Model() as model5:
             Normal('n', observed=[[1]], total_size=[2, Ellipsis, 2])
             p5 = theano.function([], model5.logpt)
-        assert p0() == p1() == p2() == p3() == p4() == p5()
+        _p0 = p0()
+        assert (
+            np.allclose(_p0, p1()) and
+            np.allclose(_p0, p2()) and
+            np.allclose(_p0, p3()) and
+            np.allclose(_p0, p4()) and
+            np.allclose(_p0, p5())
+        )
 
     def test_common_errors(self):
         with pm.Model():