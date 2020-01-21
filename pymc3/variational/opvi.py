2623aa863b795df7520cda9be932562df6baf9ce
Rushabh Vasani <vasanirushabh24@gmail.com>
2020-01-21 07:03:30 +0530

    add Apache license to all the .py files

--- pymc3/variational/opvi.py
+++ pymc3/variational/opvi.py
@@ -1,3 +1,17 @@
+#   Copyright 2020 The PyMC Developers
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 R"""
 Variational inference is a great approach for doing really complex,
 often intractable Bayesian inference in approximate form. Common methods
