b7265c94de747da3e013985e0d4acb2608b373d0
Greg Mingas <gmingas@turing.ac.uk>
2020-03-16 13:36:18 +0000

    Fix examples code style/comments

--- pymc3/examples/simple_test.py
+++ pymc3/examples/simple_test.py
@@ -12,24 +12,16 @@ seed = 12345
 np.random.seed(seed)
 
 x = np.linspace(0, 1, size)
-# y = a + b*x
 true_regression_line = true_intercept + true_slope * x
 # add noise
 y = true_regression_line + np.random.normal(scale=.5, size=size)
 
 data = dict(x=x, y=y)
 
-# fig = plt.figure(figsize=(7, 7))
-# ax = fig.add_subplot(111, xlabel='x', ylabel='y', title='Generated data and underlying model')
-# ax.plot(x, y, 'x', label='sampled data')
-# ax.plot(x, true_regression_line, label='true regression line', lw=2.)
-# plt.legend(loc=0);
-
-
 with pm.Model() as model0:
     # Define priors
-    sigma = .5  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+    sigma = .5
+    intercept = true_intercept
     x_coeff = pm.Normal('x', 0, sigma=20)
 
     # Define likelihood
@@ -38,8 +30,8 @@ with pm.Model() as model0:
 
 with pm.Model() as model1:
     # Define priors
-    sigma = .5  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+    sigma = .5
+    intercept = true_intercept
     x_coeff = pm.Normal('x', 0, sigma=20)
 
     # Define likelihood
@@ -50,8 +42,8 @@ coarse_models = [model0, model1]
 
 with pm.Model() as model:  # model specifications in PyMC3 are wrapped in a with-statement
     # Define priors
-    sigma = .5  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+    sigma = .5
+    intercept = true_intercept
     x_coeff = pm.Normal('x', 0, sigma=20)
 
     # Define likelihood
@@ -59,15 +51,10 @@ with pm.Model() as model:  # model specifications in PyMC3 are wrapped in a with
                            sigma=sigma, observed=y)
 
     # Inference!
-    # trace = pm.sample(1000, cores=2, **{"ml_models": [1, 2, 3]})
     step_temp = pm.MLDA(subsampling_rate=3, coarse_models=coarse_models)
     trace = pm.sample(50, chains=1, cores=1, tune=20, step=step_temp, random_seed=seed)
     trace2 = pm.sample(50, chains=1, cores=1, tune=20, random_seed=seed)
 
-    #import ipdb; ipdb.set_trace()
-    #plt.hist(trace.get_values(varname='x'))
-    #plt.show()
-    
     print(pm.stats.summary(trace))
     print(pm.stats.summary(trace2))
 
--- pymc3/examples/simple_test_2.py
+++ pymc3/examples/simple_test_2.py
@@ -8,53 +8,36 @@ seed = 12345
 
 np.random.seed(seed)
 size = 100
-true_mean = 2.0
+true_mean = 12.0
 
-y = np.random.normal(scale=1.0, size=size) + true_mean
-
+y = np.array([true_mean])
 
 with pm.Model() as model0:
-    # Define priors
-    sigma = 1.0  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    #intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+    sigma = 1.0
     x_coeff = pm.Normal('x', true_mean, sigma=1000.0)
-
     # Define likelihood
-    likelihood = pm.Normal('y', mu=x_coeff + 1.0,
-                           sigma=sigma, observed=y)
+    likelihood = pm.Normal('y', mu=x_coeff,
+                           sigma=sigma, observed=y+4.0)
 
 with pm.Model() as model1:
-    # Define priors
-    sigma = 1.0  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    # intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+    sigma = 1.0
     x_coeff = pm.Normal('x', true_mean, sigma=1000.0)
-
     # Define likelihood
-    likelihood = pm.Normal('y', mu=x_coeff + 0.5,
-                           sigma=sigma, observed=y)
+    likelihood = pm.Normal('y', mu=x_coeff,
+                           sigma=sigma, observed=y+2.0)
 
 coarse_models = [model0, model1]
 
-with pm.Model() as model:  # model specifications in PyMC3 are wrapped in a with-statement
-    # Define priors
-    sigma = 1.0  # pm.HalfCauchy('sigma', beta=10, testval=1.)
-    # intercept = true_intercept  # pm.Normal('Intercept', 0, sigma=20)
+with pm.Model() as model:
+    sigma = 1.0
     x_coeff = pm.Normal('x', true_mean, sigma=1000.0)
-
     # Define likelihood
     likelihood = pm.Normal('y', mu=x_coeff,
                            sigma=sigma, observed=y)
-
     # Inference!
-    # trace = pm.sample(1000, cores=2, **{"ml_models": [1, 2, 3]}) # draw 3000 posterior samples using NUTS sampling
-    # step_temp = pm.MLDA(subsampling_rate=2, **{"coarse_models": coarse_models})
-    step_temp = pm.MLDA(subsampling_rate=10, coarse_models=coarse_models)
-    trace = pm.sample(50, chains=1, cores=1, tune=20, step=step_temp, random_seed=seed)
-    trace2 = pm.sample(50, chains=1, cores=1, tune=20, random_seed=seed)
-
-    # import ipdb; ipdb.set_trace()
-    # plt.hist(trace.get_values(varname='x'))
-    # plt.show()
+    step_temp = pm.MLDA(subsampling_rate=2, coarse_models=coarse_models)
+    trace = pm.sample(200, chains=1, cores=1, tune=20, step=step_temp, random_seed=seed)
+    trace2 = pm.sample(200, chains=1, cores=1, tune=20, random_seed=seed)
 
     print(pm.stats.summary(trace))
     print(pm.stats.summary(trace2))
