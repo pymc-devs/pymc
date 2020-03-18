9df27b35c859d4b0bb754a6bfdec0216369c5f60
Ahan M R <ahanmr@gmail.com>
2020-03-19 03:16:27 +0530

    fixes #3051

--- pymc3/distributions/multivariate.py
+++ pymc3/distributions/multivariate.py
@@ -327,7 +327,7 @@ class MvNormal(_QuadFormBase):
         TensorVariable
         """
         quaddist, logdet, ok = self._quaddist(value)
-        k = intX(value.shape[-1]).astype(theano.config.floatX)
+        k = floatX(value.shape[-1])
         norm = - 0.5 * k * pm.floatX(np.log(2 * np.pi))
         return bound(norm - 0.5 * quaddist - logdet, ok)
 
@@ -441,7 +441,7 @@ class MvStudentT(_QuadFormBase):
         TensorVariable
         """
         quaddist, logdet, ok = self._quaddist(value)
-        k = intX(value.shape[-1]).astype(theano.config.floatX)
+        k = floatX(value.shape[-1])
 
         norm = (gammaln((self.nu + k) / 2.)
                 - gammaln(self.nu / 2.)
