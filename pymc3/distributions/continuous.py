b47e4746674bf74c7dab5b0e90064d041bec7c69
Fearghus Robert Keeble <f.k@cern.ch>
2020-03-30 12:01:45 +0100

    Updated for scipy rice distribution convention.

--- pymc3/distributions/continuous.py
+++ pymc3/distributions/continuous.py
@@ -3891,10 +3891,10 @@ class Rice(PositiveContinuous):
         import scipy.stats as st
         plt.style.use('seaborn-darkgrid')
         x = np.linspace(0, 8, 500)
-        nus = [0., 0., 2., 4.]
+        nus = [0., 0., 4., 4.]
         sigmas = [1., 2., 2., 1.]
         for nu, sigma in  zip(nus, sigmas):
-            pdf = st.rice.pdf(x, nu, scale=sigma)
+            pdf = st.rice.pdf(x, nu / sigma, scale=sigma)
             plt.plot(x, pdf, label=r'$\nu$ = {}, $\sigma$ = {}'.format(nu, sigma))
         plt.xlabel('x', fontsize=12)
         plt.ylabel('f(x)', fontsize=12)
