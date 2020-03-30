1992d1ede15bcafc5a676043eeb07c8706310bf5
Fearghus Robert Keeble <f.k@cern.ch>
2020-03-30 12:04:54 +0100

    Reordering sigmas in rice docstring.

--- pymc3/distributions/continuous.py
+++ pymc3/distributions/continuous.py
@@ -3892,7 +3892,7 @@ class Rice(PositiveContinuous):
         plt.style.use('seaborn-darkgrid')
         x = np.linspace(0, 8, 500)
         nus = [0., 0., 4., 4.]
-        sigmas = [1., 2., 2., 1.]
+        sigmas = [1., 2., 1., 2.]
         for nu, sigma in  zip(nus, sigmas):
             pdf = st.rice.pdf(x, nu / sigma, scale=sigma)
             plt.plot(x, pdf, label=r'$\nu$ = {}, $\sigma$ = {}'.format(nu, sigma))
