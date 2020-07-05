8e38d0db11e4a7a922be3ffb438e71ecfa6419b4
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2020-07-05 16:41:01 +0200

    Update conda packages during travis tests

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -33,9 +33,10 @@ if [ -z ${GLOBAL} ]; then
   source activate ${ENVNAME}
 fi
 
+conda update --yes --all
 conda install --yes python-graphviz ipywidgets
-conda install --yes --file requirements-dev.txt
 conda install --yes --file requirements.txt
+conda install --yes --file requirements-dev.txt
 
 #  Install editable using the setup.py
 if [ -z ${NO_SETUP} ]; then
