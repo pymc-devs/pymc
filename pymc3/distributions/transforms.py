1e047eb3d1478e7872cca9a5b8137b0ffa17c6e8
Eelke Spaak <eelke.spaak@gmail.com>
2020-08-11 09:22:25 +0200

    restoring return None behavior of TransformedDistribution::_repr_latex_

--- pymc3/distributions/transforms.py
+++ pymc3/distributions/transforms.py
@@ -196,6 +196,11 @@ class TransformedDistribution(distribution.Distribution):
         """
         return self.dist.logp(self.transform_used.backward(x))
 
+    def _repr_latex_(self, **kwargs):
+        # prevent TransformedDistributions from ending up in LaTeX representations
+        # of models
+        return None
+
 
 transform = Transform
 
