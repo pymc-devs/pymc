da52490b06f62609d60d0271f8b14f11ca748f71
AlexAndorra <andorra.alexandre@gmail.com>
2020-08-12 16:51:54 +0200

    Reorganized import statements in continuous.py

--- pymc3/distributions/continuous.py
+++ pymc3/distributions/continuous.py
@@ -17,17 +17,13 @@
 A collection of common probability distributions for stochastic
 nodes in PyMC.
 """
+import warnings
+
 import numpy as np
 import theano
 import theano.tensor as tt
-from scipy import stats
-from scipy.special import expit
-from scipy.interpolate import InterpolatedUnivariateSpline
-import warnings
 
-from pymc3.theanof import floatX
 from . import transforms
-from pymc3.util import get_variable_name
 from .special import log_i0
 from ..math import invlogit, logit, logdiffexp
 from .dist_math import (
@@ -46,6 +42,11 @@ from .dist_math import (
     clipped_beta_rvs,
 )
 from .distribution import Continuous, draw_values, generate_samples
+from pymc3.theanof import floatX
+from pymc3.util import get_variable_name
+from scipy import stats
+from scipy.special import expit
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 __all__ = [
     "Uniform",
