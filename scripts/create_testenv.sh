712e3fbfc879aa7baaf2eab0f7bfc5afb46b41c7
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2020-08-16 14:14:52 +0200

    Use conda activate in create testenv

--- scripts/create_testenv.sh
+++ scripts/create_testenv.sh
@@ -22,16 +22,17 @@ command -v conda >/dev/null 2>&1 || {
 ENVNAME="${ENVNAME:-testenv}"         # if no ENVNAME is specified, use testenv
 
 if [ -z ${GLOBAL} ]; then
+  source $(dirname $(dirname $(which conda)))/etc/profile.d/conda.sh
   if conda env list | grep -q ${ENVNAME}; then
     echo "Environment ${ENVNAME} already exists, keeping up to date"
-    source activate ${ENVNAME}
+    conda activate ${ENVNAME}
     mamba env update -f environment-dev.yml
   else
     conda config --add channels conda-forge
     conda config --set channel_priority strict
     conda install -c conda-forge mamba --yes
     mamba env create -f environment-dev.yml
-    source activate ${ENVNAME}
+    conda activate ${ENVNAME}
   fi
 fi
 
