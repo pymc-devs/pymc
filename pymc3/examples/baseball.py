5605aab8a3874d6f487d1a2cedff906135286cd9
Marco Gorelli <m.e.gorelli@gmail.com>
2020-09-19 10:31:24 +0100

    blacken examples (#4118)

--- pymc3/examples/baseball.py
+++ pymc3/examples/baseball.py
@@ -6,24 +6,27 @@
 import pymc3 as pm
 import numpy as np
 
+
 def build_model():
-    data = np.loadtxt(pm.get_data('efron-morris-75-data.tsv'), delimiter="\t", 
-                      skiprows=1, usecols=(2,3))
-    
-    atbats = pm.floatX(data[:,0])
-    hits = pm.floatX(data[:,1])
-    
+    data = np.loadtxt(
+        pm.get_data("efron-morris-75-data.tsv"), delimiter="\t", skiprows=1, usecols=(2, 3)
+    )
+
+    atbats = pm.floatX(data[:, 0])
+    hits = pm.floatX(data[:, 1])
+
     N = len(hits)
-    
+
     # we want to bound the kappa below
     BoundedKappa = pm.Bound(pm.Pareto, lower=1.0)
-    
+
     with pm.Model() as model:
-        phi = pm.Uniform('phi', lower=0.0, upper=1.0)
-        kappa = BoundedKappa('kappa', alpha=1.0001, m=1.5)
-        thetas = pm.Beta('thetas', alpha=phi*kappa, beta=(1.0-phi)*kappa, shape=N)
-        ys = pm.Binomial('ys', n=atbats, p=thetas, observed=hits)
+        phi = pm.Uniform("phi", lower=0.0, upper=1.0)
+        kappa = BoundedKappa("kappa", alpha=1.0001, m=1.5)
+        thetas = pm.Beta("thetas", alpha=phi * kappa, beta=(1.0 - phi) * kappa, shape=N)
+        ys = pm.Binomial("ys", n=atbats, p=thetas, observed=hits)
     return model
+
 
 def run(n=2000):
     model = build_model()
@@ -32,5 +35,6 @@ def run(n=2000):
 
     pm.traceplot(trace)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
--- pymc3/examples/custom_dists.py
+++ pymc3/examples/custom_dists.py
@@ -22,23 +22,25 @@ ydata = theta_true[0] + theta_true[1] * xdata
 # add scatter to points
 xdata = np.random.normal(xdata, 10)
 ydata = np.random.normal(ydata, 10)
-data = {'x': xdata, 'y': ydata}
+data = {"x": xdata, "y": ydata}
 
 # define loglikelihood outside of the model context, otherwise cores wont work:
 # Lambdas defined in local namespace are not picklable (see issue #1995)
 def loglike1(value):
-    return -1.5 * tt.log(1 + value**2)
+    return -1.5 * tt.log(1 + value ** 2)
+
+
 def loglike2(value):
     return -tt.log(tt.abs_(value))
 
+
 with pm.Model() as model:
-    alpha = pm.Normal('intercept', mu=0, sigma=100)
+    alpha = pm.Normal("intercept", mu=0, sigma=100)
     # Create custom densities
-    beta = pm.DensityDist('slope', loglike1, testval=0)
-    sigma = pm.DensityDist('sigma', loglike2, testval=1)
+    beta = pm.DensityDist("slope", loglike1, testval=0)
+    sigma = pm.DensityDist("sigma", loglike2, testval=1)
     # Create likelihood
-    like = pm.Normal('y_est', mu=alpha + beta *
-                        xdata, sigma=sigma, observed=ydata)
+    like = pm.Normal("y_est", mu=alpha + beta * xdata, sigma=sigma, observed=ydata)
 
     trace = pm.sample(2000, cores=2)
 
@@ -47,10 +49,11 @@ with pm.Model() as model:
 # Create some convenience routines for plotting
 # All functions below written by Jake Vanderplas
 
+
 def compute_sigma_level(trace1, trace2, nbins=20):
     """From a set of traces, bin by number of standard deviations"""
     L, xbins, ybins = np.histogram2d(trace1, trace2, nbins)
-    L[L == 0] = 1E-16
+    L[L == 0] = 1e-16
 
     shape = L.shape
     L = L.ravel()
@@ -73,14 +76,14 @@ def plot_MCMC_trace(ax, xdata, ydata, trace, scatter=False, **kwargs):
     xbins, ybins, sigma = compute_sigma_level(trace[0], trace[1])
     ax.contour(xbins, ybins, sigma.T, levels=[0.683, 0.955], **kwargs)
     if scatter:
-        ax.plot(trace[0], trace[1], ',k', alpha=0.1)
-    ax.set_xlabel(r'$\alpha$')
-    ax.set_ylabel(r'$\beta$')
+        ax.plot(trace[0], trace[1], ",k", alpha=0.1)
+    ax.set_xlabel(r"$\alpha$")
+    ax.set_ylabel(r"$\beta$")
 
 
 def plot_MCMC_model(ax, xdata, ydata, trace):
     """Plot the linear model and 2sigma contours"""
-    ax.plot(xdata, ydata, 'ok')
+    ax.plot(xdata, ydata, "ok")
 
     alpha, beta = trace[:2]
     xfit = np.linspace(-20, 120, 10)
@@ -88,22 +91,21 @@ def plot_MCMC_model(ax, xdata, ydata, trace):
     mu = yfit.mean(0)
     sig = 2 * yfit.std(0)
 
-    ax.plot(xfit, mu, '-k')
-    ax.fill_between(xfit, mu - sig, mu + sig, color='lightgray')
+    ax.plot(xfit, mu, "-k")
+    ax.fill_between(xfit, mu - sig, mu + sig, color="lightgray")
 
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
 
 
-def plot_MCMC_results(xdata, ydata, trace, colors='k'):
+def plot_MCMC_results(xdata, ydata, trace, colors="k"):
     """Plot both the trace and the model together"""
     _, ax = plt.subplots(1, 2, figsize=(10, 4))
     plot_MCMC_trace(ax[0], xdata, ydata, trace, True, colors=colors)
     plot_MCMC_model(ax[1], xdata, ydata, trace)
 
-pymc3_trace = [trace['intercept'],
-               trace['slope'],
-               trace['sigma']]
+
+pymc3_trace = [trace["intercept"], trace["slope"], trace["sigma"]]
 
 plot_MCMC_results(xdata, ydata, pymc3_trace)
 plt.show()
