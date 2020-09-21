ba77d8502704e8aeb112782ee104fb339393cb19
Osvaldo Martin <aloctavodia@gmail.com>
2020-09-21 11:37:54 -0300

    Change SMC metropolis kernel to independent metropolis kernel (#4115)
    * change metropolis kernel for independent metropolis kernel
    
    * fix pyupgrade
    
    * fix float32
    
    * clean code
    
    * add inline comments and update docstring
    
    * add inline comments and update docstring

--- pymc3/smc/smc.py
+++ pymc3/smc/smc.py
@@ -16,6 +16,7 @@ from collections import OrderedDict
 
 import numpy as np
 from scipy.special import logsumexp
+from scipy.stats import multivariate_normal
 from theano import function as theano_function
 import theano.tensor as tt
 
@@ -33,7 +34,7 @@ class SMC:
         n_steps=25,
         start=None,
         tune_steps=True,
-        p_acc_rate=0.99,
+        p_acc_rate=0.85,
         threshold=0.5,
         save_sim_data=False,
         model=None,
@@ -42,7 +43,7 @@ class SMC:
     ):
 
         self.draws = draws
-        self.kernel = kernel
+        self.kernel = kernel.lower()
         self.n_steps = n_steps
         self.start = start
         self.tune_steps = tune_steps
@@ -62,10 +63,7 @@ class SMC:
         self.max_steps = n_steps
         self.proposed = draws * n_steps
         self.acc_rate = 1
-        self.acc_per_chain = np.ones(self.draws)
         self.variables = inputvars(self.model.vars)
-        self.dimension = sum(v.dsize for v in self.variables)
-        self.scalings = np.ones(self.draws) * 2.38 / (self.dimension) ** 0.5
         self.weights = np.ones(self.draws) / self.draws
         self.log_marginal_likelihood = 0
         self.sim_data = []
@@ -78,7 +76,9 @@ class SMC:
         var_info = OrderedDict()
         if self.start is None:
             init_rnd = sample_prior_predictive(
-                self.draws, var_names=[v.name for v in self.model.unobserved_RVs], model=self.model,
+                self.draws,
+                var_names=[v.name for v in self.model.unobserved_RVs],
+                model=self.model,
             )
         else:
             init_rnd = self.start
@@ -102,7 +102,7 @@ class SMC:
         """
         shared = make_shared_replacements(self.variables, self.model)
 
-        if self.kernel.lower() == "abc":
+        if self.kernel == "abc":
             factors = [var.logpt for var in self.model.free_RVs]
             factors += [tt.sum(factor) for factor in self.model.potentials]
             self.prior_logp_func = logp_forw([tt.sum(factors)], self.variables, shared)
@@ -122,7 +122,7 @@ class SMC:
                 self.draws,
                 self.save_sim_data,
             )
-        elif self.kernel.lower() == "metropolis":
+        elif self.kernel == "metropolis":
             self.prior_logp_func = logp_forw([self.model.varlogpt], self.variables, shared)
             self.likelihood_logp_func = logp_forw([self.model.datalogpt], self.variables, shared)
 
@@ -136,7 +136,7 @@ class SMC:
         self.prior_logp = np.array(priors).squeeze()
         self.likelihood_logp = np.array(likelihoods).squeeze()
 
-        if self.save_sim_data:
+        if self.kernel == "abc" and self.save_sim_data:
             self.sim_data = self.likelihood_logp_func.get_data()
 
     def update_weights_beta(self):
@@ -180,8 +180,6 @@ class SMC:
         self.prior_logp = self.prior_logp[resampling_indexes]
         self.likelihood_logp = self.likelihood_logp[resampling_indexes]
         self.posterior_logp = self.prior_logp + self.likelihood_logp * self.beta
-        self.acc_per_chain = self.acc_per_chain[resampling_indexes]
-        self.scalings = self.scalings[resampling_indexes]
         if self.save_sim_data:
             self.sim_data = self.sim_data[resampling_indexes]
 
@@ -198,17 +196,13 @@ class SMC:
 
     def tune(self):
         """
-        Tune scaling and n_steps based on the acceptance rate.
+        Tune n_steps based on the acceptance rate.
         """
-        ave_scaling = np.exp(np.log(self.scalings.mean()) + (self.acc_per_chain.mean() - 0.234))
-        self.scalings = 0.5 * (
-            ave_scaling + np.exp(np.log(self.scalings) + (self.acc_per_chain - 0.234))
-        )
-
         if self.tune_steps:
             acc_rate = max(1.0 / self.proposed, self.acc_rate)
             self.n_steps = min(
-                self.max_steps, max(2, int(np.log(1 - self.p_acc_rate) / np.log(1 - acc_rate))),
+                self.max_steps,
+                max(2, int(np.log(1 - self.p_acc_rate) / np.log(1 - acc_rate))),
             )
 
         self.proposed = self.draws * self.n_steps
@@ -216,29 +210,34 @@ class SMC:
     def mutate(self):
         ac_ = np.empty((self.n_steps, self.draws))
 
-        proposals = (
-            np.random.multivariate_normal(
-                np.zeros(self.dimension), self.cov, size=(self.n_steps, self.draws)
-            )
-            * self.scalings[:, None]
-        )
         log_R = np.log(np.random.rand(self.n_steps, self.draws))
 
+        # The proposal distribution is a MVNormal, with mean and covariance computed from the previous tempered posterior
+        dist = multivariate_normal(self.posterior.mean(axis=0), self.cov)
+
         for n_step in range(self.n_steps):
-            proposal = floatX(self.posterior + proposals[n_step])
+            # The proposal is independent from the current point.
+            # We have to take that into account to compute the Metropolis-Hastings acceptance
+            proposal = floatX(dist.rvs(size=self.draws))
+            proposal = proposal.reshape(len(proposal), -1)
+            # To do that we compute the logp of moving to a new point
+            forward = dist.logpdf(proposal)
+            # And to going back from that new point
+            backward = multivariate_normal(proposal.mean(axis=0), self.cov).logpdf(self.posterior)
             ll = np.array([self.likelihood_logp_func(prop) for prop in proposal])
             pl = np.array([self.prior_logp_func(prop) for prop in proposal])
             proposal_logp = pl + ll * self.beta
-            accepted = log_R[n_step] < (proposal_logp - self.posterior_logp)
+            accepted = log_R[n_step] < (
+                (proposal_logp + backward) - (self.posterior_logp + forward)
+            )
             ac_[n_step] = accepted
             self.posterior[accepted] = proposal[accepted]
             self.posterior_logp[accepted] = proposal_logp[accepted]
             self.prior_logp[accepted] = pl[accepted]
             self.likelihood_logp[accepted] = ll[accepted]
-            if self.save_sim_data:
+            if self.kernel == "abc" and self.save_sim_data:
                 self.sim_data[accepted] = self.likelihood_logp_func.get_data()[accepted]
 
-        self.acc_per_chain = np.mean(ac_, axis=0)
         self.acc_rate = np.mean(ac_)
 
     def posterior_to_trace(self):
