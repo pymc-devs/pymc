154ea6384908fec6ff642923f7e26fba3c5ce1d2
Ali Akbar Septiandri <mail.septiandri@gmail.com>
2020-09-27 12:17:42 +0100

    Run black on step_methods/

--- pymc3/step_methods/gibbs.py
+++ pymc3/step_methods/gibbs.py
@@ -12,11 +12,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-'''
+"""
 Created on May 12, 2012
 
 @author: john
-'''
+"""
 from .arraystep import ArrayStep, Competence
 from ..distributions.discrete import Categorical
 from numpy import array, max, exp, cumsum, nested_iters, empty, searchsorted, ones, arange
@@ -26,7 +26,8 @@ from warnings import warn
 from theano.gof.graph import inputs
 from theano.tensor import add
 from ..model import modelcontext
-__all__ = ['ElemwiseCategorical']
+
+__all__ = ["ElemwiseCategorical"]
 
 
 class ElemwiseCategorical(ArrayStep):
@@ -35,13 +36,17 @@ class ElemwiseCategorical(ArrayStep):
     the variable can't be indexed into or transposed or anything otherwise that will mess things up
 
     """
+
     # TODO: It would be great to come up with a way to make
     # ElemwiseCategorical  more general (handling more complex elementwise
     # variables)
 
     def __init__(self, vars, values=None, model=None):
-        warn('ElemwiseCategorical is deprecated, switch to CategoricalGibbsMetropolis.',
-             DeprecationWarning, stacklevel = 2)
+        warn(
+            "ElemwiseCategorical is deprecated, switch to CategoricalGibbsMetropolis.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         model = modelcontext(model)
         self.var = vars[0]
         self.sh = ones(self.var.dshape, self.var.dtype)
@@ -64,8 +69,7 @@ class ElemwiseCategorical(ArrayStep):
 
 
 def elemwise_logp(model, var):
-    terms = [v.logp_elemwiset for v in model.basic_RVs if var in inputs([
-                                                                        v.logpt])]
+    terms = [v.logp_elemwiset for v in model.basic_RVs if var in inputs([v.logpt])]
     return model.fn(add(*terms))
 
 
@@ -73,9 +77,12 @@ def categorical(prob, shape):
     out = empty([1] + list(shape))
 
     n = len(shape)
-    it0, it1 = nested_iters([prob, out], [list(range(1, n + 1)), [0]],
-                            op_flags=[['readonly'], ['readwrite']],
-                            flags=['reduce_ok'])
+    it0, it1 = nested_iters(
+        [prob, out],
+        [list(range(1, n + 1)), [0]],
+        op_flags=[["readonly"], ["readwrite"]],
+        flags=["reduce_ok"],
+    )
 
     for _ in it0:
         p, o = it1.itviews
