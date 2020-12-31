02095c28709cccc48e9dda6988d05112a8c73658
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2020-12-31 00:37:17 -0800

    Fix pymc3 to work with latest theano-pymc master (#4382)
    * Update Theano-PyMC version
    
    * Update JAX imports
    
    * Update RandomStream imports and arguments
    
    * Update use of theano.config.change_flags in pymc3.tests.test_variational_inference
    
    Co-authored-by: Brandon T. Willard <brandonwillard@users.noreply.github.com>

--- pymc3/tests/helpers.py
+++ pymc3/tests/helpers.py
@@ -20,7 +20,7 @@ import numpy.random as nr
 import theano
 
 from theano.gradient import verify_grad as tt_verify_grad
-from theano.sandbox.rng_mrg import MRG_RandomStreams
+from theano.sandbox.rng_mrg import MRG_RandomStream as RandomStream
 
 from pymc3.theanof import set_tt_rng, tt_rng
 
@@ -35,7 +35,7 @@ class SeededTest:
     def setup_method(self):
         nr.seed(self.random_seed)
         self.old_tt_rng = tt_rng()
-        set_tt_rng(MRG_RandomStreams(self.random_seed))
+        set_tt_rng(RandomStream(self.random_seed))
 
     def teardown_method(self):
         set_tt_rng(self.old_tt_rng)
--- pymc3/tests/test_variational_inference.py
+++ pymc3/tests/test_variational_inference.py
@@ -27,7 +27,7 @@ import pymc3.util
 
 from pymc3.tests import models
 from pymc3.tests.helpers import not_raises
-from pymc3.theanof import change_flags, intX
+from pymc3.theanof import intX
 from pymc3.variational import flows, opvi
 from pymc3.variational.approximations import (
     Empirical,
@@ -533,17 +533,20 @@ def test_scale_cost_to_minibatch_works(aux_total_size):
     sigma = 1.0
     y_obs = np.array([1.6, 1.4])
     beta = len(y_obs) / float(aux_total_size)
-    post_mu = np.array([1.88], dtype=theano.config.floatX)
-    post_sigma = np.array([1], dtype=theano.config.floatX)
 
     # TODO: theano_config
     # with pm.Model(theano_config=dict(floatX='float64')):
     # did not not work as expected
     # there were some numeric problems, so float64 is forced
-    with pm.theanof.change_flags(floatX="float64", warn_float64="ignore"):
+    with theano.config.change_flags(floatX="float64", warn_float64="ignore"):
+
+        assert theano.config.floatX == "float64"
+        assert theano.config.warn_float64 == "ignore"
+
+        post_mu = np.array([1.88], dtype=theano.config.floatX)
+        post_sigma = np.array([1], dtype=theano.config.floatX)
+
         with pm.Model():
-            assert theano.config.floatX == "float64"
-            assert theano.config.warn_float64 == "ignore"
             mu = pm.Normal("mu", mu=mu0, sigma=sigma)
             pm.Normal("y", mu=mu, sigma=1, observed=y_obs, total_size=aux_total_size)
             # Create variational gradient tensor
@@ -552,7 +555,7 @@ def test_scale_cost_to_minibatch_works(aux_total_size):
             mean_field_1.shared_params["mu"].set_value(post_mu)
             mean_field_1.shared_params["rho"].set_value(np.log(np.exp(post_sigma) - 1))
 
-            with pm.theanof.change_flags(compute_test_value="off"):
+            with theano.config.change_flags(compute_test_value="off"):
                 elbo_via_total_size_scaled = -pm.operators.KL(mean_field_1)()(10000)
 
         with pm.Model():
@@ -566,7 +569,7 @@ def test_scale_cost_to_minibatch_works(aux_total_size):
             mean_field_2.shared_params["mu"].set_value(post_mu)
             mean_field_2.shared_params["rho"].set_value(np.log(np.exp(post_sigma) - 1))
 
-        with pm.theanof.change_flags(compute_test_value="off"):
+        with theano.config.change_flags(compute_test_value="off"):
             elbo_via_total_size_unscaled = -pm.operators.KL(mean_field_2)()(10000)
 
         np.testing.assert_allclose(
@@ -583,9 +586,12 @@ def test_elbo_beta_kl(aux_total_size):
     sigma = 1.0
     y_obs = np.array([1.6, 1.4])
     beta = len(y_obs) / float(aux_total_size)
-    post_mu = np.array([1.88], dtype=theano.config.floatX)
-    post_sigma = np.array([1], dtype=theano.config.floatX)
-    with pm.theanof.change_flags(floatX="float64", warn_float64="ignore"):
+
+    with theano.config.change_flags(floatX="float64", warn_float64="ignore"):
+
+        post_mu = np.array([1.88], dtype=theano.config.floatX)
+        post_sigma = np.array([1], dtype=theano.config.floatX)
+
         with pm.Model():
             mu = pm.Normal("mu", mu=mu0, sigma=sigma)
             pm.Normal("y", mu=mu, sigma=1, observed=y_obs, total_size=aux_total_size)
@@ -595,7 +601,7 @@ def test_elbo_beta_kl(aux_total_size):
             mean_field_1.shared_params["mu"].set_value(post_mu)
             mean_field_1.shared_params["rho"].set_value(np.log(np.exp(post_sigma) - 1))
 
-            with pm.theanof.change_flags(compute_test_value="off"):
+            with theano.config.change_flags(compute_test_value="off"):
                 elbo_via_total_size_scaled = -pm.operators.KL(mean_field_1)()(10000)
 
         with pm.Model():
@@ -606,7 +612,7 @@ def test_elbo_beta_kl(aux_total_size):
             mean_field_3.shared_params["mu"].set_value(post_mu)
             mean_field_3.shared_params["rho"].set_value(np.log(np.exp(post_sigma) - 1))
 
-            with pm.theanof.change_flags(compute_test_value="off"):
+            with theano.config.change_flags(compute_test_value="off"):
                 elbo_via_beta_kl = -pm.operators.KL(mean_field_3, beta=beta)()(10000)
 
         np.testing.assert_allclose(
@@ -1014,7 +1020,7 @@ def flow_spec(request):
 def test_flow_det(flow_spec):
     z0 = tt.arange(0, 20).astype("float32")
     flow = flow_spec(dim=20, z0=z0.dimshuffle("x", 0))
-    with change_flags(compute_test_value="off"):
+    with theano.config.change_flags(compute_test_value="off"):
         z1 = flow.forward.flatten()
         J = tt.jacobian(z1, z0)
         logJdet = tt.log(tt.abs_(tt.nlinalg.det(J)))
@@ -1030,7 +1036,7 @@ def test_flow_det_local(flow_spec):
         params[k] = np.random.randn(1, *shp).astype("float32")
     flow = flow_spec(dim=12, z0=z0.reshape((1, 1, 12)), **params)
     assert flow.batched
-    with change_flags(compute_test_value="off"):
+    with theano.config.change_flags(compute_test_value="off"):
         z1 = flow.forward.flatten()
         J = tt.jacobian(z1, z0)
         logJdet = tt.log(tt.abs_(tt.nlinalg.det(J)))
