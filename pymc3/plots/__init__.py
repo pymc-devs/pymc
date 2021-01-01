80766126bed2b5558483764b1968ff8a7c2faf9b
CloudChaoszero <rmaldonadocloud@outlook.com>
2021-01-01 02:51:02 -0800

    :tada: Start removing Diagnostics & Plots in PyMC3 development
    :fire: Remove arviz plots
    * Remove directly imported arviz plots used in pymc3 plots
    
    :fire: Remove all plots from PyMC3 Plots module
    
    :fire: Remove PyMC3 plots references in Docs
    
    :art: Mention Plotting & Diagnostics in API page and remove plots reference in __init__.py
    
    :rewind: Revert posterior_plot function, test, and docs
    
    :art: Add deprecation warning to posterior_plot function
    
    :art: Add context on plot import and import back into __init__.py
    
    :pencil2: Add warning and details of posterior_plot added
    
    Update docs/source/api/plots.rst
    
    Co-authored-by: Alexandre ANDORRA <andorra.alexandre@gmail.com>
    
    Update docs/source/api/plots.rst
    
    Co-authored-by: Alexandre ANDORRA <andorra.alexandre@gmail.com>
    
    Update pymc3/plots/__init__.py
    
    Co-authored-by: Alexandre ANDORRA <andorra.alexandre@gmail.com>
    
    Update pymc3/plots/__init__.py
    
    Co-authored-by: Alexandre ANDORRA <andorra.alexandre@gmail.com>
    
    :pencil2: Update docs to add stats.rst details
    
    :pencil2: Minor docs notation for posterioplot function(s)
    
    :pencil: Add breakline before docstring title

--- pymc3/plots/__init__.py
+++ pymc3/plots/__init__.py
@@ -14,9 +14,10 @@
 
 """PyMC3 Plotting.
 
-Plots are delegated to the ArviZ library, a general purpose library for
-"exploratory analysis of Bayesian models." See https://arviz-devs.github.io/arviz/
-for details on plots.
+Plots are delegated to the `ArviZ <https://arviz-devs.github.io/arviz/>`_ library, a general purpose library for
+exploratory analysis of Bayesian models. For more details, see https://arviz-devs.github.io/arviz/.
+
+Only `plot_posterior_predictive_glm` is kept in the PyMC code base for now, but it will move to ArviZ once the latter adds features for regression plots.
 """
 import functools
 import sys
@@ -24,98 +25,6 @@ import warnings
 
 import arviz as az
 
-
-def map_args(func):
-    swaps = [("varnames", "var_names")]
-
-    @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        for (old, new) in swaps:
-            if old in kwargs and new not in kwargs:
-                warnings.warn(
-                    f"Keyword argument `{old}` renamed to `{new}`, and will be removed in pymc3 3.8"
-                )
-                kwargs[new] = kwargs.pop(old)
-            return func(*args, **kwargs)
-
-    return wrapped
-
-
-# pymc3 custom plots: override these names for custom behavior
-autocorrplot = map_args(az.plot_autocorr)
-forestplot = map_args(az.plot_forest)
-kdeplot = map_args(az.plot_kde)
-plot_posterior = map_args(az.plot_posterior)
-energyplot = map_args(az.plot_energy)
-densityplot = map_args(az.plot_density)
-pairplot = map_args(az.plot_pair)
-
-# Use compact traceplot by default
-@map_args
-@functools.wraps(az.plot_trace)
-def traceplot(*args, **kwargs):
-    try:
-        kwargs.setdefault("compact", True)
-        return az.plot_trace(*args, **kwargs)
-    except TypeError:
-        kwargs.pop("compact")
-        return az.plot_trace(*args, **kwargs)
-
-
-# addition arg mapping for compare plot
-@functools.wraps(az.plot_compare)
-def compareplot(*args, **kwargs):
-    if "comp_df" in kwargs:
-        comp_df = kwargs["comp_df"].copy()
-    else:
-        args = list(args)
-        comp_df = args[0].copy()
-    if "WAIC" in comp_df.columns:
-        comp_df = comp_df.rename(
-            index=str,
-            columns={
-                "WAIC": "waic",
-                "pWAIC": "p_waic",
-                "dWAIC": "d_waic",
-                "SE": "se",
-                "dSE": "dse",
-                "var_warn": "warning",
-            },
-        )
-    elif "LOO" in comp_df.columns:
-        comp_df = comp_df.rename(
-            index=str,
-            columns={
-                "LOO": "loo",
-                "pLOO": "p_loo",
-                "dLOO": "d_loo",
-                "SE": "se",
-                "dSE": "dse",
-                "shape_warn": "warning",
-            },
-        )
-    if "comp_df" in kwargs:
-        kwargs["comp_df"] = comp_df
-    else:
-        args[0] = comp_df
-    return az.plot_compare(*args, **kwargs)
-
-
 from pymc3.plots.posteriorplot import plot_posterior_predictive_glm
 
-# Access to arviz plots: base plots provided by arviz
-for plot in az.plots.__all__:
-    setattr(sys.modules[__name__], plot, map_args(getattr(az.plots, plot)))
-
-__all__ = tuple(az.plots.__all__) + (
-    "autocorrplot",
-    "compareplot",
-    "forestplot",
-    "kdeplot",
-    "plot_posterior",
-    "traceplot",
-    "energyplot",
-    "densityplot",
-    "pairplot",
-    "plot_posterior_predictive_glm",
-)
+__all__ = ["plot_posterior_predictive_glm"]
