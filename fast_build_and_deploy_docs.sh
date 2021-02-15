e724067da6386dd511bc8f43eb5a7286ce0d202e
Spyros Stasis <spyros_stasis@hotmail.com>
2021-02-15 11:39:51 +0000

    Create bash script for faster build

---
+++ fast_build_and_deploy_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+latesttag=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo checking out ${latesttag}
+git checkout ${latesttag}
+git submodule update --remote
+pushd docs/source
+make html
+ghp-import -c docs.pymc.io -n -p _build/html/
+popd
+cd docs/source/pymc-examples
+git checkout fast-build-docs
