34ae673efd1c089e68ea2227230e2ffe3cbf1a50
Spyros Stasis <spyros_stasis@hotmail.com>
2021-02-15 12:34:55 +0000

    Address comments and suggestions on bash script

--- fast_build_and_deploy_docs.sh
+++ fast_build_and_deploy_docs.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 git --git-dir=docs/source/pymc-examples/.git --work-tree=docs/source/pymc-examples checkout fast-docs-build
-git submodule update --remote
 pushd docs/source
+make clean
 make html
-ghp-import -c docs.pymc.io -n -p _build/html/
 popd
