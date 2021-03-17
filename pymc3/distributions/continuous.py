81c5743d6df34fdb364b2b45ca2957b6c175ee21
Ricardo <ricardo.vieira1994@gmail.com>
2021-03-17 16:05:01 +0100

    Fix HalfNormal/HalfNormalRV parameterization

--- pymc3/distributions/continuous.py
+++ pymc3/distributions/continuous.py
@@ -788,18 +788,12 @@ class HalfNormal(PositiveContinuous):
 
         tau, sigma = get_tau_sigma(tau=tau, sigma=sigma)
 
-        # sigma = sd = sigma = at.as_tensor_variable(sigma)
-        # tau = tau = at.as_tensor_variable(tau)
-
-        # mean = at.sqrt(2 / (np.pi * tau))
-        # variance = (1.0 - 2 / np.pi) / tau
-
         assert_negative_support(tau, "tau", "HalfNormal")
         assert_negative_support(sigma, "sigma", "HalfNormal")
 
-        return super().dist([sigma, tau], **kwargs)
+        return super().dist([0.0, sigma], **kwargs)
 
-    def logp(value, sigma, tau):
+    def logp(value, loc, sigma):
         """
         Calculate log-probability of HalfNormal distribution at specified value.
 
@@ -813,14 +807,16 @@ class HalfNormal(PositiveContinuous):
         -------
         TensorVariable
         """
+        tau, sigma = get_tau_sigma(tau=None, sigma=sigma)
+
         return bound(
-            -0.5 * tau * value ** 2 + 0.5 * at.log(tau * 2.0 / np.pi),
-            value >= 0,
+            -0.5 * tau * (value - loc) ** 2 + 0.5 * at.log(tau * 2.0 / np.pi),
+            value >= loc,
             tau > 0,
             sigma > 0,
         )
 
-    def logcdf(value, sigma, tau):
+    def logcdf(value, loc, sigma):
         """
         Compute the log of the cumulative distribution function for HalfNormal distribution
         at the specified value.
@@ -835,10 +831,10 @@ class HalfNormal(PositiveContinuous):
         -------
         TensorVariable
         """
-        z = zvalue(value, mu=0, sigma=sigma)
+        z = zvalue(value, mu=loc, sigma=sigma)
         return bound(
             at.log1p(-at.erfc(z / at.sqrt(2.0))),
-            0 <= value,
+            loc <= value,
             0 < sigma,
         )
 
