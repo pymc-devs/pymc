b2445bd0e7f8cd1c48e879cbeb75da2396f9ac95
almostmeenal <meenaljhajharia2304@gmail.com>
2021-03-23 21:16:29 +0530

    checking something / no actual  changes

--- pymc3/distributions/continuous.py
+++ pymc3/distributions/continuous.py
@@ -11,7 +11,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
+#checkingsomething
 # coding: utf-8
 """
 A collection of common probability distributions for stochastic
