475d56932c4dd12965663089346d9fc2ca64e269
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2021-03-24 12:34:46 -0500

    Make sure forward transformed input is a TensorVariable in TestMatchesScipy

--- pymc3/tests/test_distributions.py
+++ pymc3/tests/test_distributions.py
@@ -1944,7 +1944,7 @@ class TestMatchesScipy:
         d_value = d.tag.value_var
         d_point = d.eval()
         if hasattr(d_value.tag, "transform"):
-            d_point_trans = d_value.tag.transform.forward(d, d_point).eval()
+            d_point_trans = d_value.tag.transform.forward(d, aet.as_tensor(d_point)).eval()
         else:
             d_point_trans = d_point
 
