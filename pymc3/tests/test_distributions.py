2a7d6b249a327e33b9bd961d74141f91ed270aff
ricardoV94 <28983449+ricardoV94@users.noreply.github.com>
2021-04-15 19:51:51 +0200

    Refactor DiscreteWeibull (#4615)

--- pymc3/tests/test_distributions.py
+++ pymc3/tests/test_distributions.py
@@ -424,10 +424,7 @@ def logpow(v, p):
 
 def discrete_weibull_logpmf(value, q, beta):
     return floatX(
-        np.log(
-            np.power(floatX(q), np.power(floatX(value), floatX(beta)))
-            - np.power(floatX(q), np.power(floatX(value + 1), floatX(beta)))
-        )
+        np.log(np.power(q, np.power(value, beta)) - np.power(q, np.power(value + 1, beta)))
     )
 
 
@@ -1556,7 +1553,6 @@ class TestMatchesScipy:
             {"p": Unit},
         )
 
-    @pytest.mark.xfail(reason="Distribution not refactored yet")
     def test_discrete_weibull(self):
         self.check_logp(
             DiscreteWeibull,
