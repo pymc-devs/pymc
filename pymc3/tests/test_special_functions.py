7e88fa508dddf4a5c0c337a61ef143250eee5a7a
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2021-06-01 16:22:28 -0500

    Remove unnecessary gammaln and psi from pymc3.distributions.special

--- pymc3/tests/test_special_functions.py
+++ pymc3/tests/test_special_functions.py
@@ -16,61 +16,30 @@ import aesara.tensor as at
 import numpy as np
 import scipy.special as ss
 
-from aesara import function
+from aesara import config, function
 
 import pymc3.distributions.special as ps
 
 from pymc3.tests.checks import close_to
 
 
-def test_functions():
-    xvals = list(map(np.atleast_1d, [0.01, 0.1, 2, 100, 10000]))
-
-    x = at.dvector("x")
-    x.tag.test_value = xvals[0]
-
-    p = at.iscalar("p")
-    p.tag.test_value = 1
-
-    gammaln = function([x], ps.gammaln(x))
-    psi = function([x], ps.psi(x))
-    function([x, p], ps.multigammaln(x, p))
-    for x in xvals:
-        check_vals(gammaln, ss.gammaln, x)
-    for x in xvals[1:]:
-        check_vals(psi, ss.psi, x)
+def check_vals(fn1, fn2, *args):
+    v = fn1(*args)
+    close_to(v, fn2(*args), 1e-6 if v.dtype == np.float64 else 1e-4)
 
 
-"""
-scipy.special.multigammaln gives bad values if you pass a non scalar to a
-In [14]:
+def test_multigamma():
+    x = at.vector("x")
+    p = at.scalar("p")
 
-    import scipy.special
-    scipy.special.multigammaln([2.1], 3)
-    Out[14]:
-        array([ 1.76253257,  1.60450306,  1.66722239])
-"""
+    xvals = [np.array([v], dtype=config.floatX) for v in [0.1, 2, 5, 10, 50, 100]]
 
-
-def t_multigamma():
-    xvals = list(map(np.atleast_1d, [0, 0.1, 2, 100]))
-
-    x = at.dvector("x")
-    x.tag.test_value = xvals[0]
-
-    p = at.iscalar("p")
-    p.tag.test_value = 1
-
-    multigammaln = function([x, p], ps.multigammaln(x, p))
+    multigammaln = function([x, p], ps.multigammaln(x, p), mode="FAST_COMPILE")
 
     def ssmultigammaln(a, b):
-        return ss.multigammaln(a[0], b)
+        return np.array(ss.multigammaln(a[0], b), config.floatX)
 
     for p in [0, 1, 2, 3, 4, 100]:
         for x in xvals:
-            check_vals(multigammaln, ssmultigammaln, x, p)
-
-
-def check_vals(fn1, fn2, *args):
-    v = fn1(*args)
-    close_to(v, fn2(*args), 1e-6)
+            if np.all(x > 0.5 * (p - 1)):
+                check_vals(multigammaln, ssmultigammaln, x, p)
