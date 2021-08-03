c81458a2e2842a630e9ac3e56df5db20bacade45
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2021-08-03 09:02:19 +0000

    fix sampling with changed shape

--- pymc/variational/opvi.py
+++ pymc/variational/opvi.py
@@ -1673,6 +1673,7 @@ class Approximation(WithMemoization):
 
         trace = NDArray(
             model=self.model,
+            test_point={name: records[1] for name, records in samples.items()},
         )
         try:
             trace.setup(draws=draws, chain=0)
