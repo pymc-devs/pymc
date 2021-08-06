f5db59004e4fbf7096085cb3ab6c2bccc631e23e
Ricardo <ricardo.vieira1994@gmail.com>
2021-08-06 16:51:25 +0200

    Remove unused import in `test_smc`

--- pymc3/tests/test_smc.py
+++ pymc3/tests/test_smc.py
@@ -12,7 +12,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import aesara
 import aesara.tensor as at
 import numpy as np
 import pytest
