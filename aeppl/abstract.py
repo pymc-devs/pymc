2210f3e6cafecd3be2a61ef860ddaaf8234289b4
Brandon T. Willard <brandonwillard@users.noreply.github.com>
2021-09-20 18:28:01 -0500

    Refactor get_measurable_outputs dispatch interface

--- aeppl/abstract.py
+++ aeppl/abstract.py
@@ -1,6 +1,9 @@
 import abc
 from functools import singledispatch
+from typing import List
 
+from aesara.graph.basic import Apply, Variable
+from aesara.graph.op import Op
 from aesara.tensor.random.op import RandomVariable
 
 
@@ -11,12 +14,19 @@ class MeasurableVariable(abc.ABC):
 MeasurableVariable.register(RandomVariable)
 
 
-@singledispatch
-def get_measurable_outputs(op, node):
+def get_measurable_outputs(op: Op, node: Apply) -> List[Variable]:
     """Return only the outputs that are measurable."""
+    if isinstance(op, MeasurableVariable):
+        return _get_measurable_outputs(op, node)
+    else:
+        return []
+
+
+@singledispatch
+def _get_measurable_outputs(op, node):
     return node.outputs
 
 
-@get_measurable_outputs.register(RandomVariable)
-def randomvariable_outputs(op, node):
+@_get_measurable_outputs.register(RandomVariable)
+def _get_measurable_outputs_RandomVariable(op, node):
     return node.outputs[1:]
