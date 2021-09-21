79153298948f1e6c1150d44d6a80926b0d962a59
Meenal Jhajharia <meenal@mjhajharia.com>
2021-09-21 19:11:29 +0530

    Update deprecated.py

--- pymc3/deprecated.py
+++ pymc3/deprecated.py
@@ -37,3 +37,18 @@ def deprecated(reason=None, version=None):
         return new_func
 
     return decorator
+
+class deprecated_param:
+    def __init__(self, deprecated_args, version, reason):
+        self.deprecated_args = set(deprecated_args.split())
+        self.version = version
+        self.reason = reason
+
+    def __call__(self, callable):
+        def wrapper(*args, **kwargs):
+            found = self.deprecated_args.intersection(kwargs)
+            if found:
+                raise Warning("Parameter(s) %s deprecated since version %s; %s" % (
+                    ', '.join(map("'{}'".format, found)), self.version, self.reason))
+            return callable(*args, **kwargs)
+        return wrapper
