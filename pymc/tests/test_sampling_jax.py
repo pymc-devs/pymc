42482fdbb0edb13246dd5f54afa8fc813ff63b16
Ricardo <ricardo.vieira1994@gmail.com>
2021-10-21 14:52:47 +0200

    Refactor sampling_jax
    sampling_jax is no longer wrapped in a symbolic Op, making sampling more performant.
    SharedVariables are now replaced by constants, and a ValueError is raised if any contains a default_update.

--- pymc/tests/test_sampling_jax.py
+++ pymc/tests/test_sampling_jax.py
@@ -1,9 +1,13 @@
 import aesara
 import numpy as np
+import pytest
+
+from aesara.compile import SharedVariable
+from aesara.graph import graph_inputs
 
 import pymc as pm
 
-from pymc.sampling_jax import sample_numpyro_nuts
+from pymc.sampling_jax import replace_shared_variables, sample_numpyro_nuts
 
 
 def test_transform_samples():
@@ -29,7 +33,20 @@ def test_transform_samples():
 
     obs_at.set_value(-obs)
     with model:
-        trace = sample_numpyro_nuts(chains=1, random_seed=1322, keep_untransformed=False)
+        trace = sample_numpyro_nuts(chains=2, random_seed=1322, keep_untransformed=False)
 
     assert -11 < trace.posterior["a"].mean() < -8
     assert 1.5 < trace.posterior["sigma"].mean() < 2.5
+
+
+def test_replace_shared_variables():
+
+    x = aesara.shared(5, name="shared_x")
+
+    new_x = replace_shared_variables([x])
+    shared_variables = [var for var in graph_inputs(new_x) if isinstance(var, SharedVariable)]
+    assert not shared_variables
+
+    x.default_update = x + 1
+    with pytest.raises(ValueError, match="shared variables with default_update"):
+        replace_shared_variables([x])
