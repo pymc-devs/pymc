cc4d440fbd83f1667751487477774919482cdda1
shivam15s <shivam.sahni@iitgn.ac.in>
2021-11-09 03:58:41 +0530

    Fixed linting

--- pymc/tests/test_distributions_moments.py
+++ pymc/tests/test_distributions_moments.py
@@ -343,17 +343,18 @@ def test_weibull_moment(alpha, beta, size, expected):
         Weibull("x", alpha=alpha, beta=beta, size=size)
     assert_moment_is_expected(model, expected)
 
+
 @pytest.mark.parametrize(
     "mu, s, size, expected",
     [
         (1, 1, None, 1),
         (1, 1, 5, np.full(5, 1)),
-        (np.arange(1, 6), np.arange(1, 6), None, np.arange(1, 6)),
+        (2, np.arange(1, 6), None, np.full(5, 2)),
         (
             np.arange(1, 6),
             np.arange(1, 6),
             (2, 5),
-            np.full((2, 5), np.arange(1,6)),
+            np.full((2, 5), np.arange(1, 6)),
         ),
     ],
 )
