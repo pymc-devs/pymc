6ea3b41c2a405745bad8853f46de36449a1c4097
patel-zeel <patel_zeel@iitgn.ac.in>
2021-11-11 22:19:15 +0530

    fix pre-commit

--- pymc/distributions/multivariate.py
+++ pymc/distributions/multivariate.py
@@ -46,7 +46,11 @@ from pymc.distributions import transforms
 from pymc.distributions.continuous import ChiSquared, Normal, assert_negative_support
 from pymc.distributions.dist_math import bound, factln, logpow, multigammaln
 from pymc.distributions.distribution import Continuous, Discrete
-from pymc.distributions.shape_utils import broadcast_dist_samples_to, rv_size_is_none, to_tuple
+from pymc.distributions.shape_utils import (
+    broadcast_dist_samples_to,
+    rv_size_is_none,
+    to_tuple,
+)
 from pymc.math import kron_diag, kron_dot
 
 __all__ = [
