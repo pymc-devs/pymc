61bb6d885260b01d0308d9177ed6a0fcb54f6091
Carlos Iguaran <ciguaran@users.noreply.github.com>
2021-12-20 16:56:21 -0300

    Create utils.py

---
+++ pymc/smc/utils.py
@@ -0,0 +1,70 @@
+import logging
+import multiprocessing as mp
+import time
+import warnings
+
+from collections import defaultdict
+from collections.abc import Iterable
+from itertools import repeat
+
+import cloudpickle
+import numpy as np
+
+from arviz import InferenceData
+from fastprogress.fastprogress import progress_bar
+
+import pymc
+
+from pymc.backends.arviz import dict_to_dataset, to_inference_data
+from pymc.backends.base import MultiTrace
+from pymc.model import modelcontext
+from pymc.parallel_sampling import _cpu_count
+from pymc.smc.smc import IMH
+
+
+def run_chains_parallel(chains, progressbar, to_run, params, random_seed, kernel_kwargs, cores):
+    pbar = progress_bar((), total=100, display=progressbar)
+    pbar.update(0)
+    pbars = [pbar] + [None] * (chains - 1)
+
+    pool = mp.Pool(cores)
+
+    # "manually" (de)serialize params before/after multiprocessing
+    params = tuple(cloudpickle.dumps(p) for p in params)
+    kernel_kwargs = {key: cloudpickle.dumps(value) for key, value in kernel_kwargs.items()}
+    results = _starmap_with_kwargs(
+        pool,
+        to_run,
+        [(*params, random_seed[chain], chain, pbars[chain]) for chain in range(chains)],
+        repeat(kernel_kwargs),
+    )
+    results = tuple(cloudpickle.loads(r) for r in results)
+    pool.close()
+    pool.join()
+    return results
+
+
+def _starmap_with_kwargs(pool, fn, args_iter, kwargs_iter):
+    # Helper function to allow kwargs with Pool.starmap
+    # Copied from https://stackoverflow.com/a/53173433/13311693
+    args_for_starmap = zip(repeat(fn), args_iter, kwargs_iter)
+    return pool.starmap(_apply_args_and_kwargs, args_for_starmap)
+
+
+def _apply_args_and_kwargs(fn, args, kwargs):
+    return fn(*args, **kwargs)
+
+
+def run_chains_sequential(chains, progressbar, to_run, params, random_seed, kernel_kwargs):
+    #TODO maybe reorder parameters
+    results = []
+    pbar = progress_bar((), total=100 * chains, display=progressbar)
+    pbar.update(0)
+    for chain in range(chains):
+        pbar.offset = 100 * chain
+        pbar.base_comment = f"Chain: {chain + 1}/{chains}"
+        results.append(
+            to_run(*params, random_seed[chain], chain, pbar, **kernel_kwargs)
+        )
+    return results
+
