d89bc1b61386dd2f3e8fa7e4199e1050fd8bc8a7
canyon289 <ravinsdrive@gmail.com>
2022-01-01 06:16:39 -0800

    Fix pylint

--- pymc/distributions/timeseries.py
+++ pymc/distributions/timeseries.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from typing import List, Optional, Tuple, Union
+from typing import Union
 
 import aesara.tensor as at
 import numpy as np
