feef960378254e4f06e26d2d329c928e1a1f7b07
reshamas <reshama.stat@gmail.com>
2022-01-30 16:03:08 -0500

    fixed numpy docstring for function instantiate_steppers

--- pymc/sampling.py
+++ pymc/sampling.py
@@ -129,13 +129,13 @@ def instantiate_steppers(
     Parameters
     ----------
     model : Model object
-        A fully-specified model object
-    steps : list
+        A fully-specified model object.
+    steps : list, array_like of shape (selected_steps, )
         A list of zero or more step function instances that have been assigned to some subset of
         the model's parameters.
     selected_steps : dict
         A dictionary that maps a step method class to a list of zero or more model variables.
-    step_kwargs : dict
+    step_kwargs : dict, default=None
         Parameters for the samplers. Keys are the lower case names of
         the step method, values a dict of arguments. Defaults to None.
 
