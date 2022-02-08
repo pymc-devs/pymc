d76fd4c4d3ccd59d1acc279a0ce77b5d51c01347
Michael Osthege <michael.osthege@outlook.com>
2022-02-08 22:40:31 +0100

    Add XFAIL on `test_negative_binomial` under float32
    This is a regression, likely caused by an updated NumPy/SciPy dependency.
    Also see https://github.com/pymc-devs/pymc/pull/5450

--- pymc3/tests/test_distributions.py
+++ pymc3/tests/test_distributions.py
@@ -1082,6 +1082,10 @@ class TestMatchesScipy:
             {"N": NatSmall, "k": NatSmall, "n": NatSmall},
         )
 
+    @pytest.mark.xfail(
+        condition=(theano.config.floatX == "float32"),
+        reason="Fails on float32 due to a float casting problem in the logcdf",
+    )
     def test_negative_binomial(self):
         def scipy_mu_alpha_logpmf(value, mu, alpha):
             return sp.nbinom.logpmf(value, alpha, 1 - mu / (mu + alpha))
