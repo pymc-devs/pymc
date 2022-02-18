1190750f694372bdf3c557b4f89ee63ff21a403a
Juan Martín Loyola <jmloyola@outlook.com>
2022-02-18 12:40:49 -0300

    Fix docstring parameters type

--- pymc/data.py
+++ pymc/data.py
@@ -569,20 +569,20 @@ def Data(
     ----------
     name : str
         The name for this variable
-    value : {List, np.ndarray, pd.Series, pd.Dataframe}
+    value : list or ndarray or pd.Series, pd.Dataframe
         A value to associate with this variable
     mutable : bool, optional
         Switches between creating a ``SharedVariable`` (``mutable=True``, default)
         vs. creating a ``TensorConstant`` (``mutable=False``).
         Consider using ``pm.ConstantData`` or ``pm.MutableData`` as less verbose
         alternatives to ``pm.Data(..., mutable=...)``.
-    dims : {str, tuple of str}, optional, default=None
+    dims : str or tuple of str, optional, default=None
         Dimension names of the random variables (as opposed to the shapes of these
         random variables). Use this when `value` is a pandas Series or DataFrame. The
         `dims` will then be the name of the Series / DataFrame's columns. See ArviZ
         documentation for more information about dimensions and coordinates:
         :ref:`arviz:quickstart`.
-    export_index_as_coords : bool, optional, default=False
+    export_index_as_coords : bool, default=False
         If True, the `Data` container will try to infer what the coordinates should be
         if there is an index in `value`.
     **kwargs : dict, optional
