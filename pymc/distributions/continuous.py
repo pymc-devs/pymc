8d06985edd75e9f7fdce37881b08bdef53a5a561
lalitmusmade <lalitmusmade@users.noreply.github.com>
2022-02-19 17:31:42 +0530

    fixing the documentation for the class flat (#5497)
    * fixing the documentation for the class flat
    
    * update the alias
    
    * update the alias

--- pymc/distributions/continuous.py
+++ pymc/distributions/continuous.py
@@ -382,7 +382,7 @@ class Flat(Continuous):
 
         Parameters
         ----------
-        value: numeric
+        value : tensor_like of float
             Value(s) for which log-probability is calculated. If the log probabilities for multiple
             values are desired the values must be provided in a numpy array or Aesara tensor
 
@@ -399,7 +399,7 @@ class Flat(Continuous):
 
         Parameters
         ----------
-        value: numeric or np.ndarray or aesara.tensor
+        value : tensor_like of float
             Value(s) for which log CDF is calculated. If the log CDF for multiple
             values are desired the values must be provided in a numpy array or Aesara tensor.
 
