d9b80b869ab9df0108e7bdd8abb53ec8b8f5ee81
Ravin Kumar <ravinsdrive@gmail.com>
2022-02-21 15:05:35 -0800

    Add self to properly override class parameters

--- pymc/tests/test_distributions_random.py
+++ pymc/tests/test_distributions_random.py
@@ -428,9 +428,9 @@ class TestFlat(BaseTestDistributionRandom):
     ]
 
     def check_rv_inferred_size(self):
-        sizes_to_check = [None, (), 1, (1,), 5, (4, 5), (2, 4, 2)]
-        sizes_expected = [(), (), (1,), (1,), (5,), (4, 5), (2, 4, 2)]
-        for size, expected in zip(sizes_to_check, sizes_expected):
+        self.sizes_to_check = [None, (), 1, (1,), 5, (4, 5), (2, 4, 2)]
+        self.sizes_expected = [(), (), (1,), (1,), (5,), (4, 5), (2, 4, 2)]
+        for size, expected in zip(self.sizes_to_check, self.sizes_expected):
             pymc_rv = self.pymc_dist.dist(**self.pymc_dist_params, size=size)
             expected_symbolic = tuple(pymc_rv.shape.eval())
             assert expected_symbolic == expected
