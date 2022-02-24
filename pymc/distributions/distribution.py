f2464a3f300f73b7bb03ab069a7278d89de28534
ntomasino <nick.w.tomasino@gmail.com>
2022-02-24 20:35:34 +0000

    BUG: fixing density dist docstring to be xarray friendly

--- pymc/distributions/distribution.py
+++ pymc/distributions/distribution.py
@@ -789,22 +789,20 @@ class DensityDist(NoDistribution):
 
                 def logp(value, mu):
                     return -(value - mu)**2
-
                 def random(mu, rng=None, size=None):
                     return rng.normal(loc=mu, scale=1, size=size)
-
                 with pm.Model():
                     mu = pm.Normal('mu', 0 , 1)
                     dens = pm.DensityDist(
-                        'density_dist',
-                        mu,
-                        logp=logp,
-                        random=random,
-                        observed=np.random.randn(100, 3),
-                        size=(100, 3),
+                    'density_dist',
+                    mu,
+                    logp=logp,
+                    random=random,
+                    observed=np.random.randn(100, 3),
+                    size=(100, 3),
                     )
-                    prior = pm.sample_prior_predictive(10)['density_dist']
-                assert prior.shape == (10, 100, 3)
+                    prior = pm.sample_prior_predictive(10).prior_predictive['density_dist']
+                assert prior.shape == (1, 10, 100, 3)
 
         """
 
