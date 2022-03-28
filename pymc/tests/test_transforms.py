c42288ce6eda85faf084c5c41c545342aafa05c0
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2022-03-28 18:20:05 +0530

    fix pre-commit

--- pymc/tests/test_transforms.py
+++ pymc/tests/test_transforms.py
@@ -568,7 +568,7 @@ def test_transforms_ordered():
             size=(10, 4),
             transform=pm.distributions.transforms.ordered,
         )
-        
+
     log_prob = model.point_logps()
     np.testing.assert_allclose(list(log_prob.values()), np.array([18.69]))
 
@@ -582,6 +582,6 @@ def test_transforms_sumto1():
             size=(10, 4),
             transform=pm.distributions.transforms.sum_to_1,
         )
-        
+
     log_prob = model.point_logps()
     np.testing.assert_allclose(list(log_prob.values()), np.array([-56.76]))
