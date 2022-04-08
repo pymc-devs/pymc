693e1455883167d844154ec18d1975e67617a717
Ricardo <ricardo.vieira1994@gmail.com>
2022-04-08 11:43:15 +0200

    Revert "tighten test_sample_after_set_data bound"
    This reverts commit 29720d003b5b6a395ed8d8da0822e6b2d319690a.

--- pymc/tests/test_data_container.py
+++ pymc/tests/test_data_container.py
@@ -121,7 +121,7 @@ class TestData(SeededTest):
 
         assert pp_trace.posterior_predictive["obs"].shape == (1, 1000, 3)
         np.testing.assert_allclose(
-            new_y, pp_trace.posterior_predictive["obs"].mean(("chain", "draw")), atol=0.015
+            new_y, pp_trace.posterior_predictive["obs"].mean(("chain", "draw")), atol=1e-1
         )
 
     def test_shared_data_as_index(self):
