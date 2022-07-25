a9b50773a073e9e2ebfab8a10499d8aab514f518
Ravin Kumar <ravinsdrive@gmail.com>
2022-07-25 06:43:41 -0700

    Readd 3.7

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
