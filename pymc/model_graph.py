fd3b37ecab7a90bc2aa9af24ecdf0097068bc5c8
Larry Dong <larry.dong@mail.utoronto.ca>
2022-07-26 10:18:08 -0400

    Repositioned 'While True' loop to better account for arbitary number of dtype casts

--- pymc/model_graph.py
+++ pymc/model_graph.py
@@ -99,11 +99,13 @@ class ModelGraph:
             parent_name = self.get_parent_names(var)
             input_map[var_name] = input_map[var_name].union(parent_name)
 
-            while True:
+            if hasattr(var.tag, "observations"):
+                obs_node = var.tag.observations
+
                 # loop created so that the elif block can go through this again
                 # and remove any intermediate ops, notably dtype casting, to observations
-                if hasattr(var.tag, "observations"):
-                    obs_node = var.tag.observations
+                while True:
+
                     obs_name = obs_node.name
                     if obs_name and obs_name != var_name:
                         input_map[var_name] = input_map[var_name].difference({obs_name})
@@ -114,16 +116,14 @@ class ModelGraph:
                         # see issue 5795: https://github.com/pymc-devs/pymc/issues/5795
                         obs_node.owner
                         and isinstance(obs_node.owner.op, Elemwise)
-                        and isinstance(obs_node.owner.inputs[0].owner.op.scalar_op, Cast)
+                        and isinstance(obs_node.owner.op.scalar_op, Cast)
                     ):
                         # go to the beginning of the loop
                         # this comment is going up the graph to retrieve the node, i.e.
                         # observations here, is subject to dtype casting
-                        var.tag.observations = obs_node.owner.inputs[0].owner.inputs[0]
+                        obs_node = obs_node.owner.inputs[0]
                     else:
                         break
-                else:
-                    break
 
         return input_map
 
