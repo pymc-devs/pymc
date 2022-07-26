a237d8398e6fb5dba7b67fa47a5faa12ab588715
Thomas Wiecki <thomas.wiecki@gmail.com>
2022-07-26 18:25:01 +0200

    Revert "Readd 3.7"
    This reverts commit b08a2fced1d894aa5872856fccc23fde04fd8411.

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
