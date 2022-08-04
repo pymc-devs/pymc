307e1c9fdfef6564e955163862ec833f7ee61074
Thomas Wiecki <thomas.wiecki@gmail.com>
2022-08-04 23:37:54 +0200

    Revert "Proposal: Readd 3.7" (#6014)
    * Revert "Update setup.py"
    
    This reverts commit 60e26481696f3b09f5ee818bb578f17109647f95.
    
    * Revert "Readd 3.7"
    
    This reverts commit b08a2fced1d894aa5872856fccc23fde04fd8411.

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ classifiers = [
     "Development Status :: 5 - Production/Stable",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
@@ -85,7 +84,7 @@ if __name__ == "__main__":
         # package_data={'docs': ['*']},
         include_package_data=True,
         classifiers=classifiers,
-        python_requires=">=3.7",
+        python_requires=">=3.8",
         install_requires=install_reqs,
         tests_require=test_reqs,
     )
