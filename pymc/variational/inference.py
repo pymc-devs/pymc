762a6ca61c4913e88997382a0eb37c65d126a571
Markus Schmaus <markus.schmaus.math@gmail.com>
2022-09-15 16:11:26 +0200

    Undo changes to ASVGD so all variational inference tests pass

--- pymc/variational/inference.py
+++ pymc/variational/inference.py
@@ -631,11 +631,7 @@ class ASVGD(ImplicitGradient):
             "is often **underestimated** when using temperature = 1."
         )
         if approx is None:
-            approx = FullRank(
-                model=kwargs.pop("model", None),
-                random_seed=kwargs.pop("random_seed", None),
-                start=kwargs.pop("start", None),
-            )
+            approx = FullRank(model=kwargs.pop("model", None))
         super().__init__(estimator=estimator, approx=approx, kernel=kernel, **kwargs)
 
     def fit(
