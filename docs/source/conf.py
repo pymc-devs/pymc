d805cedfad6cc5ab8950d52af8100cd9fb31f5ce
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2022-10-10 22:57:01 +0200

    some doc fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,10 +20,6 @@ import os
 import sys
 from pathlib import Path
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 import pymc  # isort:skip
 
 # -- General configuration ------------------------------------------------
