f18e61cf56a51f8b0a5e6fc4cb4bae0d8502a85b
Michael Osthege <michael.osthege@outlook.com>
2022-11-05 13:36:28 +0100

    Reintroduce sampling_jax.py for backward compatibility
    This is a separate commit to make sure that git tracks the rename
    of the old `sampling_jax.py` to `sampling/jax.py` correctly.

---
+++ pymc/sampling_jax.py
@@ -0,0 +1,7 @@
+# This file exists only for backward-compatibility with imports like
+# `import pymc.sampling_jax` or `from pymc import sampling_jax`.
+
+# pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
+
+from pymc.sampling.jax import *
