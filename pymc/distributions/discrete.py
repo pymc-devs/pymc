d4b5aa86649674bcf6d17344b58eb1aeaf6cb9a0
Hyosub Kim <hyosubkim1@gmail.com>
2022-11-10 07:01:09 -0500

    Updated pymc.DiscreteWeibull docstring (#6283)
    * Updated pymc.DiscreteWeibull docstring

--- pymc/distributions/discrete.py
+++ pymc/distributions/discrete.py
@@ -462,7 +462,7 @@ discrete_weibull = DiscreteWeibullRV()
 
 
 class DiscreteWeibull(Discrete):
-    R"""Discrete Weibull log-likelihood
+    R"""Discrete Weibull log-likelihood.
 
     The discrete Weibull distribution is a flexible model of count data that
     can handle both over- and under-dispersion.
@@ -471,6 +471,7 @@ class DiscreteWeibull(Discrete):
     .. math:: f(x \mid q, \beta) = q^{x^{\beta}} - q^{(x + 1)^{\beta}}
 
     .. plot::
+        :context: close-figs
 
         import matplotlib.pyplot as plt
         import numpy as np
@@ -499,6 +500,14 @@ class DiscreteWeibull(Discrete):
     Mean      :math:`\mu = \sum_{x = 1}^{\infty} q^{x^{\beta}}`
     Variance  :math:`2 \sum_{x = 1}^{\infty} x q^{x^{\beta}} - \mu - \mu^2`
     ========  ======================
+
+    Parameters
+    ----------
+    q : tensor_like of float
+        Shape parameter (0 < q < 1).
+    beta : tensor_like of float
+        Shape parameter (beta > 0).
+
     """
     rv_op = discrete_weibull
 
