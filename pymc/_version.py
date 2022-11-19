02f18364cceed174704bb6b54bb13350235ca029
Michael Osthege <michael.osthege@outlook.com>
2022-11-19 14:50:10 +0100

    Use `v` tag prefix in versioneer config
    Closes #6303

--- pymc/_version.py
+++ pymc/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "rel-"
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "pymc/_version.py"
     cfg.verbose = False
