30f5b3a81038d7221ce5c0ffb14b4d16a443d97c
Jessica Moore <jessica.writes.code.github@gmail.com>
2022-12-23 18:23:50 -0500

    Reorder imports

--- pymc/distributions/discrete.py
+++ pymc/distributions/discrete.py
@@ -11,8 +11,9 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-from typing import Optional
 import warnings
+
+from typing import Optional
 
 import numpy as np
 import pytensor.tensor as at
