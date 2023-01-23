2efd37ffc4067448e1eba4fc9b54f5474438082a
Michal Raczycki <michalraczycki@macbook-pro-michal.home>
2023-01-23 15:20:06 +0100

    added assert class with logprob_assert function

---
+++ pymc/logprob/assert.py
@@ -0,0 +1,58 @@
+#   Copyright 2022- The PyMC Developers
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   MIT License
+#
+#   Copyright (c) 2021-2022 aesara-devs
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all
+#   copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+
+from pytensor.raise_op import Assert
+
+from pymc.logprob.abstract import MeasurableVariable, _logprob, logprob
+
+
+class AssertLogprob(Assert):
+    """A placeholder used to specify a log-likelihood for a Assert sub-graph."""
+
+
+MeasurableVariable.register(AssertLogprob)
+
+
+@_logprob.register(AssertLogprob)
+def logprob_assert(op, values, base_rv, *conds, **kwargs):
+    (value,) = values
+    if "msg" in kwargs.keys:
+        msg = kwargs["msg"]
+        assert_op = Assert(msg)
+    else:
+        assert_op = Assert()
+    asserted_val = assert_op.make_node(value, *conds)
+    return logprob(base_rv, asserted_val)
