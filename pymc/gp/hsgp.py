ab339d0234f74b056dd36769a8fd2ccbc5fdd172
Bill Engels <w.j.engels@gmail.com>
2023-02-13 22:45:30 -0800

    refactor internals to work with pm.set_data, add more docstrings and example

--- pymc/gp/hsgp.py
+++ pymc/gp/hsgp.py
@@ -48,7 +48,7 @@ class HSGP(Base):
         The boundary of the space for each `active_dim`.  It is called the boundary condition.
         Choose L such that the domain `[-L, L]` contains all points in the column of X given by the
         `active_dim`.
-    c: 1.5
+    c: float
         The proportion extension factor.  Used to construct L from X.  Defined as `S = max|X|` such
         that `X` is in `[-S, S]`.  `L` is the calculated as `c * S`.  One of `c` or `L` must be
         provided.  Further information can be found in Ruitort-Mayol et. al.
@@ -66,17 +66,19 @@ class HSGP(Base):
     .. code:: python
 
         # A three dimensional column vector of inputs.
-        X = np.random.randn(100, 3)
+        X = np.random.rand(100, 3)
 
         with pm.Model() as model:
             # Specify the covariance function.  Three input dimensions, but we only want to use the
             # last two.
             cov_func = pm.gp.cov.ExpQuad(3, ls=0.1, active_dims=[1, 2])
 
-            # Specify the HSGP.  Use 50 basis vectors across each active dimension, [1, 2]  for a
-            # total of 50 * 50 = 2500.  The range of the data is inferred from X, and the boundary
-            # condition multiplier `c` uses 4 * half range of the data, `L`.
-            gp = pm.gp.HSGP(m=[50, 50], c=4.0, cov_func=cov_func)
+            # Specify the HSGP.  Use 25 basis vectors across each active dimension, [1, 2]  for a
+            # total of 25 * 25 = 625.  The value `c = 4` means the boundary of the approximation
+            # lies at four times the half width of the data.  In this example the data lie between
+            # zero and  one, so the boundaries occur at -1.5 and 2.5.  The data, both for training
+            # and prediction should reside well within that boundary..  
+            gp = pm.gp.HSGP(m=[25, 25], c=4.0, cov_func=cov_func)
 
             # Place a GP prior over the function f.
             f = gp.prior("f", X=X)
@@ -147,59 +149,131 @@ class HSGP(Base):
         self.m = m
         self.c = c
         self.n_dims = cov_func.n_dims
+        self.m_star = int(np.prod(self.m))
         self._boundary_set = False
 
+        # Record whether or not L instead of c was passed on construction.
+        self.L_given = self.L is not None
+        
         super().__init__(mean_func=mean_func, cov_func=cov_func)
 
     def __add__(self, other):
         raise NotImplementedError("Additive HSGPs aren't supported ")
 
-    def _set_boundary(self, X):
+    def _set_boundary(self, Xs):
         """Make L from X and c if L is not passed in."""
         if not self._boundary_set:
+            
+            self.S = pt.max(pt.abs(Xs), axis=0)
+            
             if self.L is None:
-                # Define new L based on c and X range
-                La = pt.abs(pt.min(X, axis=0))
-                Lb = pt.abs(pt.max(X, axis=0))
-                self.L = self.c * pt.max(pt.stack((La, Lb)), axis=0)
+                self.L = self.c * self.S
+            
             else:
+                self.c = self.L / self.S
                 self.L = pt.as_tensor_variable(self.L)
 
             self._boundary_set = True
 
-    def prior_components(self, X: Union[np.ndarray, pt.TensorVariable]):
-        """Return the basis and coefficient priors, whose product is the HSGP.  It can be useful
-        to bypass the GP API and work with the basis and coefficients directly for models with i.e.
-        multiple GPs.  It returns the basis `phi` and the power spectral density, `psd`.  The GP
-        `f` can be formed by `f = phi @ (pm.Normal("hsgp_coeffs", size=psd.size) * psd)`.
+    @property
+    def _eigenvalues(self):
+        S = np.meshgrid(*[np.arange(1, 1 + self.m[d]) for d in range(self.n_dims)])
+        S = np.vstack([s.flatten() for s in S]).T
+        return pt.square((np.pi * S) / (2 * self.L))
+
+    def _eigenvectors(self, X):
+        phi = pt.ones((X.shape[0], self.m_star))
+        for d in range(self.n_dims):
+            c = 1.0 / np.sqrt(self.L[d])
+            term1 = pt.sqrt(self._eigenvalues[:, d])
+            term2 = pt.tile(X[:, d][:, None], self.m_star) + self.L[d]
+            phi *= c * pt.sin(term1 * term2)
+        return phi
+
+    def prior_linearized(self, X: Union[np.ndarray, pt.TensorVariable]):
+        """ Returns the linearized version of the HSGP, the Laplace eigenfunctions and the square
+        root of the power spectral density needed to create the GP.  This function allows the user
+        to bypass the GP interface and work directly with the basis and coefficients directly.  
+        This format allows the user to create predictions using `pm.set_data` similarly to a 
+        linear model.  It also enables computational speed ups in multi-GP models since they may
+        share the same basis.  The return values are the Laplace eigenfunctions `phi`, and the 
+        square root of the power spectral density.
+
+        Correct results when using `prior_linearized` in tandem with `pm.set_data` and 
+        `pm.MutableData` require two conditions.  First, one must specify `L` instead of `c` when
+        the GP is constructed.  If not, a RuntimeError is raised.  Second, the `X` needs to be 
+        zero centered, so it's mean must be subtracted.  An example is given below. 
 
         Parameters
         ----------
         X: array-like
             Function input values.
+    
+        Examples
+        --------
+        .. code:: python
+
+            # A one dimensional column vector of inputs.
+            X = np.linspace(0, 10, 100)[:, None]
+
+            with pm.Model() as model:
+                eta = pm.Exponential("eta", lam=1.0)
+                ell = pm.InverseGamma("ell", mu=5.0, sigma=5.0)
+                cov_func = eta**2 * pm.gp.cov.ExpQuad(1, ls=ell)
+
+                # m = [200] means 200 basis vectors for the first dimenison
+                # L = [10] means the approximation is valid from Xs = [-10, 10]
+                gp = pm.gp.HSGP(m=[200], L=[10], cov_func=cov_func)
+
+                # Order is important.  First calculate the mean, then make X a shared variable,
+                # then subtract the mean.  When X is mutated later, the correct mean will be 
+                # subtracted.
+                X_mu = np.mean(X, axis=0)
+                X = pm.MutableData("X", X)
+                Xs = X - X_mu
+
+                # Pass the zero-subtracted Xs in to the GP
+                phi, sqrt_psd = gp.prior_linearized(Xs)
+
+                # Specify standard normal prior in the coefficients.  The number of which
+                # is given by the number of basis vectors, which is also saved in the GP object
+                # as m_star.
+                beta = pm.Normal("beta", size=gp.m_star)
+
+                # The (non-centered) GP approximation is given by 
+                f = pm.Deterministic("f", phi @ (beta * sqrt_psd))
+
+                ...
+
+
+            # Then it works just like a linear regression to predict on new data.
+            # First mutate the data X,
+            x_new = np.linspace(-10, 10, 100)
+            with model:
+                model.set_data("X", x_new[:, None])
+
+            # and then make predictions for the GP using posterior predictive sampling.
+            with model:
+                ppc = pm.sample_posterior_predictive(idata, var_names=["f"])
         """
+        if not self.L_given:
+            warnings.warn(
+                "Since `c` was given instead of `L` on construction, usage of `model.set_data` "
+                "will result in an incorrect posterior predictive.  Ignore this warning if you "
+                "dont plan on creating predictions this way.  To remove this warning pass `L` "
+                "instead of `c` to the HSGP constructor."
+            )
+
         X, _ = self.cov_func._slice(X)
+        
         self._set_boundary(X)
 
-        omega, phi, m_star = self._eigendecomposition(X, self.L, self.m, self.n_dims)
+        phi = self._eigenvectors(X)
+        omega = pt.sqrt(self._eigenvalues)
         psd = self.cov_func.power_spectral_density(omega)
-
+        
         i = int(self.drop_first == True)
         return phi[:, i:], pt.sqrt(psd[i:])
-
-    @staticmethod
-    def _eigendecomposition(X, L, m, n_dims):
-        """Construct the eigenvalues and eigenfunctions of the Laplace operator."""
-        m_star = pt.prod(m)
-        S = np.meshgrid(*[np.arange(1, 1 + m[d]) for d in range(n_dims)])
-        S = np.vstack([s.flatten() for s in S]).T
-        eigvals = pt.square((np.pi * S) / (2 * L))
-        phi = pt.ones((X.shape[0], m_star))
-        for d in range(n_dims):
-            c = 1.0 / np.sqrt(L[d])
-            phi *= c * pt.sin(pt.sqrt(eigvals[:, d]) * (pt.tile(X[:, d][:, None], m_star) + L[d]))
-        omega = pt.sqrt(eigvals)
-        return omega, phi, m_star
 
     def prior(self, name: str, X: Union[np.ndarray, pt.TensorVariable], *args, **kwargs):
         R"""
@@ -214,15 +288,16 @@ class HSGP(Base):
         dims: None
             Dimension name for the GP random variable.
         """
-
-        phi, sqrt_psd = self.prior_components(X)
+        self.X_mu = pt.mean(X, axis=0)
+        phi, sqrt_psd = self.prior_linearized(X - self.X_mu)
 
         if self.parameterization == "noncentered":
-            self.beta = pm.Normal(f"{name}_hsgp_coeffs_", size=sqrt_psd.size)
-            f = self.mean_func(X) + phi @ (self.beta * sqrt_psd)
+            self.beta = pm.Normal(f"{name}_hsgp_coeffs_", size=self.m_star)
+            self.sqrt_psd = sqrt_psd
+            f = self.mean_func(X) + phi @ (self.beta * self.sqrt_psd)
 
         elif self.parameterization == "centered":
-            self.beta = pm.Normal(f"{name}_hsgp_coeffs_", sigma=sqrt_psd, size=sqrt_psd.size)
+            self.beta = pm.Normal(f"{name}_hsgp_coeffs_", sigma=sqrt_psd)
             f = self.mean_func(X) + phi @ self.beta
 
         self.f = pm.Deterministic(name, f, dims=kwargs.get("dims"))
@@ -230,19 +305,18 @@ class HSGP(Base):
 
     def _build_conditional(self, Xnew):
         try:
-            beta = self.beta
+            beta, X_mu = self.beta, self.X_mu
         except AttributeError:
             raise ValueError(
                 "Prior is not set, can't create a conditional.  Call `.prior(name, X)` first."
             )
-
+        
         Xnew, _ = self.cov_func._slice(Xnew)
-        omega, phi, _ = self._eigendecomposition(Xnew, self.L, self.m, self.n_dims)
+        phi = self._eigenvectors(Xnew - X_mu)
         i = int(self.drop_first == True)
 
         if self.parameterization == "noncentered":
-            psd = self.cov_func.power_spectral_density(omega)
-            return self.mean_func(Xnew) + phi[:, i:] @ (beta * pt.sqrt(psd[i:]))
+            return self.mean_func(Xnew) + phi[:, i:] @ (beta * self.sqrt_psd)
 
         elif self.parameterization == "centered":
             return self.mean_func(Xnew) + phi[:, i:] @ beta
