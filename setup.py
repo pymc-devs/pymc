14cf4a05fddbb698e1673dca5333b7a9408ef87b
Virgile Andreani <armavica@ulminfo.fr>
2023-02-28 23:46:45 -0500

    Remove useless option from setup.py

--- setup.py
+++ setup.py
@@ -69,7 +69,6 @@ if __name__ == "__main__":
         # because of an upload-size limit by PyPI, we're temporarily removing docs from the tarball.
         # Also see MANIFEST.in
         # package_data={'docs': ['*']},
-        include_package_data=True,
         classifiers=classifiers,
         python_requires=">=3.8",
         install_requires=install_reqs,
