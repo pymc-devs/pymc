8464cc10fd1f2b3cc2add4410c826fabc7347f1f
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-03-06 09:22:20 +0100

    Fix SpecifyShape

--- pymc/logprob/specify_shape.py
+++ pymc/logprob/specify_shape.py
@@ -80,7 +80,8 @@ def find_measurable_specify_shapes(fgraph, node):
 
     rv = node.outputs[0]
 
-    base_rv = node.inputs[0]
+    base_rv, *shape = node.inputs
+
     if not (
         base_rv.owner
         and isinstance(base_rv.owner.op, MeasurableVariable)
@@ -88,10 +89,10 @@ def find_measurable_specify_shapes(fgraph, node):
     ):
         return None  # pragma: no cover
 
-    new_op = MeasurableSpecifyShape(node.input_shapes)
+    new_op = MeasurableSpecifyShape()
     # Make base_var unmeasurable
     unmeasurable_base_rv = assign_custom_measurable_outputs(base_rv.owner)
-    new_rv = new_op.make_node(unmeasurable_base_rv).default_output()
+    new_rv = new_op.make_node(unmeasurable_base_rv, *shape).default_output()
     new_rv.name = rv.name
 
     return [new_rv]
