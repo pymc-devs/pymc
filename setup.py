560379295b66ac103490ec1f07414c0f4e6e541e
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-03-09 16:29:29 +0100

    Add explicit support for Python 3.11

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ classifiers = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "License :: OSI Approved :: Apache Software License",
     "Intended Audience :: Science/Research",
     "Topic :: Scientific/Engineering",
