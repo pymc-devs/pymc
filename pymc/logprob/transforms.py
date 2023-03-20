Fix jacobian dimensionality alignment in ChainedTransform
The existing code only considered mixing of scalar and vector transforms, but not potentially higher dimensionality transforms (e.g., matrix)

pymc/logprob/transforms.py
@@ -892,7 +892,8 @@ class ChainedTransform(RVTransform):
         det = 0.0
         for det_ in det_list:
             if det_.ndim > ndim0:
-                det += det_.sum(axis=-1)
+                ndim_diff = det_.ndim - ndim0
+                det += det_.sum(axis=tuple(range(-ndim_diff, 0)))
             else:
                 det += det_
         return det