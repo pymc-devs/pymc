8b5d00f37cc334e602ef1d6c82880b5f55f75113
Dhruvanshu Joshi <dhruvanshu.joshi@gmail.com>
2023-03-25 04:05:57 +0530

    Updated class name

--- pymc/logprob/checks.py
+++ pymc/logprob/checks.py
@@ -66,9 +66,6 @@ def logprob_specify_shape(op, values, inner_rv, *shapes, **kwargs):
 def find_measurable_specify_shapes(fgraph, node) -> Optional[List[MeasurableSpecifyShape]]:
     r"""Finds `SpecifyShapeOp`\s for which a `logprob` can be computed."""
 
-    if not (isinstance(node.op, SpecifyShape)):
-        return None  # pragma: no cover
-
     if isinstance(node.op, MeasurableSpecifyShape):
         return None  # pragma: no cover
 
@@ -105,14 +102,14 @@ measurable_ir_rewrites_db.register(
 )
 
 
-class MeasurableAssert(CheckAndRaise):
+class MeasurableCheckAndRaise(CheckAndRaise):
     """A placeholder used to specify a log-likelihood for an assert sub-graph."""
 
 
-MeasurableVariable.register(MeasurableAssert)
+MeasurableVariable.register(MeasurableCheckAndRaise)
 
 
-@_logprob.register(MeasurableAssert)
+@_logprob.register(MeasurableCheckAndRaise)
 def logprob_assert(op, values, inner_rv, *assertion, **kwargs):
     (value,) = values
     # transfer assertion from rv to value
@@ -121,13 +118,10 @@ def logprob_assert(op, values, inner_rv, *assertion, **kwargs):
 
 
 @node_rewriter([CheckAndRaise])
-def find_measurable_asserts(fgraph, node) -> Optional[List[MeasurableAssert]]:
+def find_measurable_asserts(fgraph, node) -> Optional[List[MeasurableCheckAndRaise]]:
     r"""Finds `AssertOp`\s for which a `logprob` can be computed."""
 
-    if not (isinstance(node.op, CheckAndRaise)):
-        return None  # pragma: no cover
-
-    if isinstance(node.op, MeasurableAssert):
+    if isinstance(node.op, MeasurableCheckAndRaise):
         return None  # pragma: no cover
 
     rv_map_feature: Optional[PreserveRVMappings] = getattr(fgraph, "preserve_rv_mappings", None)
@@ -147,7 +141,7 @@ def find_measurable_asserts(fgraph, node) -> Optional[List[MeasurableAssert]]:
         return None  # pragma: no cover
 
     exception_type = ExceptionType()
-    new_op = MeasurableAssert(exc_type=exception_type)
+    new_op = MeasurableCheckAndRaise(exc_type=exception_type)
     # Make base_var unmeasurable
     unmeasurable_base_rv = ignore_logprob(base_rv)
     new_rv = new_op.make_node(unmeasurable_base_rv, *conds).default_output()
