92bacb8f8cb18b7cb9c98702ec481a1b40c2fe05
shreyas3156 <shreyashsingh31@gmail.com>
2023-04-11 19:38:31 +0530

    Combine lt and gt tests and add gt test for discrete

--- tests/logprob/test_binary.py
+++ tests/logprob/test_binary.py
@@ -14,49 +14,53 @@
 import numpy as np
 import pytensor
 import pytensor.tensor as pt
+import pytest
 import scipy.stats as st
 
 from pymc import logp
 from pymc.testing import assert_no_rvs
 
 
-def test_continuous_rv_comparison_lt():
-    x_rv = pt.random.normal(0.5, 1)
-    comp_x_rv = pt.lt(x_rv, 0.5)
+@pytest.mark.parametrize(
+    "comparison_op, exp_logp_true, exp_logp_false",
+    [
+        (pt.lt, st.norm(0, 1).logcdf, st.norm(0, 1).logsf),
+        (pt.gt, st.norm(0, 1).logsf, st.norm(0, 1).logcdf),
+    ],
+)
+def test_continuous_rv_comparison(comparison_op, exp_logp_true, exp_logp_false):
+    x_rv = pt.random.normal(0, 1)
+    comp_x_rv = comparison_op(x_rv, 0.5)
 
     comp_x_vv = comp_x_rv.clone()
-    comp_x_vv.tag.test_value = 0
 
     logprob = logp(comp_x_rv, comp_x_vv)
     assert_no_rvs(logprob)
 
     logp_fn = pytensor.function([comp_x_vv], logprob)
-    ref_scipy = st.norm(0.5, 1)
 
-    assert np.isclose(logp_fn(0), ref_scipy.logcdf(0.5))
-    assert np.isclose(logp_fn(1), ref_scipy.logsf(0.5))
-
-
-def test_continuous_rv_comparison_gt():
-    x_rv = pt.random.normal(0.5, 1)
-    comp_x_rv = pt.gt(x_rv, 0.5)
-
-    comp_x_vv = comp_x_rv.clone()
-    comp_x_vv.tag.test_value = 0
-
-    logprob = logp(comp_x_rv, comp_x_vv)
-    assert_no_rvs(logprob)
-
-    logp_fn = pytensor.function([comp_x_vv], logprob)
-    ref_scipy = st.norm(0.5, 1)
-
-    assert np.isclose(logp_fn(0), ref_scipy.logsf(0.5))
-    assert np.isclose(logp_fn(1), ref_scipy.logcdf(0.5))
+    assert np.isclose(logp_fn(0), exp_logp_false(0.5))
+    assert np.isclose(logp_fn(1), exp_logp_true(0.5))
 
 
-def test_discrete_rv_comparison():
+@pytest.mark.parametrize(
+    "comparison_op, exp_logp_true, exp_logp_false",
+    [
+        (
+            pt.lt,
+            st.poisson(2).logcdf,
+            lambda x: np.logaddexp(st.poisson(2).logsf(x), st.poisson(2).logpmf(x)),
+        ),
+        (
+            pt.gt,
+            lambda x: np.logaddexp(st.poisson(2).logsf(x), st.poisson(2).logpmf(x)),
+            st.poisson(2).logcdf,
+        ),
+    ],
+)
+def test_discrete_rv_comparison(comparison_op, exp_logp_true, exp_logp_false):
     x_rv = pt.random.poisson(2)
-    cens_x_rv = pt.lt(x_rv, 3)
+    cens_x_rv = comparison_op(x_rv, 3)
 
     cens_x_vv = cens_x_rv.clone()
 
@@ -64,7 +68,6 @@ def test_discrete_rv_comparison():
     assert_no_rvs(logprob)
 
     logp_fn = pytensor.function([cens_x_vv], logprob)
-    ref_scipy = st.poisson(2)
 
-    assert np.isclose(logp_fn(1), ref_scipy.logcdf(3))
-    assert np.isclose(logp_fn(0), np.logaddexp(ref_scipy.logsf(3), ref_scipy.logpmf(3)))
+    assert np.isclose(logp_fn(1), exp_logp_true(3))
+    assert np.isclose(logp_fn(0), exp_logp_false(3))
