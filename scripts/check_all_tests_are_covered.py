8b5f437bf8c0650eed331c94cc99c1cafef9d8ce
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2023-06-05 12:21:12 +0200

    Fix `check_all_tests_are_covered` hook

--- scripts/check_all_tests_are_covered.py
+++ scripts/check_all_tests_are_covered.py
@@ -105,9 +105,11 @@ def from_yaml():
     _log.info("Number of test runs (❌=0, ✅=once)\n%s", df.replace(0, "❌").replace(1, "✅"))
 
     if ignored_by_all:
-        _log.warning("%i tests are completely ignored:\n%s", len(ignored_by_all), ignored_by_all)
+        raise AssertionError(
+            f"{len(ignored_by_all)} tests are completely ignored:\n{ignored_by_all}"
+        )
     if run_multiple_times:
-        raise Exception(
+        raise AssertionError(
             f"{len(run_multiple_times)} tests are run multiple times with the same OS and floatX setting:\n{run_multiple_times}"
         )
     return
