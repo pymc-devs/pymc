59d59cd8dadb7c638f061a991ac65111e87785a8
Joseph Hall <joseph.hall@bp.com>
2023-06-05 21:04:52 +0100

    Inherit input_dim and active_dims from cov_func

--- pymc/gp/cov.py
+++ pymc/gp/cov.py
@@ -845,18 +845,19 @@ class WrappedPeriodic(Covariance):
     period: Period
     """
 
-    def __init__(
-        self,
-        input_dim: int,
-        cov_func: Stationary,
-        period,
-        active_dims: Optional[Sequence[int]] = None,
-    ):  
-        super().__init__(input_dim, active_dims)
+    def __init__(self, cov_func: Stationary, period):  
         if not isinstance(cov_func, Stationary):
             raise TypeError("Must inherit from the Stationary class")
         self.cov_func = cov_func
         self.period = period
+
+    @property
+    def input_dim(self):
+        return self.cov_func.input_dim
+    
+    @property
+    def active_dims(self):
+        return self.cov_func.active_dims
 
     def full(self, X, Xs=None):
         X, Xs = self._slice(X, Xs)
