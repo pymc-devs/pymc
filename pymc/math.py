b55c8773ef2aa10d95ee14fc439d831a6ca79050
Aryan <nandaaryan823@gmail.com>
2024-02-06 23:34:22 +0530

    FutureWarning Added

--- pymc/math.py
+++ pymc/math.py
@@ -527,8 +527,12 @@ def block_diagonal(*matrices, sparse=False, format="csr"):
     matrix
     """
     if len(matrices) == 1:  # graph optimization
+        warnings.warn(
+            "The behavior of block_diagonal when only one matrix is provided is deprecated. Use pytensor function instead.",
+            FutureWarning,
+            stacklevel=2,
+        )
         return matrices[0]
-
     if sparse:
         return pytensor.sparse.basic.block_diag(*matrices, format=format)
     else:
