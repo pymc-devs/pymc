00ca006d3f030757da51110b604d68e836d52f33
Anatoly Rubanov <anatolijrubanov@gmail.com>
2024-02-09 17:27:51 +0300

    fix tests

--- pymc/distributions/continuous.py
+++ pymc/distributions/continuous.py
@@ -514,7 +514,7 @@ class Normal(Continuous):
     @classmethod
     def dist(cls, mu=0, sigma=None, tau=None, **kwargs):
         tau, sigma = get_tau_sigma(tau=tau, sigma=sigma)
-        sigma = pt.as_tensor_variable(sigma).astype("floatX")
+        sigma = pt.as_tensor_variable(sigma)
 
         # tau = pt.as_tensor_variable(tau)
         # mean = median = mode = mu = pt.as_tensor_variable(floatX(mu))
