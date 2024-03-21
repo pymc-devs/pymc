53cc4d7a677f3ffadaf536607a4653ae50aeb907
Anatoly Rubanov <anatolijrubanov@gmail.com>
2024-03-21 21:29:26 +0300

    add default transform argument

--- pymc/distributions/distribution.py
+++ pymc/distributions/distribution.py
@@ -332,6 +332,7 @@ class Distribution(metaclass=DistributionMeta):
         observed=None,
         total_size=None,
         transform=UNSET,
+        default_transform=UNSET,
         **kwargs,
     ) -> TensorVariable:
         """Adds a tensor variable corresponding to a PyMC distribution to the current model.
@@ -417,6 +418,7 @@ class Distribution(metaclass=DistributionMeta):
             total_size,
             dims=dims,
             transform=transform,
+            default_transform=default_transform,
             initval=initval,
         )
 
