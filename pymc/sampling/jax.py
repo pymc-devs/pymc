efbeef954cdca4d61b5e1db2ca82cd6e45ff06f0
Chris Fonnesbeck <fonnesbeck@gmail.com>
2024-03-25 11:32:33 -0400

    Rename filtered varnames

--- pymc/sampling/jax.py
+++ pymc/sampling/jax.py
@@ -533,16 +533,18 @@ def sample_jax_nuts(
     model = modelcontext(model)
 
     if var_names is not None:
-        vars_ = [v for v in model.unobserved_value_vars if v.name in var_names]
+        filtered_var_names = [v for v in model.unobserved_value_vars if v.name in var_names]
     else:
-        vars_ = model.unobserved_value_vars
+        filtered_var_names = model.unobserved_value_vars
 
     if nuts_kwargs is None:
         nuts_kwargs = {}
     else:
         nuts_kwargs = nuts_kwargs.copy()
 
-    vars_to_sample = list(get_default_varnames(vars_, include_transformed=keep_untransformed))
+    vars_to_sample = list(
+        get_default_varnames(filtered_var_names, include_transformed=keep_untransformed)
+    )
 
     (random_seed,) = _get_seeds_per_chain(random_seed, 1)
 
