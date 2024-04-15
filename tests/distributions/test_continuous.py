8bde1d8bf58a89a98ebc4faab622e722341222a9
Chris Fonnesbeck <fonnesbeck@gmail.com>
2024-04-15 11:42:11 -0500

    Added RV test

--- tests/distributions/test_continuous.py
+++ tests/distributions/test_continuous.py
@@ -1944,6 +1944,19 @@ class TestHalfStudentT(BaseTestDistributionRandom):
     ]
 
 
+class TestSkewStudentT(BaseTestDistributionRandom):
+    pymc_dist = pm.SkewStudentTStudentT
+    pymc_dist_params = {"a": 5.0, "b": 5.0, "mu": -1.0, "sigma": 2.0}
+    expected_rv_op_params = {"a": 5.0, "b": 5.0, "mu": -1.0, "sigma": 2.0}
+    reference_dist_params = {"a": 5.0, "b": 5.0, "loc": -1.0, "scale": 2.0}
+    reference_dist = seeded_scipy_distribution_builder("jf_skew_t")
+    checks_to_run = [
+        "check_pymc_params_match_rv_op",
+        "check_pymc_draws_match_reference",
+        "check_rv_size",
+    ]
+
+
 class TestMoyal(BaseTestDistributionRandom):
     pymc_dist = pm.Moyal
     pymc_dist_params = {"mu": 0.0, "sigma": 1.0}
