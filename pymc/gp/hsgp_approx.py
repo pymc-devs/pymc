Fix L computation

pymc/gp/hsgp_approx.py
@@ -32,10 +32,10 @@ TensorLike = np.ndarray | pt.TensorVariable
 
 def set_boundary(Xs: TensorLike, c: numbers.Real | TensorLike) -> TensorLike:
     """Set the boundary using the mean-subtracted `Xs` and `c`.  `c` is usually a scalar
-    multiplyer greater than 1.0, but it may be one value per dimension or column of `Xs`.
+    multiplier greater than 1.0, but it may be one value per dimension or column of `Xs`.
     """
     S = pt.max(Xs, axis=0)
-    L = (c * S).eval()  # to make sure L is not updated with out-of-sample preds
+    L = (c * S).eval()  # eval() makes sure L is not changed with out-of-sample preds
     return L
 
 
@@ -221,7 +221,7 @@ class HSGP(Base):
         self._m_star = int(np.prod(self._m))
         self._L: pt.TensorVariable | None = None
         if L is not None:
-            self._L = pt.as_tensor(L)
+            self._L = pt.as_tensor(L).eval()  # make sure L cannot be changed
         self._c = c
         self._parameterization = parameterization
 
@@ -285,15 +285,9 @@ class HSGP(Base):
                 # L = [10] means the approximation is valid from Xs = [-10, 10]
                 gp = pm.gp.HSGP(m=[200], L=[10], cov_func=cov_func)
 
-                # Order is important.
-                # First calculate the mean, then make X a shared variable, then subtract the mean.
-                #  When X is mutated later, the correct mean will be subtracted.
-                X_mean = np.mean(X, axis=0)
-                X = pm.Data("X", X)
-                Xs = X - X_mean
-
-                # Pass the zero-subtracted Xs in to the GP
-                phi, sqrt_psd = gp.prior_linearized(Xs=Xs)
+                X = pm.Data("X", Xs)
+                # Pass the Xs in to the GP
+                phi, sqrt_psd = gp.prior_linearized(Xs=X)
 
                 # Specify standard normal prior in the coefficients.  The number of which
                 # is given by the number of basis vectors, which is also saved in the GP object
@@ -321,6 +315,11 @@ class HSGP(Base):
             with model:
                 ppc = pm.sample_posterior_predictive(idata, var_names=["f"])
         """
+        # Important: fix the computation of the mean. If X is mutated later,
+        # the training mean will be subtracted, not the testing mean.
+        if not self._X_mean:
+            self._X_mean = pt.mean(Xs, axis=0)
+        Xs = Xs - self._X_mean
 
         # Index Xs using input_dim and active_dims of covariance function
         Xs, _ = self.cov_func._slice(Xs)
@@ -354,9 +353,7 @@ class HSGP(Base):
         dims: None
             Dimension name for the GP random variable.
         """
-        self._X_mean = pt.mean(X, axis=0)
-
-        phi, sqrt_psd = self.prior_linearized(X - self._X_mean)
+        phi, sqrt_psd = self.prior_linearized(X)
         if self._parameterization == "noncentered":
             self._beta = pm.Normal(
                 f"{name}_hsgp_coeffs_", size=self._m_star - int(self._drop_first)