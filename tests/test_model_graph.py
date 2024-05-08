d1af9da44cf80bbcd957ad273e643a403b5aa3ad
Will Dean <wd60622@gmail.com>
2024-05-08 08:37:35 +0200

    use set operation

--- tests/test_model_graph.py
+++ tests/test_model_graph.py
@@ -461,14 +461,15 @@ def test_custom_node_formatting_graphviz(simple_model):
     }
 
     G = model_to_graphviz(simple_model, node_formatters=node_formatters)
-    body = [item.strip() for item in G.body]
+    body = set(item.strip() for item in G.body)
 
-    items = [
-        "a [label=a]",
-        "b [label=b]",
-        "c [label=c]",
-        "a -> b",
-        "b -> c",
-    ]
-    for item in items:
-        assert item in body
+    items = set(
+        [
+            "a [label=a]",
+            "b [label=b]",
+            "c [label=c]",
+            "a -> b",
+            "b -> c",
+        ]
+    )
+    assert body == items
