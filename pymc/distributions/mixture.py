712851b931f6d73e614c016f6ac33a933d848ea6
Tyler Burch <burcht11@gmail.com>
2024-05-30 15:16:33 -0400

    Consistent key/value names in back to back dict comprehensions

--- pymc/distributions/mixture.py
+++ pymc/distributions/mixture.py
@@ -819,7 +819,7 @@ def _hurdle_mixture(*, name, nonzero_p, nonzero_dist, dtype, **kwargs):
     weights = pt.stack([1 - nonzero_p, nonzero_p], axis=-1)
 
     truncated_kwargs = {k: v for k, v in kwargs.items() if k in {"lower", "upper", "max_n_steps"}}
-    mixture_kwargs = {key: val for key, val in kwargs.items() if key not in truncated_kwargs}
+    mixture_kwargs = {k: v for k, v in kwargs.items() if k not in truncated_kwargs}
 
     comp_dists = [
         DiracDelta.dist(zero),
