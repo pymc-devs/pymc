519d86eff49870e257446c2c69d3a909808b4d8c
Michael Osthege <michael.osthege@outlook.com>
2024-06-30 22:36:16 +0200

    Fix typing in `logprob.tensor`

--- pymc/logprob/tensor.py
+++ pymc/logprob/tensor.py
@@ -38,6 +38,7 @@
 import pytensor
 
 from pytensor import tensor as pt
+from pytensor.graph.fg import FunctionGraph
 from pytensor.graph.op import compute_test_value
 from pytensor.graph.rewriting.basic import node_rewriter
 from pytensor.tensor import TensorVariable
@@ -60,7 +61,7 @@ from pymc.pytensorf import constant_fold
 
 
 @node_rewriter([Alloc])
-def naive_bcast_rv_lift(fgraph, node):
+def naive_bcast_rv_lift(fgraph: FunctionGraph, node):
     """Lift an ``Alloc`` through a ``RandomVariable`` ``Op``.
 
     XXX: This implementation simply broadcasts the ``RandomVariable``'s
@@ -226,6 +227,7 @@ def find_measurable_stacks(fgraph, node) -> list[TensorVariable] | None:
         measurable_stack = MeasurableJoin()(axis, *base_vars)
     else:
         measurable_stack = MeasurableMakeVector(node.op.dtype)(*base_vars)
+    assert isinstance(measurable_stack, TensorVariable)
 
     return [measurable_stack]
 
@@ -242,7 +244,7 @@ MeasurableVariable.register(MeasurableDimShuffle)
 
 
 @_logprob.register(MeasurableDimShuffle)
-def logprob_dimshuffle(op, values, base_var, **kwargs):
+def logprob_dimshuffle(op: MeasurableDimShuffle, values, base_var, **kwargs):
     """Compute the log-likelihood graph for a `MeasurableDimShuffle`."""
     (value,) = values
 
@@ -300,6 +302,7 @@ def find_measurable_dimshuffles(fgraph, node) -> list[TensorVariable] | None:
     measurable_dimshuffle = MeasurableDimShuffle(node.op.input_broadcastable, node.op.new_order)(
         base_var
     )
+    assert isinstance(measurable_dimshuffle, TensorVariable)
 
     return [measurable_dimshuffle]
 
