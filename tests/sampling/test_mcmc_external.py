4e6b1fab15f121119e0d38beb427d16bda8b372a
andrewdipper <andrewpratt333@gmail.com>
2024-10-06 22:43:48 -0600

    fix lint

--- tests/sampling/test_mcmc_external.py
+++ tests/sampling/test_mcmc_external.py
@@ -12,9 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-import jax
 import numpy as np
-import numpy.testing as npt
 import pytest
 
 from pymc import Data, Model, Normal, sample
