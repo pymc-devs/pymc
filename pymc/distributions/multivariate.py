1526d214c993814132752fee819e4a634d8299e3
Virgile Andreani <virgile@pymc-devs.org>
2024-10-07 20:42:31 +0200

    Remove file encoding statements

--- pymc/distributions/multivariate.py
+++ pymc/distributions/multivariate.py
@@ -12,9 +12,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import warnings
 
 from functools import partial, reduce
--- pymc/distributions/shape_utils.py
+++ pymc/distributions/shape_utils.py
@@ -12,7 +12,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-# -*- coding: utf-8 -*-
 """
 A collection of common shape operations needed for broadcasting
 samples from probability distributions for stochastic nodes in PyMC.
