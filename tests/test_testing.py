7bf58d524040a6a6b9614213164f864321db6862
Will Dean <57733339+williambdean@users.noreply.github.com>
2025-05-01 03:09:44 -0400

    Add public testing function to mock `sample` (#7761)
    * push up pymc-marketing mock
    
    * run pre-commit
    
    * add small test
    
    * use positional arg for draws like in actual sample
    
    * better for mypy
    
    * provide the setup and breakdown for pytest fixtures
    
    * change name for testing convention
    
    * bit more explicit on the test
    
    * add to the documentation
    
    * use expand_dims method
    
    * add to the toc
    
    * alterations to docstrings
    
    * change format and provide links
    
    * link to the functions in the docstring

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -15,7 +15,10 @@ from contextlib import ExitStack as does_not_raise
 
 import pytest
 
-from pymc.testing import Domain
+import pymc as pm
+
+from pymc.testing import Domain, mock_sample, mock_sample_setup_and_teardown
+from tests.models import simple_normal
 
 
 @pytest.mark.parametrize(
@@ -32,3 +35,49 @@ from pymc.testing import Domain
 def test_domain(values, edges, expectation):
     with expectation:
         Domain(values, edges=edges)
+
+
+@pytest.mark.parametrize(
+    "args, kwargs, expected_size",
+    [
+        pytest.param((), {}, (1, 10), id="default"),
+        pytest.param((100,), {}, (1, 100), id="positional-draws"),
+        pytest.param((), {"draws": 100}, (1, 100), id="keyword-draws"),
+        pytest.param((100,), {"chains": 6}, (6, 100), id="chains"),
+    ],
+)
+def test_mock_sample(args, kwargs, expected_size) -> None:
+    expected_chains, expected_draws = expected_size
+    _, model, _ = simple_normal(bounded_prior=True)
+
+    with model:
+        idata = mock_sample(*args, **kwargs)
+
+    assert "posterior" in idata
+    assert "observed_data" in idata
+    assert "prior" not in idata
+    assert "posterior_predictive" not in idata
+    assert "sample_stats" not in idata
+
+    assert idata.posterior.sizes == {"chain": expected_chains, "draw": expected_draws}
+
+
+mock_pymc_sample = pytest.fixture(scope="function")(mock_sample_setup_and_teardown)
+
+
+@pytest.fixture(scope="function")
+def dummy_model() -> pm.Model:
+    with pm.Model() as model:
+        pm.Flat("flat")
+        pm.HalfFlat("half_flat")
+
+    return model
+
+
+def test_fixture(mock_pymc_sample, dummy_model) -> None:
+    with dummy_model:
+        idata = pm.sample()
+
+    posterior = idata.posterior
+    assert posterior.sizes == {"chain": 1, "draw": 10}
+    assert (posterior["half_flat"] >= 0).all()
