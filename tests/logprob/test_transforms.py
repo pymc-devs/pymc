ded606b91a7d4ca95d043e734e4c07672668d0d7
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-07-03 18:49:43 +0200

    Fix more issues with newer numpy API

--- tests/logprob/test_transforms.py
+++ tests/logprob/test_transforms.py
@@ -288,7 +288,7 @@ class TestLocScaleRVTransform:
     @pytest.mark.parametrize(
         "rv_size, scale_type, product",
         [
-            (None, pt.scalar, True),
+            ((), pt.scalar, True),
             (1, pt.TensorType("floatX", (True,)), True),
             ((2, 3), pt.matrix, False),
         ],
