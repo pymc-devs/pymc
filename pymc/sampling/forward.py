fd230d39a24825a624cf6182e9d50d4a32484b54
Ricardo Vieira <ricardo.vieira1994@gmail.com>
2025-07-15 12:09:19 +0200

    Move progressbar code to its own module

--- pymc/sampling/forward.py
+++ pymc/sampling/forward.py
@@ -57,12 +57,11 @@ from pymc.blocking import PointType
 from pymc.distributions.shape_utils import change_dist_size
 from pymc.logprob.utils import rvs_in_graph
 from pymc.model import Model, modelcontext
+from pymc.progress_bar import CustomProgress, default_progress_theme
 from pymc.pytensorf import compile
 from pymc.util import (
-    CustomProgress,
     RandomState,
     _get_seeds_per_chain,
-    default_progress_theme,
     get_default_varnames,
     point_wrapper,
 )
