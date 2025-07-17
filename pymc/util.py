a2318e4f8822dffe0aa8dd960d80ad5d703f8a14
Omer Yuksel <4077805+osyuksel@users.noreply.github.com>
2025-07-17 12:51:38 +0200

    Replace VarName usage with str in type hints and conversion

--- pymc/util.py
+++ pymc/util.py
@@ -214,9 +214,9 @@ def get_default_varnames(var_iterator, include_transformed):
         return [var for var in var_iterator if not is_transformed_name(get_var_name(var))]
 
 
-def get_var_name(var) -> VarName:
+def get_var_name(var) -> str:
     """Get an appropriate, plain variable name for a variable."""
-    return VarName(str(getattr(var, "name", var)))
+    return str(getattr(var, "name", var))
 
 
 def get_transformed(z):
