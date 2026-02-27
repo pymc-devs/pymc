e2caaaf651258949b1d7e73786558a8638b5b71a
Sanket Meghale <sanketmeghale@gmail.com>
2026-02-27 23:14:15 +0530

    Fix import order in tests per ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,10 +11,8 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import warnings
 # standard library imports
-import os
-import sys
+import warnings
 
 # third-party imports
 import numpy as np
