9093f5f4bcc049168165f9614dc6cc43621349d2
sankhyanreyansh <reyanshsankhyan@Ray729.local>
2026-08-07 18:29:31 +0700

    tests: fail on unexpected warnings in test_math.py

--- tests/test_math.py
+++ tests/test_math.py
@@ -34,6 +34,14 @@ from pymc.math import (
 from pymc.pytensorf import floatX
 from tests.helpers import verify_grad
 
+pytestmark = [
+    pytest.mark.filterwarnings("error"),
+    pytest.mark.filterwarnings(
+        "ignore:.*should implement `pullback` instead of `L_op`.*:FutureWarning"
+    ),
+    pytest.mark.filterwarnings("ignore:Numba will use object mode.*:UserWarning"),
+]
+
 
 def test_kronecker():
     np.random.seed(1)
