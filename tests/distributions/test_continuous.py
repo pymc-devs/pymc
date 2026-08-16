58bb79c9828b9f8551b10bee673fd77c073d87e8
Noah Shin <noahshin@Noahs-MacBook-Pro.local>
2026-08-15 17:22:52 -1000

    Converting reference_dist lambdas to defs for CI check

--- tests/distributions/test_continuous.py
+++ tests/distributions/test_continuous.py
@@ -2017,7 +2017,10 @@ class TestHalfStudentT(BaseTestDistributionRandom):
     pymc_dist_params = {"nu": 5.0, "sigma": 2.0}
     expected_rv_op_params = {"nu": 5.0, "sigma": 2.0}
     reference_dist_params = {"df": 5.0, "loc": 0, "scale": 2.0}
-    reference_dist = lambda self: ft.partial(self.halfstudentt_rng_fn, rng=self.get_random_state())
+
+    def reference_dist(self):
+        return ft.partial(self.halfstudentt_rng_fn, rng=self.get_random_state())
+
     checks_to_run = [
         "check_pymc_params_match_rv_op",
         "check_pymc_draws_match_reference",
@@ -2253,7 +2256,10 @@ class TestLogitNormal(BaseTestDistributionRandom):
     pymc_dist_params = {"mu": 5.0, "sigma": 10.0}
     expected_rv_op_params = {"mu": 5.0, "sigma": 10.0}
     reference_dist_params = {"loc": 5.0, "scale": 10.0}
-    reference_dist = lambda self: ft.partial(self.logit_normal_rng_fn, rng=self.get_random_state())
+
+    def reference_dist(self):
+        return ft.partial(self.logit_normal_rng_fn, rng=self.get_random_state())
+
     checks_to_run = [
         "check_pymc_params_match_rv_op",
         "check_pymc_draws_match_reference",
@@ -2324,7 +2330,10 @@ class TestBeta(BaseTestDistributionRandom):
     expected_rv_op_params = {"alpha": 2.0, "beta": 5.0}
     reference_dist_params = {"a": 2.0, "b": 5.0}
     size = 15
-    reference_dist = lambda self: ft.partial(clipped_beta_rvs, random_state=self.get_random_state())
+
+    def reference_dist(self):
+        return ft.partial(clipped_beta_rvs, random_state=self.get_random_state())
+
     checks_to_run = [
         "check_pymc_params_match_rv_op",
         "check_pymc_draws_match_reference",
@@ -2403,7 +2412,10 @@ class TestHalfCauchy(BaseTestDistributionRandom):
     pymc_dist_params = {"beta": 5.0}
     expected_rv_op_params = {"beta": 5.0}
     reference_dist_params = {"scale": 5.0}
-    reference_dist = lambda self: ft.partial(self.halfcauchy_rng_fn, rng=self.get_random_state())
+
+    def reference_dist(self):
+        return ft.partial(self.halfcauchy_rng_fn, rng=self.get_random_state())
+
     checks_to_run = [
         "check_pymc_params_match_rv_op",
         "check_pymc_draws_match_reference_not_numba",
@@ -2545,7 +2557,10 @@ class TestPolyaGamma(BaseTestDistributionRandom):
     pymc_dist_params = {"h": 1.0, "z": 0.0}
     expected_rv_op_params = {"h": 1.0, "z": 0.0}
     reference_dist_params = {"h": 1.0, "z": 0.0}
-    reference_dist = lambda self: ft.partial(self.polyagamma_rng_fn, rng=self.get_random_state())
+
+    def reference_dist(self):
+        return ft.partial(self.polyagamma_rng_fn, rng=self.get_random_state())
+
     checks_to_run = [
         "check_pymc_params_match_rv_op",
         "check_pymc_draws_match_reference",
@@ -2566,7 +2581,9 @@ class TestInterpolated(BaseTestDistributionRandom):
     pymc_dist_params = {"x_points": x_points, "pdf_points": pdf_points}
     reference_dist_params = {"mu": mu, "sigma": sigma}
 
-    reference_dist = lambda self: ft.partial(self.interpolated_rng_fn, rng=self.get_random_state())
+    def reference_dist(self):
+        return ft.partial(self.interpolated_rng_fn, rng=self.get_random_state())
+
     checks_to_run = [
         "check_rv_size",
         "check_draws",
